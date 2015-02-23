aecc898a9e156ad976d25ee91d4d871e6f36b582
Brandon Williams <mbw234@gmail.com>
2015-02-23 15:56:18 -0500

    Import is better.

--- app/src/main/java/com/kickstarter/services/KickstarterService.java
+++ app/src/main/java/com/kickstarter/services/KickstarterService.java
@@ -2,11 +2,12 @@ package com.kickstarter.services;
 
 import retrofit.http.GET;
 import rx.Observable;
+import com.kickstarter.services.ApiResponses.DiscoverEnvelope;
 
 /**
 * Created by brandon on 2/23/15.
 */
 /*package*/ interface KickstarterService {
   @GET("/v1/discover")
-  Observable<ApiResponses.DiscoverEnvelope> fetchProjects();
+  Observable<DiscoverEnvelope> fetchProjects();
 }

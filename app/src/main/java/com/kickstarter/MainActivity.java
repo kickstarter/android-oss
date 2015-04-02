baa690bc1542c586bd9d645384f036623b5851e9
Christopher Wright <christopherwright@gmail.com>
2015-04-02 19:16:26 -0400

    Cleanup unused import

--- app/src/main/java/com/kickstarter/MainActivity.java
+++ app/src/main/java/com/kickstarter/MainActivity.java
@@ -1,7 +1,6 @@
 package com.kickstarter;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;

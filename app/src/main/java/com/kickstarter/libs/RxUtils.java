08186d72a569719bd0f237d4e805271cba51403c
Christopher Wright <christopherwright@gmail.com>
2015-04-21 21:31:02 -0400

    RxUtils shouldn't be instantiated

--- app/src/main/java/com/kickstarter/libs/RxUtils.java
+++ app/src/main/java/com/kickstarter/libs/RxUtils.java
@@ -5,6 +5,9 @@ import android.util.Pair;
 import rx.Observable;
 
 public class RxUtils {
+  private RxUtils() {
+    throw new AssertionError();
+  }
 
   public static <F, S> Observable<Pair<F, S>> combineLatestPair(final Observable<F> ob1, final Observable<S> ob2) {
     return Observable.combineLatest(ob1, ob2, Pair::new);

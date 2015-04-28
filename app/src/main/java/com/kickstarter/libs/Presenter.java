a53958832741728bcde2d15f719dbc3c6becd7ab
Christopher Wright <christopherwright@gmail.com>
2015-04-28 16:36:30 -0400

    Add subscriptions, unsubscribe to all when presenter is destroyed

--- app/src/main/java/com/kickstarter/libs/Presenter.java
+++ app/src/main/java/com/kickstarter/libs/Presenter.java
@@ -2,10 +2,15 @@ package com.kickstarter.libs;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Subscription;
 import rx.subjects.PublishSubject;
 
 public class Presenter<ViewType> {
   protected final PublishSubject<ViewType> viewSubject = PublishSubject.create();
+  protected final List<Subscription> subscriptions = new ArrayList<>();
 
   protected void onCreate(Bundle savedInstanceState) {
     viewSubject.onNext(null);
@@ -20,11 +25,14 @@ public class Presenter<ViewType> {
   }
 
   protected void onDestroy() {
+    for (Subscription subscription : subscriptions) {
+      subscription.unsubscribe();
+    }
+
     viewSubject.onCompleted();
   }
 
   public void save(Bundle state) {
     // TODO
   }
-
 }

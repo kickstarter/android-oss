bdcc58de0e75e268ffb616a99a50c2ddc1abc509
Christopher Wright <christopherwright@gmail.com>
2015-05-01 13:37:26 -0400

    Initialize Facebook SDK, add Facebook button
    Doesn't do much yet, but I've verified that this at least hits their API with
    some tracking events.
    
    The debug.keystore is used to sign our app in debug mode. By committing this into
    our repo, we can share it between developers rather than each having to maintain
    their own keystore. This also lets us add one development key hash for our application
    for our Facebook app, rather than one for every developer machine.

--- app/src/main/java/com/kickstarter/ui/activities/LoginToutActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/LoginToutActivity.java
@@ -2,14 +2,48 @@ package com.kickstarter.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.kickstarter.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class LoginToutActivity extends AppCompatActivity {
+  @InjectView(R.id.login_button) Button login_button;
+  @InjectView(R.id.sign_up_button) Button signup_button;
+
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    FacebookSdk.sdkInitialize(this.getApplicationContext());
+
     setContentView(R.layout.login_tout_layout);
+    ButterKnife.inject(this);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    /*
+     * Temporary Facebook testing - logs 'install' and 'app activate' App Events.
+     * This hits the Facebook API, we can remove it once login is working.
+    */
+    AppEventsLogger.activateApp(this);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    /*
+     * Temporary Facebook testing - logs 'app deactivate' App Events.
+     * This hits the Facebook API, we can remove it once login is working.
+    */
+    AppEventsLogger.deactivateApp(this);
   }
 }

9fa548c847dceb649b85d99890f4e8b9d61f454c
Christopher Wright <christopherwright@gmail.com>
2015-05-05 18:55:09 -0400

    Missing import

--- app/src/main/java/com/kickstarter/ui/activities/LoginToutActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/LoginToutActivity.java
@@ -2,6 +2,7 @@ package com.kickstarter.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.facebook.FacebookSdk;

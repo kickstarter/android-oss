48d154be519ff56f31cd05ed172fbced42abf0b1
Christopher Wright <christopherwright@gmail.com>
2015-05-07 19:18:06 -0400

    Activty empty layout done

--- app/src/main/java/com/kickstarter/ui/activities/ActivityFeedActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/ActivityFeedActivity.java
@@ -1,12 +1,20 @@
 package com.kickstarter.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.kickstarter.R;
 import com.kickstarter.libs.BaseActivity;
 import com.kickstarter.models.CurrentUser;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class ActivityFeedActivity extends BaseActivity {
+  @InjectView(R.id.discover_projects_button) Button discover_projects_button;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,5 +25,15 @@ public class ActivityFeedActivity extends BaseActivity {
     } else {
       setContentView(R.layout.empty_activity_feed_layout);
     }
+
+    ButterKnife.inject(this);
+  }
+
+  public void discoverProjectsButtonOnClick(final View view) {
+    // Ideally we could pop DiscoveryActivity back to the front with
+    // FLAG_ACTIVITY_REORDER_TO_FRONT, but:
+    // https://code.google.com/p/android/issues/detail?id=63570#c2
+    Intent intent = new Intent(this, DiscoveryActivity.class);
+    startActivity(intent);
   }
 }

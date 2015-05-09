31554dca6a3f709688d9f7c38171adaae01d117a
Christopher Wright <christopherwright@gmail.com>
2015-05-08 23:32:55 -0400

    Add comment explaining ForApplication

--- app/src/main/java/com/kickstarter/libs/ForApplication.java
+++ app/src/main/java/com/kickstarter/libs/ForApplication.java
@@ -5,6 +5,13 @@ import java.lang.annotation.RetentionPolicy;
 
 import javax.inject.Qualifier;
 
+/*
+ * Allows the application context to be injected but requires a ForApplication
+ * annotation, checked at compile time.
+ *
+ * This makes it explicit that we are injecting an application context, rather than
+ * a generic context or specific context (e.g. activity context).
+*/
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ForApplication {

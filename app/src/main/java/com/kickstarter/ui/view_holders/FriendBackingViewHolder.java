6e0c15841b62ceaab16751902e05dcb33b1d50c9
Christopher Wright <christopherwright@gmail.com>
2015-05-19 15:00:51 -0400

    Add circle transform for avatars

--- app/src/main/java/com/kickstarter/ui/view_holders/FriendBackingViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/view_holders/FriendBackingViewHolder.java
@@ -1,17 +1,21 @@
 package com.kickstarter.ui.view_holders;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kickstarter.R;
+import com.kickstarter.libs.CircleTransform;
 import com.kickstarter.models.Activity;
 import com.kickstarter.presenters.ActivityFeedPresenter;
+import com.squareup.picasso.Picasso;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class FriendBackingViewHolder extends ActivityListViewHolder {
   @InjectView(R.id.project_name) TextView project_name;
+  @InjectView(R.id.avatar) ImageView avatar;
 
   public FriendBackingViewHolder(final View view, final ActivityFeedPresenter presenter) {
     super(view, presenter);
@@ -21,7 +25,10 @@ public class FriendBackingViewHolder extends ActivityListViewHolder {
   @Override
   public void onBind(final Activity activity) {
     super.onBind(activity);
-    project_name.setText(activity.category().toString());
+    Picasso.with(view.getContext())
+      .load(activity.user().avatar().small())
+      .transform(new CircleTransform())
+      .into(avatar);
   }
 }
 

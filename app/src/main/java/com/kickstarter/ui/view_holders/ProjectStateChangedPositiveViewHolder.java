d68cd9ef690719ffb7f91ab0c18a91c573bb34ee
Christopher Wright <christopherwright@gmail.com>
2015-05-21 18:14:49 -0400

    Load project image into positive view

--- app/src/main/java/com/kickstarter/ui/view_holders/ProjectStateChangedPositiveViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/view_holders/ProjectStateChangedPositiveViewHolder.java
@@ -1,13 +1,22 @@
 package com.kickstarter.ui.view_holders;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.kickstarter.R;
 import com.kickstarter.models.Activity;
 import com.kickstarter.presenters.ActivityFeedPresenter;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.Optional;
 
 public class ProjectStateChangedPositiveViewHolder extends ActivityListViewHolder {
+  @Optional @InjectView(R.id.project_photo) ImageView project_photo;
   public ProjectStateChangedPositiveViewHolder(final View view, final ActivityFeedPresenter presenter) {
     super(view, presenter);
     ButterKnife.inject(this, view);
@@ -16,5 +25,9 @@ public class ProjectStateChangedPositiveViewHolder extends ActivityListViewHolde
   @Override
   public void onBind(final Activity activity) {
     super.onBind(activity);
+
+    Picasso.with(view.getContext())
+      .load(activity.project().photo().full())
+      .into(project_photo);
   }
 }

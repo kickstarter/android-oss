4f81fe4e3c5193d079f2f5544401d91a9b5715dc
Christopher Wright <christopherwright@gmail.com>
2015-06-02 18:46:15 -0400

    Add basic collapsing toolbar layout to detail view
    Not perfect yet, need to solve some issues with the title text.

--- app/src/main/java/com/kickstarter/ui/activities/ProjectDetailActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/ProjectDetailActivity.java
@@ -2,6 +2,8 @@ package com.kickstarter.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -26,14 +28,15 @@ public class ProjectDetailActivity extends BaseActivity<ProjectDetailPresenter> 
   protected @InjectView(R.id.blurb) TextView blurb;
   protected @InjectView(R.id.category) TextView category;
   protected @InjectView(R.id.creator_name) TextView creator_name;
+  protected @InjectView(R.id.collapsing_toolbar) CollapsingToolbarLayout collapsing_toolbar_layout;
   protected @InjectView(R.id.deadline_countdown) TextView deadline_countdown;
   protected @InjectView(R.id.deadline_countdown_unit) TextView deadline_countdown_unit;
   protected @InjectView(R.id.goal) TextView goal;
   protected @InjectView(R.id.location) TextView location;
-  protected @InjectView(R.id.name) TextView name;
   protected @InjectView(R.id.percentage_funded) ProgressBar percentage_funded;
   protected @InjectView(R.id.photo) ImageView photo;
   protected @InjectView(R.id.pledged) TextView pledged;
+  protected @InjectView(R.id.toolbar) Toolbar toolbar;
 
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
@@ -42,12 +45,16 @@ public class ProjectDetailActivity extends BaseActivity<ProjectDetailPresenter> 
     setContentView(R.layout.project_detail_layout);
     ButterKnife.inject(this);
 
+    setSupportActionBar(toolbar);
+
     final Intent intent = getIntent();
     final Project project = intent.getExtras().getParcelable("project");
     presenter.takeProject(project);
   }
 
   public void show(final Project project) {
+    collapsing_toolbar_layout.setTitle(project.name());
+
     backers_count.setText(NumberFormat.getNumberInstance(Locale.getDefault()).format(project.backersCount()));
     blurb.setText(project.blurb());
     creator_name.setText(project.creator().name());
@@ -56,7 +63,6 @@ public class ProjectDetailActivity extends BaseActivity<ProjectDetailPresenter> 
     deadline_countdown_unit.setText(project.deadlineCountdownUnit());
     goal.setText(NumberFormat.getNumberInstance(Locale.getDefault()).format(project.goal()));
     location.setText(project.location().displayableName());
-    name.setText(project.name());
     percentage_funded.setProgress(Math.round(Math.min(100.0f, project.percentageFunded())));
     Picasso.with(this).load(project.photo().full()).into(photo);
     pledged.setText(NumberFormat.getNumberInstance(Locale.getDefault()).format(project.pledged()));

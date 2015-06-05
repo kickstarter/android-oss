d8c022766f91d99f8611adfd1955e1208b676824
Christopher Wright <christopherwright@gmail.com>
2015-06-05 16:59:17 -0400

    Build app, run unit tests on CircleCI

---
+++ app/src/test/java/com/kickstarter/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.kickstarter;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class SimpleTest extends TestCase {
+  @Test
+  public void test() {
+    assertEquals(1, 1);
+  }
+}

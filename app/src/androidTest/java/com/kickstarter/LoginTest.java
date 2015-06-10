27772256aaf10ee7cebc45d1e0f9032683ad9c00
Christopher Wright <christopherwright@gmail.com>
2015-06-09 22:39:28 -0400

    Add UI tests using Espresso
    Testing these on Circle's emulator is flaky, I think it will be a source
    of pain if we invest too much into them. For now, we'll run UI
    tests locally - perhaps before releases.

---
+++ app/src/androidTest/java/com/kickstarter/LoginTest.java
@@ -0,0 +1,37 @@
+package com.kickstarter;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.kickstarter.ui.activities.LoginActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static org.hamcrest.Matchers.not;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginTest {
+  @Rule
+  public ActivityTestRule<LoginActivity> activityRule =
+    new ActivityTestRule<>(LoginActivity.class);
+
+  @Test
+  public void enteringEmailAndPasswordEnablesLoginButton() {
+    onView(withId(R.id.login_button)).check(matches(not(isEnabled())));
+
+    onView(withId(R.id.email)).perform(typeText("test@kickstarter.com"));
+    onView(withId(R.id.password)).perform(typeText("password"));
+
+    onView(withId(R.id.login_button)).check(matches(isEnabled()));
+  }
+}

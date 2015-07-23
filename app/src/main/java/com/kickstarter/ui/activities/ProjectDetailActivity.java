4309d17beb765364f00c34482c74094738026e53
Lisa Luo <lluo92@gmail.com>
2015-07-23 11:54:50 -0400

    VideoView and playback logic added for project details
    Play overlay button drawable added; disappears on click. VideoView holds project's base video uri, holds video frame initially. Native MediaController to come.

--- app/src/main/java/com/kickstarter/ui/activities/ProjectDetailActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/ProjectDetailActivity.java
@@ -1,8 +1,12 @@
 package com.kickstarter.ui.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -13,6 +17,7 @@ import com.kickstarter.libs.BaseActivity;
 import com.kickstarter.libs.Money;
 import com.kickstarter.libs.RequiresPresenter;
 import com.kickstarter.models.Project;
+import com.kickstarter.models.Video;
 import com.kickstarter.presenters.ProjectDetailPresenter;
 import com.squareup.picasso.Picasso;
 
@@ -37,8 +42,11 @@ public class ProjectDetailActivity extends BaseActivity<ProjectDetailPresenter> 
   protected @InjectView(R.id.location) TextView location;
   protected @InjectView(R.id.name) TextView name;
   protected @InjectView(R.id.percentage_funded) ProgressBar percentage_funded;
-  protected @InjectView(R.id.photo) ImageView photo;
+  protected @InjectView(R.id.project_detail_photo) ImageView photo;
+  protected @InjectView(R.id.project_detail_video) VideoView video;
+  protected @InjectView(R.id.play_button_overlay) ImageView play_button;
   protected @InjectView(R.id.pledged) TextView pledged;
+
   @Inject Money money;
 
   @Override
@@ -65,8 +73,13 @@ public class ProjectDetailActivity extends BaseActivity<ProjectDetailPresenter> 
     location.setText(project.location().displayableName());
     name.setText(project.name());
     percentage_funded.setProgress(Math.round(Math.min(100.0f, project.percentageFunded())));
-    Picasso.with(this).load(project.photo().full()).into(photo);
     pledged.setText(money.formattedCurrency(project.pledged(), project.currencyOptions()));
+    if ( project.video() != null ) {
+      loadVideo(project.video(), video);
+    }
+    else {
+      Picasso.with(this).load(project.photo().full()).into(photo);
+    }
   }
 
   @Override
@@ -77,8 +90,27 @@ public class ProjectDetailActivity extends BaseActivity<ProjectDetailPresenter> 
     overridePendingTransition(R.anim.fade_in_slide_in_left, R.anim.slide_out_right);
   }
 
+  public void loadVideo(Video video, VideoView videoView) {
+    Picasso.with(this).load(video.frame()).into(photo); // todo: make this loading smoother
+    Uri video_uri = Uri.parse(video.base());
+    videoView.setVideoURI(video_uri);
+    videoView.setMediaController(new MediaController(this));
+
+    // todo: do we need a listener here?
+    play_button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        photo.setVisibility(View.GONE);
+        play_button.setVisibility(View.GONE);
+        videoView.start();
+      }
+    });
+  }
+
   public void backProjectButtonOnClick(final View v) {
     Timber.d("backProjectButtonOnClick");
     presenter.takeBackProjectClick();
   }
+
+
 }

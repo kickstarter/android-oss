a9750db672e26f214b8650ddc4c0bb27128af9c0
Christopher Wright <christopherwright@gmail.com>
2015-07-28 20:18:14 -0400

    Fix capitalization

--- app/src/main/java/com/kickstarter/services/KickstarterWebViewClient.java
+++ app/src/main/java/com/kickstarter/services/KickstarterWebViewClient.java
@@ -161,7 +161,7 @@ public class KickstarterWebViewClient extends WebViewClient {
   }
 
   protected WebResourceResponse noopWebResourceResponse() throws IOException {
-    return new WebResourceResponse("application/javaScript", null, new ByteArrayInputStream(new byte[0]));
+    return new WebResourceResponse("application/javascript", null, new ByteArrayInputStream(new byte[0]));
   }
 
   public class MimeHeaders {

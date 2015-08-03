d8b3f7e83dad90d1953d3386a62e99871d610cfc
Christopher Wright <christopherwright@gmail.com>
2015-08-03 13:50:57 -0400

    Add a few missing imports

--- app/src/main/java/com/kickstarter/ui/activities/ProjectDetailActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/ProjectDetailActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import com.kickstarter.KsrApplication;
 import com.kickstarter.R;
 import com.kickstarter.libs.BaseActivity;
+import com.kickstarter.libs.DateTimeUtils;
 import com.kickstarter.libs.Money;
 import com.kickstarter.libs.RequiresPresenter;
 import com.kickstarter.models.Project;

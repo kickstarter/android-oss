b8bf8f5fdd063720a6eb712c0f60de429d6d6624
Brandon Williams <mbw234@gmail.com>
2015-08-04 18:19:45 -0400

    Added a takeWhen rx util operator.

--- app/src/main/java/com/kickstarter/libs/RxUtils.java
+++ app/src/main/java/com/kickstarter/libs/RxUtils.java
@@ -12,4 +12,8 @@ public class RxUtils {
   public static <F, S> Observable<Pair<F, S>> combineLatestPair(final Observable<F> ob1, final Observable<S> ob2) {
     return Observable.combineLatest(ob1, ob2, Pair::new);
   }
+
+  public static <F, S> Observable<F> takeWhen(final Observable<F> ob1, final Observable<S> ob2) {
+    return RxUtils.combineLatestPair(ob1, ob2).map(x -> x.first);
+  }
 }

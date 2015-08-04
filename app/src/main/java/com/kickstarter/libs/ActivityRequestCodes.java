c138ce73a40f70f4e245b24a7b3ae801054d8017
Christopher Wright <christopherwright@gmail.com>
2015-08-04 16:04:36 -0400

    Fix up comment

--- app/src/main/java/com/kickstarter/libs/ActivityRequestCodes.java
+++ app/src/main/java/com/kickstarter/libs/ActivityRequestCodes.java
@@ -2,8 +2,11 @@ package com.kickstarter.libs;
 
 public class ActivityRequestCodes {
   /*
-   * Still figuring out how these codes should work - I think the code should identify
-   * the requesting activity, the responding activity, and the reason for the request.
+   * Format for this, if activity A starts activity B:
+   *
+   * ACTIVITY_A_ACTIVITY_B_REASON
+   *
+   * Where REASON describes why activity A is requesting a result from activity B.
    */
   public final static int CHECKOUT_ACTIVITY_LOGIN_TOUT_ACTIVITY_USER_REQUIRED = 1;
   public final static int LOGIN_TOUT_ACTIVITY_LOGIN_ACTIVITY_FORWARD = 2;

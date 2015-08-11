5687c300a3b6821c8bdb5380d6497f58efb81d27
Christopher Wright <christopherwright@gmail.com>
2015-08-11 16:56:05 -0400

    Replace reference to kitkat with api number
    It's way easier to think about api numbers than all the codenames, imo

--- app/src/main/java/com/kickstarter/presenters/CheckoutPresenter.java
+++ app/src/main/java/com/kickstarter/presenters/CheckoutPresenter.java
@@ -50,7 +50,7 @@ public class CheckoutPresenter extends Presenter<CheckoutActivity> {
 
   private void checkoutNext(final CheckoutActivity activity) {
     final String javascript = "root.checkout_next();";
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+    if (android.os.Build.VERSION.SDK_INT >= 19) {
       activity.webView.evaluateJavascript(javascript, null);
     } else {
       activity.webView.loadUrl("javascript:" + javascript);

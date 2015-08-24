b276b3b7a688846272b4a5e1ca5555be424f6b17
Christopher Wright <christopherwright@gmail.com>
2015-08-24 16:57:44 -0400

    Code standardify static constants

--- app/src/main/java/com/kickstarter/services/KickstarterUri.java
+++ app/src/main/java/com/kickstarter/services/KickstarterUri.java
@@ -10,7 +10,7 @@ public class KickstarterUri {
   }
 
   public static boolean isProjectUri(final Uri uri, final String webEndpoint) {
-    return isKickstarterUri(uri, webEndpoint) && project.matcher(uri.getPath()).matches();
+    return isKickstarterUri(uri, webEndpoint) && PROJECT_PATTERN.matcher(uri.getPath()).matches();
   }
 
   public static boolean isSignupUri(final Uri uri, final String webEndpoint) {
@@ -18,20 +18,20 @@ public class KickstarterUri {
   }
 
   public static boolean isCheckoutThanksUri(final Uri uri, final String webEndpoint) {
-    return isKickstarterUri(uri, webEndpoint) && checkoutThanks.matcher(uri.getPath()).matches();
+    return isKickstarterUri(uri, webEndpoint) && CHECKOUT_THANKS_PATTERN.matcher(uri.getPath()).matches();
   }
 
   protected boolean isProjectNewPledgeUri(final Uri uri, final String webEndpoint) {
-    return isKickstarterUri(uri, webEndpoint) && newPledge.matcher(uri.getPath()).matches();
+    return isKickstarterUri(uri, webEndpoint) && NEW_PLEDGE_PATTERN.matcher(uri.getPath()).matches();
   }
 
   // /projects/slug-1/slug-2
-  private static final Pattern project = Pattern.compile("\\A\\/projects/[a-zA-Z0-9_-]+\\/[a-zA-Z0-9_-]+\\/?\\z");
+  private static final Pattern PROJECT_PATTERN = Pattern.compile("\\A\\/projects/[a-zA-Z0-9_-]+\\/[a-zA-Z0-9_-]+\\/?\\z");
 
   // /projects/slug-1/slug-2/checkouts/1/thanks
-  private static final Pattern checkoutThanks = Pattern.compile("\\A\\/projects/[a-zA-Z0-9_-]+\\/[a-zA-Z0-9_-]+\\/checkouts\\/\\d+\\/thanks\\z");
+  private static final Pattern CHECKOUT_THANKS_PATTERN = Pattern.compile("\\A\\/projects/[a-zA-Z0-9_-]+\\/[a-zA-Z0-9_-]+\\/checkouts\\/\\d+\\/thanks\\z");
 
   // /projects/slug-1/slug-2/pledge/new
-  private static final Pattern newPledge = Pattern.compile("\\A\\/projects/[a-zA-Z0-9_-]+\\/[a-zA-Z0-9_-]+\\/pledge\\/new\\z");
+  private static final Pattern NEW_PLEDGE_PATTERN = Pattern.compile("\\A\\/projects/[a-zA-Z0-9_-]+\\/[a-zA-Z0-9_-]+\\/pledge\\/new\\z");
 
 }

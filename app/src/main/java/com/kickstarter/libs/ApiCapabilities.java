36aa1af80d9e82b0dc85e21fd46e2535aaa51c83
Christopher Wright <christopherwright@gmail.com>
2015-09-11 23:14:26 -0400

    Add check for whether we can programatically set status bar

--- app/src/main/java/com/kickstarter/libs/ApiCapabilities.java
+++ app/src/main/java/com/kickstarter/libs/ApiCapabilities.java
@@ -10,4 +10,8 @@ public class ApiCapabilities {
   public static boolean canEvaluateJavascript() {
     return android.os.Build.VERSION.SDK_INT >= 19;
   }
+
+  public static boolean canSetStatusBarColor() {
+    return android.os.Build.VERSION.SDK_INT >= 21;
+  }
 }

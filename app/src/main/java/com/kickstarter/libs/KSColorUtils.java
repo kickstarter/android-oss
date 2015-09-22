84d8ffa1dbecf8cc240fc812e661282e6ff07530
Christopher Wright <christopherwright@gmail.com>
2015-09-22 12:03:49 -0400

    Add util methods to check if color is light

--- app/src/main/java/com/kickstarter/libs/KSColorUtils.java
+++ app/src/main/java/com/kickstarter/libs/KSColorUtils.java
@@ -6,6 +6,18 @@ import android.support.v4.graphics.ColorUtils;
 public class KSColorUtils {
   private KSColorUtils() {}
 
+  private final static float KICKSTARTER_LIGHTNESS_THRESHOLD = 0.73f;
+
+  /**
+   * Set the alpha portion of the color.
+   *
+   * @param color   the (a)rgb color to set an alpha for.
+   * @param alpha   the new alpha value, between 0 and 255.
+   */
+  public static int setAlpha(final int color, final int alpha) {
+    return color | (alpha << 24);
+  }
+
   /**
    * Darken the argb color by a percentage.
    *
@@ -32,5 +44,31 @@ public class KSColorUtils {
     hsl[2] += (1.0f - hsl[2]) * percent;
     // HSLToColor sets alpha to fully opaque, so pluck the alpha from the original color.
     return (color & 0xFF000000) | (ColorUtils.HSLToColor(hsl) & 0x00FFFFFF);
+  }
+
+  /**
+   * Check whether a color is light.
+   *
+   * @param color   the argb color to check.
+   */
+  public static boolean isLight(final int color) {
+    return weightedLightness(color) >= KICKSTARTER_LIGHTNESS_THRESHOLD;
+  }
+
+  /**
+   * Check whether a color is dark.
+   *
+   * @param color   the argb color to check.
+   */
+  public static boolean isDark(final int color) {
+    return !isLight(color);
+  }
+
+  /*
+   * Return a value between 0.0 and 1.0 representing the perceived lightness of the color.
+   * More info here: https://robots.thoughtbot.com/closer-look-color-lightness
+   */
+  private static double weightedLightness(final int color) {
+    return ((Color.red(color) * 212.6 + Color.green(color) * 715.2 + Color.blue(color) * 72.2) / 1000) / 255;
   }
 }

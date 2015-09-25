ec079dd702c861aa0c321c96dd0545664f5e40eb
Lisa Luo <lluo92@gmail.com>
2015-09-25 14:59:31 -0400

    Move project activity view binding and click functions to view holder

--- app/src/main/java/com/kickstarter/ui/activities/ProjectActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/ProjectActivity.java
@@ -1,68 +1,39 @@
 package com.kickstarter.ui.activities;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Html;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.MediaController;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.util.Pair;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import com.kickstarter.KSApplication;
 import com.kickstarter.R;
 import com.kickstarter.libs.ActivityRequestCodes;
 import com.kickstarter.libs.BaseActivity;
-import com.kickstarter.libs.CircleTransform;
-import com.kickstarter.libs.DateTimeUtils;
 import com.kickstarter.libs.Money;
 import com.kickstarter.libs.RequiresPresenter;
 import com.kickstarter.models.Project;
-import com.kickstarter.models.Video;
+import com.kickstarter.models.Reward;
 import com.kickstarter.presenters.ProjectPresenter;
-import com.kickstarter.ui.adapters.RewardListAdapter;
+import com.kickstarter.ui.adapters.ProjectAdapter;
 import com.kickstarter.ui.views.IconTextView;
-import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import timber.log.Timber;
+import rx.Observable;
 
 @RequiresPresenter(ProjectPresenter.class)
 public class ProjectActivity extends BaseActivity<ProjectPresenter> {
-  RewardListAdapter rewardListAdapter;
   @Bind(R.id.rewards_recycler_view) RecyclerView rewardsRecyclerView;
-
-  protected @Bind(R.id.backers_count) TextView backersCountTextView;
-  protected @Bind(R.id.blurb) TextView blurbTextView;
-  protected @Bind(R.id.category) TextView categoryTextView;
-  protected @Bind(R.id.creator_name) TextView creatorNameTextView;
-  protected @Bind(R.id.comments_count) TextView commentsCountTextView;
-  protected @Bind(R.id.deadline_countdown) TextView deadlineCountdownTextView;
-  protected @Bind(R.id.deadline_countdown_unit) TextView deadlineCountdownUnitTextView;
-  protected @Bind(R.id.goal) TextView goalTextView;
-  protected @Bind(R.id.location) TextView locationTextView;
-  protected @Bind(R.id.project_name) TextView projectNameTextView;
-  protected @Bind(R.id.percentage_funded) ProgressBar percentageFundedProgressBar;
-  protected @Bind(R.id.project_detail_photo) ImageView photoImageView;
-  protected @Bind(R.id.project_detail_video) VideoView videoView;
-  protected @Bind(R.id.play_button_overlay) IconTextView playButtonIconTextView;
-  protected @Bind(R.id.pledged) TextView pledgedTextView;
-  protected @Bind(R.id.avatar) ImageView avatarImageView;
-  protected @Bind(R.id.avatar_name) TextView avatarNameTextView;
-  protected @Bind(R.id.fund_message) TextView fundMessageTextView;
-  protected @Bind(R.id.updates_count) TextView updatesCountTextView;
-  protected @Bind(R.id.star_icon) IconTextView starIconTextView;
+  @Nullable @Bind(R.id.star_icon) IconTextView starIconTextView;
 
   @Inject Money money;
 
@@ -77,74 +48,33 @@ public class ProjectActivity extends BaseActivity<ProjectPresenter> {
     final Intent intent = getIntent();
     final Project project = intent.getExtras().getParcelable(getString(R.string.intent_project));
     presenter.takeProject(project);
-
-    // WIP
-    rewardsRecyclerView.setHasFixedSize(true);
-    rewardsRecyclerView.getLayoutParams().height = 2000; // todo: WIP
-    rewardsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
   }
 
   public void show(final Project project) {
-
-    // Project information
-    blurbTextView.setText(Html.fromHtml(getString(R.string.Blurb_read_more, project.blurb())));
-    creatorNameTextView.setText(Html.fromHtml(getString(R.string.by_creator, project.creator().name())));
-    backersCountTextView.setText(project.formattedBackersCount());
-    categoryTextView.setText(project.category().name());
-    deadlineCountdownTextView.setText(Integer.toString(project.deadlineCountdownValue()));
-    deadlineCountdownUnitTextView.setText(project.deadlineCountdownUnit(this));
-    goalTextView.setText(money.formattedCurrency(project.goal(), project.currencyOptions(), true));
-    locationTextView.setText(project.location().displayableName());
-    projectNameTextView.setText(project.name());
-    percentageFundedProgressBar.setProgress(Math.round(Math.min(100.0f, project.percentageFunded())));
-    pledgedTextView.setText(money.formattedCurrency(project.pledged(), project.currencyOptions()));
-    Picasso.with(this).load(project.photo().full()).into(photoImageView);
-
-    // Creator information
-    Picasso.with(this).load(project.creator().avatar()
-      .medium())
-      .transform(new CircleTransform())
-      .into(avatarImageView);
-    avatarNameTextView.setText(project.creator().name());
-    fundMessageTextView.setText(String.format(getString(R.string.This_project_will_only_be_funded_if),
-      money.formattedCurrency(project.goal(), project.currencyOptions(), true),
-      project.deadline().toString(DateTimeUtils.writtenDeadline())));
-    updatesCountTextView.setText(project.formattedUpdatesCount());
-    commentsCountTextView.setText(project.formattedCommentsCount());
-
     int starColor = (project.isStarred()) ? R.color.green : R.color.dark_gray;
     starIconTextView.setTextColor(ContextCompat.getColor(this, starColor));
 
-    //////
+    loadProjectRewards(project, project.rewards());
+  }
+
+  public void loadProjectRewards(final Project project, final List<Reward> rewards) {
+    // TODO
     if (project.rewards() != null) {
-      Log.d("TEST", "project has rewards");
-      rewardListAdapter = new RewardListAdapter(project.rewards());
-      rewardsRecyclerView.setAdapter(rewardListAdapter);  // todo
+      final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+      final List<Pair<Project, Reward>> projectAndRewards = Observable.from(rewards)
+        .map(reward -> Pair.create(project, reward))
+        .toList().toBlocking().single();
+
+      final ProjectAdapter adapter = new ProjectAdapter(project, projectAndRewards, presenter);
+      rewardsRecyclerView.setLayoutManager(layoutManager);
+      rewardsRecyclerView.setAdapter(adapter);
     }
   }
 
   @Override
   public void onBackPressed() {
     super.onBackPressed();
-    Timber.d("onBackPressed %s", this.toString());
-
     overridePendingTransition(R.anim.fade_in_slide_in_left, R.anim.slide_out_right);
-  }
-
-  // todo: setting the VideoView uri here prevents the activity from GC'ing
-  // VideoView either needs to be set independent of XML, or better
-  // just use ExoPlayer library
-  public void loadVideo(Video video, VideoView videoView) {
-    final Uri videoUri = Uri.parse(video.base());
-    videoView.setVideoURI(videoUri);
-    videoView.setMediaController(new MediaController(this));
-
-    // replace with @OnClick
-    playButtonIconTextView.setOnClickListener((View v) -> {
-      photoImageView.setVisibility(View.GONE);
-      playButtonIconTextView.setVisibility(View.GONE);
-      videoView.start();
-    });
   }
 
   @OnClick(R.id.back_project_button)
@@ -152,32 +82,12 @@ public class ProjectActivity extends BaseActivity<ProjectPresenter> {
     presenter.takeBackProjectClick();
   }
 
-  @OnClick(R.id.comments)
-  public void commentsClick() {
-    presenter.takeCommentsClick();
-  }
-
   @OnClick(R.id.star_icon)
   public void starProjectClick() {
     presenter.takeStarClick();
   }
 
-  @OnClick(R.id.updates)
-  public void updatesClick() {
-    presenter.takeUpdatesClick();
-  }
-
-  @OnClick({R.id.blurb, R.id.campaign})
-  public void blurbOnClick() {
-    presenter.takeBlurbClick();
-  }
-
-  @OnClick({R.id.creator_name, R.id.creator_info})
-  public void creatorNameOnClick() {
-    presenter.takeCreatorNameClick();
-  }
-
-  @OnClick({R.id.share_icon, R.id.share_button})
+  @OnClick(R.id.share_icon)
   public void shareProject() {
     presenter.takeShareClick();
   }

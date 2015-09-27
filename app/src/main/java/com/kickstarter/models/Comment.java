976b2a340b5aa6b5d27f9b3625c0326c47d0d069
Christopher Wright <christopherwright@gmail.com>
2015-09-26 22:40:52 -0400

    Switch Comment to AutoParcel

--- app/src/main/java/com/kickstarter/models/Comment.java
+++ app/src/main/java/com/kickstarter/models/Comment.java
@@ -4,39 +4,32 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import com.hannesdorfmann.parcelableplease.annotation.ParcelablePlease;
+import com.kickstarter.libs.AutoGson;
 
 import org.joda.time.DateTime;
 
-@ParcelablePlease
-public class Comment implements Parcelable {
-  public String body = null;
-  public DateTime createdAt = null;
-  public User author = null;
-  public Integer id = null;
+import auto.parcel.AutoParcel;
 
-  public String body() {
-    return body;
+@AutoParcel
+@AutoGson
+public abstract class Comment implements Parcelable {
+  public abstract User author();
+  public abstract String body();
+  public abstract DateTime createdAt();
+  public abstract long id();
+
+  @AutoParcel.Builder
+  public abstract static class Builder {
+    public abstract Builder author(User __);
+    public abstract Builder body(String __);
+    public abstract Builder createdAt(DateTime __);
+    public abstract Builder id(long __);
+    public abstract Comment builder();
   }
-  public DateTime createdAt() {
-    return createdAt;
+
+  public static Builder builder() {
+    return new AutoParcel_Comment.Builder();
   }
-  public User author() {
-    return author;
-  }
-  public Integer id() {
-    return id;
-  }
-  
-  @Override
-  public int describeContents() { return 0; }
-  @Override
-  public void writeToParcel(Parcel dest, int flags) {CommentParcelablePlease.writeToParcel(this, dest, flags);}
-  public static final Creator<Comment> CREATOR = new Creator<Comment>() {
-    public Comment createFromParcel(Parcel source) {
-      Comment target = new Comment();
-      CommentParcelablePlease.readFromParcel(target, source);
-      return target;
-    }
-    public Comment[] newArray(int size) {return new Comment[size];}
-  };
+
+  public abstract Builder toBuilder();
 }

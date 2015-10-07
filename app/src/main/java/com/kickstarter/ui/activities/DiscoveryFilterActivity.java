340db9ab1595f83858049f03ec5aee196b1910c1
Christopher Wright <christopherwright@gmail.com>
2015-10-07 18:27:35 -0400

    Load category image in landscape, portrait

--- app/src/main/java/com/kickstarter/ui/activities/DiscoveryFilterActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/DiscoveryFilterActivity.java
@@ -2,12 +2,16 @@ package com.kickstarter.ui.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,6 +21,7 @@ import com.kickstarter.libs.BaseActivity;
 import com.kickstarter.libs.DiscoveryUtils;
 import com.kickstarter.libs.KSColorUtils;
 import com.kickstarter.libs.StatusBarUtils;
+import com.kickstarter.libs.ViewUtils;
 import com.kickstarter.libs.qualifiers.RequiresPresenter;
 import com.kickstarter.models.Category;
 import com.kickstarter.presenters.DiscoveryFilterPresenter;
@@ -38,6 +43,7 @@ public class DiscoveryFilterActivity extends BaseActivity<DiscoveryFilterPresent
 
   @Bind(R.id.close_button) TextView closeButton;
   @Bind(R.id.discovery_filter_layout) RelativeLayout layout;
+  @Bind(R.id.background_gradient) LinearLayout backgroundGradientLayout;
   @Bind(R.id.recycler_view) RecyclerView recyclerView;
   @BindColor(R.color.dark_blue_gradient_start) int darkBlueGradientStartColor;
   @BindColor(R.color.text_dark) int darkColor;
@@ -59,7 +65,15 @@ public class DiscoveryFilterActivity extends BaseActivity<DiscoveryFilterPresent
 
     loadParams(discoveryParams);
 
-    presenter.initialize(DiscoveryParams.builder().build()); // TODO: Replace with params from discovery intent
+    final RelativeLayout.LayoutParams layoutParams;
+    if (ViewUtils.isPortrait(this)) {
+      layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+    } else {
+      layoutParams = new RelativeLayout.LayoutParams(900, RelativeLayout.LayoutParams.MATCH_PARENT);
+    }
+    backgroundGradientLayout.setLayoutParams(layoutParams);
+
+    presenter.initialize(discoveryParams);
   }
 
   @OnClick(R.id.close_button)
@@ -88,9 +102,18 @@ public class DiscoveryFilterActivity extends BaseActivity<DiscoveryFilterPresent
     }
 
     if (params.isCategorySet()) {
-      recyclerView.setBackgroundColor(params.category().colorWithAlpha());
+      layout.setBackgroundColor(params.category().colorWithAlpha());
+      final Drawable backgroundDrawable = params.category().imageWithOrientation(this, getResources().getConfiguration().orientation);
+      if (backgroundDrawable != null) {
+        layout.setBackground(backgroundDrawable);
+      }
+
+      final int color = params.category().color();
+      final int[] gradientColors = new int[] {KSColorUtils.setAlpha(color, 242), KSColorUtils.setAlpha(color, 0)};
+      GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, gradientColors);
+      backgroundGradientLayout.setBackground(gradientDrawable);
     } else {
-      recyclerView.setBackground(darkBlueGradientDrawable);
+      layout.setBackground(darkBlueGradientDrawable);
     }
 
     closeButton.setTextColor(DiscoveryUtils.overlayTextColor(this, params));

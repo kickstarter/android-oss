0e467e8b31bab6aceef535dcf8e4f3edd4ae28d3
Lisa Luo <lluo92@gmail.com>
2015-10-12 18:53:07 -0400

    FriendBacking card click

--- app/src/main/java/com/kickstarter/ui/viewholders/FriendBackingViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/FriendBackingViewHolder.java
@@ -1,6 +1,7 @@
 package com.kickstarter.ui.viewholders;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,10 +10,12 @@ import android.widget.TextView;
 import com.kickstarter.R;
 import com.kickstarter.libs.CircleTransform;
 import com.kickstarter.libs.StringUtils;
+import com.kickstarter.ui.activities.ProjectActivity;
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FriendBackingViewHolder extends ActivityListViewHolder {
   @Bind(R.id.avatar) ImageView avatarImageView;
@@ -20,6 +23,8 @@ public class FriendBackingViewHolder extends ActivityListViewHolder {
   @Bind(R.id.project_name) TextView projectNameTextView;
   @Bind(R.id.project_photo) ImageView projectPhotoImageView;
   @Bind(R.id.title) TextView titleTextView;
+
+  private Context context;
 
   public FriendBackingViewHolder(@NonNull final View view) {
     super(view);
@@ -30,7 +35,7 @@ public class FriendBackingViewHolder extends ActivityListViewHolder {
   public void onBind(@NonNull final Object datum) {
     super.onBind(datum);
 
-    final Context context = view.getContext();
+    context = view.getContext();
 
     Picasso.with(context)
       .load(activity.user().avatar().small())
@@ -44,6 +49,13 @@ public class FriendBackingViewHolder extends ActivityListViewHolder {
     titleTextView.setText(StringUtils.friendBackingActivityTitle(context,
       activity.user().name(),
       activity.project().category().rootId()));
+  }
+
+  @OnClick(R.id.friend_backing_card_view)
+  public void startProjectActivity() {
+    final Intent intent = new Intent(context, ProjectActivity.class)
+      .putExtra(context.getString(R.string.intent_project), activity.project());
+    context.startActivity(intent);
   }
 }
 

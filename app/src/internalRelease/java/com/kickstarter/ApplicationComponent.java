58acb5a889a671e4b254ebe482b1f055bacfb296
Christopher Wright <christopherwright@gmail.com>
2015-10-13 17:13:15 -0400

    Add internal module

--- app/src/internalRelease/java/com/kickstarter/ApplicationComponent.java
+++ app/src/internalRelease/java/com/kickstarter/ApplicationComponent.java
@@ -1,13 +1,10 @@
 package com.kickstarter;
 
-import com.kickstarter.ui.views.DebugDrawer;
-
 import javax.inject.Singleton;
 
 import dagger.Component;
 
 @Singleton
 @Component(modules = {InternalReleaseApplicationModule.class})
-public interface ApplicationComponent extends ApplicationGraph {
-  void inject(DebugDrawer __);
+public interface ApplicationComponent extends InternalApplicationComponent {
 }

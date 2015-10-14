6a17133159bea40d8c743aa2c6cd3ec4a838f141
Lisa Luo <lluo92@gmail.com>
2015-10-14 13:24:21 -0400

    remove Spannable string
    use fromHtml instead for formatting, so string is contained in strings.xml

--- app/src/main/java/com/kickstarter/ui/viewholders/FriendFollowViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/FriendFollowViewHolder.java
@@ -2,8 +2,7 @@ package com.kickstarter.ui.viewholders;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.text.Spannable;
-import android.text.style.StyleSpan;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -35,11 +34,6 @@ public class FriendFollowViewHolder extends ActivityListViewHolder {
       .transform(new CircleTransform())
       .into(avatarImageView);
 
-    titleTextView.setText(context.getString(R.string.username_is_following_you, activity.user().name()));
-    final Spannable titleStr = (Spannable) titleTextView.getText();
-    titleStr.setSpan(new StyleSpan(android.graphics.Typeface.BOLD),
-      0,
-      activity.user().name().length(),
-      Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+    titleTextView.setText(Html.fromHtml(context.getString(R.string.username_is_following_you, activity.user().name())));
   }
 }

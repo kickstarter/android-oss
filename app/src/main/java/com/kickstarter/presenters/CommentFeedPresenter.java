36018cddc8cd65de762b24d031a965253e92230e
Christopher Wright <christopherwright@gmail.com>
2015-10-16 21:52:49 -0400

    Remove unused import

--- app/src/main/java/com/kickstarter/presenters/CommentFeedPresenter.java
+++ app/src/main/java/com/kickstarter/presenters/CommentFeedPresenter.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Pair;
 
-import com.jakewharton.rxbinding.widget.RxTextView;
 import com.kickstarter.KSApplication;
 import com.kickstarter.libs.CurrentUser;
 import com.kickstarter.libs.Presenter;

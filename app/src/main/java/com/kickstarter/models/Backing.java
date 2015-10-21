39841df8cf0f7cba330fe62864aca21f87a5c0f4
Lisa Luo <lluo92@gmail.com>
2015-10-20 20:04:56 -0400

    mark nullable values of Backing

--- app/src/main/java/com/kickstarter/models/Backing.java
+++ app/src/main/java/com/kickstarter/models/Backing.java
@@ -15,32 +15,32 @@ import auto.parcel.AutoParcel;
 @AutoParcel
 public abstract class Backing implements Parcelable {
   public abstract String projectCountry();
-  public abstract Reward reward();
+  @Nullable public abstract Reward reward();
   public abstract String status();
-  public abstract Integer shippingAmount();
-  public abstract Integer amount();
-  public abstract Project project();
+  public abstract float shippingAmount();
+  public abstract float amount();
+  @Nullable public abstract Project project();
   public abstract long backerId();
   @Nullable public abstract Location location();
   public abstract DateTime pledgedAt();
-  public abstract long rewardId();
+  @Nullable public abstract Long rewardId();
   public abstract long id();
   public abstract long projectId();
   public abstract long sequence(); // backer #
-  public abstract User backer();
+  @Nullable public abstract User backer();
 
   @AutoParcel.Builder
   public abstract static class Builder {
     public abstract Builder projectCountry(String __);
     public abstract Builder reward(Reward __);
     public abstract Builder status(String __);
-    public abstract Builder shippingAmount(Integer __);
-    public abstract Builder amount(Integer __);
+    public abstract Builder shippingAmount(float __);
+    public abstract Builder amount(float __);
     public abstract Builder project(Project __);
     public abstract Builder backerId(long __);
     public abstract Builder location(Location __);
     public abstract Builder pledgedAt(DateTime __);
-    public abstract Builder rewardId(long __);
+    public abstract Builder rewardId(Long __);
     public abstract Builder id(long __);
     public abstract Builder projectId(long __);
     public abstract Builder sequence(long __);

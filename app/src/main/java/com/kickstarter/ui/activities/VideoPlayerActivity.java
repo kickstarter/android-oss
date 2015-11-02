9a59e81802fafcef828da5ecbc6e2a4fa22e52f0
Lisa Luo <lluo92@gmail.com>
2015-11-02 13:24:32 -0500

    set up basic exoplayer with extractor renderer
    stock mediacontroller, etc.

--- app/src/main/java/com/kickstarter/ui/activities/VideoPlayerActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/VideoPlayerActivity.java
@@ -1,10 +1,13 @@
 package com.kickstarter.ui.activities;
 
 import android.content.Intent;
+import android.media.MediaCodec;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.MediaController;
@@ -12,15 +15,34 @@ import android.widget.MediaController;
 import com.google.android.exoplayer.AspectRatioFrameLayout;
 import com.google.android.exoplayer.ExoPlaybackException;
 import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
+import com.google.android.exoplayer.extractor.ExtractorSampleSource;
+import com.google.android.exoplayer.upstream.Allocator;
+import com.google.android.exoplayer.upstream.DataSource;
+import com.google.android.exoplayer.upstream.DefaultAllocator;
+import com.google.android.exoplayer.upstream.DefaultUriDataSource;
+import com.google.android.exoplayer.util.PlayerControl;
 import com.kickstarter.R;
 import com.kickstarter.libs.BaseActivity;
 import com.kickstarter.models.Project;
+import com.kickstarter.models.Video;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class VideoPlayerActivity extends BaseActivity implements ExoPlayer.Listener {
+public class VideoPlayerActivity extends BaseActivity implements ExoPlayer.Listener, SurfaceHolder.Callback {
+  private final int TRACK_RENDERER_COUNT = 3; // audio, video, text
+  private final int BUFFER_SEGMENT_SIZE = 64 * 1024;
+  private final int BUFFER_SEGMENT_COUNT = 256;
+
   private MediaController mediaController;
+  private MediaCodecVideoTrackRenderer videoRenderer;
+  private MediaCodecAudioTrackRenderer audioRenderer;
+  private ExoPlayer player;
+  private boolean playerNeedsPrepare;
+  private long playerPosition;
+  private Video video;
 
   public @Bind(R.id.root) View root;
   public @Bind(R.id.video_frame) AspectRatioFrameLayout videoFrame;
@@ -32,17 +54,53 @@ public class VideoPlayerActivity extends BaseActivity implements ExoPlayer.Liste
     setContentView(R.layout.video_player_layout);
     ButterKnife.bind(this);
 
+    final Intent intent = getIntent();
+    final Project project = intent.getParcelableExtra(getString(R.string.intent_project));
+    video = project.video();
+
     root.setOnTouchListener(((view, motionEvent) -> {
       if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-        Log.d("TEST", "touch");
+        toggleControlsVisibility();
       } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
         view.performClick();
       }
       return true;
     }));
 
-    final Intent intent = getIntent();
-    final Project project = intent.getParcelableExtra(getString(R.string.intent_project));
+    surfaceView.getHolder().addCallback(this);
+    mediaController = new MediaController(this);
+    mediaController.setAnchorView(root);
+  }
+
+  private void preparePlayer(final boolean playWhenReady) {
+    if (player == null) {
+      player = ExoPlayer.Factory.newInstance(TRACK_RENDERER_COUNT, 1000, 5000);
+      player.addListener(this);
+      player.seekTo(playerPosition);
+      playerNeedsPrepare = true;
+      mediaController.setMediaPlayer(new PlayerControl(player));
+    }
+
+    if (playerNeedsPrepare) {
+      buildRenderers(player, video);
+      playerNeedsPrepare = false;
+      // update control visibilities
+    }
+
+    player.sendMessage(videoRenderer, MediaCodecVideoTrackRenderer.MSG_SET_SURFACE, surfaceView.getHolder().getSurface());
+    player.setPlayWhenReady(playWhenReady);
+  }
+
+  public void buildRenderers(@NonNull final ExoPlayer player, @NonNull final Video video) {
+    final Allocator allocator = new DefaultAllocator(BUFFER_SEGMENT_SIZE);
+    final DataSource dataSource = new DefaultUriDataSource(this, video.high());
+    final ExtractorSampleSource sampleSource = new ExtractorSampleSource(Uri.parse(video.high()), dataSource,
+      allocator, BUFFER_SEGMENT_COUNT * BUFFER_SEGMENT_SIZE);
+
+    videoRenderer = new MediaCodecVideoTrackRenderer(this, sampleSource, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
+    audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource);
+
+    player.prepare(videoRenderer, audioRenderer);
   }
 
   @Override
@@ -58,5 +116,36 @@ public class VideoPlayerActivity extends BaseActivity implements ExoPlayer.Liste
   @Override
   public void onPlayerError(@Nullable final ExoPlaybackException error) {
 
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    if (player == null) {
+      preparePlayer(true);
+    }
+  }
+
+  @Override
+  public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+  }
+
+  @Override
+  public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+  }
+
+  @Override
+  public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+  }
+
+  public void toggleControlsVisibility() {
+    if (mediaController.isShowing()) {
+      mediaController.hide();
+    } else {
+      mediaController.show(0);
+    }
   }
 }

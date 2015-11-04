0fb3c94e4a4eeca15e8d55993ebc598b7e713983
Gina Binetti <theginbin@gmail.com>
2015-11-04 17:39:01 -0500

    add popup menu with webview links

--- app/src/main/java/com/kickstarter/ui/activities/SignupActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/SignupActivity.java
@@ -1,14 +1,16 @@
 package com.kickstarter.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kickstarter.R;
@@ -29,6 +31,7 @@ public class SignupActivity extends BaseActivity<SignupPresenter> {
   @Bind(R.id.password) EditText passwordEditText;
   @Bind(R.id.signup_button) Button signupButton;
   @Bind(R.id.newsletter_switch) Switch newsletterSwitch;
+  @Bind(R.id.more_button) TextView moreButton; // TODO: move this to a login toolbar
 
   @Override
   protected void onCreate(@Nullable final Bundle savedInstanceState) {
@@ -37,7 +40,7 @@ public class SignupActivity extends BaseActivity<SignupPresenter> {
     setContentView(R.layout.signup_layout);
     ButterKnife.bind(this);
 
-    newsletterSwitch.setChecked(true); //get initial state from presenter
+    newsletterSwitch.setChecked(true); // TODO: get initial state from presenter
   }
 
   @Override
@@ -45,6 +48,42 @@ public class SignupActivity extends BaseActivity<SignupPresenter> {
   public void onBackPressed() {
     super.onBackPressed();
     overridePendingTransition(R.anim.fade_in_slide_in_left, R.anim.slide_out_right);
+  }
+
+  @OnClick(R.id.more_button)
+  public void moreButtonOnClick() {
+   final PopupMenu popup = new PopupMenu(this, moreButton); // TODO: this should be white background and above moreButton
+    popup.getMenuInflater().inflate(R.menu.login_help_menu, popup.getMenu());
+    popup.setOnMenuItemClickListener(item -> {
+      switch (item.getItemId()) {
+        case R.id.terms:
+          final Intent intent = new Intent(this, HelpActivity.class);
+          intent.putExtra(getString(R.string.intent_help_type), HelpActivity.HELP_TYPE_TERMS);
+          startActivity(intent);
+          overridePendingTransition(R.anim.slide_in_right, R.anim.fade_out_slide_out_left);
+          break;
+        case R.id.privacy_policy:
+          final Intent intent = new Intent(this, HelpActivity.class);
+          intent.putExtra(getString(R.string.intent_help_type), HelpActivity.HELP_TYPE_PRIVACY);
+          startActivity(intent);
+          overridePendingTransition(R.anim.slide_in_right, R.anim.fade_out_slide_out_left);
+          break;
+        case R.id.cookie_policy:
+          final Intent intent = new Intent(this, HelpActivity.class);
+          intent.putExtra(getString(R.string.intent_help_type), HelpActivity.HELP_TYPE_COOKIE_POLICY);
+          startActivity(intent);
+          overridePendingTransition(R.anim.slide_in_right, R.anim.fade_out_slide_out_left);
+          break;
+        case R.id.help:
+          final Intent intent = new Intent(this, HelpActivity.class);
+          intent.putExtra(getString(R.string.intent_help_type), HelpActivity.HELP_TYPE_HOW_IT_WORKS);
+          startActivity(intent);
+          overridePendingTransition(R.anim.slide_in_right, R.anim.fade_out_slide_out_left);
+          break;
+      }
+      return true;
+    });
+    popup.show();
   }
 
   @OnTextChanged(R.id.full_name)
@@ -58,7 +97,7 @@ public class SignupActivity extends BaseActivity<SignupPresenter> {
 
   @OnCheckedChanged(R.id.newsletter_switch)
   void onNewsletterCheckedChange(@NonNull final CompoundButton newsletterSwitch) {
-    Log.d("gina", "is checked = " + newsletterSwitch.isChecked());
+    //Log.d("gina", "is checked = " + newsletterSwitch.isChecked());
   }
 
   @OnClick(R.id.signup_button)

1d6901c1985fb105eca0136699fa3af79646aec4
Lisa Luo <lluo92@gmail.com>
2015-11-04 15:57:48 -0500

    reformat shipping destination text views on reward cards

--- app/src/main/java/com/kickstarter/ui/viewholders/RewardViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/RewardViewHolder.java
@@ -28,6 +28,7 @@ public class RewardViewHolder extends KsrViewHolder {
   public @Bind(R.id.limited) TextView limitedTextView;
   public @Bind(R.id.all_gone) TextView allGoneTextView;
   public @Bind(R.id.white_overlay) View whiteOverlayView;
+  public @Bind(R.id.shipping_destination) TextView shippingDestinationTextView;
   public @Bind(R.id.shipping_summary) TextView shippingSummaryTextView;
 
   @Inject Money money;
@@ -64,12 +65,12 @@ public class RewardViewHolder extends KsrViewHolder {
     descriptionTextView.setText(reward.description());
     estimatedDeliveryTextView.setText(
       reward.estimatedDeliveryOn().toString(DateTimeUtils.estimatedDeliveryOn()));
-    shippingSummaryTextView.setText(reward.shippingSummary());
 
     toggleAllGoneRewardView();
+    toggleClickableReward();
     toggleLimitedRewardView();
     toggleSelectedRewardView();
-    toggleClickableReward();
+    toggleShippingDestinationView();
   }
 
   public void toggleAllGoneRewardView() {
@@ -100,6 +101,17 @@ public class RewardViewHolder extends KsrViewHolder {
     } else {
       greenOverlayView.setVisibility(View.INVISIBLE);
       selectedTextView.setVisibility(View.GONE);
+    }
+  }
+
+  public void toggleShippingDestinationView() {
+    if (reward.shippingSummary() != null) {
+      shippingDestinationTextView.setVisibility(View.VISIBLE);
+      shippingSummaryTextView.setVisibility(View.VISIBLE);
+      shippingSummaryTextView.setText(reward.shippingSummary());
+    } else {
+      shippingDestinationTextView.setVisibility(View.GONE);
+      shippingSummaryTextView.setVisibility(View.GONE);
     }
   }
 

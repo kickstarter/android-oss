e6593747c948096d20bd6ecfa9bbc2a215a39832
Christopher Wright <christopherwright@gmail.com>
2015-11-04 19:46:08 -0500

    Search result revisions

--- app/src/main/java/com/kickstarter/ui/viewholders/SearchTermViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/SearchTermViewHolder.java
@@ -16,7 +16,6 @@ import butterknife.ButterKnife;
 public class SearchTermViewHolder extends KsrViewHolder {
   private DiscoveryParams params;
 
-  @Bind(R.id.search_icon_text_view) IconTextView iconTextView;
   @Bind(R.id.search_term_text_view) TextView termTextView;
   @Bind(R.id.search_term_view) LinearLayout layout;
 
@@ -32,7 +31,6 @@ public class SearchTermViewHolder extends KsrViewHolder {
 
     if (params.sort() == DiscoveryParams.Sort.POPULAR) {
       termTextView.setText(context.getString(R.string.Most_Popular));
-      iconTextView.setVisibility(View.VISIBLE);
     } else {
       // NOTE: This path isn't currently executed, keeping it in case we modify the design to show this view
       // in more situations
@@ -41,7 +39,6 @@ public class SearchTermViewHolder extends KsrViewHolder {
       } else {
         termTextView.setText("");
       }
-      iconTextView.setVisibility(View.GONE);
     }
   }
 }

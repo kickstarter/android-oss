44493e42c1e16df0cf01370065ad816e86de68d5
Gina Binetti <theginbin@gmail.com>
2015-11-05 16:06:18 -0500

    display errors in toast as multiline string

--- app/src/main/java/com/kickstarter/ui/activities/SignupActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/SignupActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -118,7 +119,11 @@ public class SignupActivity extends BaseActivity<SignupPresenter> {
   }
 
   private void displayError(@NonNull final List<String> message, final boolean forward) {
-    final Toast toast = Toast.makeText(this, "error\nanother error\nyet another", Toast.LENGTH_LONG);
+    final String errors = new StringBuilder()
+      .append(TextUtils.join("\n", message))
+      .toString();
+
+    final Toast toast = Toast.makeText(this, errors, Toast.LENGTH_LONG);
     toast.show();
   }
 }

92bf2054f89a9a6945ed0b50ee14f14adf76a6fb
Christopher Wright <christopherwright@gmail.com>
2015-11-05 18:36:08 -0500

    Add activity feed toolbar, update friend follow view

---
+++ app/src/main/java/com/kickstarter/ui/toolbars/ActivityFeedToolbar.java
@@ -0,0 +1,20 @@
+package com.kickstarter.ui.toolbars;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+public class ActivityFeedToolbar extends KSToolbar {
+  public ActivityFeedToolbar(@NonNull final Context context) {
+    super(context);
+  }
+
+  public ActivityFeedToolbar(@NonNull final Context context, @Nullable final AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public ActivityFeedToolbar(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+}

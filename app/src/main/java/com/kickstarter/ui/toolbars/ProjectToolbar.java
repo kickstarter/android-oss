f9ef0a418cb218c823b2dbe5613d032b46c4d662
Christopher Wright <christopherwright@gmail.com>
2015-11-07 00:33:41 -0500

    Bind toolbar to hook up back button

--- app/src/main/java/com/kickstarter/ui/toolbars/ProjectToolbar.java
+++ app/src/main/java/com/kickstarter/ui/toolbars/ProjectToolbar.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
 import com.kickstarter.R;
 import com.kickstarter.ui.activities.ProjectActivity;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ProjectToolbar extends KSToolbar {
@@ -21,6 +22,12 @@ public class ProjectToolbar extends KSToolbar {
 
   public ProjectToolbar(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+  }
+
+  @Override
+  protected void onFinishInflate() {
+    super.onFinishInflate();
+    ButterKnife.bind(this);
   }
 
   @OnClick(R.id.back_icon)

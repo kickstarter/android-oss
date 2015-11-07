d66465ec2c0abdadb7f6e655185d44030c3bda25
Christopher Wright <christopherwright@gmail.com>
2015-11-07 01:02:22 -0500

    Add web view toolbar class

---
+++ app/src/main/java/com/kickstarter/ui/toolbars/DisplayWebViewToolbar.java
@@ -0,0 +1,37 @@
+package com.kickstarter.ui.toolbars;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import com.kickstarter.R;
+import com.kickstarter.ui.activities.DisplayWebViewActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class DisplayWebViewToolbar extends KSToolbar {
+  public DisplayWebViewToolbar(@NonNull final Context context) {
+    super(context);
+  }
+
+  public DisplayWebViewToolbar(@NonNull final Context context, @Nullable final AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public DisplayWebViewToolbar(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override
+  protected void onFinishInflate() {
+    super.onFinishInflate();
+    ButterKnife.bind(this);
+  }
+
+  @OnClick(R.id.back_button)
+  protected void backButtonClick() {
+    ((DisplayWebViewActivity) getContext()).onBackPressed();
+  }
+}

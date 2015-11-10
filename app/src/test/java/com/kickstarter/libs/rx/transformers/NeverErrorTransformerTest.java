36bd999b830563a1c90614688b0823db50f69ef5
Christopher Wright <christopherwright@gmail.com>
2015-11-10 15:47:26 -0500

    Upgrade retrolambda

---
+++ app/src/test/java/com/kickstarter/libs/rx/transformers/NeverErrorTransformerTest.java
@@ -0,0 +1,69 @@
+package com.kickstarter.libs.rx.transformers;
+
+import com.kickstarter.BuildConfig;
+import com.kickstarter.KSRobolectricGradleTestRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.multidex.ShadowMultiDex;
+
+import rx.Observable;
+import rx.observers.TestSubscriber;
+import rx.subjects.PublishSubject;
+
+@RunWith(KSRobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, shadows=ShadowMultiDex.class, sdk = KSRobolectricGradleTestRunner.DEFAULT_SDK)
+public class NeverErrorTransformerTest {
+
+  @Test
+  public void testNeverError_emitsSameValuesAsSource() {
+
+    final Observable<Integer> source = Observable.just(1, 2, 3, 4);
+    final Observable<Integer> result = source.compose(Transformers.neverError());
+
+    final TestSubscriber<Integer> resultTest = TestSubscriber.create();
+    result.subscribe(resultTest);
+
+    resultTest.assertValues(1, 2, 3, 4);
+    resultTest.assertCompleted();
+  }
+
+  @Test
+  public void testNeverError_emitsSameValuesAndSkipsError() {
+
+    final Observable<Integer> errorsOnLast = Observable.just(1, 2, 3, 4)
+      .flatMap(i -> i < 4 ? Observable.just(i) : Observable.error(new RuntimeException()));
+    final Observable<Integer> result = errorsOnLast.compose(Transformers.neverError());
+
+    final TestSubscriber<Integer> resultTest = TestSubscriber.create();
+    result.subscribe(resultTest);
+
+    resultTest.assertValues(1, 2, 3);
+    resultTest.assertCompleted();
+    resultTest.assertNoErrors();
+  }
+
+  @Test
+  public void testNeverError_pipesErrorsToPublishSubject() {
+
+    final RuntimeException exception = new RuntimeException();
+    final Observable<Integer> errorsOnLast = Observable.just(1, 2, 3, 4)
+      .flatMap(i -> i < 4 ? Observable.just(i) : Observable.error(exception));
+    final PublishSubject<Throwable> error = PublishSubject.create();
+    final Observable<Integer> result = errorsOnLast.compose(Transformers.pipeErrorsTo(error));
+
+    final TestSubscriber<Throwable> errorTest = TestSubscriber.create();
+    error.subscribe(errorTest);
+    final TestSubscriber<Integer> resultTest = TestSubscriber.create();
+    result.subscribe(resultTest);
+
+    resultTest.assertValues(1, 2, 3);
+    resultTest.assertCompleted();
+    resultTest.assertNoErrors();
+
+    errorTest.assertValues(exception);
+    errorTest.assertNotCompleted();
+    errorTest.assertNoErrors();
+  }
+}
---
+++ app/src/test/java/com/kickstarter/libs/rx/transformers/TakePairWhenTransformerTest.java
@@ -0,0 +1,78 @@
+package com.kickstarter.libs.rx.transformers;
+
+import android.util.Pair;
+
+import com.kickstarter.BuildConfig;
+import com.kickstarter.KSRobolectricGradleTestRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.multidex.ShadowMultiDex;
+
+import rx.Observable;
+import rx.observers.TestSubscriber;
+import rx.subjects.PublishSubject;
+
+@RunWith(KSRobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, shadows=ShadowMultiDex.class, sdk = KSRobolectricGradleTestRunner.DEFAULT_SDK)
+public class TakePairWhenTransformerTest {
+
+  @Test
+  public void testPairTakeWhen_sourceEmitsFirst() {
+
+    final PublishSubject<Integer> source = PublishSubject.create();
+    final PublishSubject<String> sample = PublishSubject.create();
+    final Observable<Pair<Integer, String>> pair = source.compose(Transformers.takePairWhen(sample));
+
+    final TestSubscriber<Pair<Integer, String>> resultTest = TestSubscriber.create();
+    pair.subscribe(resultTest);
+
+    source.onNext(1);
+    resultTest.assertNoValues();
+
+    source.onNext(2);
+    resultTest.assertNoValues();
+
+    sample.onNext("a");
+    resultTest.assertValues(Pair.create(2, "a"));
+
+    sample.onNext("b");
+    resultTest.assertValues(Pair.create(2, "a"), Pair.create(2, "b"));
+
+    source.onNext(3);
+    resultTest.assertValues(Pair.create(2, "a"), Pair.create(2, "b"));
+
+    sample.onNext("c");
+    resultTest.assertValues(Pair.create(2, "a"), Pair.create(2, "b"), Pair.create(3, "c"));
+  }
+
+  @Test
+  public void testPairTakeWhen_sourceEmitsSecond() {
+
+    final PublishSubject<Integer> source = PublishSubject.create();
+    final PublishSubject<String> sample = PublishSubject.create();
+    final Observable<Pair<Integer, String>> pair = source.compose(Transformers.takePairWhen(sample));
+
+    final TestSubscriber<Pair<Integer, String>> resultTest = TestSubscriber.create();
+    pair.subscribe(resultTest);
+
+    sample.onNext("a");
+    resultTest.assertNoValues();
+
+    sample.onNext("b");
+    resultTest.assertNoValues();
+
+    source.onNext(1);
+    resultTest.assertNoValues();
+
+    sample.onNext("c");
+    resultTest.assertValues(Pair.create(1, "c"));
+
+    source.onNext(2);
+    resultTest.assertValues(Pair.create(1, "c"));
+
+    sample.onNext("d");
+    resultTest.assertValues(Pair.create(1, "c"), Pair.create(2, "d"));
+  }
+}

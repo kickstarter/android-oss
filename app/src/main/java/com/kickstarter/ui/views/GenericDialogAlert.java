b7f3dae22138ce3ecdb58014c04c7b9bb8ef0c6f
Gina Binetti <theginbin@gmail.com>
2015-11-15 19:24:36 -0500

    add rounded background drawable

--- app/src/main/java/com/kickstarter/ui/views/GenericDialogAlert.java
+++ app/src/main/java/com/kickstarter/ui/views/GenericDialogAlert.java
@@ -4,7 +4,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -31,7 +30,6 @@ public class GenericDialogAlert extends Dialog {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.generic_dialog_alert);
     ButterKnife.bind(this);
   }

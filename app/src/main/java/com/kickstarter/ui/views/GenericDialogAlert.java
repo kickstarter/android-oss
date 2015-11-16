a1c0f1e36efa25e867d92fc67f651de2cb7b1dfb
Gina Binetti <theginbin@gmail.com>
2015-11-15 19:32:42 -0500

    set dialog window background to transparent to show rounded corners

--- app/src/main/java/com/kickstarter/ui/views/GenericDialogAlert.java
+++ app/src/main/java/com/kickstarter/ui/views/GenericDialogAlert.java
@@ -2,6 +2,7 @@ package com.kickstarter.ui.views;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.widget.Button;
@@ -30,6 +31,7 @@ public class GenericDialogAlert extends Dialog {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
     setContentView(R.layout.generic_dialog_alert);
     ButterKnife.bind(this);
   }

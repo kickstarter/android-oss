3e2a93e6e0ddf4f0b0a9ee7de4e1f99c2b084d27
Christopher Wright <christopherwright@gmail.com>
2015-11-17 14:12:27 -0500

    Alphabetical order

--- app/src/main/java/com/kickstarter/presenters/inputs/ActivityFeedPresenterInputs.java
+++ app/src/main/java/com/kickstarter/presenters/inputs/ActivityFeedPresenterInputs.java
@@ -2,12 +2,12 @@ package com.kickstarter.presenters.inputs;
 
 public interface ActivityFeedPresenterInputs {
   /**
-   * Invoke when the feed should be refreshed.
-   */
-  void refresh();
-
-  /**
    * Invoke when pagination should happen.
    */
   void nextPage();
+
+  /**
+   * Invoke when the feed should be refreshed.
+   */
+  void refresh();
 }
--- app/src/main/java/com/kickstarter/presenters/outputs/ActivityFeedPresenterOutputs.java
+++ app/src/main/java/com/kickstarter/presenters/outputs/ActivityFeedPresenterOutputs.java
@@ -8,12 +8,12 @@ import rx.Observable;
 
 public interface ActivityFeedPresenterOutputs {
   /**
-   * Emits a boolean indicating whether activities are being fetched from the API.
-   */
-  Observable<Boolean> isFetchingActivities();
-
-  /**
    * Emits a list of activities representing the user's activity feed.
    */
   Observable<List<Activity>> activities();
+
+  /**
+   * Emits a boolean indicating whether activities are being fetched from the API.
+   */
+  Observable<Boolean> isFetchingActivities();
 }

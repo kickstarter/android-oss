d1b911811e12ae85e5479879415fd029ca7081fb
Gina Binetti <theginbin@gmail.com>
2015-11-24 17:16:04 -0500

    loading projects into recyclerview

--- app/src/main/java/com/kickstarter/ui/activities/ProfileActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.kickstarter.ui.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,8 +17,11 @@ import com.kickstarter.libs.transformations.CircleTransformation;
 import com.kickstarter.models.Project;
 import com.kickstarter.models.User;
 import com.kickstarter.presenters.ProfilePresenter;
+import com.kickstarter.ui.adapters.ProfileAdapter;
+import com.kickstarter.ui.viewholders.ProjectCardViewHolder;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -26,12 +31,17 @@ import butterknife.ButterKnife;
 import rx.android.schedulers.AndroidSchedulers;
 
 @RequiresPresenter(ProfilePresenter.class)
-public final class ProfileActivity extends BaseActivity<ProfilePresenter> {
+public final class ProfileActivity extends BaseActivity<ProfilePresenter> implements ProfileAdapter.Delegate {
+  private ProfileAdapter adapter;
+  private final List<Project> projects = new ArrayList<>();
+  //private Paginator paginator; // TODO pagination
+
   protected @Bind(R.id.avatar) ImageView avatarImageView;
   protected @Bind(R.id.user_name) TextView userNameTextView;
   protected @Bind(R.id.created_num) TextView createdNumTextView;
   protected @Bind(R.id.backed_num) TextView backedNumTextView;
   protected @Bind(R.id.followers_num) TextView followersNumTextView;
+  public @Bind(R.id.recycler_view) RecyclerView recyclerView;
 
   @Inject CurrentUser currentUser;
 
@@ -41,6 +51,10 @@ public final class ProfileActivity extends BaseActivity<ProfilePresenter> {
     setContentView(R.layout.profile_layout);
     ButterKnife.bind(this);
     ((KSApplication) getApplication()).component().inject(this);
+
+    adapter = new ProfileAdapter(projects, this);
+    recyclerView.setAdapter(adapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     presenter.outputs.user()
       .compose(bindToLifecycle())
@@ -60,8 +74,10 @@ public final class ProfileActivity extends BaseActivity<ProfilePresenter> {
     overridePendingTransition(R.anim.fade_in_slide_in_left, R.anim.slide_out_right);
   }
 
-  private void loadProjects(@NonNull final List<Project> projects) {
-
+  private void loadProjects(@NonNull final List<Project> newProjects) {
+    projects.clear();
+    projects.addAll(newProjects);
+    adapter.notifyDataSetChanged();
   }
 
   private void setViews(@NonNull final User user) {
@@ -81,5 +97,14 @@ public final class ProfileActivity extends BaseActivity<ProfilePresenter> {
     if (backedNum != null) {
       backedNumTextView.setText(backedNum.toString());
     }
+  }
+
+  /** ProjectCardViewHolder Delegate
+   * *
+   * @param viewHolder
+   * @param project
+   */
+  public void projectCardClick(@NonNull final ProjectCardViewHolder viewHolder, @NonNull final Project project) {
+    //presenter.inputs.projectClick(project);
   }
 }
---
+++ app/src/main/java/com/kickstarter/ui/adapters/ProfileAdapter.java
@@ -0,0 +1,32 @@
+package com.kickstarter.ui.adapters;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+import com.kickstarter.R;
+import com.kickstarter.models.Project;
+import com.kickstarter.ui.viewholders.KSViewHolder;
+import com.kickstarter.ui.viewholders.ProjectCardViewHolder;
+
+import java.util.List;
+
+public final class ProfileAdapter extends KSAdapter {
+  private final Delegate delegate;
+
+  public interface Delegate extends ProjectCardViewHolder.Delegate {}
+
+  public ProfileAdapter(@NonNull final List<Project> projects, @NonNull final Delegate delegate) {
+    data().add(projects);
+    this.delegate = delegate;
+  }
+
+  protected @LayoutRes
+  int layout(@NonNull final SectionRow sectionRow) {
+    return R.layout.project_card_view;
+  }
+
+  protected KSViewHolder viewHolder(@LayoutRes final int layout, @NonNull final View view) {
+    return new ProjectCardViewHolder(view, delegate);
+  }
+}

5404e4cf99961c92ea064d2bdb5affff6b133f65
Christopher Wright <christopherwright@gmail.com>
2015-12-02 11:10:49 -0500

    Revert "Move update server config script into standalone file"
    This reverts commit f8de2745a101b087d14826b9e7080a7bffa7e0c2.

---
+++ lib/milkrun/server_config.rb
@@ -0,0 +1,43 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+module Milkrun
+  class ServerConfig
+    def initialize
+    end
+
+    def update
+      response = Net::HTTP.get_response(url)
+
+      if !response.is_a?(Net::HTTPSuccess)
+        Milkrun.say "Response from API: #{response.body}"
+        raise "Couldn't download config from server!"
+      end
+
+      json = JSON.parse(response.body)
+      config = JSON.pretty_generate(json)
+
+      File.open(self.class.config_path, "w") do |file|
+        file.write(config)
+      end
+
+      Milkrun.say "Updated config from server"
+    end
+
+    def self.config_path
+      File.expand_path(File.join(File.dirname(__FILE__), "../../app/src/main/assets/json/server-config.json"))
+    end
+
+    private
+
+    def url
+      URI.parse("https://***REMOVED***/v1/app/android/config?client_id=***REMOVED***&all_locales=true")
+    end
+    
+    # For testing
+    def local_url
+      URI.parse("http://api.ksr.dev/v1/app/android/config?client_id=***REMOVED***&all_locales=true")
+    end
+  end
+end

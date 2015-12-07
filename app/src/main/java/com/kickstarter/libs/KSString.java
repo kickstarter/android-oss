c42be94ebbb56b222b2e619cd35f45f2a0471a22
Christopher Wright <christopherwright@gmail.com>
2015-12-07 18:45:58 -0500

    KSString.format wip

--- app/src/main/java/com/kickstarter/libs/KSString.java
+++ app/src/main/java/com/kickstarter/libs/KSString.java
@@ -1,11 +1,84 @@
 package com.kickstarter.libs;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class KSString {
   private KSString() {}
 
-  public static @NonNull String format(final @NonNull String string) {
-    return string;
+  public static @NonNull String format(final @NonNull String string, final @NonNull String key1, final @Nullable String value1) {
+    Map<String, String> substitutions = new HashMap<String, String>() {{
+      put(key1, value1);
+    }};
+    return replace(string, substitutions);
+  }
+
+  public static @NonNull String format(final @NonNull String string,
+    final @NonNull String key1, final @Nullable String value1,
+    final @NonNull String key2, final @Nullable String value2) {
+    final Map<String, String> substitutions = new HashMap<String, String>() {{
+      put(key1, value1);
+      put(key2, value2);
+    }};
+    return replace(string, substitutions);
+  }
+
+  public static @NonNull String format(final @NonNull String string,
+    final @NonNull String key1, final @Nullable String value1,
+    final @NonNull String key2, final @Nullable String value2,
+    final @NonNull String key3, final @Nullable String value3) {
+    final Map<String, String> substitutions = new HashMap<String, String>() {{
+      put(key1, value1);
+      put(key2, value2);
+      put(key3, value3);
+    }};
+    return replace(string, substitutions);
+  }
+
+  public static @NonNull String format(final @NonNull String string,
+    final @NonNull String key1, final @Nullable String value1,
+    final @NonNull String key2, final @Nullable String value2,
+    final @NonNull String key3, final @Nullable String value3,
+    final @NonNull String key4, final @Nullable String value4) {
+    final Map<String, String> substitutions = new HashMap<String, String>() {{
+      put(key1, value1);
+      put(key2, value2);
+      put(key3, value3);
+      put(key4, value4);
+    }};
+    return replace(string, substitutions);
+  }
+
+  private static @NonNull String replace(final @NonNull String string, final Map<String, String> substitutions) {
+    final StringBuilder builder = new StringBuilder();
+    for (final Map.Entry<String, String> entry : substitutions.entrySet()) {
+      if (builder.length() > 0) {
+        builder.append("|");
+      }
+      // TODO: replace with string format
+      builder
+        .append("(%\\{")
+        .append(entry.getKey())
+        .append("\\})");
+    }
+
+    final Pattern pattern = Pattern.compile(builder.toString());
+    final Matcher matcher = pattern.matcher(string);
+    final StringBuffer buffer = new StringBuffer();
+
+    while (matcher.find()) {
+      final String key = matcher.group().replaceAll("[^\\w]", ""); // TODO: Could compile this into a pattern
+      final String value = substitutions.get(key);
+      final String replacement = value != null ? value : "";
+      matcher.appendReplacement(buffer, replacement);
+    }
+    matcher.appendTail(buffer);
+
+    return buffer.toString();
   }
 }
--- app/src/test/java/com/kickstarter/libs/KSStringTest.java
+++ app/src/test/java/com/kickstarter/libs/KSStringTest.java
@@ -3,7 +3,45 @@ package com.kickstarter.libs;
 import junit.framework.TestCase;
 
 public class KSStringTest extends TestCase {
-  public void testFormat_oneArgument() {
-    assertEquals(1, 1);
+  public void testFormat_oneSubstitution() {
+    final String string = "by %{name}";
+    assertEquals("by christopher", KSString.format(string, "name", "christopher"));
+  }
+
+  public void testFormat_twoSubstitutions() {
+    final String string = "%{remaining} of %{total}";
+    assertEquals("1 of 5", KSString.format(string,
+      "remaining", "1",
+      "total", "5"
+    ));
+  }
+
+  public void testFormat_threeSubstitutions() {
+    final String string = "%{one}, %{two} and %{three}";
+    assertEquals("a, b and c", KSString.format(string,
+      "one", "a",
+      "two", "b",
+      "three", "c"
+    ));
+  }
+
+  public void testFormat_fourSubstitutions() {
+    final String string = "%{one}, %{two}, %{three} and %{four}";
+    assertEquals("a, b, c and d", KSString.format(string,
+      "one", "a",
+      "two", "b",
+      "three", "c",
+      "four", "d"
+    ));
+  }
+
+  public void testFormat_replaceWithNullValue() {
+    final String string = "search term: %{term}";
+    assertEquals("search term: ", KSString.format(string, "term", null));
+  }
+
+  public void testFormat_invalidKey() {
+    final String string = "by %{name}";
+    assertEquals("by %{name}", KSString.format(string, "invalid_key", "foo"));
   }
 }

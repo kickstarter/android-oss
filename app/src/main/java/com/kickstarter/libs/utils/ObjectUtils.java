8cb5ca0e354dd1c8be366413deb6fc871de17570
Christopher Wright <christopherwright@gmail.com>
2015-12-10 10:10:53 -0500

    Swap annotation order

--- app/src/main/java/com/kickstarter/libs/utils/ObjectUtils.java
+++ app/src/main/java/com/kickstarter/libs/utils/ObjectUtils.java
@@ -8,18 +8,18 @@ import rx.functions.Func1;
 public class ObjectUtils {
   private ObjectUtils(){}
 
-  public static boolean isNull(@Nullable final Object object) {
+  public static boolean isNull(final @Nullable Object object) {
     return object == null;
   }
 
-  public static boolean isNotNull(@Nullable final Object object) {
+  public static boolean isNotNull(final @Nullable Object object) {
     return object != null;
   }
 
   /**
    * Returns the first non-`null` value of its arguments.
    */
-  @NonNull public static <T> T coalesce(@Nullable final T value, @NonNull final T theDefault) {
+  @NonNull public static <T> T coalesce(final @Nullable T value, final @NonNull T theDefault) {
     if (value != null) {
       return value;
     }
@@ -29,14 +29,14 @@ public class ObjectUtils {
   /**
    * Returns a function `T -> T` that coalesces values with `theDefault`.
    */
-  @NonNull public static <T> Func1<T, T> coalesceWith(@NonNull final T theDefault) {
+  @NonNull public static <T> Func1<T, T> coalesceWith(final @NonNull T theDefault) {
     return (value) -> ObjectUtils.coalesce(value, theDefault);
   }
 
   /**
    * Converts an {@link Integer} to a {@link String}, or null of the integer is also null.
    */
-  public static @Nullable String toString(@Nullable final Integer n) {
+  public static @Nullable String toString(final @Nullable Integer n) {
     if (n != null) {
       return Integer.toString(n);
     }
@@ -47,7 +47,7 @@ public class ObjectUtils {
   /**
    * Converts a {@link Long} to a {@link String}, or null of the long is also null.
    */
-  public static @Nullable String toString(@Nullable final Long n) {
+  public static @Nullable String toString(final @Nullable Long n) {
     if (n != null) {
       return Long.toString(n);
     }
@@ -58,7 +58,7 @@ public class ObjectUtils {
   /**
    * Converts a {@link Float} to a {@link String}, or null of the float is also null.
    */
-  public static @Nullable String toString(@Nullable final Float n) {
+  public static @Nullable String toString(final @Nullable Float n) {
     if (n != null) {
       return Float.toString(n);
     }
@@ -69,7 +69,7 @@ public class ObjectUtils {
   /**
    * Converts a {@link Double} to a {@link String}, or null of the double is also null.
    */
-  public static @Nullable String toString(@Nullable final Double n) {
+  public static @Nullable String toString(final @Nullable Double n) {
     if (n != null) {
       return Double.toString(n);
     }

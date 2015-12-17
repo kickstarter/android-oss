26d6e6d601dca2d0ccedf80fee7362c969dca469
Christopher Wright <christopherwright@gmail.com>
2015-12-16 19:52:29 -0500

    Fix ping beta

--- app/src/main/java/com/kickstarter/services/WebService.java
+++ app/src/main/java/com/kickstarter/services/WebService.java
@@ -2,10 +2,11 @@ package com.kickstarter.services;
 
 import com.kickstarter.services.apiresponses.InternalBuildEnvelope;
 
+import retrofit.Response;
 import retrofit.http.GET;
 import rx.Observable;
 
 public interface WebService {
   @GET("/mobile/beta/ping")
-  Observable<InternalBuildEnvelope> pingBeta();
+  Observable<Response<InternalBuildEnvelope>> pingBeta();
 }

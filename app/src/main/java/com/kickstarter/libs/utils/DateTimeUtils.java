98035e604952693af4f8c9d7a25f64ca6aecc45c
Christopher Wright <christopherwright@gmail.com>
2015-12-18 01:07:37 -0500

    Date i18n wip

--- app/src/main/java/com/kickstarter/libs/utils/DateTimeUtils.java
+++ app/src/main/java/com/kickstarter/libs/utils/DateTimeUtils.java
@@ -1,6 +1,13 @@
 package com.kickstarter.libs.utils;
 
+import android.content.Context;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Pair;
+
+import com.kickstarter.R;
+import com.kickstarter.libs.KSString;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -8,20 +15,132 @@ import org.joda.time.Seconds;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-public class DateTimeUtils {
-  public final static int THIRTY_DAYS_IN_SECONDS = 60 * 60 * 24 * 30;
+import java.util.Locale;
+
+import auto.parcel.AutoParcel;
+
+public final class DateTimeUtils {
+  private DateTimeUtils() {}
+
+  /**
+   * e.g.: December 2015.
+   */
+  public static @NonNull String estimatedDeliveryOn(final @NonNull DateTime dateTime) {
+    return estimatedDeliveryOn(dateTime, Locale.getDefault());
+  }
+
+  /**
+   * e.g.: December 2015.
+   */
+  public static @NonNull String estimatedDeliveryOn(final @NonNull DateTime dateTime, final @NonNull Locale locale) {
+    return dateTime.toString(DateTimeFormat.forPattern("MMMM yyyy").withLocale(locale).withZoneUTC());
+  }
+
+  /**
+   * e.g.: Dec 17, 2015.
+   */
+  public static @NonNull String mediumDate(final @NonNull DateTime dateTime) {
+    return mediumDate(dateTime, Locale.getDefault());
+  }
+
+  /**
+   * e.g.: Dec 17, 2015.
+   */
+  public static @NonNull String mediumDate(final @NonNull DateTime dateTime, final @NonNull Locale locale) {
+    return dateTime.toString(DateTimeFormat.mediumDate().withLocale(locale).withZoneUTC());
+  }
+
+  public static @NonNull String relativeDate(final @NonNull Context context, final @NonNull KSString ksString,
+    final @NonNull DateTime dateTime) {
+    return relativeDate(context, ksString, dateTime, RelativeDateOptions.builder().build());
+  }
+
+  public static @NonNull String relativeDate(final @NonNull Context context, final @NonNull KSString ksString,
+    final @NonNull DateTime dateTime, final @NonNull RelativeDateOptions options) {
+    return relativeDate(context, ksString, dateTime, options, Locale.getDefault());
+  }
+
+  public static @NonNull String relativeDate(final @NonNull Context context, final @NonNull KSString ksString,
+    final @NonNull DateTime dateTime, final @NonNull RelativeDateOptions options, final @NonNull Locale locale) {
+
+    final DateTime relativeToDateTime = ObjectUtils.coalesce(options.relativeToDateTime(), DateTime.now());
+    final Seconds seconds = Seconds.secondsBetween(dateTime, relativeToDateTime);
+    final int secondsDifference = seconds.getSeconds();
+
+    if (secondsDifference >= 0.0 && secondsDifference <= 60.0) {
+      return context.getString(R.string.dates_just_now);
+    } else if (secondsDifference >= -60.0 && secondsDifference <= 0.0) {
+      return context.getString(R.string.dates_right_now);
+    }
+
+    final Pair<String, Integer> unitAndDifference = unitAndDifference(secondsDifference, options.threshold());
+    if (unitAndDifference == null) {
+      // Couldn't find a good match, just render the date.
+      return mediumDate(dateTime);
+    }
+
+    final String unit = unitAndDifference.first;
+    final int difference = unitAndDifference.second;
+    boolean willHappenIn = false;
+    boolean happenedAgo = false;
+
+    if (options.explain()) {
+      if (secondsDifference < 0) {
+        willHappenIn = true;
+      } else if (secondsDifference > 0) {
+        happenedAgo = true;
+      }
+    }
+
+    if (happenedAgo && unit.equals("days") && difference == 1) {
+      return context.getString(R.string.dates_yesterday);
+    }
+
+    final StringBuilder baseKeyPath = new StringBuilder();
+    if (willHappenIn) {
+      baseKeyPath.append(String.format("dates_time_in_%s", unit));
+    } else if (happenedAgo) {
+      baseKeyPath.append(String.format("dates_time_%s_ago", unit));
+    } else {
+      baseKeyPath.append(String.format("dates_time_%s", unit));
+    }
+
+    if (options.shorten()) {
+      baseKeyPath.append("_abbreviated");
+    }
+
+    return ksString.format(baseKeyPath.toString(), difference,
+      "time_count", String.valueOf(difference));
+  }
+
+  private static @Nullable Pair<String, Integer> unitAndDifference(final int initialSecondsDifference, final int threshold) {
+    final int secondsDifference = Math.abs(initialSecondsDifference);
+    final int daysDifference = (int) Math.floor(secondsDifference / 86400);
+
+    if (secondsDifference < 3600) { // 1 hour
+      final int minutesDifference = (int) Math.floor(secondsDifference / 60.0);
+      return new Pair<>("minutes", minutesDifference);
+    } else if (secondsDifference < 86400) { // 24 hours
+      final int hoursDifference = (int) Math.floor(secondsDifference / 60.0 / 60.0);
+      return new Pair<>("hours", hoursDifference);
+    } else if (secondsDifference < threshold) {
+      return new Pair<>("days", daysDifference);
+    }
+
+    return null;
+  }
 
   public static DateTimeFormatter defaultFormatter() {
     // Wrapper to make this easier to refactor later.
     return DateTimeFormat.forPattern("yyyy/MM/dd");
   }
 
-  // i.e. August 20, 2015 at 7:45 PM.
+  // e.g. August 20, 2015 at 7:45 PM.
   public static DateTimeFormatter writtenDeadline() {
     return DateTimeFormat.forPattern("MMMM dd, yyyy 'at' h:mm a.");
   }
 
-  // i.e. Wednesday, September 23, 2015
+  // e.g. Wednesday, September 23, 2015
   public static DateTimeFormatter pledgedAt() {
     return DateTimeFormat.forPattern("EEEE, MMMM dd, yyyy");
   }
@@ -104,4 +223,32 @@ public class DateTimeUtils {
       return dateTime.toString(defaultFormatter());
     }
   }
+
+  @AutoParcel
+  public abstract static class RelativeDateOptions implements Parcelable {
+    public abstract boolean explain();
+    public abstract boolean shorten();
+    public abstract @Nullable DateTime relativeToDateTime();
+    public abstract int threshold();
+
+    @AutoParcel.Builder
+    public abstract static class Builder {
+      public abstract Builder explain(boolean __);
+      public abstract Builder shorten(boolean __);
+      public abstract Builder relativeToDateTime(DateTime __);
+      public abstract Builder threshold(int __);
+      public abstract RelativeDateOptions build();
+    }
+
+    public static Builder builder() {
+      return new AutoParcel_DateTimeUtils_RelativeDateOptions.Builder()
+        .explain(true)
+        .shorten(true)
+        .threshold(THIRTY_DAYS_IN_SECONDS);
+    }
+
+    public abstract Builder toBuilder();
+  }
+
+  private final static int THIRTY_DAYS_IN_SECONDS = 60 * 60 * 24 * 30;
 }
---
+++ app/src/test/java/com/kickstarter/libs/utils/DateTimeUtilsTest.java
@@ -0,0 +1,65 @@
+package com.kickstarter.libs.utils;
+
+import android.content.Context;
+
+import com.kickstarter.KSRobolectricTestCase;
+import com.kickstarter.libs.KSString;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import java.util.Locale;
+
+public final class DateTimeUtilsTest extends KSRobolectricTestCase {
+  @Test
+  public void testEstimatedDeliveryOn() {
+    assertEquals("December 2015", DateTimeUtils.estimatedDeliveryOn(DateTime.parse("2015-12-17T18:35:05Z")));
+    assertEquals("décembre 2015", DateTimeUtils.estimatedDeliveryOn(DateTime.parse("2015-12-17T18:35:05Z"), Locale.FRENCH));
+  }
+
+  @Test
+  public void testMediumDate() {
+    assertEquals("Dec 17, 2015", DateTimeUtils.mediumDate(DateTime.parse("2015-12-17T18:35:05Z")));
+    assertEquals("17 déc. 2015", DateTimeUtils.mediumDate(DateTime.parse("2015-12-17T18:35:05Z"), Locale.FRENCH));
+  }
+
+  @Test
+  public void testRelativeDate() {
+    final Context context = context();
+    final KSString ksString = ksString();
+    final DateTime dateTime = DateTime.parse("2015-12-17T18:35:05Z");
+
+    assertEquals("just now", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T18:35:10Z")).build()));
+    assertEquals("right now", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T18:35:00Z")).build()));
+    assertEquals("2 mins ago", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T18:37:05Z")).build()));
+    assertEquals("in 2 mins", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T18:33:05Z")).build()));
+    assertEquals("1 hr ago", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T19:35:05Z")).build()));
+    assertEquals("in 1 hr", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T17:35:05Z")).build()));
+    assertEquals("4 hrs ago", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T22:35:05Z")).build()));
+    assertEquals("in 4 hrs", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-17T14:35:05Z")).build()));
+    assertEquals("23 hrs ago", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-18T17:35:05Z")).build()));
+    assertEquals("in 23 hrs", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-16T19:35:05Z")).build()));
+    assertEquals("yesterday", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-18T18:35:05Z")).build()));
+    assertEquals("in 1 day", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-16T18:35:05Z")).build()));
+    assertEquals("10 days ago", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-27T18:35:05Z")).build()));
+    assertEquals("in 10 days", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-12-07T18:35:05Z")).build()));
+    assertEquals("Dec 17, 2015", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2016-01-27T18:35:05Z")).build()));
+    assertEquals("Dec 17, 2015", DateTimeUtils.relativeDate(context, ksString, dateTime,
+      DateTimeUtils.RelativeDateOptions.builder().relativeToDateTime(DateTime.parse("2015-10-17T18:35:05Z")).build()));
+  }
+}

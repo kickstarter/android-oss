a8e01b1b10399384f980d93d4ef621e6794aadf8
Gina Binetti <theginbin@gmail.com>
2015-12-18 15:41:35 -0500

    remove help menu from activity. replace incorrect back button a11y desc for all toolbars

--- app/src/main/java/com/kickstarter/ui/toolbars/ActivityFeedToolbar.java
+++ app/src/main/java/com/kickstarter/ui/toolbars/ActivityFeedToolbar.java
@@ -1,25 +1,16 @@
 package com.kickstarter.ui.toolbars;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.PopupMenu;
 import android.util.AttributeSet;
 
 import com.kickstarter.R;
-import com.kickstarter.libs.BaseActivity;
-import com.kickstarter.libs.utils.ViewUtils;
-import com.kickstarter.ui.activities.HelpActivity;
-import com.kickstarter.ui.views.IconButton;
 
-import butterknife.Bind;
 import butterknife.BindString;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public final class ActivityFeedToolbar extends KSToolbar {
-  @Bind(R.id.more_button) IconButton moreButton;
   @BindString(R.string.___Not_implemented_yet) String notImplementedYetString;
 
   public ActivityFeedToolbar(@NonNull final Context context) {
@@ -38,29 +29,5 @@ public final class ActivityFeedToolbar extends KSToolbar {
   protected void onFinishInflate() {
     super.onFinishInflate();
     ButterKnife.bind(this);
-  }
-
-  @OnClick(R.id.more_button)
-  protected void moreButtonClick() {
-    final BaseActivity activity = (BaseActivity) getContext();
-    final PopupMenu popup = new PopupMenu(activity, moreButton);
-    popup.getMenuInflater().inflate(R.menu.activity_feed_menu, popup.getMenu());
-    popup.setOnMenuItemClickListener(item -> {
-      switch (item.getItemId()) {
-        case R.id.find_friends:
-          // TODO
-          ViewUtils.showToast(activity, notImplementedYetString);
-          break;
-        case R.id.help:
-          final Intent helpIntent = new Intent(activity, HelpActivity.class);
-          helpIntent.putExtra(activity.getString(R.string.intent_help_type), HelpActivity.HELP_TYPE_GENERAL);
-          activity.startActivity(helpIntent);
-          break;
-      }
-
-      return true;
-    });
-
-    popup.show();
   }
 }

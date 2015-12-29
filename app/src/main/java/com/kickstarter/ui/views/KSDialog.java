0a0f80a6b894c243a3613e3291c6f111260a90db
Lisa Luo <lluo92@gmail.com>
2015-12-29 15:49:29 -0500

    use AppCompatDialog for old sdk compatibility

--- app/src/main/java/com/kickstarter/ui/views/KSDialog.java
+++ app/src/main/java/com/kickstarter/ui/views/KSDialog.java
@@ -1,11 +1,11 @@
 package com.kickstarter.ui.views;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -18,7 +18,7 @@ import butterknife.BindString;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class KSDialog extends Dialog {
+public class KSDialog extends AppCompatDialog {
   @NonNull @Bind(R.id.title_text_view) TextView titleTextView;
   @NonNull @Bind(R.id.message_text_view) TextView messageTextView;
   @NonNull @Bind(R.id.ok_button) Button okButton;

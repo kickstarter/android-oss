ab051314c686f1651e6ba6ed28063c49cf95204e
Lisa Luo <lluo92@gmail.com>
2015-12-31 15:32:35 -0500

    temp update FriendFollowViewHolder i18n string

--- app/src/main/java/com/kickstarter/ui/viewholders/FriendFollowViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/FriendFollowViewHolder.java
@@ -18,18 +18,20 @@ import butterknife.BindString;
 import butterknife.ButterKnife;
 
 public final class FriendFollowViewHolder extends ActivityListViewHolder {
-  @Bind(R.id.avatar) ImageView avatarImageView;
-  @Bind(R.id.follow_button) View followButton;
-  @Bind(R.id.title) TextView titleTextView;
-  @BindString(R.string.___Not_implemented_yet) String notImplementedYetString;
+  protected @Bind(R.id.avatar) ImageView avatarImageView;
+  protected @Bind(R.id.follow_button) View followButton;
+  protected @Bind(R.id.title) TextView titleTextView;
+  
+  protected @BindString(R.string.activity_friend_follow_is_following_you) String isFollowingYouString;
+  protected @BindString(R.string.___Not_implemented_yet) String notImplementedYetString;
 
-  public FriendFollowViewHolder(@NonNull final View view) {
+  public FriendFollowViewHolder(final @NonNull View view) {
     super(view);
     ButterKnife.bind(this, view);
   }
 
   @Override
-  public void onBind(@NonNull final Object datum) {
+  public void onBind(final @NonNull Object datum) {
     super.onBind(datum);
 
     final Context context = view.getContext();
@@ -39,12 +41,16 @@ public final class FriendFollowViewHolder extends ActivityListViewHolder {
       .transform(new CircleTransformation())
       .into(avatarImageView);
 
-    titleTextView.setText(Html.fromHtml(context.getString(R.string.___username_is_following_you,
-      TextUtils.htmlEncode(activity.user().name()))));
+    // TODO: bold username
+    titleTextView.setText(
+      new StringBuilder(activity.user().name())
+        .append(" ")
+        .append(isFollowingYouString)
+    );
   }
 
   @Override
-  public void onClick(@NonNull final View view) {
+  public void onClick(final @NonNull View view) {
     Toast.makeText(view.getContext(), notImplementedYetString, Toast.LENGTH_LONG).show();
   }
 }

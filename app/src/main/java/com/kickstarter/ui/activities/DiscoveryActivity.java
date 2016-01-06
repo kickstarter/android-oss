c6fb98569ce465de3572367069579cf0e8825974
Christopher Wright <christopherwright@gmail.com>
2016-01-06 00:45:30 -0500

    Fix debug code

--- app/src/main/java/com/kickstarter/ui/activities/DiscoveryActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/DiscoveryActivity.java
@@ -111,8 +111,7 @@ public final class DiscoveryActivity extends BaseActivity<DiscoveryViewModel> im
 
   public void startProjectActivity(@NonNull final Project project) {
     final Intent intent = new Intent(this, ProjectActivity.class)
-      .putExtra(IntentKey.PROJECT_PARAM, project.param());
-      //.putExtra(IntentKey.PROJECT, project);
+      .putExtra(IntentKey.PROJECT, project);
     startActivity(intent);
     overridePendingTransition(R.anim.slide_in_right, R.anim.fade_out_slide_out_left);
   }

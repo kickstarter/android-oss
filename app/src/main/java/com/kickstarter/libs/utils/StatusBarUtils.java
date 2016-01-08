53f9a50ab62d322349234bd8dced354d7451f9c4
Christopher Wright <christopherwright@gmail.com>
2016-01-08 11:10:40 -0500

    Remove unused import

--- app/src/main/java/com/kickstarter/libs/utils/StatusBarUtils.java
+++ app/src/main/java/com/kickstarter/libs/utils/StatusBarUtils.java
@@ -1,6 +1,5 @@
 package com.kickstarter.libs.utils;
 
-import android.content.Context;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.view.View;

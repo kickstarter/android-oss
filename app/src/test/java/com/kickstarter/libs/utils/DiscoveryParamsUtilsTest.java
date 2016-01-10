daf0a3747aa7fed00fe88282a7cab90305e89c8b
Brandon Williams <mbw234@gmail.com>
2016-01-10 18:20:09 -0500

    Remove static import in this test.

--- app/src/test/java/com/kickstarter/libs/utils/DiscoveryParamsUtilsTest.java
+++ app/src/test/java/com/kickstarter/libs/utils/DiscoveryParamsUtilsTest.java
@@ -7,48 +7,49 @@ import com.kickstarter.services.DiscoveryParams;
 
 import junit.framework.TestCase;
 
-import static com.kickstarter.libs.utils.DiscoveryParamsUtils.refTag;
-
 public class DiscoveryParamsUtilsTest extends TestCase {
 
   public void testRefTag() {
     assertEquals(
-      refTag(DiscoveryParams.builder().category(CategoryFactory.artCategory()).build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder().category(CategoryFactory.artCategory()).build()),
       RefTag.category()
     );
 
     assertEquals(
-      refTag(DiscoveryParams.builder().category(CategoryFactory.artCategory()).sort(DiscoveryParams.Sort.POPULAR).build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder()
+        .category(CategoryFactory.artCategory())
+        .sort(DiscoveryParams.Sort.POPULAR)
+        .build()),
       RefTag.category(DiscoveryParams.Sort.POPULAR)
     );
 
     assertEquals(
-      refTag(DiscoveryParams.builder().location(LocationFactory.germany()).build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder().location(LocationFactory.germany()).build()),
       RefTag.city()
     );
 
     assertEquals(
-      refTag(DiscoveryParams.builder().staffPicks(true).build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder().staffPicks(true).build()),
       RefTag.recommended()
     );
 
     assertEquals(
-      refTag(DiscoveryParams.builder().staffPicks(true).sort(DiscoveryParams.Sort.POPULAR).build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder().staffPicks(true).sort(DiscoveryParams.Sort.POPULAR).build()),
       RefTag.recommended(DiscoveryParams.Sort.POPULAR)
     );
 
     assertEquals(
-      refTag(DiscoveryParams.builder().social(1).build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder().social(1).build()),
       RefTag.social()
     );
 
     assertEquals(
-      refTag(DiscoveryParams.builder().term("art").build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder().term("art").build()),
       RefTag.search()
     );
 
     assertEquals(
-      refTag(DiscoveryParams.builder().build()),
+      DiscoveryParamsUtils.refTag(DiscoveryParams.builder().build()),
       RefTag.discovery()
     );
   }

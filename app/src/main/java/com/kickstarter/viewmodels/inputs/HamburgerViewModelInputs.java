a12c4e3ede96031c7684d8b2b6997500db3697a7
Christopher Wright <christopherwright@gmail.com>
2016-01-14 14:47:26 -0500

    Remove experimental files

--- app/src/main/java/com/kickstarter/viewmodels/inputs/HamburgerViewModelInputs.java
+++
@@ -1,9 +0,0 @@
-package com.kickstarter.viewmodels.inputs;
-
-import android.support.annotation.NonNull;
-
-import com.kickstarter.models.HamburgerNavigationItem;
-
-public interface HamburgerViewModelInputs {
-  void filterClicked(final @NonNull HamburgerNavigationItem item);
-}

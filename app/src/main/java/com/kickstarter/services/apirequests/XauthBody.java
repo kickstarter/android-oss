394aa51618b519ed2315abeb99f447c0797971f5
Lisa Luo <lluo92@gmail.com>
2016-01-14 12:09:32 -0500

    fix login bug
    add XauthBody to /xauth login endpoints to send properly encoded passwords containing symbols

---
+++ app/src/main/java/com/kickstarter/services/apirequests/XauthBody.java
@@ -0,0 +1,30 @@
+package com.kickstarter.services.apirequests;
+
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+
+import com.kickstarter.libs.qualifiers.AutoGson;
+
+import auto.parcel.AutoParcel;
+
+@AutoGson
+@AutoParcel
+public abstract class XauthBody implements Parcelable {
+  public abstract String email();
+  public abstract String password();
+  public abstract @Nullable String code();
+
+  @AutoParcel.Builder
+  public abstract static class Builder {
+    public abstract Builder email(String __);
+    public abstract Builder password(String __);
+    public abstract Builder code(String __);
+    public abstract XauthBody build();
+  }
+
+  public static Builder builder() {
+    return new AutoParcel_XauthBody.Builder();
+  }
+
+  public abstract Builder toBuilder();
+}

6888c15a22824054a7636e3442f433364c3afa0f
Gina Binetti <theginbin@gmail.com>
2016-01-15 18:45:48 -0500

    alphabetize them bodies

--- app/src/main/java/com/kickstarter/services/apirequests/SignupBody.java
+++ app/src/main/java/com/kickstarter/services/apirequests/SignupBody.java
@@ -9,19 +9,19 @@ import auto.parcel.AutoParcel;
 public abstract class SignupBody {
   public abstract String name();
   public abstract String email();
+  public abstract boolean newsletterOptIn();
   public abstract String password();
   public abstract String passwordConfirmation();
   public abstract boolean sendNewsletters();
-  public abstract boolean newsletterOptIn();
 
   @AutoParcel.Builder
   public abstract static class Builder {
     public abstract Builder name(String __);
     public abstract Builder email(String __);
+    public abstract Builder newsletterOptIn(boolean __);
     public abstract Builder password(String __);
     public abstract Builder passwordConfirmation(String __);
     public abstract Builder sendNewsletters(boolean __);
-    public abstract Builder newsletterOptIn(boolean __);
     public abstract SignupBody build();
   }
 

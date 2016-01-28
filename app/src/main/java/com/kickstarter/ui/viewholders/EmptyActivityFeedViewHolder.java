317b276ae23a03aa96d0ebba73d0e15c089a859f
Brandon Williams <mbw234@gmail.com>
2016-01-28 15:43:36 -0500

    Improve the empty state outputs to be more inline with unidirectional data flow.

--- app/src/main/java/com/kickstarter/ui/viewholders/EmptyActivityFeedViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/EmptyActivityFeedViewHolder.java
@@ -6,14 +6,14 @@ import android.view.View;
 import android.widget.Button;
 
 import com.kickstarter.R;
-import com.kickstarter.models.User;
+import com.kickstarter.libs.utils.BooleanUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public final class EmptyActivityFeedViewHolder extends KSViewHolder {
-  User user;
+  boolean isLoggedIn;
   protected @Bind(R.id.discover_projects_button) Button discoverProjectsButton;
   protected @Bind(R.id.login_button) Button loginButton;
 
@@ -32,12 +32,12 @@ public final class EmptyActivityFeedViewHolder extends KSViewHolder {
 
   @Override
   public void bindData(final @Nullable Object data) throws Exception {
-    user = (User) data;
+    this.isLoggedIn = BooleanUtils.isTrue((Boolean) data);
   }
 
   @Override
   public void onBind() {
-    if (user == null) {
+    if (isLoggedIn) {
       discoverProjectsButton.setVisibility(View.GONE);
       loginButton.setVisibility(View.VISIBLE);
     } else  {

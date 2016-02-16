1f22396fd8d423991aa75af79c621d0ba389b352
Christopher Wright <christopherwright@gmail.com>
2016-02-16 18:04:07 -0500

    Add apache header for CropSquareTransformation

--- app/src/main/java/com/kickstarter/libs/transformations/CropSquareTransformation.java
+++ app/src/main/java/com/kickstarter/libs/transformations/CropSquareTransformation.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2013 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ***
+ *
+ * Original: https://github.com/square/picasso/blob/000a72859ca82f28975d0ef5099b01ec110e73ea/website/index.html#L78-L91
+ * Modifications: Some modifiers and annotations have been added by Kickstarter.
+ */
 package com.kickstarter.libs.transformations;
 
 import android.graphics.Bitmap;
@@ -5,9 +25,6 @@ import android.support.annotation.NonNull;
 
 import com.squareup.picasso.Transformation;
 
-/**
- * From https://github.com/square/picasso/blob/000a72859ca82f28975d0ef5099b01ec110e73ea/website/index.html#L78-L91
- */
 public class CropSquareTransformation implements Transformation {
   @Override public @NonNull Bitmap transform(final @NonNull Bitmap source) {
     final int size = Math.min(source.getWidth(), source.getHeight());

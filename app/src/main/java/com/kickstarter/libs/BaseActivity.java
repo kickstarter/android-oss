4a12a15354246073bb3c4df4b7e563202e091e1e
Christopher Wright <christopherwright@gmail.com>
2016-02-17 23:02:26 -0500

    Revert "Remove changed to visibility of viewmodel"
    This reverts commit d5e540f6a4ce293249c7f5604ffb1dbc2f714b4b.

--- app/src/main/java/com/kickstarter/libs/BaseActivity.java
+++ app/src/main/java/com/kickstarter/libs/BaseActivity.java
@@ -31,9 +31,9 @@ public class BaseActivity<ViewModelType extends ViewModel> extends AppCompatActi
 
   private final PublishSubject<Void> back = PublishSubject.create();
   private final BehaviorSubject<ActivityEvent> lifecycle = BehaviorSubject.create();
-  protected ViewModelType viewModel;
   private static final String VIEW_MODEL_KEY = "viewModel";
   private final List<Subscription> subscriptions = new ArrayList<>();
+  public ViewModelType viewModel;
 
   /**
    * Get viewModel.

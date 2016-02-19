a885eef6e00923a930cf951e5440adcb36f8fc33
Christopher Wright <christopherwright@gmail.com>
2016-02-19 16:43:12 -0500

    Test error on api exception

--- app/src/test/java/com/kickstarter/viewmodels/ResetPasswordViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/ResetPasswordViewModelTest.java
@@ -1,12 +1,20 @@
 package com.kickstarter.viewmodels;
 
+import android.support.annotation.NonNull;
+
 import com.kickstarter.KSRobolectricTestCase;
+import com.kickstarter.factories.ApiExceptionFactory;
+import com.kickstarter.libs.Environment;
+import com.kickstarter.models.User;
+import com.kickstarter.services.ApiClientType;
+import com.kickstarter.services.MockApiClient;
 
 import org.junit.Test;
 
+import rx.Observable;
 import rx.observers.TestSubscriber;
 
-public class ResetPasswordViewModelTest extends KSRobolectricTestCase {
+public final class ResetPasswordViewModelTest extends KSRobolectricTestCase {
   @Test
   public void testResetPasswordViewModel_formValidation() {
     final ResetPasswordViewModel vm = new ResetPasswordViewModel(environment());
@@ -38,5 +46,27 @@ public class ResetPasswordViewModelTest extends KSRobolectricTestCase {
 
     vm.inputs.resetPasswordClick();
     test.assertValueCount(1);
+  }
+
+  @Test
+  public void testResetPasswordViewModel_resetFailure() {
+    final ApiClientType apiClient = new MockApiClient() {
+      @Override
+      public @NonNull Observable<User> resetPassword(final @NonNull String email) {
+        return Observable.error(ApiExceptionFactory.badRequestException());
+      }
+    };
+    final Environment environment = environment().toBuilder().apiClient(apiClient).build();
+
+    final ResetPasswordViewModel vm = new ResetPasswordViewModel(environment);
+    final TestSubscriber<String> test = new TestSubscriber<>();
+
+    vm.errors.resetError().subscribe(test);
+    test.assertNoValues();
+
+    vm.inputs.email("hello@kickstarter.com");
+    vm.inputs.resetPasswordClick();
+
+    test.assertValue("bad request");
   }
 }

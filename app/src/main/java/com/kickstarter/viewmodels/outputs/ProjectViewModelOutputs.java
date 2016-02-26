cb6a2cbc2e00b5e7ef3412ce3c54d2e3ac146cad
Brandon Williams <mbw234@gmail.com>
2016-02-26 16:36:15 -0500

    Refactor login success out and make better test.

--- app/src/main/java/com/kickstarter/viewmodels/outputs/ProjectViewModelOutputs.java
+++ app/src/main/java/com/kickstarter/viewmodels/outputs/ProjectViewModelOutputs.java
@@ -9,7 +9,8 @@ import rx.Observable;
 
 public interface ProjectViewModelOutputs {
   /**
-   * Emits a project and config when a new value is available.
+   * Emits a project and country when a new value is available. If the view model is created with a full project
+   * model, this observable will emit that project immediately, and then again when it has updated from the api.
    */
   Observable<Pair<Project, String>> projectAndUserCountry();
 

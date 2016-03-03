3c73e7dfe0259412cb5a02cfb50576b5fa093edb
Christopher Wright <christopherwright@gmail.com>
2016-03-03 15:02:08 -0500

    Add docs

--- app/src/main/java/com/kickstarter/libs/preferences/IntPreferenceType.java
+++ app/src/main/java/com/kickstarter/libs/preferences/IntPreferenceType.java
@@ -1,8 +1,23 @@
 package com.kickstarter.libs.preferences;
 
 public interface IntPreferenceType {
+  /**
+   * Get the current value of the preference.
+   */
   int get();
+
+  /**
+   * Returns whether a value has been explicitly set for the preference.
+   */
   boolean isSet();
+
+  /**
+   * Set the preference to a value.
+   */
   void set(int value);
+
+  /**
+   * Delete the currently stored preference.
+   */
   void delete();
 }
--- app/src/main/java/com/kickstarter/libs/preferences/StringPreferenceType.java
+++ app/src/main/java/com/kickstarter/libs/preferences/StringPreferenceType.java
@@ -1,8 +1,23 @@
 package com.kickstarter.libs.preferences;
 
 public interface StringPreferenceType {
+  /**
+   * Get the current value of the preference.
+   */
   String get();
+
+  /**
+   * Returns whether a value has been explicitly set for the preference.
+   */
   boolean isSet();
+
+  /**
+   * Set the preference to a value.
+   */
   void set(String value);
+
+  /**
+   * Delete the currently stored preference.
+   */
   void delete();
 }

5eb26ca217ee925760a0ab7d2d8fd7b50edda35c
Christopher Wright <christopherwright@gmail.com>
2016-03-04 11:07:41 -0500

    Add MIT license for custom robolectric test runner

--- app/src/test/java/com/kickstarter/KSRobolectricGradleTestRunner.java
+++ app/src/test/java/com/kickstarter/KSRobolectricGradleTestRunner.java
@@ -1,3 +1,32 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2010 Xtreme Labs and Pivotal Labs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ***
+ *
+ * Original: https://github.com/robolectric/robolectric/blob/b4d324353c92496e8eb65792b17da695ff78b22f/robolectric/src/main/java/org/robolectric/RobolectricGradleTestRunner.java
+ * Added suggested fix by Gabriel Moreira: https://github.com/robolectric/robolectric/issues/1430#issuecomment-102546421
+ */
+
 package com.kickstarter;
 
 import org.junit.runners.model.InitializationError;
@@ -8,10 +37,6 @@ import org.robolectric.res.FileFsFile;
 import org.robolectric.util.Logger;
 import org.robolectric.util.ReflectionHelpers;
 
-/* Custom runner because of https://github.com/robolectric/robolectric/issues/1430
- * Mostly based on https://github.com/robolectric/robolectric/blob/b4d324353c92496e8eb65792b17da695ff78b22f/robolectric/src/main/java/org/robolectric/RobolectricGradleTestRunner.java
- * Modifications from https://github.com/robolectric/robolectric/issues/1430#issuecomment-102546421
- */
 public class KSRobolectricGradleTestRunner extends RobolectricTestRunner {
   public static final int DEFAULT_SDK = 21;
   private static final String BUILD_OUTPUT = "build/intermediates";

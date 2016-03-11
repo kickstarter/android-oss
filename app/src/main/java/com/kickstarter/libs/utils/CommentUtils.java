cb91f8a23cb002d029e4f10a23fda9b6875ba780
Lisa Luo <lluo92@gmail.com>
2016-03-11 18:14:46 -0500

    add CommentFeedData object

--- app/src/main/java/com/kickstarter/libs/utils/CommentUtils.java
+++ app/src/main/java/com/kickstarter/libs/utils/CommentUtils.java
@@ -4,11 +4,29 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.kickstarter.models.Comment;
+import com.kickstarter.models.Project;
 import com.kickstarter.models.User;
+import com.kickstarter.ui.adapters.data.CommentFeedData;
+
+import java.util.List;
 
 public final class CommentUtils {
   private CommentUtils() {
     throw new AssertionError();
+  }
+
+  /**
+   * Converts all the disparate data representing the comment feed into a `CommentFeedData` object that can be used to
+   * populate a view.
+   */
+  public static @NonNull CommentFeedData deriveCommentFeedData(final @NonNull Project project,
+    final @Nullable List<Comment> comments, final @Nullable User user) {
+
+    return CommentFeedData.builder()
+      .project(project)
+      .comments(comments)
+      .user(user)
+      .build();
   }
 
   public static boolean isUserAuthor(final @NonNull Comment comment, final @Nullable User user) {
---
+++ app/src/main/java/com/kickstarter/ui/adapters/data/CommentFeedData.java
@@ -0,0 +1,34 @@
+package com.kickstarter.ui.adapters.data;
+
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.kickstarter.models.Comment;
+import com.kickstarter.models.Project;
+import com.kickstarter.models.User;
+
+import java.util.List;
+
+import auto.parcel.AutoParcel;
+
+@AutoParcel
+public abstract class CommentFeedData implements Parcelable {
+  public abstract @NonNull Project project();
+  public abstract @Nullable List<Comment> comments();
+  public abstract @Nullable User user();
+
+  @AutoParcel.Builder
+  public abstract static class Builder {
+    public abstract Builder project(Project __);
+    public abstract Builder comments(List<Comment> __);
+    public abstract Builder user(User __);
+    public abstract CommentFeedData build();
+  }
+
+  public static Builder builder() {
+    return new AutoParcel_CommentFeedData.Builder();
+  }
+
+  public abstract Builder toBuilder();
+}

e06ae79451c8bdaaae2a90bb3161cd86d960d9bf
Christopher Wright <christopherwright@gmail.com>
2016-03-18 18:07:56 -0400

    Add quality check for milkrun

---
+++ lib/milkrun/checkstyle.rb
@@ -0,0 +1,15 @@
+require_relative 'report'
+
+module Milkrun
+  class Checkstyle < Report
+    def report_path
+      File.join(Milkrun.app_dir, "build", "reports", "checkstyle", "checkstyle.html")
+    end
+
+    private
+
+    def command
+      "gradlew checkstyle"
+    end
+  end
+end

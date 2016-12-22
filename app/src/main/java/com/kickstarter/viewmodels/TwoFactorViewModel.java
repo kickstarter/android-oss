64b61b48a5b489205b2beea59059c10e9659b464
Shintaro Katafuchi <hotchemi@users.noreply.github.com>
2016-12-23 02:40:56 +0900

    Fix to use doAfterTerminate instead of finallyDo. (#12)
    * Fix to use doAfterTerminate instead of finallyDo.
    
    * Fix other points as well.

--- app/src/main/java/com/kickstarter/viewmodels/TwoFactorViewModel.java
+++ app/src/main/java/com/kickstarter/viewmodels/TwoFactorViewModel.java
@@ -170,7 +170,7 @@ public final class TwoFactorViewModel extends ActivityViewModel<TwoFactorActivit
       .compose(Transformers.pipeApiErrorsTo(tfaError))
       .compose(Transformers.neverError())
       .doOnSubscribe(() -> formSubmitting.onNext(true))
-      .finallyDo(() -> formSubmitting.onNext(false));
+      .doAfterTerminate(() -> formSubmitting.onNext(false));
   }
 
   public Observable<AccessTokenEnvelope> loginWithFacebook(final @NonNull String code, final @NonNull String fbAccessToken) {
@@ -178,7 +178,7 @@ public final class TwoFactorViewModel extends ActivityViewModel<TwoFactorActivit
       .compose(Transformers.pipeApiErrorsTo(tfaError))
       .compose(Transformers.neverError())
       .doOnSubscribe(() -> formSubmitting.onNext(true))
-      .finallyDo(() -> formSubmitting.onNext(false));
+      .doAfterTerminate(() -> formSubmitting.onNext(false));
   }
 
   private Observable<AccessTokenEnvelope> resendCode(final @NonNull String email, final @NonNull String password) {

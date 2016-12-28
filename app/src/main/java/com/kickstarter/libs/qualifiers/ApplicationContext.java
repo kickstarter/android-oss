6c0e6c43a4a211d59e80954038f799f6ad0036be
Plastix <aidanpieper@gmail.com>
2016-12-28 08:23:53 -0500

    Remove @Retention from custom Dagger qualifiers @Qualifier itself is annotated with @Retention(RUNTIME).

--- app/src/main/java/com/kickstarter/libs/qualifiers/ApplicationContext.java
+++ app/src/main/java/com/kickstarter/libs/qualifiers/ApplicationContext.java
@@ -1,12 +1,8 @@
 package com.kickstarter.libs.qualifiers;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import javax.inject.Qualifier;
 
 @Qualifier
-@Retention(RetentionPolicy.RUNTIME)
 public @interface ApplicationContext {
 }
 
--- app/src/main/java/com/kickstarter/libs/qualifiers/ConfigPreference.java
+++ app/src/main/java/com/kickstarter/libs/qualifiers/ConfigPreference.java
@@ -1,11 +1,7 @@
 package com.kickstarter.libs.qualifiers;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import javax.inject.Qualifier;
 
 @Qualifier
-@Retention(RetentionPolicy.RUNTIME)
 public @interface ConfigPreference {
 }

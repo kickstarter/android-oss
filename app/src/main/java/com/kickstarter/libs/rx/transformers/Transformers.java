5cd085a29d75e79c225cb26105f86730a61f2c48
Lisa Luo <itslisaluo@gmail.com>
2017-01-18 17:49:25 -0500

    Make documentation clearer

--- app/src/main/java/com/kickstarter/libs/rx/transformers/Transformers.java
+++ app/src/main/java/com/kickstarter/libs/rx/transformers/Transformers.java
@@ -177,8 +177,9 @@ public final class Transformers {
    * If called on the main thread, schedule the work immediately. Otherwise delay execution of the work by adding it
    * to a message queue, where it will be executed on the main thread.
    *
-   * This is useful for `RecyclerView`s where new data can appear immediately rather than waiting for a frame
-   * to appear when observing directly on `AndroidSchedulers.mainThread()`.
+   * This is particularly useful for RecyclerViews; if subscriptions in these views are delayed for a frame, then
+   * the view temporarily shows recycled content and frame rate stutters. To address that, we can use `observeForUI()`
+   * to execute the work immediately rather than wait for a frame.
    */
   public static @NonNull <T> ObserveForUITransformer<T> observeForUI() {
     return new ObserveForUITransformer<>();

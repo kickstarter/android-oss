6471e19bc4003ced3a103023f743b5e3da1c6d85
Lisa Luo <itslisaluo@gmail.com>
2017-02-06 17:55:38 -0500

    modifiers and annotations

--- app/src/main/java/com/kickstarter/libs/utils/AnimationUtils.java
+++ app/src/main/java/com/kickstarter/libs/utils/AnimationUtils.java
@@ -1,19 +1,20 @@
 package com.kickstarter.libs.utils;
 
+import android.support.annotation.NonNull;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
-public class AnimationUtils {
+public final class AnimationUtils {
   private AnimationUtils() {}
 
-  public static Animation loadingIndicatorOnPageFinished() {
+  public static @NonNull Animation loadingIndicatorOnPageFinished() {
     final AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
     animation.setDuration(300L);
     animation.setFillAfter(true);
     return animation;
   }
 
-  public static Animation loadingIndicatorOnPageStarted() {
+  public static @NonNull Animation loadingIndicatorOnPageStarted() {
     final AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
     animation.setDuration(300L);
     animation.setFillAfter(true);

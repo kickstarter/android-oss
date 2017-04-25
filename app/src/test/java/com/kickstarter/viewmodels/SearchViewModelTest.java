f6736b8e8d958ae8acc0926a2278e3fa4b1ebde9
coryroy <corymroy@gmail.com>
2017-04-25 16:34:06 -0700

    Fix broken unit tests.

--- app/src/test/java/com/kickstarter/viewmodels/SearchViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/SearchViewModelTest.java
@@ -2,6 +2,7 @@ package com.kickstarter.viewmodels;
 
 import com.kickstarter.KSRobolectricTestCase;
 import com.kickstarter.libs.Environment;
+import com.kickstarter.libs.KoalaEvent;
 
 import org.junit.Test;
 
@@ -44,37 +45,37 @@ public class SearchViewModelTest extends KSRobolectricTestCase {
     // Popular projects emit immediately.
     popularProjectsPresent.assertValues(true);
     searchProjectsPresent.assertNoValues();
-    koalaTest.assertValues("Discover Search");
+    koalaTest.assertValues(KoalaEvent.SEARCH_VIEWED);
 
     // Searching shouldn't emit values immediately
     viewModel.inputs.search("hello");
     searchProjectsPresent.assertNoValues();
-    koalaTest.assertValues("Discover Search");
+    koalaTest.assertValues(KoalaEvent.SEARCH_VIEWED);
 
     // Waiting a small amount time shouldn't emit values
     scheduler.advanceTimeBy(200, TimeUnit.MILLISECONDS);
     searchProjectsPresent.assertNoValues();
-    koalaTest.assertValues("Discover Search");
+    koalaTest.assertValues(KoalaEvent.SEARCH_VIEWED);
 
     // Waiting the rest of the time makes the search happen
     scheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS);
     searchProjectsPresent.assertValues(false, true);
-    koalaTest.assertValues("Discover Search", "Discover Search Results");
+    koalaTest.assertValues(KoalaEvent.SEARCH_VIEWED, KoalaEvent.SEARCH_RESULTS_LOADED);
 
     // Typing more search terms doesn't emit more values
     viewModel.inputs.search("hello world!");
     searchProjectsPresent.assertValues(false, true);
-    koalaTest.assertValues("Discover Search", "Discover Search Results");
+    koalaTest.assertValues(KoalaEvent.SEARCH_VIEWED, KoalaEvent.SEARCH_RESULTS_LOADED);
 
     // Waiting enough time emits search results
     scheduler.advanceTimeBy(300, TimeUnit.MILLISECONDS);
     searchProjectsPresent.assertValues(false, true, false, true);
-    koalaTest.assertValues("Discover Search", "Discover Search Results", "Discover Search Results");
+    koalaTest.assertValues(KoalaEvent.SEARCH_VIEWED, KoalaEvent.SEARCH_RESULTS_LOADED, KoalaEvent.SEARCH_RESULTS_LOADED);
 
     // Clearing search terms brings back popular projects.
     viewModel.inputs.search("");
     searchProjectsPresent.assertValues(false, true, false, true, false);
     popularProjectsPresent.assertValues(true, false, true);
-    koalaTest.assertValues("Discover Search", "Discover Search Results", "Discover Search Results");
+    koalaTest.assertValues(KoalaEvent.SEARCH_VIEWED, KoalaEvent.SEARCH_RESULTS_LOADED, KoalaEvent.SEARCH_RESULTS_LOADED, KoalaEvent.SEARCH_CLEARED);
   }
 }

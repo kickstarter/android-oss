4f5f883021ffcca69a2398ecafcbd3e9f38af4f6
Lisa Luo <itslisaluo@gmail.com>
2017-06-22 15:16:00 -0400

    Modernize PlaygroundViewModel, add test file

---
+++ app/src/test/java/com/kickstarter/viewmodels/PlaygroundViewModelTest.java
@@ -0,0 +1,15 @@
+package com.kickstarter.viewmodels;
+
+import android.support.annotation.NonNull;
+
+import com.kickstarter.KSRobolectricTestCase;
+import com.kickstarter.libs.Environment;
+import com.kickstarter.ui.viewmodels.PlaygroundViewModel;
+
+public final class PlaygroundViewModelTest extends KSRobolectricTestCase {
+  private PlaygroundViewModel.ViewModel vm;
+
+  protected void setUpEnvironment(final @NonNull Environment environment) {
+    this.vm = new PlaygroundViewModel.ViewModel(environment);
+  }
+}

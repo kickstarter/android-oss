a1780eb980df934a448c7f039f4fa938d784dcf5
Lisa Luo <itslisaluo@gmail.com>
2017-08-03 15:36:36 -0400

    Swap black for soft black

--- app/src/main/java/com/kickstarter/ui/viewholders/discoverydrawer/ChildFilterViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/discoverydrawer/ChildFilterViewHolder.java
@@ -24,7 +24,7 @@ import static com.kickstarter.libs.utils.ObjectUtils.requireNonNull;
 public final class ChildFilterViewHolder extends KSViewHolder {
   protected @Bind(R.id.filter_view) LinearLayout filterView;
   protected @Bind(R.id.filter_text_view) TextView filterTextView;
-  protected @BindColor(R.color.black) int blackColor;
+  protected @BindColor(R.color.ksr_soft_black) int ksrSoftBlackColor;
   protected @BindColor(R.color.dark_gray) int darkGrayColor;
   protected @BindColor(R.color.discovery_drawer_item_selected) int filterSelectedColor;
   protected @BindColor(R.color.transparent) int filterUnselectedColor;
@@ -47,34 +47,34 @@ public final class ChildFilterViewHolder extends KSViewHolder {
 
   @Override
   public void bindData(final @Nullable Object data) throws Exception {
-    item = requireNonNull((NavigationDrawerData.Section.Row) data, NavigationDrawerData.Section.Row.class);
+    this.item = requireNonNull((NavigationDrawerData.Section.Row) data, NavigationDrawerData.Section.Row.class);
   }
 
   @Override
   public void onBind() {
     final Context context = context();
 
-    final Category category = item.params().category();
+    final Category category = this.item.params().category();
     if (category != null && category.isRoot()) {
-      filterTextView.setText(item.params().filterString(context, ksString));
+      this.filterTextView.setText(this.item.params().filterString(context, this.ksString));
     } else {
-      filterTextView.setText(item.params().filterString(context, ksString));
+      this.filterTextView.setText(this.item.params().filterString(context, this.ksString));
     }
-    if (item.selected()) {
-      filterTextView.setTextAppearance(context, R.style.CalloutPrimaryMedium);
-      filterTextView.setTextColor(blackColor);
+    if (this.item.selected()) {
+      this.filterTextView.setTextAppearance(context, R.style.CalloutPrimaryMedium);
+      this.filterTextView.setTextColor(ksrSoftBlackColor);
     } else {
-      filterTextView.setTextAppearance(context, R.style.CalloutPrimary);
-      filterTextView.setTextColor(darkGrayColor);
+      this.filterTextView.setTextAppearance(context, R.style.CalloutPrimary);
+      this.filterTextView.setTextColor(this.darkGrayColor);
     }
 
-    filterView.setBackgroundColor(item.selected() ? filterSelectedColor : filterUnselectedColor);
+    this.filterView.setBackgroundColor(this.item.selected() ? this.filterSelectedColor : this.filterUnselectedColor);
   }
 
   @OnClick(R.id.filter_text_view)
   protected void textViewClick() {
     Timber.d("DiscoveryDrawerChildParamsViewHolder topFilterViewHolderRowClick");
-    delegate.childFilterViewHolderRowClick(this, item);
+    this.delegate.childFilterViewHolderRowClick(this, this.item);
   }
 }
 

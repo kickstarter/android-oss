fa564ed622f443d9ac90a359eb4beee72f014381
Lisa Luo <itslisaluo@gmail.com>
2017-08-03 16:14:12 -0400

    remove vm from test for now :/

--- app/src/test/java/com/kickstarter/viewmodels/PlaygroundViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/PlaygroundViewModelTest.java
@@ -4,15 +4,12 @@ import android.support.annotation.NonNull;
 
 import com.kickstarter.KSRobolectricTestCase;
 import com.kickstarter.libs.Environment;
-import com.kickstarter.ui.viewmodels.PlaygroundViewModel;
 
 import org.junit.Test;
 
 public final class PlaygroundViewModelTest extends KSRobolectricTestCase {
-  private PlaygroundViewModel.ViewModel vm;
 
   protected void setUpEnvironment(final @NonNull Environment environment) {
-    this.vm = new PlaygroundViewModel.ViewModel(environment);
   }
 
   @Test

54babbfa98e16a4ecb5cf2633176b341cf8d88e9
Lisa Luo <itslisaluo@gmail.com>
2017-08-11 13:55:19 -0400

    Continue sketching, add layout w web view

--- app/src/main/java/com/kickstarter/ui/activities/SurveyResponseActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/SurveyResponseActivity.java
@@ -3,15 +3,35 @@ package com.kickstarter.ui.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.kickstarter.R;
 import com.kickstarter.libs.BaseActivity;
 import com.kickstarter.libs.qualifiers.RequiresActivityViewModel;
+import com.kickstarter.libs.rx.transformers.Transformers;
+import com.kickstarter.ui.views.KSWebView;
 import com.kickstarter.viewmodels.SurveyResponseViewModel;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 @RequiresActivityViewModel(SurveyResponseViewModel.ViewModel.class)
 public class SurveyResponseActivity extends BaseActivity<SurveyResponseViewModel.ViewModel> {
+  protected @Bind(R.id.survey_response_web_view) KSWebView ksWebView;
 
   @Override
   protected void onCreate(final @Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.survey_response_layout);
+    ButterKnife.bind(this);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    this.viewModel.outputs.webViewUrl()
+      .take(1)
+      .compose(bindToLifecycle())
+      .compose(Transformers.observeForUI())
+      .subscribe(this.ksWebView::loadUrl);
   }
 }
--- app/src/main/java/com/kickstarter/viewmodels/SurveyResponseViewModel.java
+++ app/src/main/java/com/kickstarter/viewmodels/SurveyResponseViewModel.java
@@ -1,27 +1,70 @@
 package com.kickstarter.viewmodels;
 
 import android.support.annotation.NonNull;
+import android.support.v4.util.Pair;
 
 import com.kickstarter.libs.ActivityViewModel;
 import com.kickstarter.libs.Environment;
+import com.kickstarter.libs.RefTag;
+import com.kickstarter.models.Project;
+import com.kickstarter.models.SurveyResponse;
+import com.kickstarter.ui.IntentKey;
 import com.kickstarter.ui.activities.SurveyResponseActivity;
+
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
+import rx.subjects.PublishSubject;
 
 public interface SurveyResponseViewModel {
 
   interface Inputs {
-
+    void closeButtonClicked();
+    void okButtonClicked();
   }
 
   interface Outputs {
+    /** Emits a project and a ref tag to start the {@link com.kickstarter.ui.activities.ProjectActivity} with. */
+    Observable<Pair<Project, RefTag>> startProjectActivity();
 
+    /** Emits a url to load in the web view. */
+    Observable<String> webViewUrl();
   }
 
   final class ViewModel extends ActivityViewModel<SurveyResponseActivity> implements Inputs, Outputs {
     public ViewModel(final @NonNull Environment environment) {
       super(environment);
+
+      final Observable<SurveyResponse> surveyResponse = intent()
+        .map(i -> i.getParcelableExtra(IntentKey.SURVEY_RESPONSE))
+        .ofType(SurveyResponse.class);
+
+      surveyResponse
+        .map(s -> s.urls().web().toString())
+        .compose(bindToLifecycle())
+        .subscribe(this.webViewUrl);
     }
+
+    private final PublishSubject<Void> closeButtonClicked = PublishSubject.create();
+    private final PublishSubject<Void> okButtonClicked = PublishSubject.create();
+
+    private final PublishSubject<Pair<Project, RefTag>> startProjectActivity = PublishSubject.create();
+    private final BehaviorSubject<String> webViewUrl = BehaviorSubject.create();
 
     public final Inputs inputs = this;
     public final Outputs outputs = this;
+
+    @Override public void closeButtonClicked() {
+      this.closeButtonClicked.onNext(null);
+    }
+    @Override public void okButtonClicked() {
+      this.okButtonClicked.onNext(null);
+    }
+
+    @Override public @NonNull Observable<Pair<Project, RefTag>> startProjectActivity() {
+      return this.startProjectActivity;
+    }
+    @Override public @NonNull Observable<String> webViewUrl() {
+      return this.webViewUrl;
+    }
   }
 }

9c6604a792c0ee35b7777d396a6b40347bff7a71
Lisa Luo <itslisaluo@gmail.com>
2017-08-16 11:52:46 -0400

    add test

--- app/src/test/java/com/kickstarter/viewmodels/SurveyResponseViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/SurveyResponseViewModelTest.java
@@ -1,6 +1,47 @@
 package com.kickstarter.viewmodels;
 
+import android.support.annotation.NonNull;
+
 import com.kickstarter.KSRobolectricTestCase;
+import com.kickstarter.factories.ProjectFactory;
+import com.kickstarter.libs.Environment;
+import com.kickstarter.libs.utils.PairUtils;
+import com.kickstarter.models.Project;
+import com.kickstarter.services.ApiClientType;
+import com.kickstarter.services.MockApiClient;
+
+import org.junit.Test;
+
+import okhttp3.Request;
+import rx.Observable;
+import rx.observers.TestSubscriber;
 
 public class SurveyResponseViewModelTest extends KSRobolectricTestCase {
+  private SurveyResponseViewModel.ViewModel vm;
+  private final TestSubscriber<Project> project = new TestSubscriber<>();
+
+  protected void setUpEnvironment(final @NonNull Environment environment) {
+    this.vm = new SurveyResponseViewModel.ViewModel(environment);
+    this.vm.outputs.startProjectActivity().map(PairUtils::first).subscribe(this.project);
+  }
+
+  @Test
+  public void testStartProjectActivity() {
+    final Project project = ProjectFactory.project().toBuilder().slug("heyo").build();
+
+    final Request projectRequest = new Request.Builder()
+      .url("https://kck.str/projects/param/heyo")
+      .build();
+
+    final ApiClientType apiClient = new MockApiClient() {
+      @Override public @NonNull Observable<Project> fetchProject(final @NonNull String param) {
+        return Observable.just(project);
+      }
+    };
+
+    setUpEnvironment(environment().toBuilder().apiClient(apiClient).build());
+    this.vm.inputs.goToProjectRequest(projectRequest);
+
+    this.project.assertValues(project);
+  }
 }

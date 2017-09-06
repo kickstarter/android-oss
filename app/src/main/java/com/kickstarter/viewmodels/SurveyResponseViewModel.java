443fddce6d4dc59d5c19b2b8d99a94ecdf03e6f0
lisa luo <luoser@users.noreply.github.com>
2017-09-06 17:50:17 -0400

    Fix survey project request bug (#157)
    * refactor to properly navigate to project activity
    
    * cleanup

--- app/src/main/java/com/kickstarter/viewmodels/SurveyResponseViewModel.java
+++ app/src/main/java/com/kickstarter/viewmodels/SurveyResponseViewModel.java
@@ -5,11 +5,7 @@ import android.util.Pair;
 
 import com.kickstarter.libs.ActivityViewModel;
 import com.kickstarter.libs.Environment;
-import com.kickstarter.libs.RefTag;
-import com.kickstarter.libs.utils.PairUtils;
-import com.kickstarter.models.Project;
 import com.kickstarter.models.SurveyResponse;
-import com.kickstarter.services.ApiClientType;
 import com.kickstarter.ui.IntentKey;
 import com.kickstarter.ui.activities.SurveyResponseActivity;
 
@@ -19,22 +15,18 @@ import rx.subjects.BehaviorSubject;
 import rx.subjects.PublishSubject;
 
 import static com.kickstarter.libs.rx.transformers.Transformers.ignoreValues;
-import static com.kickstarter.libs.rx.transformers.Transformers.neverError;
 
 public interface SurveyResponseViewModel {
 
   interface Inputs {
-    /** Call when a project uri request has been made. */
-    void projectUriRequest(Request request);
-
     /** Call when the dialog's OK button has been clicked. */
     void okButtonClicked();
 
+    /** Call when a project uri request has been made. */
+    void projectUriRequest(Request request);
+
     /** Call when a project survey uri request has been made. */
     void projectSurveyUriRequest(Request request);
-
-    /** Call when a web view page has been intercepted. */
-    void webViewPageIntercepted(String url);
   }
 
   interface Outputs {
@@ -44,20 +36,14 @@ public interface SurveyResponseViewModel {
     /** Emits when we should show a confirmation dialog. */
     Observable<Void> showConfirmationDialog();
 
-    /** Emits a project and a ref tag to start the {@link com.kickstarter.ui.activities.ProjectActivity} with. */
-    Observable<Pair<Project, RefTag>> startProjectActivity();
-
     /** Emits a url to load in the web view. */
     Observable<String> webViewUrl();
   }
 
   final class ViewModel extends ActivityViewModel<SurveyResponseActivity> implements Inputs, Outputs {
-    private final ApiClientType client;
 
     public ViewModel(final @NonNull Environment environment) {
       super(environment);
-
-      this.client = environment.apiClient();
 
       final Observable<SurveyResponse> surveyResponse = intent()
         .map(i -> i.getParcelableExtra(IntentKey.SURVEY_RESPONSE))
@@ -70,19 +56,11 @@ public interface SurveyResponseViewModel {
         .compose(bindToLifecycle())
         .subscribe(this.webViewUrl);
 
-      final Observable<Pair<Request, String>> projectRequestAndSurveyUrl = Observable.zip(
+      final Observable<Pair<Request, String>> projectRequestAndSurveyUrl = Observable.combineLatest(
         this.projectUriRequest,
         surveyWebUrl,
         Pair::create
       );
-
-      final Observable<Project> project = projectRequestAndSurveyUrl
-        .filter(requestAndUrl -> !requestTagUrlIsSurveyUrl(requestAndUrl))
-        .map(PairUtils::first)
-        .map(this::extractProjectParams)
-        .switchMap(this.client::fetchProject)
-        .compose(neverError())
-        .share();
 
       projectRequestAndSurveyUrl
         .filter(this::requestTagUrlIsSurveyUrl)
@@ -90,18 +68,7 @@ public interface SurveyResponseViewModel {
         .compose(bindToLifecycle())
         .subscribe(this.showConfirmationDialog);
 
-      project
-        .compose(bindToLifecycle())
-        .subscribe(p -> this.startProjectActivity.onNext(Pair.create(p, RefTag.survey())));
-
       this.goBack = this.okButtonClicked;
-    }
-
-    /**
-     * Parses a project request for project params.
-     */
-    private @NonNull String extractProjectParams(final @NonNull Request request) {
-      return request.url().encodedPathSegments().get(2);
     }
 
     /**
@@ -113,30 +80,25 @@ public interface SurveyResponseViewModel {
         .equals(projectRequestAndSurveyUrl.second);
     }
 
-    private final PublishSubject<Request> projectUriRequest = PublishSubject.create();
     private final PublishSubject<Void> okButtonClicked = PublishSubject.create();
+    private final PublishSubject<Request> projectUriRequest = PublishSubject.create();
     private final PublishSubject<Request> projectSurveyUriRequest = PublishSubject.create();
-    private final PublishSubject<String> webViewPageIntercepted = PublishSubject.create();
 
     private final Observable<Void> goBack;
     private final PublishSubject<Void> showConfirmationDialog = PublishSubject.create();
-    private final PublishSubject<Pair<Project, RefTag>> startProjectActivity = PublishSubject.create();
     private final BehaviorSubject<String> webViewUrl = BehaviorSubject.create();
 
     public final Inputs inputs = this;
     public final Outputs outputs = this;
 
-    @Override public void projectUriRequest(final @NonNull Request request) {
-      this.projectUriRequest.onNext(request);
-    }
     @Override public void okButtonClicked() {
       this.okButtonClicked.onNext(null);
     }
+    @Override public void projectUriRequest(final @NonNull Request request) {
+      this.projectUriRequest.onNext(request);
+    }
     @Override public void projectSurveyUriRequest(final @NonNull Request request) {
       this.projectSurveyUriRequest.onNext(request);
-    }
-    @Override public void webViewPageIntercepted(final @NonNull String url) {
-      this.webViewPageIntercepted.onNext(url);
     }
 
     @Override public @NonNull Observable<Void> goBack() {
@@ -144,9 +106,6 @@ public interface SurveyResponseViewModel {
     }
     @Override public @NonNull Observable<Void> showConfirmationDialog() {
       return this.showConfirmationDialog;
-    }
-    @Override public @NonNull Observable<Pair<Project, RefTag>> startProjectActivity() {
-      return this.startProjectActivity;
     }
     @Override public @NonNull Observable<String> webViewUrl() {
       return this.webViewUrl;
--- app/src/test/java/com/kickstarter/viewmodels/SurveyResponseViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/SurveyResponseViewModelTest.java
@@ -4,28 +4,19 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import com.kickstarter.KSRobolectricTestCase;
-import com.kickstarter.factories.ProjectFactory;
 import com.kickstarter.factories.SurveyResponseFactory;
 import com.kickstarter.libs.Environment;
-import com.kickstarter.libs.RefTag;
-import com.kickstarter.libs.utils.PairUtils;
-import com.kickstarter.models.Project;
 import com.kickstarter.models.SurveyResponse;
-import com.kickstarter.services.ApiClientType;
-import com.kickstarter.services.MockApiClient;
 import com.kickstarter.ui.IntentKey;
 
 import org.junit.Test;
 
 import okhttp3.Request;
-import rx.Observable;
 import rx.observers.TestSubscriber;
 
 public class SurveyResponseViewModelTest extends KSRobolectricTestCase {
   private SurveyResponseViewModel.ViewModel vm;
   private final TestSubscriber<Void> goBack = new TestSubscriber<>();
-  private final TestSubscriber<Project> project = new TestSubscriber<>();
-  private final TestSubscriber<RefTag> refTag = new TestSubscriber<>();
   private final TestSubscriber<Void> showConfirmationDialog = new TestSubscriber<>();
   private final TestSubscriber<String> webViewUrl = new TestSubscriber<>();
 
@@ -33,8 +24,6 @@ public class SurveyResponseViewModelTest extends KSRobolectricTestCase {
     this.vm = new SurveyResponseViewModel.ViewModel(environment);
     this.vm.outputs.goBack().subscribe(this.goBack);
     this.vm.outputs.showConfirmationDialog().subscribe(this.showConfirmationDialog);
-    this.vm.outputs.startProjectActivity().map(PairUtils::first).subscribe(this.project);
-    this.vm.outputs.startProjectActivity().map(PairUtils::second).subscribe(this.refTag);
     this.vm.outputs.webViewUrl().subscribe(this.webViewUrl);
   }
 
@@ -43,30 +32,6 @@ public class SurveyResponseViewModelTest extends KSRobolectricTestCase {
     setUpEnvironment(environment());
     this.vm.inputs.okButtonClicked();
     this.goBack.assertValueCount(1);
-  }
-
-  @Test
-  public void testStartProjectActivity() {
-    final Project project = ProjectFactory.project().toBuilder().slug("heyo").build();
-
-    final Request projectRequest = new Request.Builder()
-      .url("https://kck.str/projects/param/heyo")
-      .tag(null)
-      .build();
-
-    final ApiClientType apiClient = new MockApiClient() {
-      @Override public @NonNull Observable<Project> fetchProject(final @NonNull String param) {
-        return Observable.just(project);
-      }
-    };
-
-    setUpEnvironment(environment().toBuilder().apiClient(apiClient).build());
-    this.vm.intent(new Intent().putExtra(IntentKey.SURVEY_RESPONSE, SurveyResponseFactory.surveyResponse()));
-
-    this.vm.inputs.projectUriRequest(projectRequest);
-
-    this.project.assertValues(project);
-    this.refTag.assertValues(RefTag.survey());
   }
 
   @Test

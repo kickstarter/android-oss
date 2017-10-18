a3f96b9a96e026280efce633dce5634f7eb336f6
Izzy Oji <eoji11592@gmail.com>
2017-10-18 10:58:34 -0400

    signup refactor (#166)
    * Signup refactor with updated tests.
    
    * Cleanup.

--- app/src/main/java/com/kickstarter/viewmodels/inputs/SignupViewModelInputs.java
+++
@@ -1,9 +0,0 @@
-package com.kickstarter.viewmodels.inputs;
-
-public interface SignupViewModelInputs {
-  void fullName(String __);
-  void email(String __);
-  void password(String __);
-  void sendNewslettersClick(boolean __);
-  void signupClick();
-}

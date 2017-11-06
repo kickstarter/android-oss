fcc8c5be47ff899b47ded1699e5b5d1ee7ad42b9
lisa luo <luoser@users.noreply.github.com>
2017-11-06 12:20:54 -0800

    Minimize dependency injections (#173)
    * wip
    
    * Add InternalToolsType to environment
    
    FYI, there's already a provides method for the singleton in the InternalAppModule
    
    * Add environment to base toolbar class
    
    * Add webEndpoint to environment
    
    * cleanup, mostly unused injections

--- app/src/main/java/com/kickstarter/ApplicationGraph.java
+++ app/src/main/java/com/kickstarter/ApplicationGraph.java
@@ -1,15 +1,10 @@
 package com.kickstarter;
 
 import com.kickstarter.libs.Environment;
-import com.kickstarter.libs.KSCurrency;
-import com.kickstarter.libs.Koala;
 import com.kickstarter.libs.KoalaTrackingClient;
 import com.kickstarter.libs.utils.ApplicationLifecycleUtil;
 import com.kickstarter.services.gcm.MessageService;
 import com.kickstarter.services.gcm.RegisterService;
-import com.kickstarter.ui.activities.DiscoveryActivity;
-import com.kickstarter.ui.activities.HelpActivity;
-import com.kickstarter.ui.toolbars.DiscoveryToolbar;
 import com.kickstarter.ui.views.AppRatingDialog;
 import com.kickstarter.ui.views.IconTextView;
 import com.kickstarter.ui.views.KSWebView;
@@ -18,15 +13,10 @@ public interface ApplicationGraph {
   Environment environment();
   void inject(ApplicationLifecycleUtil __);
   void inject(AppRatingDialog __);
-  void inject(Koala __);
-  void inject(DiscoveryActivity __);
-  void inject(DiscoveryToolbar __);
-  void inject(HelpActivity __);
   void inject(IconTextView __);
   void inject(KoalaTrackingClient __);
   void inject(KSWebView __);
   void inject(KSApplication __);
   void inject(MessageService __);
-  void inject(KSCurrency __);
   void inject(RegisterService __);
 }

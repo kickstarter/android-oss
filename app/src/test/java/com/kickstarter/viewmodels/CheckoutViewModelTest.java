45ea2abc787b8efa8149ffe482bd9aa76962c405
Izzy Oji <eoji11592@gmail.com>
2017-12-07 18:18:30 -0500

    adds tests that don't require mocking MaskedWallet and FullWallet :(

--- app/src/test/java/com/kickstarter/viewmodels/CheckoutViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/CheckoutViewModelTest.java
@@ -1,11 +1,13 @@
 package com.kickstarter.viewmodels;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import com.kickstarter.KSRobolectricTestCase;
 import com.kickstarter.factories.ActivityResultFactory;
 import com.kickstarter.libs.AndroidPayCapability;
 import com.kickstarter.libs.Environment;
+import com.kickstarter.ui.IntentKey;
 
 import org.junit.Test;
 
@@ -15,11 +17,15 @@ public final class CheckoutViewModelTest extends KSRobolectricTestCase {
   private CheckoutViewModel.ViewModel vm;
   private final TestSubscriber<Integer> androidPayError = new TestSubscriber<>();
   private final TestSubscriber<Boolean> isAndroidPayAvailable = new TestSubscriber<>();
+  private final TestSubscriber<String> title = new TestSubscriber<>();
+  private final TestSubscriber<String> url = new TestSubscriber<>();
 
   protected void setUpEnvironment(final @NonNull Environment environment) {
     this.vm = new CheckoutViewModel.ViewModel(environment);
     this.vm.outputs.androidPayError().subscribe(this.androidPayError);
     this.vm.outputs.isAndroidPayAvailable().subscribe(this.isAndroidPayAvailable);
+    this.vm.outputs.title().subscribe(this.title);
+    this.vm.outputs.url().subscribe(this.url);
   }
 
   @Test
@@ -50,5 +56,24 @@ public final class CheckoutViewModelTest extends KSRobolectricTestCase {
 
     setUpEnvironment(env);
     this.isAndroidPayAvailable.assertValues(true);
+  }
+
+  @Test
+  public void test_Title_FromIntent() {
+    setUpEnvironment(environment());
+    this.vm.intent(new Intent().putExtra(IntentKey.TOOLBAR_TITLE, "Test"));
+
+    this.title.assertValue("Test");
+  }
+
+  @Test
+  public void test_Url_FromIntent() {
+    setUpEnvironment(environment());
+    this.vm.intent(new Intent().putExtra(IntentKey.URL, "www.test.com"));
+
+    this.url.assertValue("www.test.com");
+
+    this.vm.inputs.pageIntercepted("www.test2.com");
+    this.url.assertValues("www.test.com", "www.test2.com");
   }
 }

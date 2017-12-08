169f0b254fc57737fd809781139b7e8c588d2b55
lisa luo <luoser@users.noreply.github.com>
2017-12-08 16:06:39 -0500

    [Modernize] Checkout (#188)
    * modernize inputs/outputs structure
    
    * cleanup and add android pay error test
    
    * lil test cleanup
    
    * adds tests that don't require mocking MaskedWallet and FullWallet :(
    
    * alpha inputs

--- app/src/test/java/com/kickstarter/viewmodels/CheckoutViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/CheckoutViewModelTest.java
@@ -1,14 +1,40 @@
 package com.kickstarter.viewmodels;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
 import com.kickstarter.KSRobolectricTestCase;
+import com.kickstarter.factories.ActivityResultFactory;
 import com.kickstarter.libs.AndroidPayCapability;
 import com.kickstarter.libs.Environment;
-
-import junit.framework.Assert;
+import com.kickstarter.ui.IntentKey;
 
 import org.junit.Test;
 
+import rx.observers.TestSubscriber;
+
 public final class CheckoutViewModelTest extends KSRobolectricTestCase {
+  private CheckoutViewModel.ViewModel vm;
+  private final TestSubscriber<Integer> androidPayError = new TestSubscriber<>();
+  private final TestSubscriber<Boolean> isAndroidPayAvailable = new TestSubscriber<>();
+  private final TestSubscriber<String> title = new TestSubscriber<>();
+  private final TestSubscriber<String> url = new TestSubscriber<>();
+
+  protected void setUpEnvironment(final @NonNull Environment environment) {
+    this.vm = new CheckoutViewModel.ViewModel(environment);
+    this.vm.outputs.androidPayError().subscribe(this.androidPayError);
+    this.vm.outputs.isAndroidPayAvailable().subscribe(this.isAndroidPayAvailable);
+    this.vm.outputs.title().subscribe(this.title);
+    this.vm.outputs.url().subscribe(this.url);
+  }
+
+  @Test
+  public void test_AndroidPayError() {
+    setUpEnvironment(environment());
+
+    this.vm.activityResult(ActivityResultFactory.androidPayErrorResult());
+    this.androidPayError.assertValueCount(1);
+  }
 
   @Test
   public void test_AndroidPayIsAvailable_WhenNotCapable() {
@@ -17,9 +43,8 @@ public final class CheckoutViewModelTest extends KSRobolectricTestCase {
       .androidPayCapability(new AndroidPayCapability(false))
       .build();
 
-    final CheckoutViewModel vm = new CheckoutViewModel(env);
-
-    vm.outputs.isAndroidPayAvailable().subscribe(Assert::assertFalse);
+    setUpEnvironment(env);
+    this.isAndroidPayAvailable.assertValues(false);
   }
 
   @Test
@@ -29,8 +54,26 @@ public final class CheckoutViewModelTest extends KSRobolectricTestCase {
       .androidPayCapability(new AndroidPayCapability(true))
       .build();
 
-    final CheckoutViewModel vm = new CheckoutViewModel(env);
+    setUpEnvironment(env);
+    this.isAndroidPayAvailable.assertValues(true);
+  }
 
-    vm.outputs.isAndroidPayAvailable().subscribe(Assert::assertTrue);
+  @Test
+  public void test_Title_FromIntent() {
+    setUpEnvironment(environment());
+    this.vm.intent(new Intent().putExtra(IntentKey.TOOLBAR_TITLE, "Test"));
+
+    this.title.assertValue("Test");
+  }
+
+  @Test
+  public void test_Url_FromIntent() {
+    setUpEnvironment(environment());
+    this.vm.intent(new Intent().putExtra(IntentKey.URL, "www.test.com"));
+
+    this.url.assertValue("www.test.com");
+
+    this.vm.inputs.pageIntercepted("www.test2.com");
+    this.url.assertValues("www.test.com", "www.test2.com");
   }
 }

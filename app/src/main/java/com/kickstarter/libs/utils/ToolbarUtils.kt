8392f18d707bcda5f87b9d2107a71e75aaa0d799
Izzy Oji <eoji11592@gmail.com>
2018-02-02 18:27:31 -0500

    adding the cute little toolbar animation and fixing the back button :sweat_smile:

--- app/src/main/java/com/kickstarter/libs/utils/ToolbarUtils.kt
+++ app/src/main/java/com/kickstarter/libs/utils/ToolbarUtils.kt
@@ -2,6 +2,7 @@ package com.kickstarter.libs.utils
 
 import android.support.design.widget.AppBarLayout
 import android.view.View
+import android.widget.TextView
 
 object ToolbarUtils {
 
@@ -15,4 +16,20 @@ object ToolbarUtils {
       toolbarTitleView.alpha = Math.abs(verticalOffset).toFloat() / layout.totalScrollRange
     }
   }
+
+  /**
+   * Adds an `offsetChangedListener` to set the toolbar title view's `alpha` and translateY based
+   * on vertical scroll position. The result is a toolbar title fade and slide down effect as the user
+   * expands the CollapsingToolbarLayout.
+   */
+  fun fadeAndTranslateToolbarTitleOnExpand(appBarLayout: AppBarLayout, toolbarTitleView: TextView) {
+    appBarLayout.addOnOffsetChangedListener { layout, verticalOffset ->
+      val alpha = calculateAlphaFromOffset(verticalOffset, layout)
+      toolbarTitleView.alpha = alpha
+      toolbarTitleView.translationY = toolbarTitleView.height - alpha * toolbarTitleView.height
+    }
+  }
+
+  private fun calculateAlphaFromOffset(verticalOffset: Int, layout: AppBarLayout) =
+    Math.abs(verticalOffset).toFloat() / layout.totalScrollRange
 }

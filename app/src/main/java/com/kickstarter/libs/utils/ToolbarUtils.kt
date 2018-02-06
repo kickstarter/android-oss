26b5300714d261d75c19194775c5e96715843fbe
Izzy Oji <eoji11592@gmail.com>
2018-02-06 12:38:22 -0500

    Creator tools toolbar (#210)
    * adding the cute little toolbar animation and fixing the back button :sweat_smile:
    
    * Starting fade and translate when the toolbar has collapsed more than halfway.
    
    * Ellipsizing project name when it's more than one line in the toolbar.

--- app/src/main/java/com/kickstarter/libs/utils/ToolbarUtils.kt
+++ app/src/main/java/com/kickstarter/libs/utils/ToolbarUtils.kt
@@ -2,6 +2,7 @@ package com.kickstarter.libs.utils
 
 import android.support.design.widget.AppBarLayout
 import android.view.View
+import android.widget.TextView
 
 object ToolbarUtils {
 
@@ -11,8 +12,28 @@ object ToolbarUtils {
    * expands the CollapsingToolbarLayout.
    */
   fun fadeToolbarTitleOnExpand(appBarLayout: AppBarLayout, toolbarTitleView: View) {
-    appBarLayout.addOnOffsetChangedListener { layout, verticalOffset ->
-      toolbarTitleView.alpha = Math.abs(verticalOffset).toFloat() / layout.totalScrollRange
+    appBarLayout.addOnOffsetChangedListener { _, verticalOffset ->
+      toolbarTitleView.alpha = calculateAlphaFromOffset(verticalOffset, appBarLayout)
     }
   }
+
+  /**
+   * Adds an `offsetChangedListener` to set the toolbar title view's `alpha` and translateY based
+   * on vertical scroll position. The result is a toolbar title fade and slide down effect as the user
+   * expands the CollapsingToolbarLayout more than half of its height.
+   */
+  fun fadeAndTranslateToolbarTitleOnExpand(appBarLayout: AppBarLayout, toolbarTitleView: TextView) {
+    appBarLayout.addOnOffsetChangedListener { _, verticalOffset ->
+      if (Math.abs(verticalOffset) > appBarLayout.totalScrollRange / 2) {
+        val alpha = calculateAlphaFromOffset(verticalOffset, appBarLayout)
+        toolbarTitleView.alpha = alpha
+        toolbarTitleView.translationY = toolbarTitleView.height - alpha * toolbarTitleView.height
+      } else {
+        toolbarTitleView.alpha = 0f
+      }
+    }
+  }
+
+  private fun calculateAlphaFromOffset(verticalOffset: Int, layout: AppBarLayout) =
+    Math.abs(verticalOffset).toFloat() / layout.totalScrollRange
 }

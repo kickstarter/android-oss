c035c38a773e697c7ea95d64620ead2bcd87d0ce
Izzy Oji <eoji11592@gmail.com>
2018-02-27 16:31:58 -0500

    at a good stopping point (without tests)

--- app/src/main/java/com/kickstarter/ui/viewholders/CreatorDashboardReferrerBreakdownViewHolder.java
+++ app/src/main/java/com/kickstarter/ui/viewholders/CreatorDashboardReferrerBreakdownViewHolder.java
@@ -3,7 +3,6 @@ package com.kickstarter.ui.viewholders;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.Pair;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,7 +13,6 @@ import com.kickstarter.libs.KSCurrency;
 import com.kickstarter.libs.utils.ViewUtils;
 import com.kickstarter.models.Project;
 import com.kickstarter.services.apiresponses.ProjectStatsEnvelope;
-import com.kickstarter.ui.views.ReferrerBreakdownView;
 import com.kickstarter.viewmodels.CreatorDashboardReferrerBreakdownHolderViewModel;
 
 import java.math.RoundingMode;
@@ -38,16 +36,19 @@ public class CreatorDashboardReferrerBreakdownViewHolder extends KSViewHolder {
   protected @Bind(R.id.percent_via_external_circle_text_view) ImageView percentExternalCircleTextView;
   protected @Bind(R.id.percent_via_kickstarter_text_view) TextView percentInternalTextView;
   protected @Bind(R.id.percent_via_kickstarter_circle_text_view) ImageView percentInternalCircleTextView;
+  protected @Bind(R.id.pledged_via_custom) View pledgedViaCustomLayout;
+  protected @Bind(R.id.pledged_via_custom_bar) View pledgedViaCustomBar;
   protected @Bind(R.id.pledged_via_custom_indicator) View pledgedViaCustomIndicator;
+  protected @Bind(R.id.pledged_via_external) View pledgedViaExternalLayout;
+  protected @Bind(R.id.pledged_via_external_bar) View pledgedViaExternalBar;
   protected @Bind(R.id.pledged_via_external_indicator) View pledgedViaExternalIndicator;
+  protected @Bind(R.id.pledged_via_kickstarter_bar) View pledgedViaInternalBar;
   protected @Bind(R.id.pledged_via_kickstarter_indicator) View pledgedViaInternalIndicator;
-  protected @Bind(R.id.pledged_via_custom_layout) View pledgedViaCustomLayout;
-  protected @Bind(R.id.pledged_via_external_layout) View pledgedViaExternalLayout;
-  protected @Bind(R.id.pledged_via_kickstarter_layout) View pledgedViaInternalLayout;
+  protected @Bind(R.id.pledged_via_kickstarter) View pledgedViaInternalLayout;
   protected @Bind(R.id.referrer_breakdown_chart_layout) ConstraintLayout referrerBreakdownLayout;
-  protected @Bind(R.id.referrer_breakdown_view) ReferrerBreakdownView referrerBreakdownView;
 
   protected @BindDimen(R.dimen.grid_1_half) int grid1HalfPixels;
+  protected @BindDimen(R.dimen.grid_1) int grid1Pixels;
   protected @BindDimen(R.dimen.grid_3) int grid3Pixels;
 
   private KSCurrency ksCurrency;
@@ -59,45 +60,35 @@ public class CreatorDashboardReferrerBreakdownViewHolder extends KSViewHolder {
 
     this.viewModel = new CreatorDashboardReferrerBreakdownHolderViewModel.ViewModel(environment());
 
-//    this.viewModel.outputs.customReferrerColor()
-//      .compose(bindToLifecycle())
-//      .compose(observeForUI())
-//      .subscribe(color -> DrawableCompat.setTint(this.percentCustomCircleTextView.getDrawable(), color));
-
     this.viewModel.outputs.customReferrerPercent()
       .compose(bindToLifecycle())
       .compose(observeForUI())
-      .subscribe(percent -> setReferrerWidth(percent, this.pledgedViaCustomLayout, this.pledgedViaCustomIndicator));
+      .subscribe(percent -> setReferrerWidth(percent, this.pledgedViaCustomBar, this.pledgedViaCustomIndicator));
 
     this.viewModel.outputs.customReferrerPercentText()
       .compose(bindToLifecycle())
       .compose(observeForUI())
       .subscribe(this.percentCustomTextView::setText);
 
-    this.viewModel.outputs.externalReferrerColor()
+    this.viewModel.outputs.externalReferrerPercent()
       .compose(bindToLifecycle())
       .compose(observeForUI())
-      .subscribe(color -> DrawableCompat.setTint(this.percentExternalCircleTextView.getDrawable(), color));
+      .subscribe(percent -> setReferrerWidth(percent, this.pledgedViaExternalBar, this.pledgedViaExternalIndicator));
 
     this.viewModel.outputs.externalReferrerPercent()
       .compose(bindToLifecycle())
       .compose(observeForUI())
-      .subscribe(percent -> setReferrerWidth(percent, this.pledgedViaExternalLayout, this.pledgedViaExternalIndicator));
+      .subscribe(__ -> flipIndicatorIfStatsOffScreen(this.pledgedViaExternalIndicator, this.pledgedViaExternalLayout));
 
     this.viewModel.outputs.externalReferrerPercentText()
       .compose(bindToLifecycle())
       .compose(observeForUI())
       .subscribe(this.percentExternalTextView::setText);
 
-//    this.viewModel.outputs.internalReferrerColor()
-//      .compose(bindToLifecycle())
-//      .compose(observeForUI())
-//      .subscribe(color -> DrawableCompat.setTint(this.percentInternalCircleTextView.getDrawable(), color));
-
     this.viewModel.outputs.internalReferrerPercent()
       .compose(bindToLifecycle())
       .compose(observeForUI())
-      .subscribe(percent -> setReferrerWidth(percent, this.pledgedViaInternalLayout, this.pledgedViaInternalIndicator));
+      .subscribe(percent -> setReferrerWidth(percent, this.pledgedViaInternalBar, this.pledgedViaInternalIndicator));
 
     this.viewModel.outputs.internalReferrerPercentText()
       .compose(bindToLifecycle())
@@ -107,17 +98,17 @@ public class CreatorDashboardReferrerBreakdownViewHolder extends KSViewHolder {
     this.viewModel.outputs.pledgedViaCustomLayoutIsGone()
       .compose(bindToLifecycle())
       .compose(observeForUI())
-      .subscribe(ViewUtils.setGone(this.pledgedViaCustomLayout));
+      .subscribe(gone -> this.hideReferrer(gone, this.pledgedViaCustomLayout, this.pledgedViaCustomBar, this.pledgedViaCustomIndicator));
 
     this.viewModel.outputs.pledgedViaExternalLayoutIsGone()
       .compose(bindToLifecycle())
       .compose(observeForUI())
-      .subscribe(ViewUtils.setGone(this.pledgedViaExternalLayout));
+      .subscribe(gone -> this.hideReferrer(gone, this.pledgedViaExternalLayout, this.pledgedViaExternalBar, this.pledgedViaExternalIndicator));
 
     this.viewModel.outputs.pledgedViaInternalLayoutIsGone()
       .compose(bindToLifecycle())
       .compose(observeForUI())
-      .subscribe(ViewUtils.setGone(this.pledgedViaInternalLayout));
+      .subscribe(gone -> this.hideReferrer(gone, this.pledgedViaInternalLayout, this.pledgedViaInternalBar, this.pledgedViaInternalIndicator));
 
     this.viewModel.outputs.projectAndCustomReferrerPledgedAmount()
       .compose(bindToLifecycle())
@@ -140,14 +131,43 @@ public class CreatorDashboardReferrerBreakdownViewHolder extends KSViewHolder {
     barLayoutParams.horizontalWeight = percent;
     bar.setLayoutParams(barLayoutParams);
 
+    adjustIndicatorMarginForShortBar(bar, indicator);
+  }
+
+  // i don't prefer to do this and am ~open~ to suggestions
+  private void adjustIndicatorMarginForShortBar(View bar, View indicator) {
     bar.post(() -> {
-      if (bar.getMeasuredWidth() < grid1HalfPixels) {
-        ConstraintLayout.LayoutParams indicatorLayoutParams = (ConstraintLayout.LayoutParams) bar.getLayoutParams();
-        indicatorLayoutParams.setMarginEnd(0);
-        indicatorLayoutParams.setMarginStart(0);
-        indicator.setLayoutParams(barLayoutParams);
+      if (bar.getMeasuredWidth() < grid3Pixels) {
+        ConstraintLayout.LayoutParams indicatorLayoutParams = (ConstraintLayout.LayoutParams) indicator.getLayoutParams();
+        indicatorLayoutParams.startToStart = bar.getId();
+        indicatorLayoutParams.endToEnd = bar.getId();
+        indicator.setLayoutParams(indicatorLayoutParams);
       }
     });
+  }
+
+  //same here
+  private void flipIndicatorIfStatsOffScreen(View indicator, View stats) {
+    stats.post(() -> {
+      if (stats.getLeft() < referrerBreakdownLayout.getLeft()) {
+        indicator.setScaleX(-1);
+        ConstraintLayout.LayoutParams indicatorLayoutParams = (ConstraintLayout.LayoutParams) indicator.getLayoutParams();
+        indicatorLayoutParams.setMarginStart(grid3Pixels);
+        indicator.setLayoutParams(indicatorLayoutParams);
+
+        ConstraintLayout.LayoutParams statsLayoutParams = (ConstraintLayout.LayoutParams) stats.getLayoutParams();
+        statsLayoutParams.startToEnd = indicator.getId();
+        statsLayoutParams.setMarginStart(grid1Pixels);
+        statsLayoutParams.endToStart = ConstraintLayout.LayoutParams.UNSET;
+        stats.setLayoutParams(statsLayoutParams);
+      }
+    });
+  }
+
+  private void hideReferrer(final boolean gone, final @NonNull View layout, final @NonNull View bar, final @NonNull View indicator) {
+    ViewUtils.setGone(layout, gone);
+    ViewUtils.setGone(bar, gone);
+    ViewUtils.setGone(indicator, gone);
   }
 
   @Override

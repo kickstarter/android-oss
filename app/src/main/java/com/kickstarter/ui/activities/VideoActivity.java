a95f58bbd2c55ab0774e8858c86882416af67bfe
Izzy Oji <eoji11592@gmail.com>
2018-03-20 17:20:30 -0400

    cleaning up imports

--- app/src/main/java/com/kickstarter/ui/activities/VideoActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/VideoActivity.java
@@ -4,7 +4,6 @@ import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 

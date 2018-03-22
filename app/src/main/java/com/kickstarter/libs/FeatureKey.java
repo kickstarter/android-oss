c20bacfd5c868b25b216091702fba049ab368f88
Izzy Oji <eoji11592@gmail.com>
2018-03-22 15:54:10 -0400

    HIDING MY BEAUTIFUL CHART need to update tests

--- app/src/main/java/com/kickstarter/libs/FeatureKey.java
+++ app/src/main/java/com/kickstarter/libs/FeatureKey.java
@@ -3,4 +3,6 @@ package com.kickstarter.libs;
 public final class FeatureKey {
   private FeatureKey() {}
 
+  public static final String NATIVE_CREATOR_BREAKDOWN_CHART = "native_creator_breakdown_chart";
+
 }

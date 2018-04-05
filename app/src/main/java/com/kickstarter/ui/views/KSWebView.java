b21d831ae524417f00871010c30eb3fda43165c3
Izzy Oji <eoji11592@gmail.com>
2018-04-05 18:20:00 -0400

    Setting the User-Agent string on all webview requests.

--- app/src/main/java/com/kickstarter/ui/views/KSWebView.java
+++ app/src/main/java/com/kickstarter/ui/views/KSWebView.java
@@ -10,13 +10,16 @@ import android.webkit.WebView;
 
 import com.kickstarter.KSApplication;
 import com.kickstarter.libs.ApiCapabilities;
+import com.kickstarter.libs.Build;
 import com.kickstarter.libs.WebViewJavascriptInterface;
 import com.kickstarter.services.KSWebViewClient;
+import com.kickstarter.services.interceptors.WebRequestInterceptor;
 
 import javax.inject.Inject;
 
 public class KSWebView extends WebView {
   @Inject KSWebViewClient client;
+  @Inject Build build;
 
   public KSWebView(final @NonNull Context context) {
     this(context, null);
@@ -39,6 +42,7 @@ public class KSWebView extends WebView {
     setWebChromeClient(new WebChromeClient());
     getSettings().setJavaScriptEnabled(true);
     getSettings().setAllowFileAccess(false);
+    getSettings().setUserAgentString(WebRequestInterceptor.userAgent(this.build));
     enableDebugging();
 
     addJavascriptInterface(new WebViewJavascriptInterface(this.client), "WebViewJavascriptInterface");

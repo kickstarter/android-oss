9f37207592f846057160130c356c3c85dd1926f6
Izzy Oji <eoji11592@gmail.com>
2018-06-15 14:11:26 -0400

    updating robolectric dependencies

--- app/src/test/java/com/kickstarter/KSRobolectricGradleTestRunner.java
+++ app/src/test/java/com/kickstarter/KSRobolectricGradleTestRunner.java
@@ -32,9 +32,6 @@ package com.kickstarter;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.manifest.AndroidManifest;
-import org.robolectric.res.FileFsFile;
-import org.robolectric.util.Logger;
 import org.robolectric.util.ReflectionHelpers;
 
 public class KSRobolectricGradleTestRunner extends RobolectricTestRunner {
@@ -45,55 +42,55 @@ public class KSRobolectricGradleTestRunner extends RobolectricTestRunner {
     super(testClass);
   }
 
-  @Override
-  protected AndroidManifest getAppManifest(final Config config) {
-    if (config.constants() == Void.class) {
-      Logger.error("Field 'constants' not specified in @Config annotation");
-      Logger.error("This is required when using RobolectricGradleTestRunner!");
-      throw new RuntimeException("No 'constants' field in @Config annotation!");
-    }
-
-    final String type = getType(config);
-    final String flavor = getFlavor(config);
-    final String packageName = getPackageName(config);
-
-    final FileFsFile res;
-    final FileFsFile assets;
-    final FileFsFile manifest;
-
-    // res/merged added in Android Gradle plugin 1.3-beta1
-    if (FileFsFile.from(BUILD_OUTPUT, "res", "merged").exists()) {
-      res = FileFsFile.from(BUILD_OUTPUT, "res", "merged", flavor, type);
-    } else if (FileFsFile.from(BUILD_OUTPUT, "res").exists()) {
-      res = FileFsFile.from(BUILD_OUTPUT, "res", flavor, type);
-    } else {
-      res = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "res");
-    }
-
-    if (FileFsFile.from(BUILD_OUTPUT, "assets").exists()) {
-      assets = FileFsFile.from(BUILD_OUTPUT, "assets", flavor, type);
-    } else {
-      assets = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "assets");
-    }
-
-    if (FileFsFile.from(BUILD_OUTPUT, "manifests").exists()) {
-      manifest = FileFsFile.from(BUILD_OUTPUT, "manifests", "full", flavor, type, "AndroidManifest.xml");
-    } else {
-      manifest = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "AndroidManifest.xml");
-    }
-
-    Logger.debug("Robolectric assets directory: " + assets.getPath());
-    Logger.debug("   Robolectric res directory: " + res.getPath());
-    Logger.debug("   Robolectric manifest path: " + manifest.getPath());
-    Logger.debug("    Robolectric package name: " + packageName);
-
-    return new AndroidManifest(manifest, res, assets) {
-      @Override
-      public String getRClassName() throws Exception {
-        return com.kickstarter.R.class.getName();
-      }
-    };
-  }
+//  @Override
+//  protected AndroidManifest getAppManifest(final Config config) {
+//    if (config.constants() == Void.class) {
+//      Logger.error("Field 'constants' not specified in @Config annotation");
+//      Logger.error("This is required when using RobolectricGradleTestRunner!");
+//      throw new RuntimeException("No 'constants' field in @Config annotation!");
+//    }
+//
+//    final String type = getType(config);
+//    final String flavor = getFlavor(config);
+//    final String packageName = getPackageName(config);
+//
+//    final FileFsFile res;
+//    final FileFsFile assets;
+//    final FileFsFile manifest;
+//
+//    // res/merged added in Android Gradle plugin 1.3-beta1
+//    if (FileFsFile.from(BUILD_OUTPUT, "res", "merged").exists()) {
+//      res = FileFsFile.from(BUILD_OUTPUT, "res", "merged", flavor, type);
+//    } else if (FileFsFile.from(BUILD_OUTPUT, "res").exists()) {
+//      res = FileFsFile.from(BUILD_OUTPUT, "res", flavor, type);
+//    } else {
+//      res = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "res");
+//    }
+//
+//    if (FileFsFile.from(BUILD_OUTPUT, "assets").exists()) {
+//      assets = FileFsFile.from(BUILD_OUTPUT, "assets", flavor, type);
+//    } else {
+//      assets = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "assets");
+//    }
+//
+//    if (FileFsFile.from(BUILD_OUTPUT, "manifests").exists()) {
+//      manifest = FileFsFile.from(BUILD_OUTPUT, "manifests", "full", flavor, type, "AndroidManifest.xml");
+//    } else {
+//      manifest = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "AndroidManifest.xml");
+//    }
+//
+//    Logger.debug("Robolectric assets directory: " + assets.getPath());
+//    Logger.debug("   Robolectric res directory: " + res.getPath());
+//    Logger.debug("   Robolectric manifest path: " + manifest.getPath());
+//    Logger.debug("    Robolectric package name: " + packageName);
+//
+//    return new AndroidManifest(manifest, res, assets) {
+//      @Override
+//      public String getRClassName() throws Exception {
+//        return com.kickstarter.R.class.getName();
+//      }
+//    };
+//  }
 
   private static String getType(final Config config) {
     try {

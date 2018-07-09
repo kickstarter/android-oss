85dcca4e767d73dd55f82633051acd6b6387cc5d
Rashad Cureton <rashad@kickstarter.com>
2018-07-09 15:27:01 -0400

    [Enhancement] - Added new activities to new SettingsActivity (#273)
    * added logout click to the viewmodel
    
    * added skeleton activities for settings and attached click listeners to them
    
    * alphabetized methods in settings

---
+++ app/src/main/java/com/kickstarter/ui/activities/NewsletterActivity.kt
@@ -0,0 +1,13 @@
+package com.kickstarter.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kickstarter.R
+
+class NewsletterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newsletter)
+    }
+}
--- app/src/main/java/com/kickstarter/ui/activities/SettingsNewActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/SettingsNewActivity.kt
@@ -3,6 +3,7 @@ package com.kickstarter.ui.activities
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.kickstarter.KSApplication
 import com.kickstarter.R
 import com.kickstarter.databinding.SettingsLayoutNewBinding
 import com.kickstarter.viewmodels.SettingsNewViewModel
@@ -13,6 +14,7 @@ class SettingsNewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: SettingsLayoutNewBinding = DataBindingUtil.setContentView(this, R.layout.settings_layout_new)
 
-        binding.viewModel = SettingsNewViewModel(this)
+        val environment = (application as KSApplication).component().environment()
+        binding.viewModel = SettingsNewViewModel(this, environment)
     }
 }

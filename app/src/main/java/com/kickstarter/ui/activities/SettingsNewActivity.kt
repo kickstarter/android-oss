7ddbdfc020b05129f7839a73ba8c35aab5fe65d4
Rashad Cureton <rashad@kickstarter.com>
2018-07-09 11:14:45 -0400

    [Enhancement] - Updated Settings skeleton (#271)
    * setup settingsViewModel and SettingsActivity with a different MVVM pattern
    
    * removed line from gradle
    
    * fixeds activity line in manifest

---
+++ app/src/main/java/com/kickstarter/ui/activities/SettingsNewActivity.kt
@@ -0,0 +1,18 @@
+package com.kickstarter.ui.activities
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kickstarter.R
+import com.kickstarter.databinding.SettingsLayoutNewBinding
+import com.kickstarter.viewmodels.SettingsNewViewModel
+
+class SettingsNewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: SettingsLayoutNewBinding = DataBindingUtil.setContentView(this, R.layout.settings_layout_new)
+
+        binding.viewModel = SettingsNewViewModel(this)
+    }
+}

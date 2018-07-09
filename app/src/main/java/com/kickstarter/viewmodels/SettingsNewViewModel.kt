b8478c2d031b46880da3b3abc404aae7fb0b10b9
Rashad Cureton <rashad.cureton@gmail.com>
2018-07-09 14:07:17 -0400

    added logout click to the viewmodel

--- app/src/main/java/com/kickstarter/viewmodels/SettingsNewViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/SettingsNewViewModel.kt
@@ -3,9 +3,12 @@ package com.kickstarter.viewmodels
 import android.content.Context
 import android.databinding.BaseObservable
 import com.kickstarter.BuildConfig
+import com.kickstarter.libs.Environment
+import com.kickstarter.libs.Logout
+import com.kickstarter.libs.utils.ApplicationUtils
 import com.kickstarter.libs.utils.ViewUtils
 
-class SettingsNewViewModel(private val context: Context) : BaseObservable() {
+class SettingsNewViewModel(private val context: Context, private val environment: Environment) : BaseObservable() {
 
     fun getVersion() = BuildConfig.VERSION_NAME
 
@@ -13,4 +16,24 @@ class SettingsNewViewModel(private val context: Context) : BaseObservable() {
         ViewUtils.openStoreRating(context, context.packageName)
     }
 
+    fun logoutClick() {
+        environment.logout().execute()
+        ApplicationUtils.startNewDiscoveryActivity(context)
+    }
+
+    fun notificationsClick() {
+
+    }
+
+    fun newsletterClick() {
+
+    }
+
+    fun helpClick() {
+
+    }
+
+    fun privacyClick() {
+
+    }
 }

8c3acc2b05260ac7549271bbed17b5dddddac212
Rashad Cureton <rashad@kickstarter.com>
2018-07-09 15:27:01 -0400

    [Enhancement] - Added new activities to new SettingsActivity (#273)
    * added logout click to the viewmodel
    
    * added skeleton activities for settings and attached click listeners to them
    
    * alphabetized methods in settings

--- app/src/main/java/com/kickstarter/viewmodels/SettingsNewViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/SettingsNewViewModel.kt
@@ -1,16 +1,34 @@
 package com.kickstarter.viewmodels
 
 import android.content.Context
+import android.content.Intent
 import android.databinding.BaseObservable
 import com.kickstarter.BuildConfig
+import com.kickstarter.libs.Environment
+import com.kickstarter.libs.utils.ApplicationUtils
 import com.kickstarter.libs.utils.ViewUtils
+import com.kickstarter.ui.activities.HelpNewActivity
+import com.kickstarter.ui.activities.NewsletterActivity
+import com.kickstarter.ui.activities.NotificationsActivity
+import com.kickstarter.ui.activities.PrivacyActivity
 
-class SettingsNewViewModel(private val context: Context) : BaseObservable() {
+class SettingsNewViewModel(private val context: Context, private val environment: Environment) : BaseObservable() {
 
     fun getVersion() = BuildConfig.VERSION_NAME
 
-    fun rateUsClick() {
-        ViewUtils.openStoreRating(context, context.packageName)
+    fun helpClick() = context.startActivity(Intent(context, HelpNewActivity::class.java))
+
+    fun logoutClick() {
+        environment.logout().execute()
+        ApplicationUtils.startNewDiscoveryActivity(context)
     }
+
+    fun newsletterClick() = context.startActivity(Intent(context, NewsletterActivity::class.java))
+
+    fun notificationsClick() = context.startActivity(Intent(context, NotificationsActivity::class.java))
+
+    fun privacyClick() = context.startActivity(Intent(context, PrivacyActivity::class.java))
+
+    fun rateUsClick() = ViewUtils.openStoreRating(context, context.packageName)
 
 }

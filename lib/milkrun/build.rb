67bec4a2775aebd3345b34b8ac6cac9221589aff
Izzy Oji <eoji11592@gmail.com>
2018-07-13 16:35:45 -0400

    updating output folder of apk when milkrunning

--- lib/milkrun/build.rb
+++ lib/milkrun/build.rb
@@ -31,7 +31,7 @@ module Milkrun
     def path
       raise 'Build has not been compiled yet!' unless assembled?
 
-      File.join(Milkrun.app_dir, "build/outputs/apk/app-#{components.join('-')}.apk")
+      File.join(Milkrun.app_dir, "build/outputs/apk/#{components[0] + components[1]}/#{components[2]}/app-#{components.join('-')}.apk")
     end
 
     def task

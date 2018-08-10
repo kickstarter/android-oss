2323c46c41f7298785de1ea014f0129f8ac560a0
Rashad Cureton <rashad@kickstarter.com>
2018-08-10 15:42:38 -0400

    [Refactor] -removed old settings and refactored the class names (#298)
    * added cells for edit profile and change password
    
    * made rows for account in settings and added account activity layout with rows
    
    * added toolbar layout, account layout, AccountActivity, Updated SettingsNewViewModel
    
    * added edit text and line restrictions
    
    * removed old settings and refactored the class names
    
    * added meta back to manifest and fixed formatting
    
    * removed files
    
    * fixed PR comments
    
    * added settings view model test
    
    * deleted old settings xml
    
    * renamed settings layout class

---
+++ app/src/test/java/com/kickstarter/viewmodels/SettingsViewModelTest.kt
@@ -0,0 +1,49 @@
+package com.kickstarter.viewmodels
+
+import com.kickstarter.KSRobolectricTestCase
+import com.kickstarter.factories.UserFactory
+import com.kickstarter.libs.MockCurrentUser
+import com.kickstarter.models.User
+import org.junit.Test
+import rx.observers.TestSubscriber
+
+class SettingsViewModelTest : KSRobolectricTestCase() {
+
+    private lateinit var vm: SettingsViewModel.ViewModel
+    private val currentUserTest = TestSubscriber<User>()
+    private val showConfirmLogoutPrompt = TestSubscriber<Boolean>()
+
+    private fun setUpEnvironment(user: User) {
+        val currentUser = MockCurrentUser(user)
+        val environment = environment().toBuilder()
+                .currentUser(currentUser)
+                .build()
+
+        currentUser.observable().subscribe(this.currentUserTest)
+
+        this.vm = SettingsViewModel.ViewModel(environment)
+        this.vm.outputs.showConfirmLogoutPrompt().subscribe(this.showConfirmLogoutPrompt)
+    }
+
+    @Test
+    fun testUserEmits() {
+        val user = UserFactory.user()
+
+        setUpEnvironment(user)
+
+        this.currentUserTest.assertValues(user)
+        this.vm.outputs.showConfirmLogoutPrompt().subscribe(showConfirmLogoutPrompt)
+    }
+
+    @Test
+    fun testLogoutConfirmation() {
+        val user = UserFactory.user()
+
+        setUpEnvironment(user)
+
+        this.currentUserTest.assertValues(user)
+
+        this.vm.inputs.logoutClicked()
+        this.showConfirmLogoutPrompt.assertValue(true)
+    }
+}

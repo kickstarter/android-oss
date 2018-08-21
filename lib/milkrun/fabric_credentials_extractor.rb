1e65fd06eb5c94ec47bc01c1936c2c275bef0c62
Rashad Cureton <rashad@kickstarter.com>
2018-08-21 17:57:06 -0400

    Added Credentials to Fastlane for Internal Builds (#315)
    * added secret file
    
    * added extractor
    
    * Updating fabric creds extractor to have helper methods that return key and secret. Providing those values inline to the crashlytics method.
    
    * added slack extractor
    
    * updated git igonore
    
    * removed sample
    
    * added fallback
    
    * updated external lane
    
    * updated make file
    
    * fixed pr comments

--- lib/milkrun/fabric_credentials_extractor.rb
+++ lib/milkrun/fabric_credentials_extractor.rb
@@ -1,9 +1,17 @@
 module Milkrun
   class FabricCredentialsExtractor
 
-    def creds
+    def properties
       lines = File.readlines("../app/fabric.properties")
-      cred = lines.map { |line| line.strip.split('=') }.to_h
+      properties = lines.map { |line| line.strip.split('=') }.to_h
+    end
+
+    def api_key
+      properties["apiKey"]
+    end
+
+    def api_secret
+      properties["apiSecret"]
     end
   end
 end

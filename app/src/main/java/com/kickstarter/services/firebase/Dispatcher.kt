3b3a20d3281133d58124f1f299164fa0c8f4188b
Rashad Cureton <rashad.cureton@gmail.com>
2018-09-07 16:44:23 -0400

    added retry strategy

--- app/src/main/java/com/kickstarter/services/firebase/Dispatcher.kt
+++ app/src/main/java/com/kickstarter/services/firebase/Dispatcher.kt
@@ -1,16 +1,14 @@
 package com.kickstarter.services.firebase
 
 import android.content.Context
-import com.firebase.jobdispatcher.Constraint
-import com.firebase.jobdispatcher.FirebaseJobDispatcher
-import com.firebase.jobdispatcher.GooglePlayDriver
-import com.firebase.jobdispatcher.JobService
+import com.firebase.jobdispatcher.*
 
 fun dispatchJob(context: Context, serviceClas: Class<out JobService>, jobName: String) {
     val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(context))
     val job = dispatcher.newJobBuilder()
             .setConstraints(Constraint.ON_ANY_NETWORK)
             .setService(serviceClas)
+            .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
             .setTag(jobName)
             .build()
 

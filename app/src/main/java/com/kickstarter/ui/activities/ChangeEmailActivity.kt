6a649dc82d2a39c2615b95dfc639175ac5159bdc
Rashad Cureton <rashad@kickstarter.com>
2018-09-13 10:41:56 -0400

    [Enhancement] - Completed Change Email screen layout (#329)
    * completed screen for change email
    
    * added proper dimens from dimens.xml
    
    * fixed pr feedback

---
+++ app/src/main/java/com/kickstarter/ui/activities/ChangeEmailActivity.kt
@@ -0,0 +1,13 @@
+package com.kickstarter.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kickstarter.R
+
+class ChangeEmailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_email)
+    }
+}

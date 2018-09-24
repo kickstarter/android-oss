73abbb979d36e99b8c5f647e232945cee4023a1a
Rashad Cureton <rashad.cureton@gmail.com>
2018-09-24 14:32:45 -0400

    fixed circle errors

--- app/src/main/java/com/kickstarter/extensions/SnackbarExt.kt
+++ app/src/main/java/com/kickstarter/extensions/SnackbarExt.kt
@@ -2,6 +2,7 @@ package com.kickstarter.extensions
 
 import android.content.Context
 import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewCompat
 import android.view.ViewGroup
 import com.kickstarter.R
@@ -10,6 +11,6 @@ fun Snackbar.success(context: Context) {
     val params = this.view.layoutParams as ViewGroup.MarginLayoutParams
     params.setMargins(12, 12, 12, 12)
     this.view.layoutParams = params
-    this.view.background = context.getDrawable(R.drawable.change_email_success_bg)
+    this.view.background = ContextCompat.getDrawable(context, R.drawable.change_email_success_bg)
     ViewCompat.setElevation(this.view, 6f)
 }

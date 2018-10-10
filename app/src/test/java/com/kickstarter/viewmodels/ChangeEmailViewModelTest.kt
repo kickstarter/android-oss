86aa602bcb7988a6725ac4c3dc019f4ddf2a98a4
Izzy Oji <izzyoji92@gmail.com>
2018-10-10 11:50:14 -0400

    change email validation (#352)
    Switched to horizontal progress bar below toolbar.
    No longer crashing when user can't connect to the server.
    Added back CustomAdapter for Email because the mutation fails without it. (I wonder if it'll work if we pass in Any)
    Added StringUtils. isValidPassword.
    Added options menu to ChangeEmailActivity with save button that is enabled/disabled when input is valid or network call is in progress.
    Hiding the keyboard when the user clicks save.
    Created data class ChangeEmail so the validation is easier to follow in the view model.
    Moved the start and end margin to the TextInputLayout instead of the TextInputEditText so the error is left aligned.
    Showing error on email input when it loses focus and isn't valid.
    Showing error snackbar. (will be restyled in a different PR)
    Tests ✨

--- app/src/test/java/com/kickstarter/viewmodels/ChangeEmailViewModelTest.kt
+++ app/src/test/java/com/kickstarter/viewmodels/ChangeEmailViewModelTest.kt
@@ -13,31 +13,95 @@ class ChangeEmailViewModelTest : KSRobolectricTestCase() {
 
     private lateinit var vm: ChangeEmailViewModel.ViewModel
 
-    private val email = TestSubscriber<String>()
+    private val currentEmail = TestSubscriber<String>()
+    private val emailErrorIsVisible = TestSubscriber<Boolean>()
     private val error = TestSubscriber<String>()
+    private val progressBarIsVisible = TestSubscriber<Boolean>()
+    private val saveButtonIsEnabled = TestSubscriber<Boolean>()
     private val success = TestSubscriber<Void>()
 
     private fun setUpEnvironment(environment: Environment) {
         this.vm = ChangeEmailViewModel.ViewModel(environment)
 
-        this.vm.errors.error().subscribe(this.error)
-        this.vm.outputs.email().subscribe(this.email)
+        this.vm.outputs.currentEmail().subscribe(this.currentEmail)
+        this.vm.outputs.emailErrorIsVisible().subscribe(this.emailErrorIsVisible)
+        this.vm.outputs.error().subscribe(this.error)
+        this.vm.outputs.progressBarIsVisible().subscribe(this.progressBarIsVisible)
+        this.vm.outputs.saveButtonIsEnabled().subscribe(this.saveButtonIsEnabled)
         this.vm.outputs.success().subscribe(this.success)
     }
 
     @Test
-    fun testEmail() {
+    fun testCurrentEmail() {
         setUpEnvironment(environment().toBuilder().apolloClient(object : MockApolloClient() {
             override fun userPrivacy(): Observable<UserPrivacyQuery.Data> {
-                return Observable.just(UserPrivacyQuery.Data(UserPrivacyQuery.Me("", "", "rashad@test.com", "")))
+                return Observable.just(UserPrivacyQuery.Data(UserPrivacyQuery.Me("", "",
+                        "rashad@test.com", "")))
             }
         }).build())
 
-        this.email.assertValue("rashad@test.com")
+        this.currentEmail.assertValue("rashad@test.com")
     }
 
     @Test
-    fun testEmailMutation() {
+    fun testEmailErrorIsVisible() {
+        setUpEnvironment(environment())
+
+        this.vm.inputs.emailFocus(true)
+        this.vm.inputs.email("izzy@")
+        this.emailErrorIsVisible.assertValue(false)
+        this.vm.inputs.emailFocus(false)
+        this.emailErrorIsVisible.assertValues(false, true)
+    }
+
+    @Test
+    fun testError() {
+        setUpEnvironment(environment().toBuilder().apolloClient(object : MockApolloClient() {
+            override fun userPrivacy(): Observable<UserPrivacyQuery.Data> {
+                return Observable.error(Throwable("beep"))
+            }
+
+            override fun updateUserEmail(email: String, currentPassword: String): Observable<UpdateUserEmailMutation.Data> {
+                return Observable.error(Throwable("boop"))
+            }
+        }).build())
+
+        this.currentEmail.assertNoValues()
+        this.error.assertNoValues()
+
+        this.vm.inputs.email("test@email.com")
+        this.vm.inputs.password("password")
+        this.vm.inputs.updateEmailClicked()
+        this.error.assertValue("boop")
+    }
+
+    @Test
+    fun testProgressBarIsVisible() {
+        setUpEnvironment(environment())
+
+        this.vm.inputs.email("izzy@email.com")
+        this.vm.inputs.password("password")
+        this.vm.inputs.updateEmailClicked()
+        this.progressBarIsVisible.assertValues(true, false)
+    }
+
+    @Test
+    fun testSaveButtonIsEnabled() {
+        setUpEnvironment(environment())
+
+        this.vm.inputs.email("izzy@email.com")
+        this.vm.inputs.password("password")
+        this.saveButtonIsEnabled.assertValue(true)
+        this.vm.inputs.email("izzy@emailcom")
+        this.saveButtonIsEnabled.assertValues(true, false)
+        this.vm.inputs.email("izzy@email.com")
+        this.saveButtonIsEnabled.assertValues(true, false, true)
+        this.vm.inputs.password("passw")
+        this.saveButtonIsEnabled.assertValues(true, false, true, false)
+    }
+
+    @Test
+    fun testSuccess() {
         setUpEnvironment(environment().toBuilder().apolloClient(object : MockApolloClient() {
             override fun updateUserEmail(email: String, currentPassword: String): Observable<UpdateUserEmailMutation.Data> {
                 return Observable.just(UpdateUserEmailMutation.Data(UpdateUserEmailMutation
@@ -45,9 +109,11 @@ class ChangeEmailViewModelTest : KSRobolectricTestCase() {
             }
         }).build())
 
-        this.email.assertValue("some@email.com")
-        this.vm.inputs.updateEmailClicked("rashad@gmail.com", "somepassword")
-        this.email.assertValues("some@email.com", "rashad@gmail.com")
+        this.currentEmail.assertValue("some@email.com")
+        this.vm.inputs.email("rashad@gmail.com")
+        this.vm.inputs.password("password")
+        this.vm.inputs.updateEmailClicked()
+        this.currentEmail.assertValues("some@email.com", "rashad@gmail.com")
         this.success.assertValueCount(1)
     }
 }

a9689522d13acc67fcf6d2eaef1ef0efd328b6f7
Rashad Cureton <rashad.cureton@gmail.com>
2018-10-24 14:26:35 -0400

    added emailVerified field

--- app/src/main/java/com/kickstarter/mock/services/MockApolloClient.kt
+++ app/src/main/java/com/kickstarter/mock/services/MockApolloClient.kt
@@ -26,7 +26,7 @@ open class MockApolloClient : ApolloClientType {
 
     override fun userPrivacy(): Observable<UserPrivacyQuery.Data> {
         return Observable.just(UserPrivacyQuery.Data(UserPrivacyQuery.Me("", "Some Name",
-                "some@email.com",  "USD")))
+                "some@email.com",  true, "USD")))
     }
 }
 

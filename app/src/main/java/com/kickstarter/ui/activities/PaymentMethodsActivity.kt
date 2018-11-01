886449c858962f5b29d08705c5fb6f7bc5c147cf
Izzy Oji <izzyoji92@gmail.com>
2018-11-01 18:50:31 -0400

    starting NewCardActivity for result and fixing import

--- app/src/main/java/com/kickstarter/ui/activities/PaymentMethodsActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/PaymentMethodsActivity.kt
@@ -1,14 +1,19 @@
 package com.kickstarter.ui.activities
 
 import UserPaymentsQuery
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import com.kickstarter.R
+import com.kickstarter.extensions.showConfirmationSnackbar
+import com.kickstarter.libs.ActivityRequestCodes
 import com.kickstarter.libs.BaseActivity
 import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
 import com.kickstarter.ui.adapters.PaymentMethodsAdapter
 import com.kickstarter.viewmodels.PaymentMethodsViewModel
-import kotlinx.android.synthetic.main.activity_payment_methods.*
+import kotlinx.android.synthetic.main.activity_settings_payment_methods.*
+import kotlinx.android.synthetic.main.payment_methods_toolbar.*
 import rx.android.schedulers.AndroidSchedulers
 
 @RequiresActivityViewModel(PaymentMethodsViewModel.ViewModel::class)
@@ -20,18 +25,27 @@ class PaymentMethodsActivity : BaseActivity<PaymentMethodsViewModel.ViewModel>()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings_payment_methods)
 
-        setupRecyclerView()
+        setUpRecyclerView()
 
         this.viewModel.outputs.getCards()
                 .compose(bindToLifecycle())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { setCards(it) }
 
+        add_new_card.setOnClickListener { startActivityForResult(Intent(this, PaymentMethodsActivity::class.java), ActivityRequestCodes.SAVE_NEW_PAYMENT_METHOD) }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if (requestCode == ActivityRequestCodes.SAVE_NEW_PAYMENT_METHOD && resultCode == Activity.RESULT_OK) {
+            showConfirmationSnackbar(payment_methods_toolbar, R.string.Got_it_your_changes_have_been_saved)
+        }
     }
 
     private fun setCards(cards: MutableList<UserPaymentsQuery.Node>) = this.adapter.populateCards(cards)
 
-    private fun setupRecyclerView() {
+    private fun setUpRecyclerView() {
         this.adapter = PaymentMethodsAdapter(this.viewModel)
         recycler_view.adapter = this.adapter
         recycler_view.layoutManager = LinearLayoutManager(this)

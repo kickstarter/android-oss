874d4a8d4346050045cf1ae763f08bbe22872788
Izzy Oji <izzyoji92@gmail.com>
2018-11-01 18:01:49 -0400

    merging in settings-v3 and adding fake stripe keys

--- app/src/main/java/com/kickstarter/mock/services/MockApolloClient.kt
+++ app/src/main/java/com/kickstarter/mock/services/MockApolloClient.kt
@@ -8,17 +8,14 @@ import UserPaymentsQuery
 import UserPrivacyQuery
 import com.kickstarter.services.ApolloClientType
 import rx.Observable
-import type.CreditCardPaymentType
-import type.CreditCardState
-import type.CreditCardTypes
-import type.CurrencyCode
-import type.PaymentTypes
+import type.*
+import java.util.*
 
 open class MockApolloClient : ApolloClientType {
     override fun getStoredCards(): Observable<UserPaymentsQuery.Data> {
         return Observable.just(UserPaymentsQuery.Data(UserPaymentsQuery.Me("",
-                UserPaymentsQuery.StoredCards("", List<UserPaymentsQuery.Node>(1
-                ) { _ -> UserPaymentsQuery.Node("","4333", Date(), "1234",
+                UserPaymentsQuery.StoredCards("", List(1)
+                { _ -> UserPaymentsQuery.Node("","4333", Date(), "1234",
                         CreditCardState.ACTIVE, CreditCardPaymentType.CREDIT_CARD, CreditCardTypes.VISA )}))))
     }
 

ebc5e6e49a492b4db837de0925104760177acb05
Rashad Cureton <rashad.cureton@gmail.com>
2018-11-14 11:38:27 -0500

    fixed reward viewmodel test

--- app/src/main/java/com/kickstarter/viewmodels/RewardViewModel.java
+++ app/src/main/java/com/kickstarter/viewmodels/RewardViewModel.java
@@ -129,7 +129,7 @@ public interface RewardViewModel {
       this.ksCurrency = environment.ksCurrency();
 
       final Observable<String> formattedMinimum = this.projectAndReward
-        .map(pr -> this.ksCurrency.formatWithUserPreference(pr.second.minimum(), pr.first, RoundingMode.DOWN, pr.first.currency()));
+        .map(pr -> this.ksCurrency.formatWithUserPreference(pr.second.minimum(), pr.first, RoundingMode.UP, pr.first.currency()));
 
       final Observable<Boolean> isSelectable = this.projectAndReward
         .map(pr -> isSelectable(pr.first, pr.second));

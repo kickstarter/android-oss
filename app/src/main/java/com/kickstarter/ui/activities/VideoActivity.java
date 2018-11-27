863de959455584a289363c635f9ee03728cfdf89
Izzy Oji <izzyoji92@gmail.com>
2018-11-27 13:36:28 -0500

    cleaning up subtitles stuff

--- app/src/main/java/com/kickstarter/ui/activities/VideoActivity.java
+++ app/src/main/java/com/kickstarter/ui/activities/VideoActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.google.android.exoplayer2.C;
@@ -42,7 +41,6 @@ public final class VideoActivity extends BaseActivity<VideoViewModel.ViewModel> 
   private long playerPosition;
   private DefaultTrackSelector trackSelector;
 
-  protected @Bind(R.id.closed_captions) Button closedCaptionsButton;
   protected @Bind(R.id.video_player_layout) View rootView;
   protected @Bind(R.id.player_view) PlayerView playerView;
   protected @Bind(R.id.loading_indicator) ProgressBar loadingIndicatorProgressBar;

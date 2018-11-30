8500ed825f6fcc6a97f7628d7478917b1e62cc35
Izzy Oji <izzyoji92@gmail.com>
2018-11-30 15:56:05 -0500

    YAY TESTS ARE PASSING. Fixed bug where we never sent info about logged in user. Added missing fields to updates tracking. Removed prefixes from disco tracking. Alphabetizing.

--- app/src/test/java/com/kickstarter/libs/KoalaTest.kt
+++ app/src/test/java/com/kickstarter/libs/KoalaTest.kt
@@ -5,10 +5,9 @@ import com.kickstarter.mock.factories.CategoryFactory
 import com.kickstarter.mock.factories.LocationFactory
 import com.kickstarter.mock.factories.ProjectFactory
 import com.kickstarter.mock.factories.UserFactory
-import com.kickstarter.models.Project
 import com.kickstarter.models.User
+import com.kickstarter.services.DiscoveryParams
 import org.joda.time.DateTime
-import org.junit.Assert
 import org.junit.Test
 import rx.subjects.BehaviorSubject
 
@@ -44,10 +43,85 @@ class KoalaTest : KSRobolectricTestCase() {
 
         assertDefaultProperties(user)
         val expectedProperties = propertiesTest.value
-        Assert.assertEquals(15L, expectedProperties["user_uid"])
-        Assert.assertEquals(3, expectedProperties["user_backed_projects_count"])
-        Assert.assertEquals(2, expectedProperties["user_created_projects_count"])
-        Assert.assertEquals(10, expectedProperties["user_starred_projects_count"])
+        assertEquals(15L, expectedProperties["user_uid"])
+        assertEquals(3, expectedProperties["user_backed_projects_count"])
+        assertEquals(2, expectedProperties["user_created_projects_count"])
+        assertEquals(10, expectedProperties["user_starred_projects_count"])
+    }
+
+    @Test
+    fun testDiscoveryProperties() {
+        val user = user()
+        val client = MockTrackingClient(MockCurrentUser(user))
+        client.eventNames.subscribe(this.koalaTest)
+        client.eventProperties.subscribe(this.propertiesTest)
+        val koala = Koala(client)
+
+        val params = DiscoveryParams.builder().staffPicks(true).category(CategoryFactory.artCategory()).build()
+
+        koala.trackDiscovery(params, false)
+
+        assertDefaultProperties(user)
+        val expectedProperties = propertiesTest.value
+        assertEquals(1L, expectedProperties["discover_category_id"])
+        assertEquals(false, expectedProperties["discover_recommended"])
+        assertEquals(false, expectedProperties["discover_social"])
+        assertEquals(true, expectedProperties["discover_staff_picks"])
+        assertEquals(false, expectedProperties["discover_starred"])
+        assertEquals(null, expectedProperties["discover_term"])
+        assertEquals(false, expectedProperties["discover_everything"])
+        assertEquals(1, expectedProperties["page"])
+        assertEquals(15, expectedProperties["per_page"])
+    }
+
+    @Test
+    fun testDiscoveryProperties_AllProjects() {
+        val user = user()
+        val client = MockTrackingClient(MockCurrentUser(user))
+        client.eventNames.subscribe(this.koalaTest)
+        client.eventProperties.subscribe(this.propertiesTest)
+        val koala = Koala(client)
+
+        val params = DiscoveryParams.builder().build()
+
+        koala.trackDiscovery(params, false)
+
+        assertDefaultProperties(user)
+        val expectedProperties = propertiesTest.value
+        assertNull(expectedProperties["discover_category_id"])
+        assertEquals(false, expectedProperties["discover_recommended"])
+        assertEquals(false, expectedProperties["discover_social"])
+        assertEquals(false, expectedProperties["discover_staff_picks"])
+        assertEquals(false, expectedProperties["discover_starred"])
+        assertEquals(null, expectedProperties["discover_term"])
+        assertEquals(true, expectedProperties["discover_everything"])
+        assertEquals(1, expectedProperties["page"])
+        assertEquals(15, expectedProperties["per_page"])
+    }
+
+    @Test
+    fun testDiscoveryProperties_NoCategory() {
+        val user = user()
+        val client = MockTrackingClient(MockCurrentUser(user))
+        client.eventNames.subscribe(this.koalaTest)
+        client.eventProperties.subscribe(this.propertiesTest)
+        val koala = Koala(client)
+
+        val params = DiscoveryParams.builder().staffPicks(true).build()
+
+        koala.trackDiscovery(params, false)
+
+        assertDefaultProperties(user)
+        val expectedProperties = propertiesTest.value
+        assertNull(expectedProperties["discover_category_id"])
+        assertEquals(false, expectedProperties["discover_recommended"])
+        assertEquals(false, expectedProperties["discover_social"])
+        assertEquals(true, expectedProperties["discover_staff_picks"])
+        assertEquals(false, expectedProperties["discover_starred"])
+        assertEquals(null, expectedProperties["discover_term"])
+        assertEquals(false, expectedProperties["discover_everything"])
+        assertEquals(1, expectedProperties["page"])
+        assertEquals(15, expectedProperties["per_page"])
     }
 
     @Test
@@ -80,9 +154,9 @@ class KoalaTest : KSRobolectricTestCase() {
         assertDefaultProperties(user)
         assertProjectProperties()
         val expectedProperties = propertiesTest.value
-        Assert.assertEquals(false, expectedProperties["user_is_project_creator"])
-        Assert.assertEquals(false, expectedProperties["user_is_backer"])
-        Assert.assertEquals(false, expectedProperties["user_has_starred"])
+        assertEquals(false, expectedProperties["user_is_project_creator"])
+        assertEquals(false, expectedProperties["user_is_backer"])
+        assertEquals(false, expectedProperties["user_has_starred"])
 
         this.koalaTest.assertValues("Project Page", "Viewed Project Page")
     }
@@ -101,9 +175,30 @@ class KoalaTest : KSRobolectricTestCase() {
         assertDefaultProperties(user)
         assertProjectProperties()
         val expectedProperties = propertiesTest.value
-        Assert.assertEquals(false, expectedProperties["user_is_project_creator"])
-        Assert.assertEquals(true, expectedProperties["user_is_backer"])
-        Assert.assertEquals(false, expectedProperties["user_has_starred"])
+        assertEquals(false, expectedProperties["user_is_project_creator"])
+        assertEquals(true, expectedProperties["user_is_backer"])
+        assertEquals(false, expectedProperties["user_has_starred"])
+
+        this.koalaTest.assertValues("Project Page", "Viewed Project Page")
+    }
+
+    @Test
+    fun testProjectProperties_LoggedInUser_IsProjectCreator() {
+        val project = project().toBuilder().build()
+        val creator = creator()
+        val client = MockTrackingClient(MockCurrentUser(creator))
+        client.eventNames.subscribe(this.koalaTest)
+        client.eventProperties.subscribe(this.propertiesTest)
+        val koala = Koala(client)
+
+        koala.trackProjectShow(project, RefTag.discovery(), RefTag.recommended())
+
+        assertDefaultProperties(creator)
+        assertProjectProperties()
+        val expectedProperties = propertiesTest.value
+        assertEquals(true, expectedProperties["user_is_project_creator"])
+        assertEquals(false, expectedProperties["user_is_backer"])
+        assertEquals(false, expectedProperties["user_has_starred"])
 
         this.koalaTest.assertValues("Project Page", "Viewed Project Page")
     }
@@ -122,76 +217,81 @@ class KoalaTest : KSRobolectricTestCase() {
         assertDefaultProperties(user)
         assertProjectProperties()
         val expectedProperties = propertiesTest.value
-        Assert.assertEquals(false, expectedProperties["user_is_project_creator"])
-        Assert.assertEquals(false, expectedProperties["user_is_backer"])
-        Assert.assertEquals(true, expectedProperties["user_has_starred"])
+        assertEquals(false, expectedProperties["user_is_project_creator"])
+        assertEquals(false, expectedProperties["user_is_backer"])
+        assertEquals(true, expectedProperties["user_has_starred"])
 
         this.koalaTest.assertValues("Project Page", "Viewed Project Page")
     }
 
     private fun assertDefaultProperties(user: User?) {
         val expectedProperties = propertiesTest.value
-        Assert.assertEquals(false, expectedProperties["android_pay_capable"])
-        Assert.assertEquals("uuid", expectedProperties["android_uuid"])
-        Assert.assertEquals("9.9.9", expectedProperties["app_version"])
-        Assert.assertEquals("Google", expectedProperties["brand"])
-        Assert.assertEquals("android", expectedProperties["client_platform"])
-        Assert.assertEquals("native", expectedProperties["client_type"])
-        Assert.assertEquals("uuid", expectedProperties["device_fingerprint"])
-        Assert.assertEquals("phone", expectedProperties["device_format"])
-        Assert.assertEquals("portrait", expectedProperties["device_orientation"])
-        Assert.assertEquals("uuid", expectedProperties["distinct_id"])
-        Assert.assertEquals("unavailable", expectedProperties["google_play_services"])
-        Assert.assertEquals("kickstarter_android", expectedProperties["koala_lib"])
-        Assert.assertEquals("Google", expectedProperties["manufacturer"])
-        Assert.assertEquals("Pixel 3", expectedProperties["model"])
-        Assert.assertEquals("android", expectedProperties["mp_lib"])
-        Assert.assertEquals("Android", expectedProperties["os"])
-        Assert.assertEquals("9", expectedProperties["os_version"])
-        Assert.assertEquals(DateTime.parse("2018-11-02T18:42:05Z").millis, expectedProperties["time"])
-        Assert.assertEquals(user != null, expectedProperties["user_logged_in"])
+        assertEquals(false, expectedProperties["android_pay_capable"])
+        assertEquals("uuid", expectedProperties["android_uuid"])
+        assertEquals("9.9.9", expectedProperties["app_version"])
+        assertEquals("Google", expectedProperties["brand"])
+        assertEquals("android", expectedProperties["client_platform"])
+        assertEquals("native", expectedProperties["client_type"])
+        assertEquals("uuid", expectedProperties["device_fingerprint"])
+        assertEquals("phone", expectedProperties["device_format"])
+        assertEquals("portrait", expectedProperties["device_orientation"])
+        assertEquals("uuid", expectedProperties["distinct_id"])
+        assertEquals("unavailable", expectedProperties["google_play_services"])
+        assertEquals("kickstarter_android", expectedProperties["koala_lib"])
+        assertEquals("Google", expectedProperties["manufacturer"])
+        assertEquals("Pixel 3", expectedProperties["model"])
+        assertEquals("android", expectedProperties["mp_lib"])
+        assertEquals("Android", expectedProperties["os"])
+        assertEquals("9", expectedProperties["os_version"])
+        assertEquals(DateTime.parse("2018-11-02T18:42:05Z").millis, expectedProperties["time"])
+        assertEquals(user != null, expectedProperties["user_logged_in"])
     }
 
     private fun assertProjectProperties() {
         val expectedProperties = propertiesTest.value
-        Assert.assertEquals(100, expectedProperties["project_backers_count"])
-        Assert.assertEquals("US", expectedProperties["project_country"])
-        Assert.assertEquals("USD", expectedProperties["project_currency"])
-        Assert.assertEquals(100f, expectedProperties["project_goal"])
-        Assert.assertEquals(true, expectedProperties["project_has_video"])
-        Assert.assertEquals(4L, expectedProperties["project_pid"])
-        Assert.assertEquals(50f, expectedProperties["project_pledged"])
-        Assert.assertEquals(.5f, expectedProperties["project_percent_raised"])
-        Assert.assertEquals("Ceramics", expectedProperties["project_category"])
-        Assert.assertEquals("Art", expectedProperties["project_parent_category"])
-        Assert.assertEquals("Brooklyn", expectedProperties["project_location"])
-        Assert.assertEquals(10 * 24, expectedProperties["project_hours_remaining"])
-        Assert.assertEquals(60 * 60 * 24 * 20, expectedProperties["project_duration"])
-        Assert.assertEquals("discovery", expectedProperties["ref_tag"])
-        Assert.assertEquals("recommended", expectedProperties["referrer_credit"])
-        Assert.assertEquals(3L, expectedProperties["creator_uid"])
-        Assert.assertEquals(17, expectedProperties["creator_backed_projects_count"])
-        Assert.assertEquals(5, expectedProperties["creator_created_projects_count"])
-        Assert.assertEquals(2, expectedProperties["creator_starred_projects_count"])
+        assertEquals(100, expectedProperties["project_backers_count"])
+        assertEquals("US", expectedProperties["project_country"])
+        assertEquals("USD", expectedProperties["project_currency"])
+        assertEquals(100f, expectedProperties["project_goal"])
+        assertEquals(true, expectedProperties["project_has_video"])
+        assertEquals(4L, expectedProperties["project_pid"])
+        assertEquals(50f, expectedProperties["project_pledged"])
+        assertEquals(.5f, expectedProperties["project_percent_raised"])
+        assertEquals("Ceramics", expectedProperties["project_category"])
+        assertEquals("Art", expectedProperties["project_parent_category"])
+        assertEquals("Brooklyn", expectedProperties["project_location"])
+        assertEquals(10 * 24, expectedProperties["project_hours_remaining"])
+        assertEquals(60 * 60 * 24 * 20, expectedProperties["project_duration"])
+        assertEquals("discovery", expectedProperties["ref_tag"])
+        assertEquals("recommended", expectedProperties["referrer_credit"])
+        assertEquals(3L, expectedProperties["creator_uid"])
+        assertEquals(17, expectedProperties["creator_backed_projects_count"])
+        assertEquals(5, expectedProperties["creator_created_projects_count"])
+        assertEquals(2, expectedProperties["creator_starred_projects_count"])
     }
 
-    private fun project(): Project {
-        return ProjectFactory.project().toBuilder()
-                .id(4)
-                .category(CategoryFactory.ceramicsCategory())
-                .location(LocationFactory.unitedStates())
-                .creator(UserFactory.creator().toBuilder().id(3).backedProjectsCount(17).starredProjectsCount(2).build())
-                .build()
-    }
+    private fun project() =
+            ProjectFactory.project().toBuilder()
+                    .id(4)
+                    .category(CategoryFactory.ceramicsCategory())
+                    .location(LocationFactory.unitedStates())
+                    .creator(creator())
+                    .build()
 
-    private fun user(): User {
-        return UserFactory.user()
-                .toBuilder()
-                .id(15)
-                .backedProjectsCount(3)
-                .createdProjectsCount(2)
-                .starredProjectsCount(10)
-                .build()
-    }
+    private fun creator() =
+            UserFactory.creator().toBuilder()
+                    .id(3)
+                    .backedProjectsCount(17)
+                    .starredProjectsCount(2)
+                    .build()
+
+    private fun user() =
+            UserFactory.user()
+                    .toBuilder()
+                    .id(15)
+                    .backedProjectsCount(3)
+                    .createdProjectsCount(2)
+                    .starredProjectsCount(10)
+                    .build()
 
 }

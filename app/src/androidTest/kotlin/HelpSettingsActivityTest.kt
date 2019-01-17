5aba3a9a35e61e0aa8b1e3f1bafb84006e6db14f
Rashad Cureton <rashad.cureton@gmail.com>
2019-01-17 17:30:15 -0500

    still writing tests just need to fix the double launch

--- app/src/androidTest/kotlin/HelpSettingsActivityTest.kt
+++ app/src/androidTest/kotlin/HelpSettingsActivityTest.kt
@@ -1,6 +1,5 @@
 
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import androidx.browser.customtabs.CustomTabsIntent
@@ -10,7 +9,6 @@ import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.kickstarter.R
-import com.kickstarter.ui.activities.HelpActivity
 import com.kickstarter.ui.activities.HelpSettingsActivity
 import junit.framework.Assert.*
 import org.hamcrest.Matchers.allOf
@@ -28,6 +26,10 @@ class HelpSettingsActivityTest {
 
     private val events = Events()
     private val checkThat = Matchers()
+    private val COOKIES = "https://kickstarter.com/cookies"
+    private val HELP = "https://kickstarter.com/help"
+    private val PRIVACY = "https://kickstarter.com/privacy"
+    private val TERMS = "https://kickstarter.com/terms-of-use"
 
     @Rule
     @JvmField
@@ -53,15 +55,25 @@ class HelpSettingsActivityTest {
     @Test
     fun testCookiePolicyClick() {
         events.clickOnView(R.id.cookie_policy)
-        checkThat.nextOpenActivityIs(HelpActivity.CookiePolicy::class.java)
+        val uri = Uri.parse("https://kickstarter.com/cookies")
+        val customTabsIntent = CustomTabsIntent.Builder().build()
+        customTabsIntent.launchUrl(activityRule.activity, uri)
+
+        intended(allOf(
+                IntentMatchers.hasAction(Intent.ACTION_VIEW),
+                IntentMatchers.hasData(COOKIES)))
     }
 
     @Test
     fun testHelpCenterClick() {
         events.clickOnView(R.id.help_center)
+        val uri = Uri.parse("https://kickstarter.com/help")
+        val customTabsIntent = CustomTabsIntent.Builder().build()
+        customTabsIntent.launchUrl(activityRule.activity, uri)
+
         intended(allOf(
                 IntentMatchers.hasAction(Intent.ACTION_VIEW),
-                IntentMatchers.hasData("https://kickstarter.com/help")))
+                IntentMatchers.hasData(HELP)))
     }
 
     @Test
@@ -83,18 +95,31 @@ class HelpSettingsActivityTest {
 
         intended(allOf(
                 IntentMatchers.hasAction(Intent.ACTION_VIEW),
-                IntentMatchers.hasData("https://kickstarter.com/privacy")))
+                IntentMatchers.hasData(PRIVACY)))
     }
 
     @Test
     fun testTermsClick() {
         events.clickOnView(R.id.terms_of_use)
-        checkThat.nextOpenActivityIs(HelpActivity.Terms::class.java)
+        val uri = Uri.parse("https://kickstarter.com/terms-of-use")
+        val customTabsIntent = CustomTabsIntent.Builder().build()
+        customTabsIntent.launchUrl(activityRule.activity, uri)
+        val intent = customTabsIntent.intent
+        assertNotNull(intent)
+        assertNull(customTabsIntent.startAnimationBundle)
+
+        assertEquals(Intent.ACTION_VIEW, intent.action)
+        assertTrue(intent.hasExtra(CustomTabsIntent.EXTRA_SESSION))
+
+
+        intended(allOf(
+                IntentMatchers.hasAction(Intent.ACTION_VIEW),
+                IntentMatchers.hasData(TERMS)))
     }
 
     @Test
     fun testToolbarColor() {
-        val color = Color.RED
+        val color = R.color.primary
         val intent = CustomTabsIntent.Builder().setToolbarColor(color).build().intent
         assertTrue(intent.hasExtra(CustomTabsIntent.EXTRA_TOOLBAR_COLOR))
         assertEquals(color, intent.getIntExtra(CustomTabsIntent.EXTRA_TOOLBAR_COLOR, 0))

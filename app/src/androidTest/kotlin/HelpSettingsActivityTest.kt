de20819bcfa8ddd71071c70d1c879fc9cafcf71b
Rashad Cureton <rashad.cureton@gmail.com>
2019-01-18 13:47:24 -0500

    fixed tests for the intents

--- app/src/androidTest/kotlin/HelpSettingsActivityTest.kt
+++ app/src/androidTest/kotlin/HelpSettingsActivityTest.kt
@@ -1,7 +1,6 @@
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
@@ -10,7 +9,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.kickstarter.R
 import com.kickstarter.ui.activities.HelpSettingsActivity
-import junit.framework.Assert.*
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
 import org.hamcrest.Matchers.allOf
 import org.junit.After
 import org.junit.Before
@@ -25,7 +25,6 @@ import utils.Matchers
 class HelpSettingsActivityTest {
 
     private val events = Events()
-    private val checkThat = Matchers()
     private val COOKIES = "https://kickstarter.com/cookies"
     private val HELP = "https://kickstarter.com/help"
     private val PRIVACY = "https://kickstarter.com/privacy"
@@ -82,16 +81,6 @@ class HelpSettingsActivityTest {
         val uri = Uri.parse("https://kickstarter.com/privacy")
         val customTabsIntent = CustomTabsIntent.Builder().build()
         customTabsIntent.launchUrl(activityRule.activity, uri)
-        val intent = customTabsIntent.intent
-        assertNotNull(intent)
-        assertNull(customTabsIntent.startAnimationBundle)
-
-        assertEquals(Intent.ACTION_VIEW, intent.action)
-        assertTrue(intent.hasExtra(CustomTabsIntent.EXTRA_SESSION))
-        if (Build.VERSION.SDK_INT >= 18) {
-            assertNull(intent.extras.getBinder(CustomTabsIntent.EXTRA_SESSION))
-        }
-        assertNull(intent.component)
 
         intended(allOf(
                 IntentMatchers.hasAction(Intent.ACTION_VIEW),
@@ -104,13 +93,6 @@ class HelpSettingsActivityTest {
         val uri = Uri.parse("https://kickstarter.com/terms-of-use")
         val customTabsIntent = CustomTabsIntent.Builder().build()
         customTabsIntent.launchUrl(activityRule.activity, uri)
-        val intent = customTabsIntent.intent
-        assertNotNull(intent)
-        assertNull(customTabsIntent.startAnimationBundle)
-
-        assertEquals(Intent.ACTION_VIEW, intent.action)
-        assertTrue(intent.hasExtra(CustomTabsIntent.EXTRA_SESSION))
-
 
         intended(allOf(
                 IntentMatchers.hasAction(Intent.ACTION_VIEW),

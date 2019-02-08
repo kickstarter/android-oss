49ad35511bc4de94f83f9011c48129ea0d8afd86
Izzy Oji <izzyoji92@gmail.com>
2019-02-08 14:34:48 -0500

    wip headers and dividers

--- app/src/main/java/com/kickstarter/ui/adapters/DiscoveryDrawerAdapter.java
+++ app/src/main/java/com/kickstarter/ui/adapters/DiscoveryDrawerAdapter.java
@@ -8,6 +8,7 @@ import com.kickstarter.ui.adapters.data.NavigationDrawerData;
 import com.kickstarter.ui.viewholders.EmptyViewHolder;
 import com.kickstarter.ui.viewholders.KSViewHolder;
 import com.kickstarter.ui.viewholders.discoverydrawer.ChildFilterViewHolder;
+import com.kickstarter.ui.viewholders.discoverydrawer.HeaderViewHolder;
 import com.kickstarter.ui.viewholders.discoverydrawer.LoggedInViewHolder;
 import com.kickstarter.ui.viewholders.discoverydrawer.LoggedOutViewHolder;
 import com.kickstarter.ui.viewholders.discoverydrawer.ParentFilterViewHolder;
@@ -62,8 +63,10 @@ public class DiscoveryDrawerAdapter extends KSAdapter {
       } else {
         return R.layout.discovery_drawer_child_filter_view;
       }
+    } else if (datum instanceof Integer) {
+      return R.layout.discovery_drawer_header;
     }
-    return R.layout.discovery_drawer_divider_view;
+    return R.layout.horizontal_line_1dp_view;
   }
 
   @Override
@@ -73,7 +76,7 @@ public class DiscoveryDrawerAdapter extends KSAdapter {
     if (object == null) {
       return null;
     }
-    if (object instanceof User) {
+    if (object instanceof User || object instanceof Integer) {
       return object;
     }
 
@@ -106,6 +109,8 @@ public class DiscoveryDrawerAdapter extends KSAdapter {
         return new TopFilterViewHolder(view, this.delegate);
       case R.layout.discovery_drawer_child_filter_view:
         return new ChildFilterViewHolder(view, this.delegate);
+      case R.layout.discovery_drawer_header:
+        return new HeaderViewHolder(view);
       case R.layout.discovery_drawer_divider_view:
       default:
         return new EmptyViewHolder(view);
@@ -123,6 +128,10 @@ public class DiscoveryDrawerAdapter extends KSAdapter {
     final List<List<Object>> newSections = new ArrayList<>();
 
     newSections.add(Collections.singletonList(data.user()));
+
+    newSections.add(Collections.singletonList(null)); // Divider
+
+    newSections.add(Collections.singletonList(R.string.Collections)); // Divider
 
     final List<NavigationDrawerData.Section> topFilterSections = Observable.from(data.sections())
       .filter(NavigationDrawerData.Section::isTopFilter)

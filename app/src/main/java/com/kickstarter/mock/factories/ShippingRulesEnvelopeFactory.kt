d8fb59bfe0005d234ce9623fcf4e5cca91705c59
Rashad Cureton <rashad.cureton@gmail.com>
2019-04-23 15:44:18 -0400

    added tests to pledgefragment

---
+++ app/src/main/java/com/kickstarter/mock/factories/ShippingRulesEnvelopeFactory.kt
@@ -0,0 +1,18 @@
+package com.kickstarter.mock.factories
+
+import com.kickstarter.models.ShippingRule
+import com.kickstarter.services.apiresponses.ShippingRulesEnvelope
+
+class ShippingRulesEnvelopeFactory private constructor() {
+
+    companion object {
+
+        fun shippingRuleEvelope(): ShippingRulesEnvelope {
+            return listOf(ShippingRule.builder()
+                    .cost(30.00)
+                    .id(1)
+                    .location(LocationFactory.unitedStates())
+                    .build()) as ShippingRulesEnvelope
+        }
+    }
+}

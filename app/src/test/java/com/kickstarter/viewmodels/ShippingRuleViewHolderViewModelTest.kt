69b92fca49a608a58518dd6b05839e39fad9584c
Rashad Cureton <rashad.cureton@gmail.com>
2019-04-24 14:14:23 -0400

    wrote tests for the shippingruleviewholderviewmodel

--- app/src/test/java/com/kickstarter/viewmodels/ShippingRuleViewHolderViewModelTest.kt
+++ app/src/test/java/com/kickstarter/viewmodels/ShippingRuleViewHolderViewModelTest.kt
@@ -2,6 +2,10 @@ package com.kickstarter.viewmodels
 
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.libs.Environment
+import com.kickstarter.mock.MockCurrentConfig
+import com.kickstarter.mock.factories.ConfigFactory
+import com.kickstarter.mock.factories.ProjectFactory
+import com.kickstarter.mock.factories.ShippingRulesEnvelopeFactory
 import com.kickstarter.models.ShippingRule
 import org.junit.Test
 import rx.observers.TestSubscriber
@@ -24,11 +28,30 @@ class ShippingRuleViewHolderViewModelTest : KSRobolectricTestCase() {
     fun testShippingRule() {
         setupEnvironment(environment())
 
+        val shippingRules = ShippingRulesEnvelopeFactory.shippingRules().shippingRules().single()
+        val project = ProjectFactory.project()
 
+        this.vm.inputs.configureWith(shippingRules, project)
+        this.vm.inputs.shippingRuleClicked()
+        this.shippingRule.assertValueCount(1)
     }
 
     @Test
     fun testShippingRuleText() {
-        setupEnvironment(environment())
+        val config = ConfigFactory.configForUSUser()
+        val currentConfig = MockCurrentConfig()
+        currentConfig.config(config)
+
+        val environment = environment().toBuilder()
+                .currentConfig(currentConfig)
+                .build()
+
+        setupEnvironment(environment)
+
+        val shippingRules = ShippingRulesEnvelopeFactory.shippingRules().shippingRules().single()
+        val project = ProjectFactory.project()
+
+        this.vm.inputs.configureWith(shippingRules, project)
+        this.shippingRuleText.assertValue("Brooklyn, NY $30.00")
     }
 }

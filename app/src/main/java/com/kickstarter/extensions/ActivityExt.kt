58e1c7c7c27389d9d99ea8793052c26bce230eef
Izzy Oji <izzyoji92@gmail.com>
2019-07-30 16:59:37 -0400

    [🔢] Pledge input (#570)
    * - Updated strings
    - Updated `ActivityExt.hideKeyboard` to set proper flag and clear focus after hiding keyboard.
    - Added `NumberUtils.parse`.
    - Moved `KSCurrency.getPrecision` and `KSCurrency.isWholeNumber` to `NumberUtils`.
    - `StringUtils.trim` also trims extra whitespace
    - `ViewUtils.styleCurrency` was split into three methods because the reward screen has a different style from the pledge screen: `RewardViewUtils.styleCurrency`, `ProjectViewUtils.currencySymbolAndPosition`, `ProjectViewUtils.styleCurrency`.
    - `ProjectActivity` hides the keyboard when clicking outside an `EditText`
    - `PledgeFragment` now accepts pledge amounts via the numerical keyboard. A warning is displayed when a pledge is attempted with an invalid number.
    - Moved reward card animation helper methods to the bottom of `PledgeFragment`
    - Updated our `Dialog` background style with `background_dialog.xml`
    - Gave `pledge_amounts_loading_states` a fixed height of `grid_2`
    - Moved the major sections of `fragment_pledge.xml` to their own layout files (`fragment_pledge_section_delivery.xml`, `fragment_pledge_section_payment.xml`, `fragment_pledge_section_pledge_amount.xml`, `fragment_pledge_section_shipping.xml`, `fragment_pledge_section_.total.xml`) so it's much cleaner.
    - TESTS. Nino and I decided to organize the tests with descriptive method names for understandability

--- app/src/main/java/com/kickstarter/extensions/ActivityExt.kt
+++ app/src/main/java/com/kickstarter/extensions/ActivityExt.kt
@@ -21,7 +21,8 @@ fun Activity.startActivityWithSlideLeftTransition(intent: Intent) {
 fun Activity.hideKeyboard() {
     val inputManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     if (inputManager.isAcceptingText) {
-        inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+        currentFocus?.clearFocus()
     }
 }
 

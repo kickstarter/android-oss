524b56a89f472d1a1452f254b057d71eaa391fa6
Izzy Oji <izzyoji92@gmail.com>
2019-09-09 15:08:05 -0400

    [🔙] Reward buttons, take 2 (#603)
    * Backed reward button CTA is now Selected.
    Backed reward buttons are disabled.
    The disabled background for all reward buttons is ksr_grey_500 with default disabled text color.
    Testz.

--- app/src/main/java/com/kickstarter/libs/utils/RewardViewUtils.kt
+++ app/src/main/java/com/kickstarter/libs/utils/RewardViewUtils.kt
@@ -3,8 +3,6 @@ package com.kickstarter.libs.utils
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.RelativeSizeSpan
-import androidx.annotation.ColorRes
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.kickstarter.R
 import com.kickstarter.libs.KSCurrency
@@ -14,48 +12,15 @@ import com.kickstarter.models.Reward
 import java.math.RoundingMode
 
 object RewardViewUtils {
-
-    /**
-     * Returns the drawable resource ID of the check background based on project status.
-     */
-    @DrawableRes
-    fun checkBackgroundDrawable(project: Project): Int {
-        return if (project.isLive) {
-            R.drawable.circle_blue_alpha_6
-        } else {
-            R.drawable.circle_grey_300
-        }
-    }
-
-    /**
-     * Returns the color resource ID of the rewards button based on project and if user has backed reward.
-     */
-    @ColorRes
-    fun pledgeButtonColor(project: Project, reward: Reward): Int {
-        return if (BackingUtils.isBacked(project, reward) && project.isLive) {
-            R.color.button_pledge_manage
-        } else if (!project.isLive) {
-            R.color.button_pledge_ended
-        } else {
-            R.color.button_pledge_live
-        }
-    }
-
     /**
      * Returns the string resource ID of the rewards button based on project and reward status.
      */
     @StringRes
     fun pledgeButtonText(project: Project, reward: Reward): Int {
-        return if (BackingUtils.isBacked(project, reward) && project.isLive) {
-            R.string.Manage_your_pledge
-        } else if (BackingUtils.isBacked(project, reward) && !project.isLive) {
-            R.string.View_your_pledge
-        } else if (RewardUtils.isAvailable(project, reward) && project.isBacking) {
-            R.string.Select_this_instead
-        } else if (!RewardUtils.isAvailable(project, reward)) {
-            R.string.No_longer_available
-        } else {
-            R.string.Select
+        return when {
+            BackingUtils.isBacked(project, reward) -> R.string.Selected
+            RewardUtils.isAvailable(project, reward) -> R.string.Select
+            else -> R.string.No_longer_available
         }
     }
 

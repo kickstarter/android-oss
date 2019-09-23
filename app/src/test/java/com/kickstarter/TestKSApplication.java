531270c9a55cf6658e3ecd32386b7b6cbb196fa7
Izzy Oji <izzyoji92@gmail.com>
2019-09-23 15:41:39 -0400

    removing unnecessary override

--- app/src/test/java/com/kickstarter/TestKSApplication.java
+++ app/src/test/java/com/kickstarter/TestKSApplication.java
@@ -1,10 +1,6 @@
 package com.kickstarter;
 
 public class TestKSApplication extends KSApplication {
-  @Override
-  public void onCreate() {
-    super.onCreate();
-  }
 
   @Override
   public boolean isInUnitTests() {

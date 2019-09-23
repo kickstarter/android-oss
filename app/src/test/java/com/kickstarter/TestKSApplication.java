3288c603f2d416b1f11c2500780ffe1095f36a2f
Izzy Oji <izzyoji92@gmail.com>
2019-09-23 15:41:14 -0400

    Fixed broken tests and updated robolectric

--- app/src/test/java/com/kickstarter/TestKSApplication.java
+++ app/src/test/java/com/kickstarter/TestKSApplication.java
@@ -1,20 +1,9 @@
 package com.kickstarter;
 
-import com.facebook.FacebookSdk;
-import com.google.android.gms.common.ConnectionResult;
-
-import org.robolectric.shadows.gms.ShadowGooglePlayServicesUtil;
-
 public class TestKSApplication extends KSApplication {
   @Override
   public void onCreate() {
-    // TODO: Roboelectric is incompatible with Play Services 7.8.0+.
-    // See issue: https://github.com/robolectric/robolectric/issues/1995
-    // For now we just return that Play Services is disabled.
-    ShadowGooglePlayServicesUtil.setIsGooglePlayServicesAvailable(ConnectionResult.SERVICE_DISABLED);
     super.onCreate();
-
-    FacebookSdk.sdkInitialize(this);
   }
 
   @Override

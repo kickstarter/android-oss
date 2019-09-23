159a16610f20e7e63f782146e8aea2113f92266a
Izzy Oji <izzyoji92@gmail.com>
2019-09-23 15:56:13 -0400

    heh turns out i did need to initialize facebook

--- app/src/test/java/com/kickstarter/TestKSApplication.java
+++ app/src/test/java/com/kickstarter/TestKSApplication.java
@@ -1,6 +1,15 @@
 package com.kickstarter;
 
+import com.facebook.FacebookSdk;
+
 public class TestKSApplication extends KSApplication {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    FacebookSdk.sdkInitialize(this);
+  }
 
   @Override
   public boolean isInUnitTests() {

550165a67db240625dd19eae5b0427428d583ae5
Izzy Oji <izzyoji92@gmail.com>
2019-11-19 10:56:37 -0500

    copy pasta monster got me

--- app/src/external/java/com/kickstarter/ExternalApplicationModule.java
+++ app/src/external/java/com/kickstarter/ExternalApplicationModule.java
@@ -1,15 +1,24 @@
 package com.kickstarter;
 
-import androidx.annotation.NonNull;
-
+import com.apollographql.apollo.ApolloClient;
+import com.google.gson.Gson;
 import com.kickstarter.libs.ApiEndpoint;
 import com.kickstarter.libs.BuildCheck;
 import com.kickstarter.libs.InternalToolsType;
 import com.kickstarter.libs.NoopBuildCheck;
 import com.kickstarter.libs.NoopInternalTools;
+import com.kickstarter.libs.utils.Secrets;
+import com.kickstarter.mock.services.MockApiClient;
+import com.kickstarter.mock.services.MockApolloClient;
+import com.kickstarter.services.ApiClient;
+import com.kickstarter.services.ApiClientType;
+import com.kickstarter.services.ApiService;
+import com.kickstarter.services.ApolloClientType;
+import com.kickstarter.services.KSApolloClient;
 
 import javax.inject.Singleton;
 
+import androidx.annotation.NonNull;
 import dagger.Module;
 import dagger.Provides;
 
--- app/src/internal/java/com/kickstarter/InternalApplicationModule.java
+++ app/src/internal/java/com/kickstarter/InternalApplicationModule.java
@@ -2,6 +2,8 @@ package com.kickstarter;
 
 import android.content.SharedPreferences;
 
+import com.apollographql.apollo.ApolloClient;
+import com.google.gson.Gson;
 import com.kickstarter.libs.ApiEndpoint;
 import com.kickstarter.libs.BuildCheck;
 import com.kickstarter.libs.InternalTools;
@@ -9,6 +11,14 @@ import com.kickstarter.libs.InternalToolsType;
 import com.kickstarter.libs.preferences.StringPreference;
 import com.kickstarter.libs.preferences.StringPreferenceType;
 import com.kickstarter.libs.qualifiers.ApiEndpointPreference;
+import com.kickstarter.libs.utils.Secrets;
+import com.kickstarter.mock.services.MockApiClient;
+import com.kickstarter.mock.services.MockApolloClient;
+import com.kickstarter.services.ApiClient;
+import com.kickstarter.services.ApiClientType;
+import com.kickstarter.services.ApiService;
+import com.kickstarter.services.ApolloClientType;
+import com.kickstarter.services.KSApolloClient;
 
 import javax.inject.Singleton;
 

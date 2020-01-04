7c2103410782aff2f8e6443afac3c3a062a6b443
Izzy Oji <izzyoji92@gmail.com>
2020-01-03 19:01:29 -0500

    whitespace

--- app/src/main/java/com/kickstarter/models/QualtricsIntercept.kt
+++ app/src/main/java/com/kickstarter/models/QualtricsIntercept.kt
@@ -4,8 +4,8 @@ enum class QualtricsIntercept(private val prodId: String, private val testId: St
     NATIVE_APP_FEEDBACK("SI_3VjP7wWiUZg2wBf", "SI_6nSwomRDiWXeXEV");
 
     fun id(packageName: String) :String {
-        return when (packageName){
-             "com.kickstarter.kickstarter" -> prodId
+        return when (packageName) {
+            "com.kickstarter.kickstarter" -> prodId
             else -> testId
         }
     }

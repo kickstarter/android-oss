8fb3cf7d13d54672a71791ec3314b30e1631d5ee
Izzy Oji <izzyoji92@gmail.com>
2020-02-21 11:04:35 -0500

    Changed base to master and fixed tests.

--- app/src/main/java/com/kickstarter/viewmodels/PledgeFragmentViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/PledgeFragmentViewModel.kt
@@ -18,10 +18,7 @@ import com.kickstarter.services.apiresponses.ShippingRulesEnvelope
 import com.kickstarter.services.mutations.CreateBackingData
 import com.kickstarter.services.mutations.UpdateBackingData
 import com.kickstarter.ui.ArgumentsKey
-import com.kickstarter.ui.data.CardState
-import com.kickstarter.ui.data.PledgeData
-import com.kickstarter.ui.data.PledgeFlowContext
-import com.kickstarter.ui.data.PledgeReason
+import com.kickstarter.ui.data.*
 import com.kickstarter.ui.fragments.PledgeFragment
 import com.stripe.android.StripeIntentResult
 import rx.Observable

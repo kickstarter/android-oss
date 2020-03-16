12b254c1a2576df6bf3fa70cf961b42209cbc91b
Izzy Oji <izzyoji92@gmail.com>
2020-03-16 17:02:28 -0400

    ## `FeatureFlagActivity` - Removed native checkout feature flag override ## `activity_feature_flags.xml` - Removed override container ## `strings.xml` - Removed `Native_Checkout` - Deleted `NativeCheckoutPreference` ## Environment - Removed `nativeCheckoutPreference` ## `SharedPreferenceKey` - Removed `NATIVE_CHECKOUT` ## `ProjectHolderViewModel` - Removed unused `nativeCheckoutPreference` ## `ApplicationModule` - Removed `NativeCheckoutPreference` dependency injection

--- app/src/main/java/com/kickstarter/ui/SharedPreferenceKey.java
+++ app/src/main/java/com/kickstarter/ui/SharedPreferenceKey.java
@@ -10,6 +10,5 @@ public final class SharedPreferenceKey {
   public static final String HAS_SEEN_GAMES_NEWSLETTER = "has_seen_games_newsletter";
   public static final String LAST_SEEN_ACTIVITY_ID = "last_seen_activity_id";
   public static final String MESSAGE_THREAD_HAS_UNREAD_MESSAGES = "message_thread_has_unread_messages";
-  public static final String NATIVE_CHECKOUT = "native_checkout";
   public static final String USER = "user";
 }

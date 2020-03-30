c9394631a4b2780f3b9c787a304293123fea43ac
Izzy Oji <izzyoji92@gmail.com>
2020-03-30 11:42:39 -0400

    [⛔️] NT-1050 Adding ErroredBackingViewHolder (#805)
    * ## XML
    - Added layout for `ErroredBacking` `item_errored_backing.xml`
    - Added icon `ic_icon_alert.xml` (it still needs a11y copy)
    ## `ErroredBackingViewHolder`
    - Added inputs `erroredBacking` and `manageButtonClicked`
    - Added outputs `projectFinalCollectionDate`, `projectName`, and `notifyDelegateToStartFixPaymentMethod`
    - Tests
      - Added `ErroredBackingFactory` helper
    
    * PR feedback from Colleen:
    Moved project name and final collection date to same container and moved `Manage` button out.

---
+++ app/src/main/java/com/kickstarter/ui/viewholders/ErroredBackingViewHolder.kt
@@ -0,0 +1,62 @@
+package com.kickstarter.ui.viewholders
+
+import android.view.View
+import com.jakewharton.rxbinding.view.RxView
+import com.kickstarter.libs.RelativeDateTimeOptions
+import com.kickstarter.libs.rx.transformers.Transformers.observeForUI
+import com.kickstarter.libs.utils.DateTimeUtils
+import com.kickstarter.libs.utils.ObjectUtils.requireNonNull
+import com.kickstarter.models.ErroredBacking
+import com.kickstarter.viewmodels.ErroredBackingViewHolderViewModel
+import kotlinx.android.synthetic.main.item_errored_backing.view.*
+import org.joda.time.DateTime
+
+class ErroredBackingViewHolder(private val view: View, val delegate: Delegate?) : KSViewHolder(view) {
+
+    interface Delegate {
+        fun managePledgeClicked(projectSlug: String)
+    }
+
+    private val ksString = environment().ksString()
+    private var viewModel = ErroredBackingViewHolderViewModel.ViewModel(environment())
+
+    init {
+
+        this.viewModel.outputs.notifyDelegateToStartFixPaymentMethod()
+                .compose(bindToLifecycle())
+                .compose(observeForUI())
+                .subscribe { delegate?.managePledgeClicked(it) }
+
+        this.viewModel.outputs.projectFinalCollectionDate()
+                .compose(bindToLifecycle())
+                .compose(observeForUI())
+                .subscribe { setProjectFinaCollectionDateText(it) }
+
+        this.viewModel.outputs.projectName()
+                .compose(bindToLifecycle())
+                .compose(observeForUI())
+                .subscribe { this.view.errored_backing_project_title.text = it }
+
+        RxView.clicks(this.view.errored_backing_manage_button)
+                .compose(bindToLifecycle())
+                .subscribe { this.viewModel.inputs.manageButtonClicked() }
+
+    }
+
+    private fun setProjectFinaCollectionDateText(finalCollectionDate: DateTime) {
+        val options = RelativeDateTimeOptions.builder()
+                .absolute(true)
+                .relativeToDateTime(DateTime.now())
+                .build()
+
+        this.view.errored_backing_project_collection_date.text = DateTimeUtils.relative(context(), this.ksString, finalCollectionDate, options)
+    }
+
+    override fun bindData(data: Any?) {
+        @Suppress("UNCHECKED_CAST")
+        val erroredBacking = requireNonNull(data as ErroredBacking)
+
+        this.viewModel.inputs.configureWith(erroredBacking)
+    }
+
+}

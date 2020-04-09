55d21bdd176cf01fb19ea2266c5421d447a1d0c9
Izzy Oji <izzyoji92@gmail.com>
2020-04-09 16:21:17 -0400

    ## `ToastExt` - :new: shiny helper function to show error `Toast`s ## `PledgeFragment` - Using shiny :new: aforementioned helper function to show pledge errors ## XML - Added `ic_icon_alert.xml` - Created a new layout for the error `Toast`s `toast_error.xml` - Added `Alert` a11y string

---
+++ app/src/main/java/com/kickstarter/extensions/ToastExt.kt
@@ -0,0 +1,23 @@
+package com.kickstarter.extensions
+
+import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.kickstarter.R
+
+fun showErrorToast(applicationContext: Context, root: ViewGroup, message: String) {
+    val layout: ViewGroup = LayoutInflater.from(applicationContext).inflate(R.layout.toast_error, root, false) as ViewGroup
+    val text: TextView = layout.findViewById(R.id.toast_text_view)
+    text.text = message
+    val resources = applicationContext.resources
+    val yOffset = resources.getDimensionPixelSize(R.dimen.ks_toolbar_height) + resources.getDimensionPixelSize(R.dimen.grid_1)
+    with (Toast(applicationContext)) {
+        setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL or Gravity.FILL_HORIZONTAL, 0, yOffset)
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
+    }
+}

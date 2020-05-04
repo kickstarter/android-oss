df755d853c913f08e77ba50c682de2da23220eda
Izzy Oji <izzyoji92@gmail.com>
2020-05-04 17:56:07 -0400

    ## `FeatureFlagsActivity` - Displaying `optimizelyFeatures` in new `optimizely_flags` `RecyclerView` ## `FeatureFlagsViewModel` - Added output `optimizelyFeatures` that emits `OptimizelyExperimentsClient` enabled features - Added test

--- app/src/internal/java/com/kickstarter/viewmodels/FeatureFlagsViewModel.kt
+++ app/src/internal/java/com/kickstarter/viewmodels/FeatureFlagsViewModel.kt
@@ -1,25 +1,29 @@
 package com.kickstarter.viewmodels
 
 import androidx.annotation.NonNull
-import com.kickstarter.libs.ActivityViewModel
-import com.kickstarter.libs.CurrentConfigType
-import com.kickstarter.libs.Environment
+import com.kickstarter.libs.*
 import com.kickstarter.ui.activities.FeatureFlagsActivity
-import com.kickstarter.ui.activities.ProjectActivity
 import rx.Observable
 import rx.subjects.BehaviorSubject
 
 interface FeatureFlagsViewModel {
     interface Inputs
     interface Outputs {
-        fun features(): Observable<List<Pair<String, Boolean>>>
+        /** Emits "android_" prefixed feature flags from the [Config]. */
+        fun configFeatures(): Observable<List<Pair<String, Boolean>>>
+
+        /** Emits [OptimizelyExperimentsClient] feature flags. */
+        fun optimizelyFeatures(): Observable<List<Pair<String, Boolean>>>
     }
 
     class ViewModel(@NonNull val environment: Environment) : ActivityViewModel<FeatureFlagsActivity>(environment), Inputs, Outputs {
 
         private val currentConfig: CurrentConfigType = environment.currentConfig()
+        private val currentUser: CurrentUserType = environment.currentUser()
+        private val optimizely: ExperimentsClientType = environment.optimizely()
 
-        private val features = BehaviorSubject.create<List<Pair<String, Boolean>>>()
+        private val configFeatures = BehaviorSubject.create<List<Pair<String, Boolean>>>()
+        private val optimizelyFeatures = BehaviorSubject.create<List<Pair<String, Boolean>>>()
 
         val inputs: Inputs = this
         val outputs: Outputs = this
@@ -33,9 +37,18 @@ interface FeatureFlagsViewModel {
                     .map { it.sortedBy { entry -> entry.key } }
                     .map { it.map { entry -> Pair(entry.key, entry.value) }.toList() }
                     .compose(bindToLifecycle())
-                    .subscribe(this.features)
+                    .subscribe(this.configFeatures)
+
+            this.currentUser
+                    .observable()
+                    .map { this.optimizely.enabledFeatures(it) }
+                    .map { it.map { entry -> Pair(entry, true) }.toList() }
+                    .compose(bindToLifecycle())
+                    .subscribe(this.optimizelyFeatures)
         }
 
-        override fun features(): Observable<List<Pair<String, Boolean>>> = this.features
+        override fun configFeatures(): Observable<List<Pair<String, Boolean>>> = this.configFeatures
+
+        override fun optimizelyFeatures(): Observable<List<Pair<String, Boolean>>> = this.optimizelyFeatures
     }
 }
--- app/src/testInternal/java/com/kickstarter/viewmodels/FeatureFlagsViewModelTest.kt
+++ app/src/testInternal/java/com/kickstarter/viewmodels/FeatureFlagsViewModelTest.kt
@@ -2,42 +2,61 @@ package com.kickstarter.viewmodels
 
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.mock.MockCurrentConfig
+import com.kickstarter.mock.MockExperimentsClientType
 import com.kickstarter.mock.factories.ConfigFactory
+import com.kickstarter.models.User
 import org.junit.Test
 import rx.observers.TestSubscriber
 
 class FeatureFlagsViewModelTest : KSRobolectricTestCase() {
     private lateinit var vm: FeatureFlagsViewModel.ViewModel
 
-    private val features = TestSubscriber<List<Pair<String, Boolean>>>()
+    private val configFeatures = TestSubscriber<List<Pair<String, Boolean>>>()
+    private val optimizelyFeatures = TestSubscriber<List<Pair<String, Boolean>>>()
 
-    private fun setUpEnvironment(features: Map<String, Boolean>?) {
+    private fun setUpEnvironment(features: Map<String, Boolean>?, optimizelyFeatures: List<String>) {
         val mockConfig = MockCurrentConfig()
         mockConfig.config(ConfigFactory.config().toBuilder()
                 .features(features)
                 .build())
-        val environment = environment().toBuilder().currentConfig(mockConfig).build()
+        val environment = environment()
+                .toBuilder()
+                .currentConfig(mockConfig)
+                .optimizely(object: MockExperimentsClientType() {
+                    override fun enabledFeatures(user: User?): List<String> {
+                        return optimizelyFeatures
+                    }
+                })
+                .build()
         this.vm = FeatureFlagsViewModel.ViewModel(environment)
 
-        this.vm.outputs.features().subscribe(this.features)
+        this.vm.outputs.configFeatures().subscribe(this.configFeatures)
+        this.vm.outputs.optimizelyFeatures().subscribe(this.optimizelyFeatures)
     }
 
     @Test
-    fun testFeatures_whenNull() {
-        setUpEnvironment(null)
+    fun testConfigFeatures_whenFeaturesMapIsNull() {
+        setUpEnvironment(null, emptyList())
 
-        this.features.assertValue(listOf())
+        this.configFeatures.assertValue(listOf())
     }
 
     @Test
-    fun testFeatures() {
+    fun testConfigFeatures_whenFeaturesMapIsNotNull() {
         val features = mapOf(Pair("ios_feature_one", true),
                 Pair("ios_feature_two", false),
                 Pair("android_feature_one", true),
                 Pair("android_feature_two", false))
-        setUpEnvironment(features)
+        setUpEnvironment(features, emptyList())
 
-        this.features.assertValue(listOf(Pair("android_feature_one", true),
+        this.configFeatures.assertValue(listOf(Pair("android_feature_one", true),
                 Pair("android_feature_two", false)))
+    }
+
+    @Test
+    fun testOptimizelyFeatures() {
+        setUpEnvironment(null, listOf("android_optimizely_feature"))
+
+        this.optimizelyFeatures.assertValue(listOf(Pair("android_optimizely_feature", true)))
     }
 }

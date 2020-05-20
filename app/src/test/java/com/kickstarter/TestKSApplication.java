200c6d14b9eef0542fe14ecb0c44064aa264e697
Isabel Martin <arkariang@gmail.com>
2020-05-20 14:50:16 -0700

    - Failing Unit tests

--- app/src/test/java/com/kickstarter/TestKSApplication.java
+++ app/src/test/java/com/kickstarter/TestKSApplication.java
@@ -1,6 +1,7 @@
 package com.kickstarter;
 
 import com.facebook.FacebookSdk;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class TestKSApplication extends KSApplication {
 
@@ -8,6 +9,7 @@ public class TestKSApplication extends KSApplication {
   public void onCreate() {
     super.onCreate();
 
+    FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(false);
     FacebookSdk.sdkInitialize(this);
   }
 

6e8a4fd0b6e7353cfe8d4faaf8fbab5f148ce91d
Jade Byfield <jade@thoughtbot.com>
2020-06-08 19:54:02 -0400

    Remove unused import

--- app/src/main/java/com/kickstarter/ui/fragments/PledgeFragment.kt
+++ app/src/main/java/com/kickstarter/ui/fragments/PledgeFragment.kt
@@ -14,7 +14,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager

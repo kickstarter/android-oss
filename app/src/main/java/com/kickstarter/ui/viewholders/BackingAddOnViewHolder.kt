41f0f1b14b6472121bdbcd1c4d0f8e9cf9c17275
Jade Byfield <jade@thoughtbot.com>
2020-07-20 18:23:09 -0400

    Display amount, description, reward items

--- app/src/main/java/com/kickstarter/ui/viewholders/BackingAddOnViewHolder.kt
+++ app/src/main/java/com/kickstarter/ui/viewholders/BackingAddOnViewHolder.kt
@@ -1,24 +1,118 @@
 package com.kickstarter.ui.viewholders
 
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.util.Pair
 import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kickstarter.libs.rx.transformers.Transformers
+import com.kickstarter.libs.utils.ViewUtils
 import com.kickstarter.models.Reward
+import com.kickstarter.ui.adapters.RewardItemsAdapter
 import com.kickstarter.ui.data.ProjectData
 import com.kickstarter.viewmodels.BackingAddOnViewHolderViewModel
-import kotlinx.android.synthetic.main.item_add_on.view.add_on_description_text_view
+import kotlinx.android.synthetic.main.add_on_items.view.*
+import kotlinx.android.synthetic.main.add_on_title.view.*
+import kotlinx.android.synthetic.main.item_add_on.view.*
+import kotlinx.android.synthetic.main.item_add_on_pledge.view.*
 
-class BackingAddOnViewHolder (private val view: View) : KSViewHolder(view) {
+class BackingAddOnViewHolder(private val view: View) : KSViewHolder(view) {
 
     private var viewModel = BackingAddOnViewHolderViewModel.ViewModel(environment())
 
     init {
-        // TODO: https://kickstarter.atlassian.net/browse/NT-1385
+
+        val rewardItemAdapter = setUpItemAdapter()
+
+
         this.viewModel.outputs
                 .description()
                 .compose(bindToLifecycle())
                 .subscribe {
                     this.view.add_on_description_text_view.text = it
                 }
+
+
+        this.viewModel.outputs.rewardItems()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { rewardItemAdapter.rewardsItems(it) }
+
+
+        this.viewModel.outputs.titleForAddOn()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { this.view.add_on_title_text_view.text = it }
+
+
+        this.viewModel.outputs.description()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { this.view.add_on_description.text = it }
+
+
+        this.viewModel.outputs.minimum()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe {
+                    val amountLength = it.toString().length
+                    val minimumPlusShipping = "$it + $5 shipping each"
+                    this.view.add_on_minimum.text = minimumPlusShipping
+                    setBoldSpanOnTextView(amountLength, this.view.add_on_minimum)
+                }
+
+        this.viewModel.outputs.conversionIsGone()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { ViewUtils.setGone(this.view.add_on_conversion, it) }
+
+
+        this.viewModel.outputs.conversion()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { this.view.add_on_conversion.text = "About $it" }
+
+
+        this.viewModel.outputs.deadlinePillIsGone()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { ViewUtils.setGone(this.view.addon_time_left, it) }
+
+        this.viewModel.outputs.backerLimitPillIsGone()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { ViewUtils.setGone(this.view.addon_backer_limit, it) }
+
+        this.viewModel.outputs.remainingQuantityPillIsGone()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { ViewUtils.setGone(this.view.addon_quantity_remaining, it) }
+
+        this.viewModel.outputs.backerLimit()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { this.view.addon_backer_limit.text = "Limit $it" }
+
+        this.viewModel.outputs.remainingQuantity()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe { this.view.addon_quantity_remaining.text = "$it left" }
+
+
+    }
+
+    private fun setBoldSpanOnTextView(numCharacters: Int, textView: TextView) {
+        val spannable = SpannableString(textView.text)
+        //spannable.setSpan(ForegroundColorSpan(spanColor), 0, numCharacters, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(
+                StyleSpan(Typeface.BOLD),
+                0, numCharacters,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textView.text = spannable
     }
 
     override fun bindData(data: Any?) {
@@ -31,5 +125,13 @@ class BackingAddOnViewHolder (private val view: View) : KSViewHolder(view) {
 
     private fun bindAddonsList(projectDataAndAddOn: Pair<ProjectData, Reward>) {
         this.viewModel.inputs.configureWith(projectDataAndAddOn)
+    }
+
+    private fun setUpItemAdapter(): RewardItemsAdapter {
+        val rewardItemAdapter = RewardItemsAdapter()
+        val itemRecyclerView = view.add_on_item_recycler_view
+        itemRecyclerView.adapter = rewardItemAdapter
+        itemRecyclerView.layoutManager = LinearLayoutManager(context())
+        return rewardItemAdapter
     }
 }
--- app/src/main/java/com/kickstarter/viewmodels/BackingAddOnViewHolderViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/BackingAddOnViewHolderViewModel.kt
@@ -4,12 +4,15 @@ import android.util.Pair
 import androidx.annotation.NonNull
 import com.kickstarter.libs.ActivityViewModel
 import com.kickstarter.libs.Environment
-import com.kickstarter.libs.utils.ObjectUtils
+import com.kickstarter.libs.KSCurrency
+import com.kickstarter.libs.utils.*
 import com.kickstarter.models.Reward
+import com.kickstarter.models.RewardsItem
 import com.kickstarter.ui.data.ProjectData
 import com.kickstarter.ui.viewholders.BackingAddOnViewHolder
 import rx.Observable
 import rx.subjects.PublishSubject
+import java.math.RoundingMode
 
 class BackingAddOnViewHolderViewModel {
     interface Inputs {
@@ -24,8 +27,36 @@ class BackingAddOnViewHolderViewModel {
         /** Emits a pair with the add on title and the quantity in order to build the stylized title  */
         fun titleForAddOn(): Observable<String>
 
-        /** Emits the reward's description.  */
+        /** Emits the add on's description.  */
         fun description(): Observable<String>
+
+        /** Emits the add on's minimum amount */
+        fun minimum(): Observable<CharSequence>
+
+        /** Emits the add on's converted minimum amount */
+        fun convertedMinimum(): Observable<CharSequence>
+
+        fun conversionIsGone(): Observable<Boolean>
+
+        fun conversion(): Observable<CharSequence>
+
+        fun rewardItems(): Observable<List<RewardsItem>>
+
+        fun deadlinePillIsGone(): Observable<Boolean>
+
+        fun remainingQuantityPillIsGone(): Observable<Boolean>
+
+        fun backerLimitPillIsGone(): Observable<Boolean>
+
+        fun timeRemaining(): Observable<String>
+
+        fun backerLimit(): Observable<String>
+
+        fun remainingQuantity(): Observable<String>
+
+        fun shippingAmountIsGone(): Observable<Boolean>
+
+        fun shippingAmount(): Observable<Double>
 
     }
 
@@ -36,9 +67,24 @@ class BackingAddOnViewHolderViewModel {
      *  - Loading in [BackingAddOnViewHolder] -> [BackingAddOnsAdapter] -> [BackingAddOnsFragment]
      */
     class ViewModel(@NonNull environment: Environment) : ActivityViewModel<BackingAddOnViewHolder>(environment), Inputs, Outputs {
+
+        private val ksCurrency: KSCurrency = environment.ksCurrency()
         private val projectDataAndAddOn = PublishSubject.create<Pair<ProjectData, Reward>>()
         private val title = PublishSubject.create<String>()
         private val description = PublishSubject.create<String>()
+        private val minimum = PublishSubject.create<CharSequence>()
+        private val convertedMinimum = PublishSubject.create<CharSequence>()
+        private val conversionIsGone = PublishSubject.create<Boolean>()
+        private val conversion = PublishSubject.create<CharSequence>()
+        private val rewardItems = PublishSubject.create<List<RewardsItem>>()
+        private val timeRemaining = PublishSubject.create<String>()
+        private val deadlinePillIsGone = PublishSubject.create<Boolean>()
+        private val remainingQuantityPillIsGone = PublishSubject.create<Boolean>()
+        private val backerLimitPillIsGone = PublishSubject.create<Boolean>()
+        private val remainingQuantity = PublishSubject.create<String>()
+        private val backerLimit = PublishSubject.create<String>()
+        private val shippingAmountIsGone = PublishSubject.create<Boolean>()
+        private val shippingAmount = PublishSubject.create<Double>()
 
         val inputs: Inputs = this
         val outputs: Outputs = this
@@ -57,12 +103,87 @@ class BackingAddOnViewHolderViewModel {
                     .filter { ObjectUtils.isNotNull(it) }
                     .subscribe(this.description)
 
+            addOn.filter { RewardUtils.isItemized(it) }
+                    .map { it.rewardsItems() }
+                    .compose(bindToLifecycle())
+                    .subscribe(this.rewardItems)
+
+            projectDataAndAddOn.map { ProjectViewUtils.styleCurrency(it.second.minimum(), it.first.project(), this.ksCurrency) }
+                    .subscribe(this.minimum)
+
+
+            projectDataAndAddOn.map { ProjectViewUtils.styleCurrency(it.second.convertedMinimum(), it.first.project(), this.ksCurrency) }
+                    .subscribe(this.convertedMinimum)
+
+            projectDataAndAddOn.map { it.first.project() }
+                    .map { it.currency() == it.currentCurrency() }
+                    .compose(bindToLifecycle())
+                    .subscribe(this.conversionIsGone)
+
+            projectDataAndAddOn
+                    .map { this.ksCurrency.format(it.second.convertedMinimum(), it.first.project(), true, RoundingMode.HALF_UP, true) }
+                    .compose(bindToLifecycle())
+                    .subscribe(this.conversion)
+
+            projectDataAndAddOn.map { it.first.project() }
+                    .map { ProjectUtils.isCompleted(it) }
+                    .compose(bindToLifecycle())
+                    .subscribe(this.deadlinePillIsGone)
+
+            addOn.map { it.limit() }
+                    .map { !ObjectUtils.isNotNull(it) }
+                    .compose(bindToLifecycle())
+                    .subscribe { this.backerLimitPillIsGone }
+
+            addOn.map { it.quantity() }
+                    .map { !ObjectUtils.isNotNull(it) }
+                    .compose(bindToLifecycle())
+                    .subscribe { this.remainingQuantityPillIsGone }
+
+            addOn.map { it.limit().toString() }
+                    .compose(bindToLifecycle())
+                    .subscribe(this.backerLimit)
+
+            addOn.map { it.remaining().toString() }
+                    .compose(bindToLifecycle())
+                    .subscribe (this.remainingQuantity)
+
+            //addOn.map { it.shippingRules()?.get(0)?.cost() }
+
+
         }
+
 
         override fun configureWith(projectDataAndAddOn: Pair<ProjectData, Reward>) = this.projectDataAndAddOn.onNext(projectDataAndAddOn)
 
         override fun titleForAddOn() = this.title
 
         override fun description() = this.description
+
+        override fun minimum() = this.minimum
+
+        override fun convertedMinimum() = this.convertedMinimum
+
+        override fun conversionIsGone() = this.conversionIsGone
+
+        override fun conversion() = this.conversion
+
+        override fun rewardItems() = this.rewardItems
+
+        override fun timeRemaining() = this.timeRemaining
+
+        override fun deadlinePillIsGone() = this.deadlinePillIsGone
+
+        override fun remainingQuantityPillIsGone() = this.remainingQuantityPillIsGone
+
+        override fun backerLimitPillIsGone() = this.backerLimitPillIsGone
+
+        override fun backerLimit() = this.backerLimit
+
+        override fun remainingQuantity() = this.remainingQuantity
+
+        override fun shippingAmountIsGone() = this.shippingAmountIsGone
+
+        override fun shippingAmount() = this.shippingAmount
     }
 }

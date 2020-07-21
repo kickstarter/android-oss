14846871185613c5b10bb8f2ef865d12b95c1cc6
Jade Byfield <jade@thoughtbot.com>
2020-07-21 16:15:38 -0400

    Add tests

--- app/src/test/java/com/kickstarter/viewmodels/BackingAddOnViewHolderViewModelTest.kt
+++ app/src/test/java/com/kickstarter/viewmodels/BackingAddOnViewHolderViewModelTest.kt
@@ -16,14 +16,18 @@ class BackingAddOnViewHolderViewModelTest : KSRobolectricTestCase() {
     private lateinit var vm: BackingAddOnViewHolderViewModel.ViewModel
 
     private val backerLimitIsGone = TestSubscriber.create<Boolean>()
+    private val remainingQuantityIsGone = TestSubscriber.create<Boolean>()
+    private val countdownIsGone = TestSubscriber.create<Boolean>()
 
     private fun setupEnvironment(@NonNull environment: Environment ) {
         this.vm = BackingAddOnViewHolderViewModel.ViewModel(environment)
         this.vm.outputs.backerLimitPillIsGone().subscribe(this.backerLimitIsGone)
+        this.vm.outputs.remainingQuantityPillIsGone().subscribe(this.remainingQuantityIsGone)
+        this.vm.outputs.deadlineCountdownIsGone().subscribe(this.countdownIsGone)
     }
 
     @Test
-    fun testAddOnBackerLimitGone(){
+    fun testAddOnBackerLimitPillGone(){
         setupEnvironment(environment())
 
         val addOn = RewardFactory.reward().toBuilder().isAddOn(true).limit(null).build()
@@ -32,4 +36,26 @@ class BackingAddOnViewHolderViewModelTest : KSRobolectricTestCase() {
         this.backerLimitIsGone.assertValue(true)
 
     }
+
+    @Test
+    fun testAddOnRemainingQuantityPillIsGone(){
+        setupEnvironment(environment())
+
+        val addOn = RewardFactory.reward().toBuilder().isAddOn(true).remaining(null).build()
+        this.vm.inputs.configureWith(android.util.Pair<ProjectData, Reward>(ProjectDataFactory.project(ProjectFactory.project()), addOn))
+
+        this.remainingQuantityIsGone.assertValue(true)
+
+    }
+
+    @Test
+    fun testCountdownPillIsGone(){
+        setupEnvironment(environment())
+
+        val addOn = RewardFactory.reward().toBuilder().isAddOn(true).endsAt(null).build()
+        this.vm.inputs.configureWith(android.util.Pair<ProjectData, Reward>(ProjectDataFactory.project(ProjectFactory.project()), addOn))
+
+        this.countdownIsGone.assertValue(true)
+    }
+
 }

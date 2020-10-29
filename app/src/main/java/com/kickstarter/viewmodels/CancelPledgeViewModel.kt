4f8d2ba44530197d3dbb05d554ea327f646f7aa5
Isabel Martin <arkariang@gmail.com>
2020-10-29 16:51:03 -0700

    - Specify type explicitly

--- app/src/main/java/com/kickstarter/viewmodels/CancelPledgeViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/CancelPledgeViewModel.kt
@@ -1,12 +1,10 @@
 package com.kickstarter.viewmodels
 
-import android.os.Bundle
 import android.util.Pair
 import androidx.annotation.NonNull
 import com.kickstarter.libs.Environment
 import com.kickstarter.libs.FragmentViewModel
 import com.kickstarter.libs.rx.transformers.Transformers.*
-import com.kickstarter.libs.utils.ObjectUtils
 import com.kickstarter.models.Backing
 import com.kickstarter.models.Project
 import com.kickstarter.ui.ArgumentsKey
@@ -71,8 +69,8 @@ interface CancelPledgeViewModel {
         init {
 
             val project = arguments()
-                    .filter { ObjectUtils.isNotNull(it.getParcelable(ArgumentsKey.CANCEL_PLEDGE_PROJECT)) }
-                    .map { it as Project }
+                    .map { it.getParcelable(ArgumentsKey.CANCEL_PLEDGE_PROJECT) as Project? }
+                    .ofType(Project::class.java)
 
             val backing = project
                     .map { it.backing() }

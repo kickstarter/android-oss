9dc91a77b7c9a4236e47831fbd35f73fd9d0d1a3
leighdouglas <leighcdouglas1@gmail.com>
2020-11-10 11:35:01 -0500

    NT-1650: The user can navigate to their inbox from the app (#1038)
    * Add input method for interstitial CTA
    
    * tests
    
    * Add test
    
    * Cleanup test
    
    * Add this to global variable

--- app/src/main/java/com/kickstarter/ui/fragments/EmailVerificationInterstitialFragment.kt
+++ app/src/main/java/com/kickstarter/ui/fragments/EmailVerificationInterstitialFragment.kt
@@ -1,5 +1,6 @@
 package com.kickstarter.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,9 @@ import com.kickstarter.libs.BaseFragment
 import com.kickstarter.libs.qualifiers.RequiresFragmentViewModel
 import com.kickstarter.models.User
 import com.kickstarter.ui.ArgumentsKey
+import com.kickstarter.libs.rx.transformers.Transformers
 import com.kickstarter.viewmodels.EmailVerificationInterstitialFragmentViewModel
+import kotlinx.android.synthetic.main.fragment_email_verification_interstitial.*
 
 @RequiresFragmentViewModel(EmailVerificationInterstitialFragmentViewModel.ViewModel::class)
 class EmailVerificationInterstitialFragment : BaseFragment<EmailVerificationInterstitialFragmentViewModel.ViewModel>() {
@@ -17,6 +20,21 @@ class EmailVerificationInterstitialFragment : BaseFragment<EmailVerificationInte
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_email_verification_interstitial, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.viewModel.outputs.startEmailActivity()
+                .compose(bindToLifecycle())
+                .compose(Transformers.observeForUI())
+                .subscribe {
+                    startActivity(Intent.createChooser(Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_EMAIL), "Choose Email"))
+                }
+
+        email_verification_interstitial_cta_button.setOnClickListener {
+            this.viewModel.inputs.openInboxButtonPressed()
+        }
     }
 
     companion object {
---
+++ app/src/test/java/com/kickstarter/viewmodels/EmailVerificationInterstitialFragmentViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.kickstarter.viewmodels
+
+import androidx.annotation.NonNull
+import com.kickstarter.KSRobolectricTestCase
+import com.kickstarter.libs.Environment
+import org.junit.Test
+import rx.observers.TestSubscriber
+
+class EmailVerificationInterstitialFragmentViewModelTest : KSRobolectricTestCase() {
+    private lateinit var vm: EmailVerificationInterstitialFragmentViewModel.ViewModel
+    private val startEmailActivity = TestSubscriber.create<Void>()
+
+    private fun setUpEnvironment(@NonNull environment: Environment) {
+        this.vm = EmailVerificationInterstitialFragmentViewModel.ViewModel(environment)
+        this.vm.outputs.startEmailActivity().subscribe(startEmailActivity)
+    }
+
+    @Test
+    fun init_whenOpenEmailInboxPressedEmits_shouldEmitToStartEmailActivityStream() {
+        setUpEnvironment(environment())
+
+        this.vm.inputs.openInboxButtonPressed()
+        this.startEmailActivity.assertValue(null)
+    }
+}

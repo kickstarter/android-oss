7404803b4e802a5cd06dcabe88417b0f8dece8a8
Isabel Martin <arkariang@gmail.com>
2020-11-10 12:12:04 -0800

    - NT-1651: skip link feature

--- app/src/main/java/com/kickstarter/ui/fragments/EmailVerificationInterstitialFragment.kt
+++ app/src/main/java/com/kickstarter/ui/fragments/EmailVerificationInterstitialFragment.kt
@@ -11,6 +11,9 @@ import com.kickstarter.libs.qualifiers.RequiresFragmentViewModel
 import com.kickstarter.models.User
 import com.kickstarter.ui.ArgumentsKey
 import com.kickstarter.libs.rx.transformers.Transformers
+import com.kickstarter.libs.rx.transformers.Transformers.observeForUI
+import com.kickstarter.libs.utils.ViewUtils
+import com.kickstarter.services.apiresponses.AccessTokenEnvelope
 import com.kickstarter.viewmodels.EmailVerificationInterstitialFragmentViewModel
 import kotlinx.android.synthetic.main.fragment_email_verification_interstitial.*
 
@@ -27,14 +30,49 @@ class EmailVerificationInterstitialFragment : BaseFragment<EmailVerificationInte
 
         this.viewModel.outputs.startEmailActivity()
                 .compose(bindToLifecycle())
-                .compose(Transformers.observeForUI())
+                .compose(observeForUI())
                 .subscribe {
                     startActivity(Intent.createChooser(Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_EMAIL), "Choose Email"))
                 }
 
+        this.viewModel.outputs.isSkipLinkShown()
+                .compose(bindToLifecycle())
+                .compose(observeForUI())
+                .subscribe {
+                    ViewUtils.setGone(email_verification_interstitial_skip, !it)
+                }
+
+        this.viewModel.outputs.dismissInterstitial()
+                .compose(bindToLifecycle())
+                .compose(observeForUI())
+                .subscribe { dismiss() }
+
         email_verification_interstitial_cta_button.setOnClickListener {
             this.viewModel.inputs.openInboxButtonPressed()
         }
+
+        email_verification_interstitial_skip.setOnClickListener {
+            this.viewModel.inputs.skipButtonPressed()
+        }
+    }
+
+    /** Configure with current [AccessTokenEnvelope]. */
+    fun configureWith(accessTokenEnvelope: AccessTokenEnvelope) {
+        this.viewModel.inputs.configureWith(accessTokenEnvelope)
+    }
+
+    /**
+     * OnDetach the current fragment from it's parent activity
+     * and force the parent activity to finish
+     */
+    private fun dismiss() = apply {
+        onDetach()
+        activity?.finish()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        this.viewModel = null
     }
 
     companion object {
--- app/src/main/java/com/kickstarter/viewmodels/EmailVerificationInterstitialFragmentViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/EmailVerificationInterstitialFragmentViewModel.kt
@@ -3,40 +3,83 @@ package com.kickstarter.viewmodels
 import androidx.annotation.NonNull
 import com.kickstarter.libs.Environment
 import com.kickstarter.libs.FragmentViewModel
+import com.kickstarter.libs.utils.extensions.EMAIL_VERIFICATION_SKIP
+import com.kickstarter.libs.utils.extensions.isFeatureFlagEnabled
+import com.kickstarter.services.apiresponses.AccessTokenEnvelope
 import com.kickstarter.ui.fragments.EmailVerificationInterstitialFragment
 import rx.Observable
 import rx.subjects.BehaviorSubject
+import rx.subjects.PublishSubject
 
 class EmailVerificationInterstitialFragmentViewModel {
     interface Inputs {
+        /** Input to trigger the logic contained within the ViewModel */
+        fun configureWith(accessTokenEnvelope: AccessTokenEnvelope)
+
         /** Invoked when the open inbox button is pressed */
         fun openInboxButtonPressed()
+
+        /** Invoked when the open inbox button is pressed */
+        fun skipButtonPressed()
     }
 
     interface Outputs {
+        /** Launch Email app  */
         fun startEmailActivity(): Observable<Void>
+
+        /** Skip link button should be shown/hide */
+        fun isSkipLinkShown(): Observable<Boolean>
+
+        /** Dismiss the Interstitial Screen */
+        fun dismissInterstitial(): Observable<Void>
     }
 
     class ViewModel(@NonNull val environment: Environment) : FragmentViewModel<EmailVerificationInterstitialFragment>(environment), Outputs, Inputs {
         val inputs = this
         val outputs = this
 
-        private val openInboxButtonPressed = BehaviorSubject.create<Void>()
+        private val accessTokenEnvelope = BehaviorSubject.create<AccessTokenEnvelope>()
+        private val openInboxButtonPressed = PublishSubject.create<Void>()
+        private val skipLinkPressed = PublishSubject.create<Void>()
 
-        private val startEmailActivity = BehaviorSubject.create<Void>()
+        private val isSkipLinkShown = PublishSubject.create<Boolean>()
+        private val startEmailActivity = PublishSubject.create<Void>()
+        private val dismissInterstitial = PublishSubject.create<Void>()
 
+        private val currentConfig = this.environment.currentConfig().observable()
+        private val currentUser = this.environment.currentUser()
 
         init {
+            // - Log in the user in the app
+            this.accessTokenEnvelope
+                    .subscribe {
+                        this.currentUser.login(it.user(), it.accessToken())
+                    }
+
+            this.currentConfig
+                    .compose(bindToLifecycle())
+                    .subscribe {
+                        this.isSkipLinkShown.onNext(it.isFeatureFlagEnabled(EMAIL_VERIFICATION_SKIP))
+                    }
+
             this.openInboxButtonPressed
                     .compose(bindToLifecycle())
                     .subscribe(this.startEmailActivity)
+
+            this.dismissInterstitial
+                    .compose(bindToLifecycle())
+                    .subscribe(this.dismissInterstitial)
         }
 
         // - Inputs
+        override fun configureWith(accessTokenEnvelope: AccessTokenEnvelope) = this.accessTokenEnvelope.onNext(accessTokenEnvelope)
         override fun openInboxButtonPressed() = this.openInboxButtonPressed.onNext(null)
+        override fun skipButtonPressed() = this.skipLinkPressed.onNext(null)
 
         // - Outputs
+        override fun isSkipLinkShown(): Observable<Boolean> = this.isSkipLinkShown
         override fun startEmailActivity(): Observable<Void> = this.startEmailActivity
+        override fun dismissInterstitial(): Observable<Void> = this.dismissInterstitial
     }
 
 }

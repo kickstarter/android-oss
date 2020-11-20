9ca69d45bb80a4dc12e5e44d981ec49f7d7d324f
Isabel Martin <arkariang@gmail.com>
2020-11-20 14:52:07 -0800

    - Deep Link into activity - Make a network request to have the response to the validation

--- app/src/main/java/com/kickstarter/viewmodels/EmailVerificationDeepLinkViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/EmailVerificationDeepLinkViewModel.kt
@@ -1,9 +1,18 @@
 package com.kickstarter.viewmodels
 
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import androidx.annotation.NonNull
 import com.kickstarter.libs.ActivityViewModel
 import com.kickstarter.libs.Environment
 import com.kickstarter.ui.activities.EmailVerificationDeepLinkActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import rx.Observable
+import rx.schedulers.Schedulers
+import java.io.IOException
 
 class EmailVerificationDeepLinkViewModel {
     interface Inputs {
@@ -17,6 +26,43 @@ class EmailVerificationDeepLinkViewModel {
         val outputs = this
 
         init {
+            val uriFromIntent = intent()
+                    .map { obj: Intent -> obj.data }
+                    .ofType(Uri::class.java)
+
+            uriFromIntent
+                    .observeOn(Schedulers.io())
+                    .subscribeOn(Schedulers.io())
+                    .switchMap {
+                        makeCall(it)
+                    }
+                    .compose(bindToLifecycle())
+                    .subscribe { response ->
+                        val body = response?.body()?.string()
+                        val isRedirect = response?.priorResponse()?.isRedirect?.toString()
+                        val responseCode = response?.code()?.toString()
+                        val message = response?.message()
+                        Log.i("Response body: ", body)
+                        Log.i("fromRedirect: ", isRedirect.toString())
+                        Log.i("code: ", responseCode.toString())
+                        Log.i("message: ", message)
+                    }
+        }
+
+        // TODO: Transform into a Observable<Notification>
+        private fun makeCall(uri: Uri): Observable<Response?> {
+            val url = uri.toString()
+            val httpClient = OkHttpClient.Builder()
+                    .build()
+            val request = Request.Builder()
+                    .url(url)
+                    .build()
+            return try {
+                val response = httpClient.newCall(request).execute()
+                Observable.just(response)
+            } catch (exception: IOException) {
+                Observable.just(null)
+            }
         }
     }
 

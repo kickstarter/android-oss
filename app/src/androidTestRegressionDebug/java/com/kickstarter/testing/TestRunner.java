aa976bcab8bacf8c583d8245b1891ec076c5fd15
Arnold Noronha <arnold@tdrhq.com>
2021-01-06 14:18:39 -0800

    Make sure I can do a basic build on circleci. TODO: remove the xlarge comment in the config.yml

--- app/src/androidTestRegressionDebug/java/com/kickstarter/testing/TestRunner.java
+++ app/src/androidTestRegressionDebug/java/com/kickstarter/testing/TestRunner.java
@@ -6,16 +6,16 @@ import com.facebook.testing.screenshot.ScreenshotRunner;
 import android.os.Bundle;
 
 public class TestRunner extends AndroidJUnitRunner {
-    @Override
-    public void onCreate(Bundle arguments) {
-        MultiDex.install(getTargetContext());
-        ScreenshotRunner.onCreate(this, arguments);
-        super.onCreate(arguments);
-    }
+  @Override
+  public void onCreate(final Bundle arguments) {
+    MultiDex.install(getTargetContext());
+    ScreenshotRunner.onCreate(this, arguments);
+    super.onCreate(arguments);
+  }
 
-    @Override
-    public void finish(int resultCode, Bundle results) {
-        ScreenshotRunner.onDestroy();
-        super.finish(resultCode, results);
-    }
+  @Override
+  public void finish(final int resultCode, final Bundle results) {
+    ScreenshotRunner.onDestroy();
+    super.finish(resultCode, results);
+  }
 }

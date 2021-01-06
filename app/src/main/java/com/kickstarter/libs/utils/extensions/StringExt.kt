67400389d8a791c6f1f18b9d9d4810a59ce33c49
leighdouglas <leighcdouglas1@gmail.com>
2021-01-06 13:55:16 -0500

    NT-1700 Create StringExt.kt and Test Suite (#1064)
    * Create new StringExt class and corresponding test suite
    
    * Add constants and create tests for isValidPassword and wrapInParentheses
    
    * Fix broken test
    
    * addressing feedback
    
    * Update function names, address feedback, update trim method

---
+++ app/src/main/java/com/kickstarter/libs/utils/extensions/StringExt.kt
@@ -0,0 +1,55 @@
+@file:JvmName("StringExt")
+package com.kickstarter.libs.utils.extensions
+
+import android.util.Patterns
+import java.util.Locale
+
+    const val MINIMUM_PASSWORD_LENGTH = 6
+
+    /**
+     * Returns a boolean that reflects if the string is an email address
+     */
+    fun String.isEmail(): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+
+    /**
+    * Returns a boolean that reflects if the string is empty or the length is zero when white space
+    * characters are trimmed
+    */
+    fun String.isTrimmedEmpty(): Boolean {
+        return this.isNullOrEmpty() || this.trimAllWhitespace().length == 0
+    }
+
+    /**
+    * Returns a boolean of if the string is not empty
+    */
+    fun String.isPresent() = !this.isTrimmedEmpty()
+
+    /**
+    * Returns a boolean of if the string is not empty and has more than 5 characters
+    */
+    fun String.isValidPassword(): Boolean {
+        return !this.isTrimmedEmpty() && this.length >= MINIMUM_PASSWORD_LENGTH
+    }
+
+    /**
+     * Returns a string with only the first character capitalized.
+     */
+    fun String.sentenceCase(): String {
+        return if (this.length <= 1) this.toUpperCase(Locale.getDefault())
+        else this.substring(0, 1).toUpperCase(Locale.getDefault()) + this.substring(1).toLowerCase(Locale.getDefault())
+    }
+
+    /**
+     * Returns a string with no leading or trailing whitespace. This method calls on the kotlin trim string method,
+     * which removes more types of whitespace than the java string method for trim.
+     */
+    fun String.trimAllWhitespace(): String {
+        return this.trim().replace(" +".toRegex(), " ")
+    }
+
+    /**
+     * Returns a string wrapped in parentheses.
+     */
+    fun String.wrapInParentheses() = "($this)"
---
+++ app/src/test/java/com/kickstarter/libs/utils/extensions/StringExtKtTest.kt
@@ -0,0 +1,137 @@
+package com.kickstarter.libs.utils.extensions
+
+import com.kickstarter.KSRobolectricTestCase
+import org.junit.Test
+
+class StringExtKtTest : KSRobolectricTestCase() {
+
+    @Test
+    fun isEmail_whenGivenEmail_shouldReturnTrue() {
+        assertTrue(VALID_EMAIL.isEmail())
+    }
+
+    @Test
+    fun isEmail_whenNotGivenEmail_shouldReturnFalse() {
+        assertFalse(INVALID_EMAIL.isEmail())
+    }
+
+    @Test
+    fun isEmpty_whenGivenEmptyString_shouldReturnTrue() {
+        assertTrue(EMPTY_STRING.isTrimmedEmpty())
+        assertTrue(EMPTY_SPACE.isTrimmedEmpty())
+        assertTrue(EMPTY_MULTI_SPACE.isTrimmedEmpty())
+    }
+
+    @Test
+    fun isEmpty_whenStringNotEmpty_shouldReturnFalse() {
+        assertFalse("k".isTrimmedEmpty())
+        assertFalse(" k ".isTrimmedEmpty())
+    }
+
+    @Test
+    fun isPresent_whenStringEmpty_shouldReturnFalse() {
+        assertFalse(EMPTY_STRING.isPresent())
+        assertFalse(EMPTY_SPACE.isPresent())
+        assertFalse(EMPTY_MULTI_SPACE.isPresent())
+    }
+
+    @Test
+    fun isPresent_whenStringNotEmpty_shouldReturnTrue() {
+        assertTrue("k".isPresent())
+        assertTrue(" k ".isPresent())
+    }
+
+    @Test
+    fun isValidPassword_whenStringIsEmpty_shouldReturnFalse(){
+        assertFalse(EMPTY_STRING.isValidPassword())
+        assertFalse(EMPTY_SPACE.isValidPassword())
+        assertFalse(EMPTY_MULTI_SPACE.isValidPassword())
+    }
+
+    @Test
+    fun isValidPassword_whenStringIsLessThanRequiredLength_shouldReturnFalse(){
+        assertFalse(PASSWORD_LENGTH_FIVE.isValidPassword())
+    }
+
+    @Test
+    fun isValidPassword_whenStringIsNotNullAndGreaterThanOrEqualToRequiredLength_shouldReturnTrue() {
+        assertTrue(PASSWORD_LENGTH_SIX.isValidPassword())
+        assertTrue(PASSWORD_LENGTH_TEN.isValidPassword())
+    }
+
+    @Test
+    fun sentenceCase_whenGivenLowerCaseString_shouldReformatToSentenceCase() {
+        assertEquals("K", "k".sentenceCase());
+        assertEquals("Kickstarter", "kickstarter".sentenceCase());
+    }
+
+    @Test
+    fun sentenceCase_whenGivenUpperCaseString_shouldReformatToSentenceCase() {
+        assertEquals("Kickstarter", "KICKSTARTER".sentenceCase());
+    }
+
+    @Test
+    fun sentenceCase_whenGivenMixedCaseString_shouldReformatToSentenceCase() {
+        assertEquals("Kickstarter", "KiCkSTarTer".sentenceCase());
+    }
+
+    @Test
+    fun sentenceCase_whenGivenTitleCaseString_shouldReformatToSentenceCase() {
+        assertEquals("Kickstarter kickstarter", "Kickstarter Kickstarter".sentenceCase());
+        assertEquals("Kickstarter is great", "Kickstarter Is Great".sentenceCase());
+    }
+
+    @Test
+    fun sentenceCase_whenGivenEmptyString_shouldEmptyString() {
+        assertEquals(EMPTY_STRING, EMPTY_STRING.sentenceCase());
+    }
+
+    @Test
+    fun trim_whenGivenEmptyString_shouldReturnEmptyString() {
+        assertEquals(EMPTY_STRING, EMPTY_STRING.trimAllWhitespace())
+        assertEquals(EMPTY_STRING, EMPTY_SPACE.trimAllWhitespace())
+    }
+
+    @Test
+    fun trim_whenGivenStringWithLeadingOrTrailingSpace_shouldReturnTrimmedString() {
+        assertEquals("A", "A ".trimAllWhitespace())
+        assertEquals("A", " A".trimAllWhitespace())
+        assertEquals("A", " A ".trimAllWhitespace())
+    }
+
+    @Test
+    fun trim_whenGivenStringWithNonBreakingSpaceWithChar_shouldReturnTrimmedString() {
+        assertEquals("A", "\u00A0A".trimAllWhitespace())
+        assertEquals("A", "A\u00A0".trimAllWhitespace())
+        assertEquals("A", "\u00A0A\u00A0".trimAllWhitespace())
+        assertEquals("A", "\u00A0 A".trimAllWhitespace())
+        assertEquals("A", "A \u00A0".trimAllWhitespace())
+        assertEquals("A", "A\u00A0 ".trimAllWhitespace())
+        assertEquals("A", "\u00A0 A \u00A0".trimAllWhitespace())
+    }
+
+    @Test
+    fun trim_whenGivenStringWithNonBreakingSpace_shouldReturnEmptydString() {
+        assertEquals("", "\u00A0".trimAllWhitespace())
+        assertEquals("", "\u00A0 ".trimAllWhitespace())
+    }
+
+    @Test
+    fun wrapInParentheses_whenGivenString_shouldReturnFlankedString() {
+        assertEquals(KICKSTARTER.wrapInParentheses(), KICKSTARTER_PARENTHESES)
+        assertEquals(EMPTY_STRING.wrapInParentheses(), "()")
+    }
+
+    companion object {
+        private const val VALID_EMAIL = "hello@kickstarter.com"
+        private const val INVALID_EMAIL = "hello@kickstarer"
+        private const val EMPTY_STRING = ""
+        private const val EMPTY_SPACE = " "
+        private const val EMPTY_MULTI_SPACE = "    "
+        private const val KICKSTARTER = "kickstarter"
+        private const val KICKSTARTER_PARENTHESES= "(kickstarter)"
+        private const val PASSWORD_LENGTH_FIVE = "kicks"
+        private const val PASSWORD_LENGTH_SIX = "kickst"
+        private const val PASSWORD_LENGTH_TEN = "kickstarts"
+    }
+}

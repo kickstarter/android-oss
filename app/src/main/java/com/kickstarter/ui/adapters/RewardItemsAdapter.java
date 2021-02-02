0400d1c0bb11215e80025b7db81a746551ff9f28
hadia <hadiamohamed.iti@gmail.com>
2021-02-02 15:33:18 +0200

    Update RewardsAdapter and RewardsItemViewHolder.kt

--- app/src/main/java/com/kickstarter/ui/adapters/RewardItemsAdapter.java
+++ app/src/main/java/com/kickstarter/ui/adapters/RewardItemsAdapter.java
@@ -1,16 +1,17 @@
 package com.kickstarter.ui.adapters;
 
-import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 
 import com.kickstarter.R;
+import com.kickstarter.databinding.RewardsItemViewBinding;
 import com.kickstarter.models.RewardsItem;
 import com.kickstarter.ui.viewholders.KSViewHolder;
 import com.kickstarter.ui.viewholders.RewardsItemViewHolder;
 
 import java.util.List;
-
-import androidx.annotation.LayoutRes;
-import androidx.annotation.NonNull;
 
 import static java.util.Collections.emptyList;
 
@@ -30,8 +31,9 @@ public final class RewardItemsAdapter extends KSAdapter {
     return R.layout.rewards_item_view;
   }
 
-  @Override
-  protected @NonNull KSViewHolder viewHolder(final @LayoutRes int layout, final @NonNull View view) {
-    return new RewardsItemViewHolder(view);
-  }
+    @Override
+    protected @NonNull
+    KSViewHolder viewHolder(int layout, @NonNull ViewGroup viewGroup) {
+        return new RewardsItemViewHolder(RewardsItemViewBinding.inflate(LayoutInflater.from(viewGroup.getContext()), viewGroup, false));
+    }
 }

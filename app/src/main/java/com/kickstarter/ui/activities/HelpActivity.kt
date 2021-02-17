c0c44b4ad7809eeef864bc92248f4ef3c291f8d5
hadia <hadiamohamed.iti@gmail.com>
2021-02-17 23:01:40 +0200

    Change to view binding in the HelpSettingsActivity.kt Change HelpActivity.java to HelpActivity.kt Remove Butter-knife

--- app/src/main/java/com/kickstarter/ui/activities/HelpActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/HelpActivity.kt
@@ -1,104 +1,86 @@
-package com.kickstarter.ui.activities;
+package com.kickstarter.ui.activities
 
-import android.net.Uri;
-import android.os.Bundle;
+import android.net.Uri
+import android.os.Bundle
+import androidx.annotation.IntDef
+import com.kickstarter.databinding.HelpLayoutBinding
+import com.kickstarter.libs.BaseActivity
+import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
+import com.kickstarter.libs.qualifiers.WebEndpoint
+import com.kickstarter.viewmodels.HelpViewModel
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
 
-import com.kickstarter.R;
-import com.kickstarter.libs.BaseActivity;
-import com.kickstarter.libs.qualifiers.RequiresActivityViewModel;
-import com.kickstarter.libs.qualifiers.WebEndpoint;
-import com.kickstarter.ui.views.KSWebView;
-import com.kickstarter.viewmodels.HelpViewModel;
+@RequiresActivityViewModel(HelpViewModel::class)
+open class HelpActivity : BaseActivity<HelpViewModel>() {
+    @IntDef(HELP_TYPE_TERMS, HELP_TYPE_PRIVACY, HELP_TYPE_HOW_IT_WORKS, HELP_TYPE_COOKIE_POLICY, HELP_TYPE_ACCESSIBILITY)
+    @Retention(RetentionPolicy.SOURCE)
+    annotation class HelpType
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+    @HelpType
+    private var helpType = 0
 
-import androidx.annotation.IntDef;
-import androidx.annotation.Nullable;
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
-@RequiresActivityViewModel(HelpViewModel.class)
-public class HelpActivity extends BaseActivity<HelpViewModel> {
-  public static final int HELP_TYPE_TERMS = 0;
-  public static final int HELP_TYPE_PRIVACY = 1;
-  public static final int HELP_TYPE_HOW_IT_WORKS = 2;
-  public static final int HELP_TYPE_COOKIE_POLICY = 3;
-  public static final int HELP_TYPE_ACCESSIBILITY = 4;
-  public static final String TERMS_OF_USE = "terms-of-use";
-  public static final String PRIVACY = "privacy";
-  public static final String HELLO = "hello";
-  public static final String COOKIES = "cookies";
-  public static final String ACCESSIBILITY = "accessibility";
-
-  @IntDef({HELP_TYPE_TERMS, HELP_TYPE_PRIVACY, HELP_TYPE_HOW_IT_WORKS, HELP_TYPE_COOKIE_POLICY, HELP_TYPE_ACCESSIBILITY})
-  @Retention(RetentionPolicy.SOURCE)
-  public @interface HelpType {}
-
-  private @HelpType int helpType;
-
-  protected @Bind(R.id.kickstarter_web_view) KSWebView kickstarterWebView;
-
-  private @WebEndpoint String webEndpoint;
-
-  protected void helpType(final @HelpType int helpType) {
-    this.helpType = helpType;
-  }
-
-  public static class Terms extends HelpActivity {
-    public Terms() {
-      helpType(HELP_TYPE_TERMS);
+    @WebEndpoint
+    private val webEndpoint: String? = null
+    protected fun helpType(@HelpType helpType: Int) {
+        this.helpType = helpType
     }
-  }
 
-  public static class Privacy extends HelpActivity {
-    public Privacy() {
-      helpType(HELP_TYPE_PRIVACY);
+    class Terms : HelpActivity() {
+        init {
+            helpType(HELP_TYPE_TERMS)
+        }
     }
-  }
 
-  public static class CookiePolicy extends HelpActivity {
-    public CookiePolicy() {
-      helpType(HELP_TYPE_COOKIE_POLICY);
+    class Privacy : HelpActivity() {
+        init {
+            helpType(HELP_TYPE_PRIVACY)
+        }
     }
-  }
 
-  public static class AccessibilityStatement extends HelpActivity {
-    public AccessibilityStatement() {
-      helpType(HELP_TYPE_ACCESSIBILITY);
+    class CookiePolicy : HelpActivity() {
+        init {
+            helpType(HELP_TYPE_COOKIE_POLICY)
+        }
     }
-  }
 
-  @Override
-  protected void onCreate(final @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.help_layout);
-    ButterKnife.bind(this);
-    this.webEndpoint = environment().webEndpoint();
-
-    final String url = getUrlForHelpType(this.helpType);
-    this.kickstarterWebView.loadUrl(url);
-  }
-
-  protected String getUrlForHelpType(final @HelpType int helpType) {
-    final Uri.Builder builder = Uri.parse(this.webEndpoint).buildUpon();
-    switch (helpType) {
-      case HELP_TYPE_TERMS:
-        builder.appendEncodedPath(TERMS_OF_USE);
-        break;
-      case HELP_TYPE_PRIVACY:
-        builder.appendEncodedPath(PRIVACY);
-        break;
-      case HELP_TYPE_HOW_IT_WORKS:
-        builder.appendEncodedPath(HELLO);
-        break;
-      case HELP_TYPE_COOKIE_POLICY:
-        builder.appendEncodedPath(COOKIES);
-        break;
-      case HELP_TYPE_ACCESSIBILITY:
-        builder.appendEncodedPath(ACCESSIBILITY);
-        break;
+    class AccessibilityStatement : HelpActivity() {
+        init {
+            helpType(HELP_TYPE_ACCESSIBILITY)
+        }
     }
-    return builder.toString();
-  }
+
+    private lateinit var binding: HelpLayoutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = HelpLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val url = getUrlForHelpType(helpType)
+        binding.kickstarterWebView.loadUrl(url)
+    }
+
+    protected fun getUrlForHelpType(@HelpType helpType: Int): String {
+        val builder = Uri.parse(webEndpoint).buildUpon()
+        when (helpType) {
+            HELP_TYPE_TERMS -> builder.appendEncodedPath(TERMS_OF_USE)
+            HELP_TYPE_PRIVACY -> builder.appendEncodedPath(PRIVACY)
+            HELP_TYPE_HOW_IT_WORKS -> builder.appendEncodedPath(HELLO)
+            HELP_TYPE_COOKIE_POLICY -> builder.appendEncodedPath(COOKIES)
+            HELP_TYPE_ACCESSIBILITY -> builder.appendEncodedPath(ACCESSIBILITY)
+        }
+        return builder.toString()
+    }
+
+    companion object {
+        const val HELP_TYPE_TERMS = 0
+        const val HELP_TYPE_PRIVACY = 1
+        const val HELP_TYPE_HOW_IT_WORKS = 2
+        const val HELP_TYPE_COOKIE_POLICY = 3
+        const val HELP_TYPE_ACCESSIBILITY = 4
+        const val TERMS_OF_USE = "terms-of-use"
+        const val PRIVACY = "privacy"
+        const val HELLO = "hello"
+        const val COOKIES = "cookies"
+        const val ACCESSIBILITY = "accessibility"
+    }
 }
--- app/src/main/java/com/kickstarter/ui/activities/HelpSettingsActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/HelpSettingsActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import com.kickstarter.R
+import com.kickstarter.databinding.ActivityHelpSettingsBinding
 import com.kickstarter.libs.BaseActivity
 import com.kickstarter.libs.Build
 import com.kickstarter.libs.CurrentUserType
@@ -15,12 +16,9 @@ import com.kickstarter.libs.utils.UrlUtils
 import com.kickstarter.libs.utils.UrlUtils.baseCustomTabsIntent
 import com.kickstarter.models.User
 import com.kickstarter.models.chrome.ChromeTabsHelperActivity
-import com.kickstarter.ui.activities.HelpActivity.*
 import com.kickstarter.viewmodels.HelpSettingsViewModel
-import kotlinx.android.synthetic.main.activity_help_settings.*
 import rx.android.schedulers.AndroidSchedulers
 import java.util.*
-
 
 @RequiresActivityViewModel(HelpSettingsViewModel.ViewModel::class)
 class HelpSettingsActivity : BaseActivity<HelpSettingsViewModel.ViewModel>() {
@@ -33,40 +31,44 @@ class HelpSettingsActivity : BaseActivity<HelpSettingsViewModel.ViewModel>() {
     private val supportEmailBody = R.string.support_email_body
     private val supportEmailSubject = R.string.support_email_subject
 
+    private lateinit var binding: ActivityHelpSettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_help_settings)
+        binding = ActivityHelpSettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         this.build = environment().build()
         this.currentUser = environment().currentUser()
 
-        contact.setOnClickListener {
+        binding.contact.setOnClickListener {
             this.viewModel.contactClicked()
 
             this.currentUser.observable()
-                    .take(1)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(this::composeContactEmail)
+                .take(1)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::composeContactEmail)
         }
 
-        accessibility_statement.setOnClickListener {
-            startChromeTab(buildWebEndpointUrl(ACCESSIBILITY), Intent(this, HelpActivity.AccessibilityStatement::class.java))
+        binding.accessibilityStatement.setOnClickListener {
+            startChromeTab(buildWebEndpointUrl(HelpActivity.ACCESSIBILITY), Intent(this, HelpActivity.AccessibilityStatement::class.java))
         }
 
-        cookie_policy.setOnClickListener {
-            startChromeTab(buildWebEndpointUrl(COOKIES), Intent(this, HelpActivity.CookiePolicy::class.java))
+        binding.cookiePolicy.setOnClickListener {
+            startChromeTab(buildWebEndpointUrl(HelpActivity.COOKIES), Intent(this, HelpActivity.CookiePolicy::class.java))
         }
 
-        help_center.setOnClickListener {
+        binding.helpCenter.setOnClickListener {
             startChromeTab(Secrets.HelpCenter.ENDPOINT, null)
         }
 
-        privacy_policy.setOnClickListener {
-            startChromeTab(buildWebEndpointUrl(PRIVACY), Intent(this, HelpActivity.Privacy::class.java))
+        binding.privacyPolicy.setOnClickListener {
+            startChromeTab(buildWebEndpointUrl(HelpActivity.PRIVACY), Intent(this, HelpActivity.Privacy::class.java))
         }
 
-        terms_of_use.setOnClickListener {
-            startChromeTab(buildWebEndpointUrl(TERMS_OF_USE), Intent(this, HelpActivity.Terms::class.java))
+        binding.termsOfUse.setOnClickListener {
+            startChromeTab(buildWebEndpointUrl(HelpActivity.TERMS_OF_USE), Intent(this, HelpActivity.Terms::class.java))
         }
     }
 
@@ -76,20 +78,20 @@ class HelpSettingsActivity : BaseActivity<HelpSettingsViewModel.ViewModel>() {
 
     private fun composeContactEmail(user: User?) {
         val debugInfo = Arrays.asList(
-                user?.id()?: getString(R.string.Logged_Out),
-                this.build.versionName(),
-                android.os.Build.VERSION.RELEASE + " (SDK " + Integer.toString(android.os.Build.VERSION.SDK_INT) + ")",
-                android.os.Build.MANUFACTURER + " " + android.os.Build.MODEL
+            user?.id() ?: getString(R.string.Logged_Out),
+            this.build.versionName(),
+            android.os.Build.VERSION.RELEASE + " (SDK " + Integer.toString(android.os.Build.VERSION.SDK_INT) + ")",
+            android.os.Build.MANUFACTURER + " " + android.os.Build.MODEL
         )
         val body = StringBuilder()
-                .append(getString(this.supportEmailBody))
-                .append(TextUtils.join(" | ", debugInfo))
-                .toString()
+            .append(getString(this.supportEmailBody))
+            .append(TextUtils.join(" | ", debugInfo))
+            .toString()
         val intent = Intent(Intent.ACTION_SENDTO)
-                .setData(Uri.parse(getString(this.mailto)))
-                .putExtra(Intent.EXTRA_SUBJECT, "[Android] " + getString(this.supportEmailSubject))
-                .putExtra(Intent.EXTRA_TEXT, body)
-                .putExtra(Intent.EXTRA_EMAIL, arrayOf<String>(getString(this.supportEmail)))
+            .setData(Uri.parse(getString(this.mailto)))
+            .putExtra(Intent.EXTRA_SUBJECT, "[Android] " + getString(this.supportEmailSubject))
+            .putExtra(Intent.EXTRA_TEXT, body)
+            .putExtra(Intent.EXTRA_EMAIL, arrayOf<String>(getString(this.supportEmail)))
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(Intent.createChooser(intent, getString(R.string.support_email_chooser)))
         }

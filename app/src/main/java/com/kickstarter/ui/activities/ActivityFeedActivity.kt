894bf6a648b61726dadf89e1fabf20e05012a420
Hadia <hadiamohamed.iti@gmail.com>
2021-02-18 17:23:44 +0200

    NT-1765:Refactor ActivityFeedActivity (#1097)
    * Rename .java to .kt
    
    * Refactor ot kotlin friendly
    Remove butter knife + use ViewBinding
    
    * Update ProjectCardViewHolder
    
    * Format File
    
    * Update ThanksAdapter
    ThanksCategoryViewHolder.kt
    ThanksShareViewHolder.kt
    
    * Update CreatorDashboardReferrerStatsAdapter and
    CreatorDashboardReferrerStatsRowViewHolder.kt
    
    * Update CreatorDashboardAdapter and
    CreatorDashboardRewardStatsViewHolder.kt
    CreatorDashboardReferrerStatsViewHolder.kt
    CreatorDashboardReferrerBreakdownViewHolder.kt
    CreatorDashboardHeaderViewHolder.kt
    
    * Update CreatorDashboardBottomSheetAdapter.java
    CreatorDashboardBottomSheetViewHolder.kt
    
    * Update CreatorDashboardRewardStatsAdapter and
    CreatorDashboardRewardStatsRowViewHolder.kt
    
    * Update MessagesAdapter and
    MessageCenterTimestampViewHolder.kt
    MessageViewHolder.kt
    
    * Update MessageViewHolder.kt and
    MessageCenterTimestampViewHolder.kt.kt
    
    * Rename .java to .kt
    
    * Update DiscoveryDrawerAdapter and
    ChildFilterViewHolder.kt
    HeaderViewHolder.kt
    LoggedInViewHolder.kt
    LoggedOutViewHolder.kt
    ParentFilterViewHolder.kt
    TopFilterViewHolder.kt
    
    * Update RewardsAdapter and
    RewardsItemViewHolder.kt
    
    * Update ProjectNotificationSettingsAdapter and
    ProjectNotificationViewHolder.kt
    
    * Update SearchActivity , SearchAdapter , SearchAdapterTest
    ProjectSearchResultViewHolder.kt
    PopularSearchTitleViewHolder.kt
    and create FeaturedSearchResultViewHolder.kt to separate view-binding layout
    
    * Update CommentsAdapter and ProjectSocialAdapter
    ProjectSocialViewHolder
    ProjectContextViewHolder.kt
    
    * Rename .java to .kt
    
    * Update view models
    
    * Update Adapter to remove butter knife
    
    * add layout id to binding
    
    * add viewBinding
    
    * Update project view holder
    
    * Fix Project Crash
    
    * Change EditorialViewHolder click listener
    
    * Update PaymentMethodsViewHolder.kt
    
    * update ThanksShareViewHolder.kt
    
    * Update MessageThreadViewHolder
    
    * Adapters
    
    * fix viewListener issue
    
    * Fix pledge backing item issue
    
    * Remove java class
    
    * Add id to thanks toolbar  to bind it
    
    * Rename .java to .kt
    
    * Convert ThanksActivity.java to ThanksActivity.kt
    and replace butter knife with view-binding
    
    * Fix app:checkstyle issues
    
    * solve indent issue
    
    * Convert ActivityFeedActivity.java to ActivityFeedActivity.kt
    remove Butter knife lib
    
    * Convert ActivityFeedActivity.java to ActivityFeedActivity.kt
    remove Butter knife lib
    
    * Use ContextCompat.getDrawable()
    
    * Use resource string with placeholders.
    
    * Delete extra line
    
    * Change to private
    
    * Fix identation
    
    * Change to private
    
    * Change to ConstraintLayout
    
    * Change to ConstraintLayout
    
    * change to private fun
    
    * Reformat code
    
    * Change currentUser.user
    
    * Handle null url for Picasso
    
    * Fix file format
    
    * fix adapter
    
    * Fix code comments

---
+++ app/src/main/java/com/kickstarter/ui/activities/ActivityFeedActivity.kt
@@ -0,0 +1,159 @@
+package com.kickstarter.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kickstarter.R
+import com.kickstarter.databinding.ActivityFeedLayoutBinding
+import com.kickstarter.libs.*
+import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
+import com.kickstarter.libs.rx.transformers.Transformers
+import com.kickstarter.libs.utils.ApplicationUtils
+import com.kickstarter.libs.utils.ObjectUtils
+import com.kickstarter.models.*
+import com.kickstarter.ui.IntentKey
+import com.kickstarter.ui.adapters.ActivityFeedAdapter
+import com.kickstarter.ui.data.LoginReason
+import com.kickstarter.viewmodels.ActivityFeedViewModel
+
+@RequiresActivityViewModel(ActivityFeedViewModel.ViewModel::class)
+class ActivityFeedActivity : BaseActivity<ActivityFeedViewModel.ViewModel>() {
+    private var adapter: ActivityFeedAdapter? = null
+    private var currentUser: CurrentUserType? = null
+    private var recyclerViewPaginator: RecyclerViewPaginator? = null
+    private var swipeRefresher: SwipeRefresher? = null
+    private lateinit var binding: ActivityFeedLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFeedLayoutBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        currentUser = environment().currentUser()
+
+        adapter = ActivityFeedAdapter(viewModel.inputs)
+
+        binding.recyclerView.adapter = adapter
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this@ActivityFeedActivity)
+
+        recyclerViewPaginator = RecyclerViewPaginator(binding.recyclerView, { viewModel.inputs.nextPage() }, viewModel.outputs.isFetchingActivities)
+
+        swipeRefresher = SwipeRefresher(
+            this, binding.activityFeedSwipeRefreshLayout, { viewModel.inputs.refresh() }
+        ) { viewModel.outputs.isFetchingActivities }
+
+        // Only allow refreshing if there's a current user
+        currentUser?.observable()
+            ?.map { `object`: User? -> ObjectUtils.isNotNull(`object`) }
+            ?.compose(bindToLifecycle())
+            ?.compose(Transformers.observeForUI())
+            ?.subscribe { binding.activityFeedSwipeRefreshLayout.isEnabled = it }
+
+        viewModel.outputs.activityList()
+            .compose(bindToLifecycle())
+            .compose<List<Activity?>>(Transformers.observeForUI())
+            .subscribe { showActivities(it) }
+
+        viewModel.outputs.erroredBackings()
+            .compose(bindToLifecycle())
+            .compose<List<ErroredBacking?>>(Transformers.observeForUI())
+            .subscribe { showErroredBackings(it) }
+
+        viewModel.outputs.goToDiscovery()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { resumeDiscoveryActivity() }
+
+        viewModel.outputs.goToLogin()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { startActivityFeedLogin() }
+
+        viewModel.outputs.goToProject()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { startProjectActivity(it) }
+
+        viewModel.outputs.startFixPledge()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { startFixPledge(it) }
+
+        viewModel.outputs.startUpdateActivity()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { startUpdateActivity(it) }
+
+        viewModel.outputs.loggedOutEmptyStateIsVisible()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { adapter?.showLoggedOutEmptyState(it) }
+
+        viewModel.outputs.loggedInEmptyStateIsVisible()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { adapter?.showLoggedInEmptyState(it) }
+
+        viewModel.outputs.surveys()
+            .compose(bindToLifecycle())
+            .compose<List<SurveyResponse?>>(Transformers.observeForUI())
+            .subscribe { showSurveys(it) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.inputs.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        recyclerViewPaginator?.stop()
+        binding.recyclerView.adapter = null
+    }
+
+    private fun showActivities(activities: List<Activity?>) {
+        adapter?.takeActivities(activities)
+    }
+
+    private fun showErroredBackings(erroredBackings: List<ErroredBacking?>) {
+        adapter?.takeErroredBackings(erroredBackings)
+    }
+
+    private fun showSurveys(surveyResponses: List<SurveyResponse?>) {
+        adapter?.takeSurveys(surveyResponses)
+    }
+
+    private fun resumeDiscoveryActivity() {
+        ApplicationUtils.resumeDiscoveryActivity(this)
+    }
+
+    private fun startActivityFeedLogin() {
+        val intent = Intent(this, LoginToutActivity::class.java)
+            .putExtra(IntentKey.LOGIN_REASON, LoginReason.ACTIVITY_FEED)
+        startActivityForResult(intent, ActivityRequestCodes.LOGIN_FLOW)
+    }
+
+    private fun startFixPledge(projectSlug: String) {
+        val intent = Intent(this, ProjectActivity::class.java)
+            .putExtra(IntentKey.PROJECT_PARAM, projectSlug)
+            .putExtra(IntentKey.EXPAND_PLEDGE_SHEET, true)
+            .putExtra(IntentKey.REF_TAG, RefTag.activity())
+        startActivityWithTransition(intent, R.anim.slide_in_right, R.anim.fade_out_slide_out_left)
+    }
+
+    private fun startProjectActivity(project: Project) {
+        val intent = Intent(this, ProjectActivity::class.java)
+            .putExtra(IntentKey.PROJECT, project)
+            .putExtra(IntentKey.REF_TAG, RefTag.activity())
+        startActivityWithTransition(intent, R.anim.slide_in_right, R.anim.fade_out_slide_out_left)
+    }
+
+    private fun startUpdateActivity(activity: Activity) {
+        val intent = Intent(this, UpdateActivity::class.java)
+            .putExtra(IntentKey.PROJECT, activity.project())
+            .putExtra(IntentKey.UPDATE, activity.update())
+        startActivityWithTransition(intent, R.anim.slide_in_right, R.anim.fade_out_slide_out_left)
+    }
+}

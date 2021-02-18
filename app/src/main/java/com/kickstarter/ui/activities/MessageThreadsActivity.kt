1f9dae0b1fc17f38929924a304373d9fd00d7e7a
hadia <hadiamohamed.iti@gmail.com>
2021-02-18 22:22:24 +0200

    convert MessageThreadsActivity.java to MessageThreadsActivity.kt Replace butterknife with viewbinding

--- app/src/main/java/com/kickstarter/ui/activities/MessageThreadsActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/MessageThreadsActivity.kt
@@ -1,188 +1,155 @@
-package com.kickstarter.ui.activities;
+package com.kickstarter.ui.activities
 
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.util.Pair;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.graphics.Typeface
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kickstarter.R
+import com.kickstarter.databinding.MessageThreadsLayoutBinding
+import com.kickstarter.libs.BaseActivity
+import com.kickstarter.libs.KSString
+import com.kickstarter.libs.RecyclerViewPaginator
+import com.kickstarter.libs.SwipeRefresher
+import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
+import com.kickstarter.libs.rx.transformers.Transformers
+import com.kickstarter.libs.utils.NumberUtils
+import com.kickstarter.libs.utils.ToolbarUtils.fadeToolbarTitleOnExpand
+import com.kickstarter.libs.utils.TransitionUtils
+import com.kickstarter.libs.utils.ViewUtils
+import com.kickstarter.libs.utils.extensions.wrapInParentheses
+import com.kickstarter.models.MessageThread
+import com.kickstarter.ui.adapters.MessageThreadsAdapter
+import com.kickstarter.ui.data.Mailbox
+import com.kickstarter.viewmodels.MessageThreadsViewModel
 
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.kickstarter.R;
-import com.kickstarter.libs.BaseActivity;
-import com.kickstarter.libs.KSString;
-import com.kickstarter.libs.RecyclerViewPaginator;
-import com.kickstarter.libs.SwipeRefresher;
-import com.kickstarter.libs.qualifiers.RequiresActivityViewModel;
-import com.kickstarter.libs.utils.NumberUtils;
-import com.kickstarter.libs.utils.ToolbarUtils;
-import com.kickstarter.libs.utils.ViewUtils;
-import com.kickstarter.libs.utils.extensions.StringExt;
-import com.kickstarter.models.MessageThread;
-import com.kickstarter.ui.adapters.MessageThreadsAdapter;
-import com.kickstarter.ui.data.Mailbox;
-import com.kickstarter.viewmodels.MessageThreadsViewModel;
+@RequiresActivityViewModel(MessageThreadsViewModel.ViewModel::class)
+class MessageThreadsActivity : BaseActivity<MessageThreadsViewModel.ViewModel>() {
+    private lateinit var adapter: MessageThreadsAdapter
+    private lateinit var ksString: KSString
+    private lateinit var recyclerViewPaginator: RecyclerViewPaginator
+    private lateinit var binding: MessageThreadsLayoutBinding
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.DiffUtil;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import butterknife.Bind;
-import butterknife.BindString;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MessageThreadsLayoutBinding.inflate(layoutInflater)
 
-import static com.kickstarter.libs.rx.transformers.Transformers.observeForUI;
-import static com.kickstarter.libs.utils.TransitionUtils.slideInFromLeft;
+        setContentView(binding.root)
 
-@RequiresActivityViewModel(MessageThreadsViewModel.ViewModel.class)
-public class MessageThreadsActivity extends BaseActivity<MessageThreadsViewModel.ViewModel> {
-  private MessageThreadsAdapter adapter;
-  private KSString ksString;
-  private RecyclerViewPaginator recyclerViewPaginator;
+        setUpAdapter()
 
-  protected @Bind(R.id.message_threads_app_bar_layout) AppBarLayout appBarLayout;
-  protected @Bind(R.id.message_threads_collapsed_toolbar_title) View collapsedToolbarTitle;
-  protected @Bind(R.id.message_threads_collapsed_toolbar_mailbox_title) TextView collapsedToolbarMailboxTitle;
-  protected @Bind(R.id.message_threads_collapsing_toolbar_layout) CollapsingToolbarLayout collapsingToolbarLayout;
-  protected @Bind(R.id.mailbox_text_view) TextView mailboxTextView;
-  protected @Bind(R.id.message_threads_recycler_view) RecyclerView recyclerView;
-  protected @Bind(R.id.message_threads_swipe_refresh_layout) SwipeRefreshLayout swipeRefreshLayout;
-  protected @Bind(R.id.switch_mailbox_button) Button switchMailboxButton;
-  protected @Bind(R.id.unread_count_text_view) TextView unreadCountTextView;
-  protected @Bind(R.id.message_threads_toolbar_unread_count_text_view) TextView unreadCountToolbarTextView;
+        ksString = environment().ksString()
 
-  protected @BindString(R.string.messages_navigation_inbox) String inboxString;
-  protected @BindString(R.string.messages_navigation_sent) String sentString;
-  protected @BindString(R.string.No_messages) String noMessagesString;
-  protected @BindString(R.string.No_unread_messages) String noUnreadMessagesString;
-  protected @BindString(R.string.font_family_sans_serif) String sansSerifString;
-  protected @BindString(R.string.unread_count_unread) String unreadCountUnreadString;
+        binding.messageThreadsRecyclerView.adapter = adapter
+        binding.messageThreadsRecyclerView.layoutManager = LinearLayoutManager(this)
 
-  @Override
-  protected void onCreate(final @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.message_threads_layout);
-    ButterKnife.bind(this);
+        recyclerViewPaginator = RecyclerViewPaginator(binding.messageThreadsRecyclerView, { viewModel.inputs.nextPage() }, viewModel.outputs.isFetchingMessageThreads)
 
-    setUpAdapter();
-    this.ksString = environment().ksString();
+        SwipeRefresher(
+            this, binding.messageThreadsSwipeRefreshLayout, { viewModel.inputs.swipeRefresh() }
+        ) { viewModel.outputs.isFetchingMessageThreads }
 
-    this.recyclerView.setAdapter(this.adapter);
-    this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        fadeToolbarTitleOnExpand(binding.messageThreadsAppBarLayout, binding.messageThreadsToolbar.messageThreadsCollapsedToolbarTitle)
 
-    this.recyclerViewPaginator = new RecyclerViewPaginator(this.recyclerView, this.viewModel.inputs::nextPage, this.viewModel.outputs.isFetchingMessageThreads());
-    new SwipeRefresher(
-      this, this.swipeRefreshLayout, this.viewModel.inputs::swipeRefresh, this.viewModel.outputs::isFetchingMessageThreads
-    );
+        viewModel.outputs.mailboxTitle()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { setMailboxStrings(it) }
 
-    ToolbarUtils.INSTANCE.fadeToolbarTitleOnExpand(this.appBarLayout, this.collapsedToolbarTitle);
+        viewModel.outputs.hasNoMessages()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { binding.unreadCountTextView.text = getString(R.string.No_messages) }
 
-    this.viewModel.outputs.mailboxTitle()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(this::setMailboxStrings);
+        viewModel.outputs.hasNoUnreadMessages()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { binding.unreadCountTextView.text = getString(R.string.No_unread_messages) }
+        viewModel.outputs.messageThreadList()
+            .compose(bindToLifecycle())
+            .compose<List<MessageThread?>>(Transformers.observeForUI())
+            .subscribe { adapter.messageThreads(it) }
 
-    this.viewModel.outputs.hasNoMessages()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(__ -> this.unreadCountTextView.setText(this.noMessagesString));
+        viewModel.outputs.unreadCountTextViewColorInt()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { binding.unreadCountTextView.setTextColor(ContextCompat.getColor(this, it)) }
 
-    this.viewModel.outputs.hasNoUnreadMessages()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(__ -> this.unreadCountTextView.setText(this.noUnreadMessagesString));
+        viewModel.outputs.unreadCountTextViewTypefaceInt()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe {
+                binding.unreadCountTextView.typeface = Typeface.create(getString(R.string.font_family_sans_serif), it)
+            }
 
-    this.viewModel.outputs.messageThreadList()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(this.adapter::messageThreads);
+        viewModel.outputs.unreadCountToolbarTextViewIsGone()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe(ViewUtils.setGone(binding.messageThreadsToolbar.messageThreadsToolbarUnreadCountTextView))
 
-    this.viewModel.outputs.unreadCountTextViewColorInt()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(colorInt -> this.unreadCountTextView.setTextColor(ContextCompat.getColor(this, colorInt)));
+        viewModel.outputs.unreadMessagesCount()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { setUnreadTextViewText(it) }
 
-    this.viewModel.outputs.unreadCountTextViewTypefaceInt()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(typeInt -> this.unreadCountTextView.setTypeface(Typeface.create(this.sansSerifString, typeInt)));
+        viewModel.outputs.unreadMessagesCountIsGone()
+            .compose(bindToLifecycle())
+            .compose(Transformers.observeForUI())
+            .subscribe { ViewUtils.setGone(binding.unreadCountTextView, it) }
 
-    this.viewModel.outputs.unreadCountToolbarTextViewIsGone()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(ViewUtils.setGone(this.unreadCountToolbarTextView));
+        binding.switchMailboxButton.setOnClickListener {
+            mailboxSwitchClicked()
+        }
+    }
 
-    this.viewModel.outputs.unreadMessagesCount()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(this::setUnreadTextViewText);
+    fun mailboxSwitchClicked() = viewModel.inputs.mailbox(
+        if (binding.mailboxTextView.text == this.getString(R.string.messages_navigation_inbox))
+            Mailbox.SENT
+        else
+            Mailbox.INBOX
+    )
 
-    this.viewModel.outputs.unreadMessagesCountIsGone()
-      .compose(bindToLifecycle())
-      .compose(observeForUI())
-      .subscribe(gone -> ViewUtils.setGone(this.unreadCountTextView, gone));
-  }
+    private fun setMailboxStrings(stringRes: Int) {
+        val mailbox = getString(stringRes)
+        binding.mailboxTextView.text = mailbox
+        binding.messageThreadsToolbar.messageThreadsCollapsedToolbarMailboxTitle.text = mailbox
+        binding.switchMailboxButton.text = if (mailbox == this.getString(R.string.messages_navigation_inbox)) getString(R.string.messages_navigation_sent) else this.getString(R.string.messages_navigation_inbox)
+    }
 
-  private void setMailboxStrings(final @NonNull Integer stringRes) {
-    final String mailbox = getString(stringRes);
-    this.mailboxTextView.setText(mailbox);
-    this.collapsedToolbarMailboxTitle.setText(mailbox);
-    this.switchMailboxButton.setText(mailbox.equals(this.inboxString) ? this.sentString : this.inboxString);
-  }
+    override fun exitTransition() = TransitionUtils.slideInFromLeft()
 
-  @Override
-  protected @Nullable Pair<Integer, Integer> exitTransition() {
-    return slideInFromLeft();
-  }
+    override fun onDestroy() {
+        super.onDestroy()
+        recyclerViewPaginator.stop()
+        binding.messageThreadsRecyclerView.adapter = null
+    }
 
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    this.recyclerViewPaginator.stop();
-    this.recyclerView.setAdapter(null);
-  }
+    override fun onResume() {
+        super.onResume()
+        viewModel.inputs.onResume()
+    }
 
-  @Override
-  protected void onResume() {
-    super.onResume();
-    this.viewModel.inputs.onResume();
-  }
+    private fun setUnreadTextViewText(unreadCount: Int) {
+        val unreadCountString = NumberUtils.format(unreadCount)
+        binding.unreadCountTextView.text = ksString.format(getString(R.string.unread_count_unread), "unread_count", unreadCountString)
+        binding.messageThreadsToolbar.messageThreadsToolbarUnreadCountTextView.text = unreadCountString.wrapInParentheses()
+    }
 
-  @OnClick(R.id.switch_mailbox_button)
-  protected void mailboxSwitchClicked() {
-    this.viewModel.inputs.mailbox(this.mailboxTextView.getText().equals(this.inboxString) ? Mailbox.SENT : Mailbox.INBOX);
-  }
+    private fun setUpAdapter() {
+        adapter = MessageThreadsAdapter(object : DiffUtil.ItemCallback<Any>() {
+            override fun areItemsTheSame(oldItem: Any, newItem: Any): Boolean {
+                return threadsAreTheSame(oldItem, newItem)
+            }
 
-  private void setUnreadTextViewText(final @NonNull Integer unreadCount) {
-    final String unreadCountString = NumberUtils.format(unreadCount);
-    this.unreadCountTextView.setText(
-      this.ksString.format(this.unreadCountUnreadString, "unread_count", unreadCountString)
-    );
-    this.unreadCountToolbarTextView.setText(StringExt.wrapInParentheses(unreadCountString));
-  }
+            override fun areContentsTheSame(oldItem: Any, newItem: Any): Boolean {
+                return threadsAreTheSame(oldItem, newItem)
+            }
 
-  private void setUpAdapter() {
-    this.adapter = new MessageThreadsAdapter(new DiffUtil.ItemCallback<Object>() {
-      @Override
-      public boolean areItemsTheSame(final @NonNull Object oldItem, final @NonNull Object newItem) {
-        return threadsAreTheSame(oldItem, newItem);
-      }
-      @Override
-      public boolean areContentsTheSame(final @NonNull Object oldItem, final @NonNull Object newItem) {
-        return threadsAreTheSame(oldItem, newItem);
-      }
-
-      private boolean threadsAreTheSame(final @NonNull Object oldItem, final @NonNull Object newItem) {
-        final MessageThread oldThread = (MessageThread) oldItem;
-        final MessageThread newThread = (MessageThread) newItem;
-
-        return oldThread.id() == newThread.id();
-      }
-    });
-  }
+            private fun threadsAreTheSame(oldItem: Any, newItem: Any): Boolean {
+                val oldThread = oldItem as MessageThread
+                val newThread = newItem as MessageThread
+                return oldThread.id() == newThread.id()
+            }
+        })
+    }
 }

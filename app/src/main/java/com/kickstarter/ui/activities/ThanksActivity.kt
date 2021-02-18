f634a45e57d5958952360125881b6d3f4703f8a5
hadia <hadiamohamed.iti@gmail.com>
2021-02-18 16:19:17 +0200

    fix comment

--- app/src/main/java/com/kickstarter/ui/activities/ThanksActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/ThanksActivity.kt
@@ -4,7 +4,6 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Pair
-import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -31,22 +30,26 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ThanksLayoutBinding.inflate(layoutInflater)
-        val view: View = binding.root
-        setContentView(view)
+        setContentView(binding.root)
         ksString = environment().ksString()
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.orientation = RecyclerView.VERTICAL
-        binding.thanksRecyclerView.layoutManager = layoutManager
+
+        binding.thanksRecyclerView.layoutManager = LinearLayoutManager(this).apply {
+            orientation = RecyclerView.VERTICAL
+        }
+
         val adapter = ThanksAdapter(viewModel.inputs)
         binding.thanksRecyclerView.adapter = adapter
+
         viewModel.outputs.adapterData()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { adapter.takeData(it) }
+
         viewModel.outputs.showConfirmGamesNewsletterDialog()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { showConfirmGamesNewsletterDialog() }
+
         viewModel.outputs.finish()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
@@ -63,6 +66,7 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
                     showGamesNewsletterDialog()
                 }
             }
+
         viewModel.outputs.showRatingDialog()
             .compose(bindToLifecycle())
             .take(1)
@@ -73,14 +77,17 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
                     showRatingDialog()
                 }
             }
+
         viewModel.outputs.startDiscoveryActivity()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { startDiscoveryActivity(it) }
+
         viewModel.outputs.startProjectActivity()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { startProjectActivity(it) }
+
         binding.thanksToolbar.closeButton.setOnClickListener { closeButtonClick() }
     }
 
@@ -97,6 +104,7 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
             "newsletter",
             getString(R.string.profile_settings_newsletter_games)
         )
+
         val builder = AlertDialog.Builder(this)
             .setMessage(optInDialogMessageString)
             .setTitle(R.string.profile_settings_newsletter_opt_in_title)

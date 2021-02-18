cebc62161ade2707aa38b194a5286ce5851864ae
Hadia <hadiamohamed.iti@gmail.com>
2021-02-18 22:38:33 +0200

    NT-1764:Refactor ThanksActivity (#1095)
    * Rename .java to .kt
    
    * Refactor ot kotlin friendly
    Remove butter knife + use ViewBinding
    
    * Update ProjectCardViewHolder
    
    * Format File
    
    * Update ThanksAdapter
    ThanksCategoryViewHolder.kt
    ThanksShareViewHolder.kt
    
    * Update CreatorDashboardReferrerStatsAdapter and
    CreatorDashboardReferrerStatsRowViewHolder.kt
    
    * Update CreatorDashboardAdapter and
    CreatorDashboardRewardStatsViewHolder.kt
    CreatorDashboardReferrerStatsViewHolder.kt
    CreatorDashboardReferrerBreakdownViewHolder.kt
    CreatorDashboardHeaderViewHolder.kt
    
    * Update CreatorDashboardBottomSheetAdapter.java
    CreatorDashboardBottomSheetViewHolder.kt
    
    * Update CreatorDashboardRewardStatsAdapter and
    CreatorDashboardRewardStatsRowViewHolder.kt
    
    * Update MessagesAdapter and
    MessageCenterTimestampViewHolder.kt
    MessageViewHolder.kt
    
    * Update MessageViewHolder.kt and
    MessageCenterTimestampViewHolder.kt.kt
    
    * Rename .java to .kt
    
    * Update DiscoveryDrawerAdapter and
    ChildFilterViewHolder.kt
    HeaderViewHolder.kt
    LoggedInViewHolder.kt
    LoggedOutViewHolder.kt
    ParentFilterViewHolder.kt
    TopFilterViewHolder.kt
    
    * Update RewardsAdapter and
    RewardsItemViewHolder.kt
    
    * Update ProjectNotificationSettingsAdapter and
    ProjectNotificationViewHolder.kt
    
    * Update SearchActivity , SearchAdapter , SearchAdapterTest
    ProjectSearchResultViewHolder.kt
    PopularSearchTitleViewHolder.kt
    and create FeaturedSearchResultViewHolder.kt to separate view-binding layout
    
    * Update CommentsAdapter and ProjectSocialAdapter
    ProjectSocialViewHolder
    ProjectContextViewHolder.kt
    
    * Rename .java to .kt
    
    * Update view models
    
    * Update Adapter to remove butter knife
    
    * add layout id to binding
    
    * add viewBinding
    
    * Update project view holder
    
    * Fix Project Crash
    
    * Change EditorialViewHolder click listener
    
    * Update PaymentMethodsViewHolder.kt
    
    * update ThanksShareViewHolder.kt
    
    * Update MessageThreadViewHolder
    
    * Adapters
    
    * fix viewListener issue
    
    * Fix pledge backing item issue
    
    * Remove java class
    
    * Add id to thanks toolbar  to bind it
    
    * Rename .java to .kt
    
    * Convert ThanksActivity.java to ThanksActivity.kt
    and replace butter knife with view-binding
    
    * Fix app:checkstyle issues
    
    * solve indent issue
    
    * Use ContextCompat.getDrawable()
    
    * Use resource string with placeholders.
    
    * Delete extra line
    
    * Change to private
    
    * Fix identation
    
    * Change to private
    
    * Change to ConstraintLayout
    
    * Change to ConstraintLayout
    
    * change to private fun
    
    * Reformat code
    
    * Change currentUser.user
    
    * Handle null url for Picasso
    
    * Fix file format
    
    * Fix format
    
    * Solve init issue
    
    * Reformat file
    
    * fix comment

--- app/src/main/java/com/kickstarter/ui/activities/ThanksActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/ThanksActivity.kt
@@ -4,13 +4,13 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Pair
-import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kickstarter.R
 import com.kickstarter.databinding.ThanksLayoutBinding
 import com.kickstarter.libs.BaseActivity
+import com.kickstarter.libs.KSString
 import com.kickstarter.libs.RefTag
 import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
 import com.kickstarter.libs.rx.transformers.Transformers
@@ -24,28 +24,32 @@ import java.util.concurrent.TimeUnit
 
 @RequiresActivityViewModel(ThanksViewModel.ViewModel::class)
 class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
-    private var ksString = environment().ksString()
+    private lateinit var ksString: KSString
     private lateinit var binding: ThanksLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ThanksLayoutBinding.inflate(layoutInflater)
-        val view: View = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+        ksString = environment().ksString()
 
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.orientation = RecyclerView.VERTICAL
-        binding.thanksRecyclerView.layoutManager = layoutManager
+        binding.thanksRecyclerView.layoutManager = LinearLayoutManager(this).apply {
+            orientation = RecyclerView.VERTICAL
+        }
+
         val adapter = ThanksAdapter(viewModel.inputs)
         binding.thanksRecyclerView.adapter = adapter
+
         viewModel.outputs.adapterData()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { adapter.takeData(it) }
+
         viewModel.outputs.showConfirmGamesNewsletterDialog()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { showConfirmGamesNewsletterDialog() }
+
         viewModel.outputs.finish()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
@@ -62,6 +66,7 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
                     showGamesNewsletterDialog()
                 }
             }
+
         viewModel.outputs.showRatingDialog()
             .compose(bindToLifecycle())
             .take(1)
@@ -72,14 +77,17 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
                     showRatingDialog()
                 }
             }
+
         viewModel.outputs.startDiscoveryActivity()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { startDiscoveryActivity(it) }
+
         viewModel.outputs.startProjectActivity()
             .compose(bindToLifecycle())
             .compose(Transformers.observeForUI())
             .subscribe { startProjectActivity(it) }
+
         binding.thanksToolbar.closeButton.setOnClickListener { closeButtonClick() }
     }
 
@@ -88,9 +96,7 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
         binding.thanksRecyclerView.adapter = null
     }
 
-    fun closeButtonClick() {
-        viewModel.inputs.closeButtonClicked()
-    }
+    private fun closeButtonClick() = viewModel.inputs.closeButtonClicked()
 
     private fun showConfirmGamesNewsletterDialog() {
         val optInDialogMessageString = ksString.format(
@@ -98,6 +104,7 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
             "newsletter",
             getString(R.string.profile_settings_newsletter_games)
         )
+
         val builder = AlertDialog.Builder(this)
             .setMessage(optInDialogMessageString)
             .setTitle(R.string.profile_settings_newsletter_opt_in_title)
@@ -113,9 +120,7 @@ class ThanksActivity : BaseActivity<ThanksViewModel.ViewModel>() {
         builder.show()
     }
 
-    private fun showRatingDialog() {
-        ViewUtils.showRatingDialog(this)
-    }
+    private fun showRatingDialog() = ViewUtils.showRatingDialog(this)
 
     private fun startDiscoveryActivity(params: DiscoveryParams) {
         val intent = Intent(this, DiscoveryActivity::class.java)

6ca8e817690075fa5aa0e282d1c07f5fe6b83103
hadia <hadiamohamed.iti@gmail.com>
2021-02-19 16:45:29 +0200

    convert ProjectNotificationSettingsActivity.java to ProjectNotificationSettingsActivity.kt

--- app/src/main/java/com/kickstarter/ui/activities/ProjectNotificationSettingsActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/ProjectNotificationSettingsActivity.kt
@@ -1,52 +1,48 @@
-package com.kickstarter.ui.activities;
+package com.kickstarter.ui.activities
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kickstarter.R
+import com.kickstarter.databinding.ProjectNotificationSettingsLayoutBinding
+import com.kickstarter.libs.BaseActivity
+import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
+import com.kickstarter.ui.adapters.ProjectNotificationSettingsAdapter
+import com.kickstarter.ui.extensions.showSnackbar
+import com.kickstarter.viewmodels.ProjectNotificationSettingsViewModel
+import rx.android.schedulers.AndroidSchedulers
 
-import com.kickstarter.R;
-import com.kickstarter.libs.BaseActivity;
-import com.kickstarter.libs.qualifiers.RequiresActivityViewModel;
-import com.kickstarter.ui.adapters.ProjectNotificationSettingsAdapter;
-import com.kickstarter.ui.extensions.ActivityExtKt;
-import com.kickstarter.viewmodels.ProjectNotificationSettingsViewModel;
+@RequiresActivityViewModel(ProjectNotificationSettingsViewModel.ViewModel::class)
+class ProjectNotificationSettingsActivity : BaseActivity<ProjectNotificationSettingsViewModel.ViewModel>() {
+    private lateinit var binding: ProjectNotificationSettingsLayoutBinding
 
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import butterknife.Bind;
-import butterknife.BindString;
-import butterknife.ButterKnife;
-import rx.android.schedulers.AndroidSchedulers;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ProjectNotificationSettingsLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-@RequiresActivityViewModel(ProjectNotificationSettingsViewModel.ViewModel.class)
-public final class ProjectNotificationSettingsActivity extends BaseActivity<ProjectNotificationSettingsViewModel.ViewModel> {
-  protected @Bind(R.id.project_notification_settings_recycler_view) RecyclerView recyclerView;
+        val adapter = ProjectNotificationSettingsAdapter()
 
-  protected @BindString(R.string.general_error_something_wrong) String generalErrorString;
+        binding.projectNotificationSettingsRecyclerView.adapter = adapter
+        binding.projectNotificationSettingsRecyclerView.layoutManager = LinearLayoutManager(this)
 
-  @Override
-  protected void onCreate(final @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.project_notification_settings_layout);
-    ButterKnife.bind(this);
+        viewModel.outputs.projectNotifications()
+            .compose(bindToLifecycle())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { adapter.projectNotifications(it) }
 
-    final ProjectNotificationSettingsAdapter adapter = new ProjectNotificationSettingsAdapter();
-    this.recyclerView.setAdapter(adapter);
-    this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        viewModel.outputs.unableToFetchProjectNotificationsError()
+            .compose(bindToLifecycle())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                showSnackbar(
+                    binding.projectNotificationSettingsRecyclerView,
+                    getString(R.string.general_error_something_wrong)
+                )
+            }
+    }
 
-    this.viewModel.outputs.projectNotifications()
-      .compose(bindToLifecycle())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(adapter::projectNotifications);
-
-    this.viewModel.outputs.unableToFetchProjectNotificationsError()
-      .compose(bindToLifecycle())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(__ -> ActivityExtKt.showSnackbar(this.recyclerView, this.generalErrorString));
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    this.recyclerView.setAdapter(null);
-  }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.projectNotificationSettingsRecyclerView.adapter = null
+    }
 }

9fb44d76137f746844f98c340432757ba53a1717
hadia <hadiamohamed.iti@gmail.com>
2021-02-22 18:18:30 +0200

    convert VideoActivity.java to VideoActivity.kt

--- app/src/main/java/com/kickstarter/ui/activities/VideoActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/VideoActivity.kt
@@ -1,143 +1,129 @@
-package com.kickstarter.ui.activities;
+package com.kickstarter.ui.activities
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelection
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.kickstarter.databinding.VideoPlayerLayoutBinding
+import com.kickstarter.libs.BaseActivity
+import com.kickstarter.libs.Build
+import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
+import com.kickstarter.libs.rx.transformers.Transformers
+import com.kickstarter.libs.utils.WebUtils.userAgent
+import com.kickstarter.viewmodels.VideoViewModel
+import com.trello.rxlifecycle.ActivityEvent
 
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.hls.HlsMediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
-import com.kickstarter.R;
-import com.kickstarter.libs.BaseActivity;
-import com.kickstarter.libs.Build;
-import com.kickstarter.libs.qualifiers.RequiresActivityViewModel;
-import com.kickstarter.libs.rx.transformers.Transformers;
-import com.kickstarter.libs.utils.WebUtils;
-import com.kickstarter.viewmodels.VideoViewModel;
-import com.trello.rxlifecycle.ActivityEvent;
+@RequiresActivityViewModel(VideoViewModel.ViewModel::class)
+class VideoActivity : BaseActivity<VideoViewModel.ViewModel>() {
+    private lateinit var build: Build
+    private var player: ExoPlayer? = null
+    private var playerPosition: Long? = null
+    private var trackSelector: DefaultTrackSelector? = null
+    private lateinit var binding: VideoPlayerLayoutBinding
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import butterknife.Bind;
-import butterknife.ButterKnife;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = VideoPlayerLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-@RequiresActivityViewModel(VideoViewModel.ViewModel.class)
-public final class VideoActivity extends BaseActivity<VideoViewModel.ViewModel> {
-  private Build build;
-  private ExoPlayer player;
-  private long playerPosition;
-  private DefaultTrackSelector trackSelector;
+        build = environment().build()
 
-  protected @Bind(R.id.video_player_layout) View rootView;
-  protected @Bind(R.id.player_view) PlayerView playerView;
-  protected @Bind(R.id.loading_indicator) ProgressBar loadingIndicatorProgressBar;
-
-  @Override
-  public void onCreate(final @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.video_player_layout);
-    ButterKnife.bind(this);
-
-    this.build = environment().build();
-
-    this.viewModel.outputs.preparePlayerWithUrl()
-      .compose(Transformers.takeWhen(lifecycle().filter(ActivityEvent.RESUME::equals)))
-      .compose(bindToLifecycle())
-      .subscribe(this::preparePlayer);
-  }
-
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
-    releasePlayer();
-  }
-
-  @Override
-  public void onPause() {
-    super.onPause();
-    releasePlayer();
-  }
-
-  @Override
-  public void onWindowFocusChanged(final boolean hasFocus) {
-    super.onWindowFocusChanged(hasFocus);
-
-    if (hasFocus) {
-      this.rootView.setSystemUiVisibility(systemUIFlags());
-    }
-  }
-
-  private int systemUIFlags() {
-    return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-      | View.SYSTEM_UI_FLAG_FULLSCREEN
-      | View.SYSTEM_UI_FLAG_IMMERSIVE;
-  }
-
-  private void onStateChanged(final int playbackState) {
-    if (playbackState == Player.STATE_ENDED) {
-      finish();
+        viewModel.outputs.preparePlayerWithUrl()
+            .compose(Transformers.takeWhen(lifecycle().filter { other: ActivityEvent? -> ActivityEvent.RESUME.equals(other) }))
+            .compose(bindToLifecycle())
+            .subscribe { preparePlayer(it) }
     }
 
-    if (playbackState == Player.STATE_BUFFERING) {
-      this.loadingIndicatorProgressBar.setVisibility(View.VISIBLE);
-    } else {
-      this.loadingIndicatorProgressBar.setVisibility(View.GONE);
+    public override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
     }
-  }
 
-  private void preparePlayer(final @NonNull String videoUrl) {
-    final TrackSelection.Factory adaptiveTrackSelectionFactory = new AdaptiveTrackSelection.Factory();
-    this.trackSelector = new DefaultTrackSelector(adaptiveTrackSelectionFactory);
-
-    this.player = ExoPlayerFactory.newSimpleInstance(this, this.trackSelector);
-    this.playerView.setPlayer(this.player);
-    this.player.addListener(this.eventListener);
-
-    this.player.seekTo(this.playerPosition);
-    final boolean playerIsResuming = this.playerPosition != 0;
-    this.player.prepare(getMediaSource(videoUrl), playerIsResuming, false);
-    this.player.setPlayWhenReady(true);
-  }
-
-  private MediaSource getMediaSource(final @NonNull String videoUrl) {
-    final DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory(WebUtils.INSTANCE.userAgent(this.build));
-    final Uri videoUri = Uri.parse(videoUrl);
-    final int fileType = Util.inferContentType(videoUri);
-
-    if (fileType == C.TYPE_HLS) {
-      return new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(videoUri);
-    } else {
-      return new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(videoUri);
+    public override fun onPause() {
+        super.onPause()
+        releasePlayer()
     }
-  }
 
-  private void releasePlayer() {
-    if (this.player != null) {
-      this.playerPosition = this.player.getCurrentPosition();
-      this.player.removeListener(this.eventListener);
-      this.player.release();
-      this.trackSelector = null;
-      this.player = null;
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            binding.videoPlayerLayout.systemUiVisibility = systemUIFlags()
+        }
     }
-  }
 
-  private @NonNull Player.EventListener eventListener =
-    new Player.EventListener() {
-      @Override
-      public void onPlayerStateChanged(final boolean playWhenReady, final int playbackState) {
-        onStateChanged(playbackState);
-      }
-    };
+    private fun systemUIFlags(): Int {
+        return (
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE
+            )
+    }
+
+    private fun onStateChanged(playbackState: Int) {
+        if (playbackState == Player.STATE_ENDED) {
+            finish()
+        }
+
+        if (playbackState == Player.STATE_BUFFERING) {
+            binding.loadingIndicator.visibility = View.VISIBLE
+        } else {
+            binding.loadingIndicator.visibility = View.GONE
+        }
+    }
+
+    private fun preparePlayer(videoUrl: String) {
+        val adaptiveTrackSelectionFactory: TrackSelection.Factory = AdaptiveTrackSelection.Factory()
+        trackSelector = DefaultTrackSelector(adaptiveTrackSelectionFactory)
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+
+        binding.playerView.player = player
+        player?.addListener(eventListener)
+        playerPosition?.let {
+            player?.seekTo(it)
+        }
+
+        val playerIsResuming = (playerPosition != 0L)
+        player?.prepare(getMediaSource(videoUrl), playerIsResuming, false)
+        player?.playWhenReady = true
+    }
+
+    private fun getMediaSource(videoUrl: String): MediaSource {
+        val dataSourceFactory = DefaultHttpDataSourceFactory(userAgent(build))
+        val videoUri = Uri.parse(videoUrl)
+        val fileType = Util.inferContentType(videoUri)
+
+        return if (fileType == C.TYPE_HLS) {
+            HlsMediaSource.Factory(dataSourceFactory).createMediaSource(videoUri)
+        } else {
+            ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(videoUri)
+        }
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playerPosition = player?.currentPosition
+            player?.removeListener(eventListener)
+            player?.release()
+            trackSelector = null
+            player = null
+        }
+    }
+
+    private val eventListener: Player.EventListener = object : Player.EventListener {
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            onStateChanged(playbackState)
+        }
+    }
 }

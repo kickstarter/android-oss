e104b81ddb7367d13a21535cc036b2dce4a31bf1
hadia <hadiamohamed.iti@gmail.com>
2021-02-23 18:01:20 +0200

    convert FacebookConfirmationActivity.java into FacebookConfirmationActivity.kt

--- app/src/main/java/com/kickstarter/ui/activities/FacebookConfirmationActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/FacebookConfirmationActivity.kt
@@ -1,100 +1,84 @@
-package com.kickstarter.ui.activities;
+package com.kickstarter.ui.activities
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Pair;
-import android.widget.TextView;
+import android.content.Intent
+import android.os.Bundle
+import android.util.Pair
+import com.jakewharton.rxbinding.view.RxView
+import com.kickstarter.R
+import com.kickstarter.databinding.FacebookConfirmationLayoutBinding
+import com.kickstarter.libs.ActivityRequestCodes
+import com.kickstarter.libs.BaseActivity
+import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
+import com.kickstarter.libs.utils.SwitchCompatUtils
+import com.kickstarter.libs.utils.TransitionUtils
+import com.kickstarter.libs.utils.ViewUtils
+import com.kickstarter.viewmodels.FacebookConfirmationViewModel
+import rx.android.schedulers.AndroidSchedulers
 
-import com.jakewharton.rxbinding.view.RxView;
-import com.kickstarter.R;
-import com.kickstarter.libs.ActivityRequestCodes;
-import com.kickstarter.libs.BaseActivity;
-import com.kickstarter.libs.qualifiers.RequiresActivityViewModel;
-import com.kickstarter.libs.utils.SwitchCompatUtils;
-import com.kickstarter.libs.utils.ViewUtils;
-import com.kickstarter.ui.toolbars.LoginToolbar;
-import com.kickstarter.viewmodels.FacebookConfirmationViewModel;
+@RequiresActivityViewModel(FacebookConfirmationViewModel.ViewModel::class)
+class FacebookConfirmationActivity : BaseActivity<FacebookConfirmationViewModel.ViewModel>() {
+    private lateinit var binding: FacebookConfirmationLayoutBinding
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.SwitchCompat;
-import butterknife.Bind;
-import butterknife.BindString;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import rx.android.schedulers.AndroidSchedulers;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FacebookConfirmationLayoutBinding.inflate(layoutInflater)
+    
+        setContentView(binding.root)
 
-import static com.kickstarter.libs.utils.TransitionUtils.slideInFromLeft;
-import static com.kickstarter.libs.utils.TransitionUtils.slideInFromRight;
-import static com.kickstarter.libs.utils.TransitionUtils.transition;
+        binding.signUpWithFacebookToolbar.loginToolbar.title = getString(R.string.facebook_confirmation_navbar_title)
+        
+        viewModel.outputs.prefillEmail()
+            .compose(bindToLifecycle())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { prefillEmail(it) }
+        
+        viewModel.outputs.signupSuccess()
+            .compose(bindToLifecycle())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { onSuccess() }
+        viewModel.outputs.sendNewslettersIsChecked()
+            .compose(bindToLifecycle())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { SwitchCompatUtils.setCheckedWithoutAnimation(binding.newsletterSwitch, it) }
+        
+        viewModel.outputs.signupError()
+            .compose(bindToLifecycle())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { ViewUtils.showDialog(this, getString(R.string.signup_error_title), it) }
+        
+        RxView.clicks(binding.newsletterSwitch)
+            .compose(bindToLifecycle())
+            .subscribe { viewModel.inputs.sendNewslettersClick(binding.newsletterSwitch.isChecked) }
+        
+        binding.createNewAccountButton.setOnClickListener {
+            createNewAccountClick()
+        }
 
-@RequiresActivityViewModel(FacebookConfirmationViewModel.ViewModel.class)
-public class FacebookConfirmationActivity extends BaseActivity<FacebookConfirmationViewModel.ViewModel> {
-  protected @Bind(R.id.email) TextView emailTextView;
-  protected @Bind(R.id.help_button) TextView helpButton;
-  protected @Bind(R.id.sign_up_with_facebook_toolbar) LoginToolbar signUpWithFacebookToolbar;
-  protected @Bind(R.id.newsletter_switch) SwitchCompat newsletterSwitch;
+        binding.loginButton.setOnClickListener {
+            loginWithEmailClick()
+        }
+    }
 
-  protected @BindString(R.string.facebook_confirmation_navbar_title) String signUpWithFacebookString;
-  protected @BindString(R.string.signup_error_title) String errorTitleString;
+    private fun createNewAccountClick() {
+        viewModel.inputs.createNewAccountClick()
+    }
 
-  @Override
-  public void onCreate(final @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    private fun loginWithEmailClick() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivityForResult(intent, ActivityRequestCodes.LOGIN_FLOW)
+        TransitionUtils.transition(this, TransitionUtils.slideInFromRight())
+    }
 
-    setContentView(R.layout.facebook_confirmation_layout);
-    ButterKnife.bind(this);
-    this.signUpWithFacebookToolbar.setTitle(this.signUpWithFacebookString);
+    private fun onSuccess() {
+        setResult(RESULT_OK)
+        finish()
+    }
 
-    this.viewModel.outputs.prefillEmail()
-      .compose(bindToLifecycle())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(this::prefillEmail);
+    override fun exitTransition(): Pair<Int, Int>? {
+        return TransitionUtils.slideInFromLeft()
+    }
 
-    this.viewModel.outputs.signupSuccess()
-      .compose(bindToLifecycle())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(__ -> onSuccess());
-
-    this.viewModel.outputs.sendNewslettersIsChecked()
-      .compose(bindToLifecycle())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(b -> SwitchCompatUtils.setCheckedWithoutAnimation(this.newsletterSwitch, b));
-
-    this.viewModel.outputs.signupError()
-      .compose(bindToLifecycle())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(e -> ViewUtils.showDialog(this, this.errorTitleString, e));
-
-    RxView.clicks(this.newsletterSwitch)
-      .compose(bindToLifecycle())
-      .subscribe(__ -> this.viewModel.inputs.sendNewslettersClick(this.newsletterSwitch.isChecked()));
-  }
-
-  @OnClick(R.id.create_new_account_button)
-  public void createNewAccountClick() {
-    this.viewModel.inputs.createNewAccountClick();
-  }
-
-  @OnClick(R.id.login_button)
-  public void loginWithEmailClick() {
-    final Intent intent = new Intent(this, LoginActivity.class);
-    startActivityForResult(intent, ActivityRequestCodes.LOGIN_FLOW);
-    transition(this, slideInFromRight());
-  }
-
-  public void onSuccess() {
-    setResult(Activity.RESULT_OK);
-    finish();
-  }
-
-  @Override
-  protected @Nullable Pair<Integer, Integer> exitTransition() {
-    return slideInFromLeft();
-  }
-
-  private void prefillEmail(final @NonNull String email) {
-    this.emailTextView.setText(email);
-  }
+    private fun prefillEmail(email: String) {
+        binding.email.text = email
+    }
 }

02567a53fdd660f340e4fa120d75647d948bcf11
hadia <hadiamohamed.iti@gmail.com>
2021-02-24 19:08:55 +0200

    convert ConfirmDialog.java into ConfirmDialog.kt

--- app/src/main/java/com/kickstarter/ui/views/ConfirmDialog.kt
+++ app/src/main/java/com/kickstarter/ui/views/ConfirmDialog.kt
@@ -1,100 +1,79 @@
-package com.kickstarter.ui.views;
+package com.kickstarter.ui.views
 
-import android.content.Context;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDialog
+import com.kickstarter.R
+import com.kickstarter.databinding.GenericDialogAlertBinding
 
-import com.kickstarter.R;
+class ConfirmDialog @JvmOverloads constructor(
+    context: Context,
+    val title: String?,
+    val message: String,
+    private val buttonText: String? = null
+) : AppCompatDialog(context) {
+    
+    private lateinit var binding: GenericDialogAlertBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        binding = GenericDialogAlertBinding.inflate(layoutInflater)
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDialog;
-import butterknife.Bind;
-import butterknife.BindString;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-public class ConfirmDialog extends AppCompatDialog {
-  private final @Nullable String title;
-  private final @NonNull String message;
-  private final @Nullable String buttonText;
-
-  protected @Bind(R.id.title_text_view) TextView titleTextView;
-  protected @Bind(R.id.message_text_view) TextView messageTextView;
-  protected @Bind(R.id.ok_button) Button okButton;
-
-  protected @BindString(R.string.general_alert_buttons_ok) String okString;
-
-  public ConfirmDialog(final @NonNull Context context, final @Nullable String title, final @NonNull String message) {
-    super(context);
-    this.title = title;
-    this.message = message;
-    this.buttonText = null;
-  }
-
-  public ConfirmDialog(final @NonNull Context context, final @Nullable String title, final @NonNull String message,
-    final @Nullable String buttonText) {
-    super(context);
-    this.title = title;
-    this.message = message;
-    this.buttonText = buttonText;
-  }
-
-  @Override
-  protected void onCreate(final @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-    setContentView(R.layout.generic_dialog_alert);
-    ButterKnife.bind(this);
-
-    if (this.title != null) {
-      setTitleText(this.title);
-    } else {
-      this.titleTextView.setVisibility(View.GONE);
+        setContentView(binding.root)
+       
+        if (title != null) {
+            setTitleText(title)
+        } else {
+            binding.titleTextView.visibility = View.GONE
+        }
+        
+        if (buttonText != null) {
+            setButtonText(buttonText)
+        } else {
+            setButtonText(context.getString(R.string.general_alert_buttons_ok))
+        }
+        
+        setMessage(message)
+        
+        binding.okButton.setOnClickListener { 
+            okButtonClick()
+        }
     }
 
-    if (this.buttonText != null) {
-      setButtonText(this.buttonText);
-    } else {
-      setButtonText(this.okString);
+    private fun setButtonText(buttonText: String) {
+        binding.okButton.text = buttonText
     }
 
-    setMessage(this.message);
-  }
+    /**
+     * Set the title on the TextView with id title_text_view.
+     * Note, default visibility is GONE since we may not always want a title.
+     */
+    private fun setTitleText(title: String) {
+        binding.titleTextView.text = title
+        binding.titleTextView.visibility = TextView.VISIBLE
+        val params = binding.messageTextView.layoutParams as LinearLayout.LayoutParams
+        params.topMargin = context.resources.getDimension(R.dimen.grid_1).toInt()
+        binding.messageTextView.layoutParams = params
+    }
 
-  public void setButtonText(final @NonNull String buttonText) {
-    this.okButton.setText(buttonText);
-  }
+    /**
+     * Set the message on the TextView with id message_text_view.
+     */
+    private fun setMessage(message: String) {
+        binding.messageTextView.text = message
+    }
 
-  /**
-   * Set the title on the TextView with id title_text_view.
-   * Note, default visibility is GONE since we may not always want a title.
-   */
-  public void setTitleText(final @NonNull String title) {
-    this.titleTextView.setText(title);
-    this.titleTextView.setVisibility(TextView.VISIBLE);
+    /**
+     * Dismiss the dialog on click ok_button".
+     */
 
-    final LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) this.messageTextView.getLayoutParams();
-    params.topMargin = (int) getContext().getResources().getDimension(R.dimen.grid_1);
-    this.messageTextView.setLayoutParams(params);
-  }
-
-  /**
-   * Set the message on the TextView with id message_text_view.
-   */
-  public void setMessage(final @NonNull String message) {
-    this.messageTextView.setText(message);
-  }
-
-  /**
-   * Dismiss the dialog on click ok_button".
-   */
-  @OnClick(R.id.ok_button)
-  protected void okButtonClick() {
-    dismiss();
-  }
+    private fun okButtonClick() {
+        dismiss()
+    }
 }

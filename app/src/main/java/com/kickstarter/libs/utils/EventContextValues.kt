8d7cc9d2fca5448ab5b62423f03ab12f734139fe
Sunday Okpoluaefe <sunday.okpoluaefe@gmail.com>
2021-03-01 22:36:02 +0100

    removed comments

--- app/src/main/java/com/kickstarter/libs/utils/EventContextValues.kt
+++ app/src/main/java/com/kickstarter/libs/utils/EventContextValues.kt
@@ -50,7 +50,8 @@ class EventContextValues {
 
     enum class LocationContextName(val contextName: String) {
         DISCOVER_ADVANCED("discover_advanced"),
-        DISCOVER_OVERLAY("discover_overlay")
+        DISCOVER_OVERLAY("discover_overlay"),
+        GLOBAL_NAV("global_nav")
     }
 
     enum class DiscoveryContextType(val contextName: String) {

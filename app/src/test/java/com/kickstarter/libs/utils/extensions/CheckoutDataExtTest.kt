7d4da0893ebca10cebae3429d8765bd4ac47515b
Isabel Martin <arkariang@gmail.com>
2021-03-03 13:15:15 -0800

    - Added more tests

--- app/src/test/java/com/kickstarter/libs/utils/extensions/CheckoutDataExtTest.kt
+++ app/src/test/java/com/kickstarter/libs/utils/extensions/CheckoutDataExtTest.kt
@@ -1,5 +1,6 @@
 package com.kickstarter.libs.utils.extensions
 
+import com.kickstarter.mock.factories.ProjectFactory
 import com.kickstarter.ui.data.CheckoutData
 import junit.framework.TestCase
 import type.CreditCardPaymentType
@@ -24,5 +25,51 @@ class CheckoutDataExtTest: TestCase() {
                 .build()
 
         assertEquals(coData.totalAmount(), 230.0)
+    }
+
+    fun testBonus_withUSDProject() {
+        val project = ProjectFactory.project()
+        val coData = CheckoutData.builder()
+                .paymentType(CreditCardPaymentType.CREDIT_CARD)
+                .shippingAmount(30.0)
+                .amount(200.0)
+                .build()
+
+        assertEquals(coData.bonus(), coData.bonus(project.staticUsdRate()))
+    }
+
+    fun testBonus_withCAProject() {
+        val project = ProjectFactory.caProject()
+        val coData = CheckoutData.builder()
+                .paymentType(CreditCardPaymentType.CREDIT_CARD)
+                .shippingAmount(30.0)
+                .amount(200.0)
+                .build()
+
+        val expectedBonusUSD = coData.bonus() * project.staticUsdRate()
+        assertEquals(expectedBonusUSD, coData.bonus(project.staticUsdRate()))
+    }
+
+    fun testShipping_withUSDProject() {
+        val project = ProjectFactory.project()
+        val coData = CheckoutData.builder()
+                .paymentType(CreditCardPaymentType.CREDIT_CARD)
+                .shippingAmount(30.0)
+                .amount(200.0)
+                .build()
+
+        assertEquals(coData.shippingAmount(), coData.shippingAmount(project.staticUsdRate()))
+    }
+
+    fun testShipping_withCAProject() {
+        val project = ProjectFactory.caProject()
+        val coData = CheckoutData.builder()
+                .paymentType(CreditCardPaymentType.CREDIT_CARD)
+                .shippingAmount(30.0)
+                .amount(200.0)
+                .build()
+
+        val expectedShippingInUSD = coData.shippingAmount() * project.staticUsdRate()
+        assertEquals(expectedShippingInUSD, coData.shippingAmount(project.staticUsdRate()))
     }
 }
--- app/src/test/java/com/kickstarter/libs/utils/extensions/PledgeDataExtTest.kt
+++ app/src/test/java/com/kickstarter/libs/utils/extensions/PledgeDataExtTest.kt
@@ -1,15 +1,60 @@
 package com.kickstarter.libs.utils.extensions
 
+import com.kickstarter.mock.factories.ProjectDataFactory.Companion.project
+import com.kickstarter.mock.factories.ProjectFactory
+import com.kickstarter.mock.factories.RewardFactory
+import com.kickstarter.models.Reward
+import com.kickstarter.ui.data.PledgeData.Companion.with
+import com.kickstarter.ui.data.PledgeFlowContext
 import junit.framework.TestCase
 
 class PledgeDataExtTest : TestCase() {
 
     fun testAddOnsCountTotalEmpty() {
+        val project = ProjectFactory.project()
+        val rw = RewardFactory.reward()
+        val addOns = emptyList<Reward>() as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        assertEquals(pledgeData.totalCountUnique(), 0)
+        assertEquals(pledgeData.totalQuantity(), 0)
     }
 
     fun testAddOnsCountTotal_whenMultipleQuantity_singleAddOn() {
+        val project = ProjectFactory.project()
+        val rw = RewardFactory.rewardHasAddOns()
+        val addOn = RewardFactory.addOnSingle()
+        val addOns = listOf(addOn, addOn) as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        assertEquals(pledgeData.totalCountUnique(), addOns.size)
+        assertEquals(pledgeData.totalQuantity(), 2)
     }
 
     fun testAddOnsCountTotal_whenMultipleQuantity_multipleAddOns() {
+        val project = ProjectFactory.project()
+        val rw = RewardFactory.rewardHasAddOns()
+        val addOn = RewardFactory.addOnMultiple()
+        val addOns = listOf(addOn, addOn) as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        assertEquals(pledgeData.totalCountUnique(), addOns.size)
+        assertEquals(pledgeData.totalQuantity(), 10)
+    }
+
+    fun testAddOnsCountTotal_whenSingleAndMultiple_total() {
+        val project = ProjectFactory.project()
+        val rw = RewardFactory.rewardHasAddOns()
+        val addOnSingle = RewardFactory.addOnSingle()
+        val addOnMultiple = RewardFactory.addOnMultiple()
+        val addOns = listOf(addOnSingle, addOnMultiple) as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        assertEquals(pledgeData.totalCountUnique(), addOns.size)
+        assertEquals(pledgeData.totalQuantity(), 6)
     }
 }

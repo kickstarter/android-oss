6ca9a068aa30e999052ef50f93c066f222b2330f
Isabel Martin <arkariang@gmail.com>
2021-03-03 13:52:56 -0800

    - Added more tests

--- app/src/test/java/com/kickstarter/libs/utils/extensions/PledgeDataExtTest.kt
+++ app/src/test/java/com/kickstarter/libs/utils/extensions/PledgeDataExtTest.kt
@@ -57,4 +57,53 @@ class PledgeDataExtTest : TestCase() {
         assertEquals(pledgeData.totalCountUnique(), addOns.size)
         assertEquals(pledgeData.totalQuantity(), 6)
     }
+
+    fun testAddOnsCost_whenUSDProject() {
+        val project = ProjectFactory.project()
+        val rw = RewardFactory.rewardHasAddOns()
+        val addOnSingle = RewardFactory.addOnSingle().toBuilder().minimum(30.0).build()
+        val addOnMultiple = RewardFactory.addOnMultiple().toBuilder().minimum(5.0).build()
+        val addOns = listOf(addOnSingle, addOnMultiple) as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        val expectedValue = 30.0 + (5.0 * 5)
+        assertEquals(pledgeData.addOnsCost(project.staticUsdRate()), expectedValue)
+    }
+
+    fun testAddOnsCost_whenCAProject() {
+        val project = ProjectFactory.caProject()
+        val rw = RewardFactory.rewardHasAddOns()
+        // - quantity = 1 on addOnSingle
+        val addOnSingle = RewardFactory.addOnSingle().toBuilder().minimum(30.0).build()
+        // - quantity = 5 on addOnMultiple
+        val addOnMultiple = RewardFactory.addOnMultiple().toBuilder().minimum(5.0).build()
+        val addOns = listOf(addOnSingle, addOnMultiple) as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        val expectedValue = (30.0 + (5.0 * 5)) * project.staticUsdRate()
+        assertEquals(pledgeData.addOnsCost(project.staticUsdRate()), expectedValue)
+    }
+
+    fun testRewardCost_whenUSDProject() {
+        val project = ProjectFactory.project()
+        val rw = RewardFactory.rewardHasAddOns()
+        val addOns = emptyList<Reward>() as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        assertEquals(pledgeData.rewardCost(project.staticUsdRate()), rw.minimum())
+    }
+
+    fun testRewardCost_whenCAProject() {
+        val project = ProjectFactory.caProject()
+        val rw = RewardFactory.rewardHasAddOns()
+        val addOns = emptyList<Reward>() as java.util.List<Reward>
+        val pledgeData = with(PledgeFlowContext.NEW_PLEDGE,
+                project(project), rw, addOns, null)
+
+        val expectedValue = rw.minimum() * project.staticUsdRate()
+        assertEquals(pledgeData.rewardCost(project.staticUsdRate()), expectedValue)
+    }
 }

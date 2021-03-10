62cf2574757ca053df859d7c29a95ccc6d701b40
hadia <hadiamohamed.iti@gmail.com>
2021-03-10 18:46:25 +0200

    Reformat code

--- app/src/main/java/com/kickstarter/libs/utils/ViewUtils.java
+++ app/src/main/java/com/kickstarter/libs/utils/ViewUtils.java
@@ -211,10 +211,10 @@ public final class ViewUtils {
     }
   }
 
-/**
- *  @deprecated Sets the visibility of a view . Setting the view to INVISIBLE makes it hidden,
- *  but it still takes up space.
- */
+  /**
+   * @deprecated Sets the visibility of a view . Setting the view to INVISIBLE makes it hidden,
+   * but it still takes up space.
+   */
   @Deprecated
   public static Action1<Boolean> setInvisible(final @NonNull View view) {
     return (invisible) -> setInvisible(view, invisible);

28cdddc995fdb78e3ab4d8db27e7937d756a0231
Isabel Martin <arkariang@gmail.com>
2021-03-15 11:51:46 -0700

    - added report to milkrun

--- lib/milkrun/checkstyle.rb
+++ lib/milkrun/checkstyle.rb
@@ -3,7 +3,7 @@ require_relative 'report'
 module Milkrun
   class Checkstyle < Report
     def report_path
-      File.join(Milkrun.app_dir, "build", "reports", "checkstyle", "checkstyle.html")
+      File.join(Milkrun.app_dir, "build", "reports", "checkstyle", "checkstyle.html", "ktlint.html")
     end
 
     private

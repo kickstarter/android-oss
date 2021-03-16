412bc71d14de8a4b243f082cb213e98c324ee1b9
Sunday Okpoluaefe <sunday.okpoluaefe@gmail.com>
2021-03-16 13:05:52 +0100

    added event method and fix failing test cases

--- app/src/main/java/com/kickstarter/libs/utils/EventContextValues.kt
+++ app/src/main/java/com/kickstarter/libs/utils/EventContextValues.kt
@@ -29,7 +29,8 @@ class EventContextValues {
         WATCH_PROJECT("watch_project"),
         LOGIN_INITIATE("log_in_initiate"),
         CAMPAIGN_DETAILS("campaign_details"),
-        CREATOR_DETAILS("creator_details")
+        CREATOR_DETAILS("creator_details"),
+        SIGN_UP_SUBMIT("sign_up_submit")
     }
 
     /**
@@ -56,7 +57,8 @@ class EventContextValues {
         SEARCH("search"),
         THANKS("thanks"),
         UPDATE_PLEDGE("update_pledge"),
-        LOGIN_SIGN_UP("log_in_sign_up")
+        LOGIN_SIGN_UP("log_in_sign_up"),
+        SIGN_UP("sign_up")
     }
 
     /**

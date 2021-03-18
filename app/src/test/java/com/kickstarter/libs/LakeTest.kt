505b5ae642f5891ac57edb45801d266688172b78
hadia <hadiamohamed.iti@gmail.com>
2021-03-18 23:42:23 +0200

    move test from LakeTest to SegmentTest

--- app/src/test/java/com/kickstarter/libs/LakeTest.kt
+++ app/src/test/java/com/kickstarter/libs/LakeTest.kt
@@ -475,31 +475,6 @@ class LakeTest : KSRobolectricTestCase() {
         this.lakeTest.assertValues("Project Page Pledge Button Clicked")
     }
 
-    @Test
-    fun testVideoProperties() {
-        val project = project()
-        val user = user()
-        val client = client(user)
-        client.eventNames.subscribe(this.lakeTest)
-        client.eventProperties.subscribe(this.propertiesTest)
-
-        val videoLength = 100L
-        val videoPosition = 0L
-
-        val lake = AnalyticEvents(listOf(client))
-
-        lake.trackProjectPageViewed(ProjectDataFactory.project(project, RefTag.discovery(), RefTag.recommended()), null)
-
-        assertSessionProperties(user)
-        assertProjectProperties(project)
-        assertContextProperties()
-
-        lake.trackVideoStarted(project, videoLength, videoPosition)
-        assertVideoProperties(videoLength, videoPosition)
-
-        this.lakeTest.assertValues("Project Page Viewed", "Video Playback Started")
-    }
-
     private fun client(user: User?) = MockTrackingClient(
         user?.let { MockCurrentUser(it) }
             ?: MockCurrentUser(),
@@ -602,12 +577,6 @@ class LakeTest : KSRobolectricTestCase() {
         assertEquals("agent", expectedProperties["session_user_agent"])
         assertEquals(user != null, expectedProperties["session_user_is_logged_in"])
         assertEquals(false, expectedProperties["session_wifi_connection"])
-    }
-
-    private fun assertVideoProperties(videoLength: Long, videoPosition: Long) {
-        val expectedProperties = this.propertiesTest.value
-        assertEquals(videoLength, expectedProperties["video_length"])
-        assertEquals(videoPosition, expectedProperties["video_position"])
     }
 
     private fun mockCurrentConfig() = MockCurrentConfig().apply {

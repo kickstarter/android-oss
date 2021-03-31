fd91f2fd0aa0c39bd314bdcd74338979557e3f28
Isabel Martin <arkariang@gmail.com>
2021-03-30 17:48:22 -0700

    - linter

--- app/src/main/java/com/kickstarter/viewmodels/BackingFragmentViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/BackingFragmentViewModel.kt
@@ -219,13 +219,13 @@ interface BackingFragmentViewModel {
                 .share()
 
             this.isExpanded
-                    .filter { it}
-                    .compose(combineLatestPair(backing))
-                    .map { it.second }
-                    .compose(bindToLifecycle())
-                    .subscribe {
-                        //this.lake.trackManagePledgePageViewed(it)
-                    }
+                .filter { it }
+                .compose(combineLatestPair(backing))
+                .map { it.second }
+                .compose(bindToLifecycle())
+                .subscribe {
+                    // this.lake.trackManagePledgePageViewed(it)
+                }
 
             val isCreator = Observable.combineLatest(this.currentUser.observable(), backedProject) { user, project ->
                 Pair(user, project)

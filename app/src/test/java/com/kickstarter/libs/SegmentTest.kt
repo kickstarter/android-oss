408935d6aa99350f1b1bedd63a835e1eb465ad84
sunday-okpoluaefe <63934292+sunday-okpoluaefe@users.noreply.github.com>
2021-04-08 16:20:30 +0100

    EP-451: Page Viewed (sign_up) Unit Test (#1196)
    * fixed failing test cases
    
    * removed un used imports
    
    * fixed lint errors

--- app/src/test/java/com/kickstarter/libs/SegmentTest.kt
+++ app/src/test/java/com/kickstarter/libs/SegmentTest.kt
@@ -935,6 +935,26 @@ class SegmentTest : KSRobolectricTestCase() {
         this.segmentTrack.assertValue(EventName.CTA_CLICKED.eventName)
     }
 
+    @Test
+    fun testSignUpPageViewed_Properties() {
+
+        val client = client(null)
+        client.eventNames.subscribe(this.segmentTrack)
+        client.eventProperties.subscribe(this.propertiesTest)
+
+        val segment = AnalyticEvents(listOf(client))
+        segment.trackSignUpPageViewed()
+
+        assertSessionProperties(null)
+        assertContextProperties()
+
+        val properties = this.propertiesTest.value
+        assertNull(properties["user_uid"])
+        assertEquals(EventContextValues.ContextPageName.SIGN_UP.contextName, properties[CONTEXT_PAGE.contextName])
+
+        this.segmentTrack.assertValue(EventName.PAGE_VIEWED.eventName)
+    }
+
     private fun client(user: User?) = MockTrackingClient(
         user?.let { MockCurrentUser(it) }
             ?: MockCurrentUser(),

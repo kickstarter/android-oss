3b0e4348722939dc78432e23899ceae0b6b15a83
Sunday Okpoluaefe <sunday.okpoluaefe@gmail.com>
2021-04-09 08:41:13 +0100

    fixed failing test cases

--- app/src/test/java/com/kickstarter/viewmodels/SearchViewModelTest.java
+++ app/src/test/java/com/kickstarter/viewmodels/SearchViewModelTest.java
@@ -84,7 +84,7 @@ public class SearchViewModelTest extends KSRobolectricTestCase {
     this.vm.inputs.search("hello world!");
     this.searchProjectsPresent.assertValues(false, true);
     scheduler.advanceTimeBy(300, TimeUnit.MILLISECONDS);
-    this.lakeTest.assertValues("Search Button Clicked", EventName.CTA_CLICKED.getEventName(), "Search Page Viewed", "Search Results Loaded", EventName.PAGE_VIEWED.getEventName());
+    this.lakeTest.assertValues("Search Button Clicked", EventName.CTA_CLICKED.getEventName(), "Search Page Viewed", "Search Results Loaded");
 
     // Waiting enough time emits search results
     scheduler.advanceTimeBy(500, TimeUnit.MILLISECONDS);

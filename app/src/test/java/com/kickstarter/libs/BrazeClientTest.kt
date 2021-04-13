1d420228ad44a3c660d671aed6c3e09f082baed5
Isabel Martin <arkariang@gmail.com>
2021-04-13 15:56:54 -0700

    - Adding tests

--- app/src/test/java/com/kickstarter/libs/BrazeClientTest.kt
+++ app/src/test/java/com/kickstarter/libs/BrazeClientTest.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import com.google.firebase.messaging.RemoteMessage
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.libs.braze.BrazeClient
+import com.kickstarter.libs.utils.ConfigFeatureName
+import com.kickstarter.mock.MockCurrentConfig
+import com.kickstarter.mock.factories.ConfigFactory
 import org.junit.Test
 
 const val mockSenderId = "MockSender"
@@ -22,15 +25,38 @@ class BrazeClientTest : KSRobolectricTestCase() {
     }
 
     @Test
-    fun testInitialize() {
-        val mockClient = MockBrazeClient(build, context)
+    fun testInitialize_whenEnabledFeatureFlag() {
+        val mockConfig = mockCurrentConfig(enabledFeatureFlag = true)
+        val mockClient = MockBrazeClient(build, context, mockConfig)
         assertEquals(mockSenderId, mockClient.getIdSender())
         assertNotNull(mockClient.getLifeCycleCallbacks())
+        assertTrue(mockClient.isInitialized)
     }
 
     @Test
-    fun testHandleMessageNotBraze() {
-        val mockClient = MockBrazeClient(build, context)
+    fun testInitialize_whenDisabledFeatureFlag() {
+        val mockConfig = mockCurrentConfig(enabledFeatureFlag = false)
+        val mockClient = MockBrazeClient(build, context, mockConfig)
+        assertEquals(mockSenderId, "")
+        assertFalse(mockClient.isInitialized)
+    }
+
+    @Test
+    fun testHandleMessageNotBraze_whenEnabledFeatureFlag() {
+        val mockConfig = mockCurrentConfig(enabledFeatureFlag = true)
+        val mockClient = MockBrazeClient(build, context, mockConfig)
+        assertEquals(mockSenderId, mockClient.getIdSender())
+        assertNotNull(mockClient.getLifeCycleCallbacks())
+        assertTrue(mockClient.isInitialized)
+
+        val message: RemoteMessage = RemoteMessage(Bundle())
+        assertFalse(mockClient.handleRemoteMessages(context, message))
+    }
+
+    @Test
+    fun testHandleMessageNotBraze_whenDisabledFeatureFlag() {
+        val mockConfig = mockCurrentConfig(enabledFeatureFlag = false)
+        val mockClient = MockBrazeClient(build, context, mockConfig)
         assertEquals(mockSenderId, mockClient.getIdSender())
         assertNotNull(mockClient.getLifeCycleCallbacks())
 
@@ -38,17 +64,30 @@ class BrazeClientTest : KSRobolectricTestCase() {
         assertFalse(mockClient.handleRemoteMessages(context, message))
     }
 
+    private fun mockCurrentConfig(enabledFeatureFlag: Boolean) = MockCurrentConfig().apply {
+        var config =
+                if (enabledFeatureFlag)
+                    ConfigFactory.configWithFeatureEnabled(ConfigFeatureName.BRAZE_ENABLED.configFeatureName)
+                else ConfigFactory.configWithFeatureDisabled(ConfigFeatureName.BRAZE_ENABLED.configFeatureName)
+        config(config)
+    }
+
     class MockBrazeClient(
         private val build: Build,
-        private val context: Context
-    ) : BrazeClient(build = build, context = context) {
+        private val context: Context,
+        private val config: CurrentConfigType
+    ) : BrazeClient(build = build, context = context, configuration = config) {
+        private var initialized = false
+
+        override val isInitialized: Boolean
+            get() = this.initialized
 
         override fun getIdSender(): String {
             return mockSenderId
         }
 
         override fun init() {
-            // - Do nothing
+            initialized = this.isSDKEnabled()
         }
 
         override fun getLifeCycleCallbacks(): Application.ActivityLifecycleCallbacks {

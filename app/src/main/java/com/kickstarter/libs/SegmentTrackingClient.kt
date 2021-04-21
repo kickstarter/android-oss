36d67045da21d06f1c6e9e176ec2420371e50a6c
Isabel Martin <arkariang@gmail.com>
2021-04-21 15:07:25 -0700

    - Added User preferences Opt-in on the Identify call for Segment

--- app/src/main/java/com/kickstarter/libs/SegmentTrackingClient.kt
+++ app/src/main/java/com/kickstarter/libs/SegmentTrackingClient.kt
@@ -2,6 +2,22 @@ package com.kickstarter.libs
 
 import android.content.Context
 import com.kickstarter.models.User
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_BACKINGS
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_CREATOR_DIG
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_CREATOR_EDU
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_FOLLOWER
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_FRIEND_ACTIVITY
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_MESSAGE
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_NOTIFY_COMMENT
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_REPLAY
+import com.kickstarter.ui.data.OptInSubscriptionTypes.Email.EMAIL_UPDATES
+import com.kickstarter.ui.data.OptInSubscriptionTypes.PushNotification.PUSH_BACKINGS
+import com.kickstarter.ui.data.OptInSubscriptionTypes.PushNotification.PUSH_FOLLOWER
+import com.kickstarter.ui.data.OptInSubscriptionTypes.PushNotification.PUSH_FRIEND_ACTIVITY
+import com.kickstarter.ui.data.OptInSubscriptionTypes.PushNotification.PUSH_LIKE
+import com.kickstarter.ui.data.OptInSubscriptionTypes.PushNotification.PUSH_MESSAGE
+import com.kickstarter.ui.data.OptInSubscriptionTypes.PushNotification.PUSH_NOTIFY_COMMENT
+import com.kickstarter.ui.data.OptInSubscriptionTypes.PushNotification.PUSH_UPDATES
 import com.segment.analytics.Analytics
 import com.segment.analytics.Properties
 import com.segment.analytics.Traits
@@ -72,9 +88,29 @@ class SegmentTrackingClient(
      * In order to send custom properties to segment for the Identify method we need to use
      * the method Traits() from the Segment SDK
      * see https://segment.com/docs/connections/sources/catalog/libraries/mobile/android/#identify
+     *
+     * Added as trait the user name
+     * Added as traits the user preferences for Email and Push Notifications Subscriptions
      */
     private fun getTraits(user: User) = Traits().apply {
         this.putName(user.name())
-        this.putAvatar(user.avatar().toString())
+        // - Email related subscriptions
+        this[EMAIL_BACKINGS.name] = user.notifyOfBackings()
+        this[EMAIL_UPDATES.name] = user.notifyOfUpdates()
+        this[EMAIL_FOLLOWER.name] = user.notifyOfFollower()
+        this[EMAIL_FRIEND_ACTIVITY.name] = user.notifyOfFriendActivity()
+        this[EMAIL_NOTIFY_COMMENT.name] = user.notifyOfComments()
+        this[EMAIL_CREATOR_EDU.name] = user.notifyOfCreatorEdu()
+        this[EMAIL_CREATOR_DIG.name] = user.notifyOfCreatorDigest()
+        this[EMAIL_MESSAGE.name] = user.notifyOfMessages()
+        this[EMAIL_REPLAY.name] = user.notifyOfCommentReplies()
+        // - Push Notifications related subscriptions
+        this[PUSH_BACKINGS.name] = user.notifyMobileOfBackings()
+        this[PUSH_UPDATES.name] = user.notifyMobileOfUpdates()
+        this[PUSH_FOLLOWER.name] = user.notifyMobileOfFollower()
+        this[PUSH_FRIEND_ACTIVITY.name] = user.notifyMobileOfFriendActivity()
+        this[PUSH_NOTIFY_COMMENT.name] = user.notifyMobileOfComments()
+        this[PUSH_MESSAGE.name] = user.notifyMobileOfMessages()
+        this[PUSH_LIKE.name] = user.notifyMobileOfPostLikes()
     }
 }

acfa1372fc1e6a99618e1992f629dc834da5b241
Hadia <hadiamohamed.iti@gmail.com>
2021-05-11 18:23:55 +0200

    Change click DisclaimerItem  click to open app directly (#1239)
    * Change click DisclaimerItem  click to open app directly
    
    * change to final

---
+++ app/src/main/java/com/kickstarter/ui/extensions/TextViewExt.kt
@@ -0,0 +1,55 @@
+package com.kickstarter.ui.extensions
+
+import android.text.Selection
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import org.jsoup.Jsoup
+import java.util.Locale
+
+fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
+    val spannableString = SpannableString(this.text)
+    var startIndexOfLink = -1
+    for (link in links) {
+        val clickableSpan = object : ClickableSpan() {
+            override fun updateDrawState(textPaint: TextPaint) {
+                textPaint.isUnderlineText = true
+            }
+
+            override fun onClick(view: View) {
+                Selection.setSelection((view as TextView).text as Spannable, 0)
+                view.invalidate()
+                link.second.onClick(view)
+            }
+        }
+
+        startIndexOfLink = this.text.toString().indexOf(link.first.toLowerCase(Locale.getDefault()), startIndexOfLink + 1)
+
+        if (startIndexOfLink == -1) continue // todo if you want to verify your texts contains links text
+
+        spannableString.setSpan(
+            clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+
+    this.setText(spannableString, TextView.BufferType.SPANNABLE)
+    this.movementMethod =
+        LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+    this.linksClickable = true
+}
+
+/**
+Parse HTML into a Document. As no base URI is specified, absolute URL detection relies on the HTML including a
+{@code <base href>} tag.
+
+@see #Jsoup.parse(String)
+ */
+fun TextView.parseHtmlTag() {
+    this.text = Jsoup.parse(this.text.toString()).text()
+}

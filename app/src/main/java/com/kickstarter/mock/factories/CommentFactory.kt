7766d1b87d220d600b01a18f2d6fb273fba760f3
sunday-okpoluaefe <63934292+sunday-okpoluaefe@users.noreply.github.com>
2021-05-18 15:24:06 +0100

    NT-1924; Create GraphQL Comment Data Models (#1246)
    * added comment model
    
    * added unit testing to model
    
    * added comment cursor
    
    * fixed lint errors
    
    * refactored equals method
    
    * refactored equals method
    
    * added replies to comment model
    
    * added replies to comment model
    
    * refactored comment model
    
    * added replies count to model
    
    * added replies count to model
    
    * renamed comment model
    
    * renamed comment model
    
    Co-authored-by: Sunday Onoriode <sunday-onoriode@Sunday-Onoriode-MacBook-Pro.local>
    Co-authored-by: Isabel Martin <arkariang@gmail.com>

---
+++ app/src/main/java/com/kickstarter/mock/factories/CommentFactory.kt
@@ -0,0 +1,28 @@
+package com.kickstarter.mock.factories
+
+import com.kickstarter.models.Comment
+import org.joda.time.DateTime
+
+class CommentFactory {
+
+    companion object {
+        fun comment(): Comment {
+            return Comment.builder()
+                .id(1)
+                .author(
+                    UserFactory.user()
+                        .toBuilder()
+                        .id(1)
+                        .build()
+                )
+                .parentId(1)
+                .repliesCount(0)
+                .cursor("")
+                .authorBadges(listOf())
+                .deleted(false)
+                .createdAt(DateTime.parse("2021-01-01T00:00:00Z"))
+                .body("Some Comment")
+                .build()
+        }
+    }
+}

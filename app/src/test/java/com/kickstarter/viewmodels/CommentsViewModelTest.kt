4a9a697e02d400db4449c508551a599f374199a0
Leigh Douglas <leighcdouglas1@gmail.com>
2021-06-01 19:20:49 -0400

    Fix wildcard import

--- app/src/test/java/com/kickstarter/viewmodels/CommentsViewModelTest.kt
+++ app/src/test/java/com/kickstarter/viewmodels/CommentsViewModelTest.kt
@@ -3,7 +3,12 @@ package com.kickstarter.viewmodels
 import android.content.Intent
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.libs.MockCurrentUser
-import com.kickstarter.mock.factories.*
+import com.kickstarter.mock.factories.UserFactory
+import com.kickstarter.mock.factories.ProjectFactory
+import com.kickstarter.mock.factories.AvatarFactory
+import com.kickstarter.mock.factories.UpdateFactory
+import com.kickstarter.mock.factories.CommentEnvelopeFactory
+import com.kickstarter.mock.factories.CommentFactory
 import com.kickstarter.mock.services.MockApolloClient
 import com.kickstarter.services.apiresponses.commentresponse.CommentEnvelope
 import com.kickstarter.ui.IntentKey

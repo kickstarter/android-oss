5c4462e2013180e555a35d2f2da77e5d12eb55bd
hadia <hadiamohamed.iti@gmail.com>
2021-06-03 00:02:05 +0200

    fix code comment

--- app/src/main/java/com/kickstarter/viewmodels/CommentsViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/CommentsViewModel.kt
@@ -165,11 +165,9 @@ interface CommentsViewModel {
                 .compose(Transformers.takeWhen(this.refresh))
                 .doOnNext {
                     this.isRefreshing.onNext(true)
-                }
-                .doOnNext {
+                    // reset cursor
                     lastCommentCursour = null
-                }
-                .compose(CommentEnvelopeTransformer(initialProject))
+                }.compose(CommentEnvelopeTransformer(initialProject))
                 .compose(bindToLifecycle())
                 .subscribe {
                     bindCommentList(it.first, LoadingType.PULL_REFRESH, it.second)

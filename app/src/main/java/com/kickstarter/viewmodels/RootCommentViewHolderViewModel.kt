a163b49d23c1cc1d2f5a3b846503011deae6e4c6
hadia <hadiamohamed.iti@gmail.com>
2021-06-30 15:23:22 +0200

    create RootCommentViewHolderViewModel

---
+++ app/src/main/java/com/kickstarter/viewmodels/RootCommentViewHolderViewModel.kt
@@ -0,0 +1,40 @@
+package com.kickstarter.viewmodels
+
+import com.kickstarter.libs.ActivityViewModel
+import com.kickstarter.libs.Environment
+import com.kickstarter.models.Comment
+import com.kickstarter.ui.viewholders.PaginationErrorViewHolder
+import rx.Observable
+import rx.subjects.BehaviorSubject
+
+interface RootCommentViewHolderViewModel {
+    interface Inputs {
+        fun configureWith(configureCellWith: Comment)
+    }
+
+    interface Outputs {
+        fun bindRootComment(): Observable<Comment>
+    }
+
+    class ViewModel(environment: Environment) : ActivityViewModel<PaginationErrorViewHolder>(environment), Inputs, Outputs {
+        private val bindRootComment = BehaviorSubject.create<Comment>()
+        private val initCellConfig = BehaviorSubject.create<Comment>()
+
+        val inputs = this
+        val outputs = this
+
+        init {
+            this.initCellConfig
+                .compose(bindToLifecycle())
+                .subscribe {
+                    this.bindRootComment.onNext(it)
+                }
+        }
+
+        // - Inputs
+        override fun configureWith(comment: Comment) = this.initCellConfig.onNext(comment)
+
+        // - Outputs
+        override fun bindRootComment(): Observable<Comment> = this.bindRootComment
+    }
+}

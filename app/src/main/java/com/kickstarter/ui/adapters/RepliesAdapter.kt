bc8063e97fa245d8cfd9ad523a4656364d8efbe6
Hadia <hadiamohamed.iti@gmail.com>
2021-07-13 21:13:36 +0200

    Hadia/refactor replies adapter (#1318)
    * Use same cell to show error and view more action
    
    * Refactor root comment in separate layout to solve sections binding issue
    
    * Remove unneeded   displayPaginationError
    
    * Fix crash for first reply
    
    * Fix crash for first reply
    
    * ConcatAdapter and create 3 different adapters
    
    * Fix Scroll issue
    
    * Fix Scroll issue
    
    * Rename variables
    
    * add code comments
    
    * add code comments
    
    * Remove Delegate
    
    * Fix code adapter

--- app/src/main/java/com/kickstarter/ui/adapters/RepliesAdapter.kt
+++ app/src/main/java/com/kickstarter/ui/adapters/RepliesAdapter.kt
@@ -5,72 +5,36 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import com.kickstarter.R
 import com.kickstarter.databinding.ItemCommentCardBinding
-import com.kickstarter.databinding.ItemErrorPaginationBinding
-import com.kickstarter.databinding.ItemShowMoreRepliesBinding
-import com.kickstarter.models.Comment
 import com.kickstarter.ui.data.CommentCardData
 import com.kickstarter.ui.viewholders.CommentCardViewHolder
 import com.kickstarter.ui.viewholders.KSViewHolder
-import com.kickstarter.ui.viewholders.PaginationErrorViewHolder
-import com.kickstarter.ui.viewholders.PaginationViewMoreRepliesViewHolder
-import com.kickstarter.ui.viewholders.RootCommentViewHolder
 
+/** Replies list adapter to show replies list **/
 class RepliesAdapter(private val delegate: Delegate) : KSListAdapter() {
     interface Delegate :
-        CommentCardViewHolder.Delegate,
-        PaginationViewMoreRepliesViewHolder.ViewListener,
-        PaginationErrorViewHolder.ViewListener
+        CommentCardViewHolder.Delegate
 
     init {
         insertSection(SECTION_COMMENTS, emptyList<CommentCardData>())
-        insertSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, emptyList<Boolean>())
-        insertSection(SECTION_ERROR_PAGINATING, emptyList<Boolean>())
-        insertSection(SECTION_ROOT_COMMENT, emptyList<Comment>())
     }
 
-    fun takeData(replies: List<CommentCardData>, shouldViewMoreRepliesCell: Boolean) {
+    fun takeData(replies: List<CommentCardData>) {
         if (replies.isNotEmpty()) {
             setSection(SECTION_COMMENTS, replies)
-            setSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, listOf(shouldViewMoreRepliesCell))
+        } else {
+            setSection(SECTION_COMMENTS, emptyList<CommentCardData>())
         }
-        setSection(SECTION_ERROR_PAGINATING, listOf(false))
+
         submitList(items())
     }
 
-    fun updateRootCommentCell(rootComment: Comment) {
-        setSection(SECTION_ROOT_COMMENT, listOf(rootComment))
-        submitList(items())
-    }
-
-    fun addErrorPaginationCell(shouldShowErrorCell: Boolean) {
-        setSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, listOf(false))
-        // - we want to display SECTION_COMMENTS & SECTION_ERROR_PAGINATING at the same time so we should not clean SECTION_COMMENTS
-        setSection(SECTION_ERROR_PAGINATING, listOf(shouldShowErrorCell))
-        submitList(items())
-    }
-
-    override fun layout(sectionRow: SectionRow): Int = when (sectionRow.section()) {
-        SECTION_COMMENTS -> R.layout.item_comment_card
-        SECTION_SHOW_MORE_REPLIES_PAGINATING -> R.layout.item_show_more_replies
-        SECTION_ERROR_PAGINATING -> R.layout.item_error_pagination
-        else -> 0
-    }
+    override fun layout(sectionRow: SectionRow): Int = R.layout.item_comment_card
 
     override fun viewHolder(@LayoutRes layout: Int, viewGroup: ViewGroup): KSViewHolder {
-        return when (layout) {
-            R.layout.item_comment_card -> CommentCardViewHolder(ItemCommentCardBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false), delegate, true)
-            R.layout.item_show_more_replies -> PaginationViewMoreRepliesViewHolder(
-                ItemShowMoreRepliesBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false), delegate
-            )
-            R.layout.item_error_pagination -> PaginationErrorViewHolder(ItemErrorPaginationBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false), delegate, true)
-            else -> RootCommentViewHolder(ItemCommentCardBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
-        }
+        return CommentCardViewHolder(ItemCommentCardBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false), delegate, true)
     }
 
     companion object {
         private const val SECTION_COMMENTS = 0
-        private const val SECTION_SHOW_MORE_REPLIES_PAGINATING = 1
-        private const val SECTION_ERROR_PAGINATING = 2
-        private const val SECTION_ROOT_COMMENT = 3
     }
 }
---
+++ app/src/main/java/com/kickstarter/ui/adapters/RepliesStatusAdapter.kt
@@ -0,0 +1,48 @@
+package com.kickstarter.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import com.kickstarter.R
+import com.kickstarter.databinding.ItemShowMoreRepliesBinding
+import com.kickstarter.ui.viewholders.KSViewHolder
+import com.kickstarter.ui.viewholders.RepliesStatusCellType
+import com.kickstarter.ui.viewholders.RepliesStatusCellViewHolder
+
+/**  Replies cell status viewMore or Error  **/
+class RepliesStatusAdapter(private val delegate: Delegate) : KSListAdapter() {
+    interface Delegate :
+        RepliesStatusCellViewHolder.ViewListener
+    init {
+        insertSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, emptyList<RepliesStatusCellType>())
+    }
+
+    fun addViewMoreCell(shouldViewMoreRepliesCell: Boolean) {
+
+        if (shouldViewMoreRepliesCell)
+            setSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, listOf(RepliesStatusCellType.VIEW_MORE))
+        else
+            setSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, emptyList<RepliesStatusCellType>())
+        submitList(items())
+    }
+
+    fun addErrorPaginationCell(shouldShowErrorCell: Boolean) {
+        if (shouldShowErrorCell)
+            setSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, listOf(RepliesStatusCellType.PAGINATION_ERROR))
+        else
+            setSection(SECTION_SHOW_MORE_REPLIES_PAGINATING, emptyList<RepliesStatusCellType>())
+        submitList(items())
+    }
+
+    override fun layout(sectionRow: SectionRow): Int = R.layout.item_show_more_replies
+
+    override fun viewHolder(@LayoutRes layout: Int, viewGroup: ViewGroup): KSViewHolder {
+        return RepliesStatusCellViewHolder(
+            ItemShowMoreRepliesBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false), delegate
+        )
+    }
+
+    companion object {
+        private const val SECTION_SHOW_MORE_REPLIES_PAGINATING = 0
+    }
+}

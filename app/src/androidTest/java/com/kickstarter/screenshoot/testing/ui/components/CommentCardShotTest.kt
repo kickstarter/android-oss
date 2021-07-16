173ef79ac6490fb8bc421430b62127290b42cfbd
Isabel Martin <arkariang@gmail.com>
2021-07-16 11:09:49 -0700

    - Recorded first test

--- app/src/androidTest/java/com/kickstarter/screenshoot/testing/ui/components/CommentCardShotTest.kt
+++ app/src/androidTest/java/com/kickstarter/screenshoot/testing/ui/components/CommentCardShotTest.kt
@@ -2,22 +2,28 @@ package com.kickstarter.screenshoot.testing.ui.components
 
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.test.core.app.ApplicationProvider
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.karumi.shot.ScreenshotTest
-import com.kickstarter.KSApplication
 import com.kickstarter.R
+import com.kickstarter.libs.utils.extensions.asKSApplication
 import com.kickstarter.mock.factories.UserFactory
 import com.kickstarter.ui.views.CommentCard
 import com.kickstarter.ui.views.CommentCardStatus
 import org.joda.time.DateTime
+import org.junit.Before
 import org.junit.Test
 
 class CommentCardShotTest : ScreenshotTest {
-    private val context = ApplicationProvider.getApplicationContext<KSApplication>()
+
+    @Before
+    fun setup() {
+        val app = InstrumentationRegistry.getInstrumentation().targetContext.asKSApplication()
+    }
 
     @Test
     fun commentCardScreenshotTest_COMMENT_FOR_LOGIN_BACKED_USERS() {
-        var commentCard: CommentCard = (LayoutInflater.from(context).inflate(R.layout.item_comment_card, null) as ConstraintLayout)
+        var commentCard: CommentCard = (LayoutInflater.from(getInstrumentation().targetContext).inflate(R.layout.item_comment_card, null) as ConstraintLayout)
             .findViewById(R.id.comments_card_view)
 
         val user = UserFactory.user()

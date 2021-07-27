5d02cae7eefa2cf98d83920134aafcffd344eb12
Leigh Douglas <leighcdouglas1@gmail.com>
2021-07-27 11:13:36 -0400

    Cleanup tests

--- app/src/test/java/com/kickstarter/viewmodels/CommentsViewModelTest.kt
+++ app/src/test/java/com/kickstarter/viewmodels/CommentsViewModelTest.kt
@@ -38,8 +38,6 @@ class CommentsViewModelTest : KSRobolectricTestCase() {
     private val shouldShowPaginatedCell = TestSubscriber.create<Boolean>()
     private val openCommentGuideLines = TestSubscriber<Void>()
     private val startThreadActivity = BehaviorSubject.create<Pair<CommentCardData, Boolean>>()
-    private val hasPendingComments = TestSubscriber<Pair<Boolean, Boolean>>()
-    private val isFetching = TestSubscriber<Boolean>()
 
     @Test
     fun testCommentsViewModel_whenUserLoggedInAndBacking_shouldShowEnabledComposer() {

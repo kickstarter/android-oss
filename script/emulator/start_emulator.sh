e8497b4c842e4a0cc8b0d8e15cd718230403337b
Isabel Martin <arkariang@gmail.com>
2021-07-27 15:31:26 -0700

    - dubiging script on docker image

--- script/emulator/start_emulator.sh
+++ script/emulator/start_emulator.sh
@@ -2,7 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "ANDROID_SDK_HOME: "
 echo $ANDROID_SDK_HOME
+echo "ANDROID_HOME: "
 echo $ANDROID_HOME
 
 if ! $ANDROID_HOME/tools/android list avd | grep -q Pixel_3a_API_30; then

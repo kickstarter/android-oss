b97ac26a0655604a6f9ad01966af4387d0f81449
Isabel Martin <arkariang@gmail.com>
2021-07-29 10:25:41 -0700

    - modified scripts - run tests

--- script/emulator/copy_emulator_configuration.sh
+++ script/emulator/copy_emulator_configuration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+echo "Copy configuration to the emulator the one specified by us with concrete dimensions WxH and density"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 cp $DIR/config.ini ~/.android/avd/Pixel_3a_API_30.avd/config.ini
+cat ~/.android/avd/Pixel_3a_API_30.avd/config.ini
 # Configuration to be able to do Screenshot Test on API higher that 27
-adb shell settings put global hidden_api_policy_p_apps 1
-adb shell settings put global hidden_api_policy_pre_p_apps 1
-adb shell settings put global hidden_api_policy  1
+

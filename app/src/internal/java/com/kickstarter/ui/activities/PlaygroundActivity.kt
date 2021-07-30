f1b42095f9463229ebd4f2dd8c3b82652025f5f9
Isabel Martin <arkariang@gmail.com>
2021-07-30 12:28:10 -0700

    - Use Playground activity to showcase the stepper component

--- app/src/internal/java/com/kickstarter/ui/activities/PlaygroundActivity.kt
+++ app/src/internal/java/com/kickstarter/ui/activities/PlaygroundActivity.kt
@@ -1,18 +1,22 @@
 package com.kickstarter.ui.activities
 
 import android.os.Bundle
+import android.view.View
 import com.kickstarter.databinding.PlaygroundLayoutBinding
 import com.kickstarter.libs.BaseActivity
 import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
+import com.kickstarter.ui.extensions.snackbar
 import com.kickstarter.viewmodels.PlaygroundViewModel
 
 @RequiresActivityViewModel(PlaygroundViewModel.ViewModel::class)
 class PlaygroundActivity : BaseActivity<PlaygroundViewModel.ViewModel?>() {
-    val binding = PlaygroundLayoutBinding.inflate(layoutInflater)
-    val view = binding.root
+    private lateinit var binding :PlaygroundLayoutBinding
+    private lateinit var view :View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = PlaygroundLayoutBinding.inflate(layoutInflater)
+        view = binding.root
         setContentView(view)
 
         setStepper()
@@ -22,5 +26,8 @@ class PlaygroundActivity : BaseActivity<PlaygroundViewModel.ViewModel?>() {
         binding.stepper.minimum = 0
         binding.stepper.maximum = 10
         binding.stepper.initialValue = 10
+        binding.stepper.stepperListener.onStepperUpdated().subscribe {
+            snackbar(view, " $it is the amount on the stepper")
+        }
     }
 }

daa3a9af0ecdde0eaaad073fbe7e9392cdb9163e
hadia <hadiamohamed.iti@gmail.com>
2021-08-02 17:51:56 +0200

    Add screen shot test

---
+++ app/src/androidTest/java/com/kickstarter/screenshoot/testing/ui/components/AddOnTagShotTest.kt
@@ -0,0 +1,42 @@
+package com.kickstarter.screenshoot.testing.ui.components
+
+import android.view.LayoutInflater
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.test.platform.app.InstrumentationRegistry
+import com.karumi.shot.ScreenshotTest
+import com.kickstarter.ApplicationComponent
+import com.kickstarter.R
+import com.kickstarter.screenshoot.testing.InstrumentedApp
+import com.kickstarter.ui.views.AddOnTagComponent
+import org.junit.Before
+import org.junit.Test
+
+class AddOnTagShotTest : ScreenshotTest {
+
+    lateinit var addOnTagComponent: AddOnTagComponent
+    lateinit var component: ApplicationComponent
+
+    @Before
+    fun setup() {
+        // - Test Application
+        val app =
+            InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as InstrumentedApp
+        // - Test Dagger component for injecting on environment Mock Objects
+        component = app.component()
+
+        addOnTagComponent =
+            (
+                LayoutInflater.from(InstrumentationRegistry.getInstrumentation().targetContext)
+                    .inflate(
+                        R.layout.item_add_on_pledge, null
+                    ) as CoordinatorLayout
+                )
+                .findViewById(R.id.addon_quantity_remaining)
+    }
+
+    @Test
+    fun addOnTagComponentViewScreenshotTest_TextValue() {
+        addOnTagComponent.setAddOnTagText("30 left")
+        compareScreenshot(addOnTagComponent)
+    }
+}

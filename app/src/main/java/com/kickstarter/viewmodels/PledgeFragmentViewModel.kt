ef4f8cbf828fc50ba98a2b14badbfbb00c754286
Isabel Martin <arkariang@gmail.com>
2021-09-21 14:08:12 -0700

    - linter

--- app/src/main/java/com/kickstarter/viewmodels/PledgeFragmentViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/PledgeFragmentViewModel.kt
@@ -20,13 +20,13 @@ import com.kickstarter.libs.rx.transformers.Transformers.values
 import com.kickstarter.libs.rx.transformers.Transformers.zipPair
 import com.kickstarter.libs.utils.BooleanUtils
 import com.kickstarter.libs.utils.DateTimeUtils
+import com.kickstarter.libs.utils.ExperimentData
 import com.kickstarter.libs.utils.NumberUtils
 import com.kickstarter.libs.utils.ObjectUtils
 import com.kickstarter.libs.utils.ProjectUtils
 import com.kickstarter.libs.utils.ProjectViewUtils
 import com.kickstarter.libs.utils.RefTagUtils
 import com.kickstarter.libs.utils.RewardUtils
-import com.kickstarter.libs.utils.ExperimentData
 import com.kickstarter.models.Backing
 import com.kickstarter.models.Checkout
 import com.kickstarter.models.Project

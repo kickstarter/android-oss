cd5d91b2e2a227d95d628bf55040e000f005a617
Leigh Douglas <leighcdouglas1@gmail.com>
2021-10-13 16:58:47 -0400

    fix failing tests

--- app/src/test/java/com/kickstarter/viewmodels/ProjectPageViewModelTest.kt
+++ app/src/test/java/com/kickstarter/viewmodels/ProjectPageViewModelTest.kt
@@ -167,7 +167,7 @@ class ProjectPageViewModelTest : KSRobolectricTestCase() {
 
         this.vm.intent(Intent().putExtra(IntentKey.PROJECT, project))
 
-        projectPhoto.assertValues("https://ksr-ugc.imgix.net/assets/012/032/069/46817a8c099133d5bf8b64aad282a696_original.png?crop=faces&w=1552&h=873&fit=crop&v=1463725702&auto=format&q=92&s=72501d155e4a5e399276632687c77959", "https://ksr-ugc.imgix.net/assets/012/032/069/46817a8c099133d5bf8b64aad282a696_original.png?crop=faces&w=1552&h=873&fit=crop&v=1463725702&auto=format&q=92&s=72501d155e4a5e399276632687c77959")
+        projectPhoto.assertValue("https://ksr-ugc.imgix.net/assets/012/032/069/46817a8c099133d5bf8b64aad282a696_original.png?crop=faces&w=1552&h=873&fit=crop&v=1463725702&auto=format&q=92&s=72501d155e4a5e399276632687c77959")
     }
 
     @Test
@@ -185,7 +185,7 @@ class ProjectPageViewModelTest : KSRobolectricTestCase() {
 
         this.vm.intent(Intent().putExtra(IntentKey.PROJECT, project))
 
-        playButtonIsVisible.assertValues(false, false)
+        playButtonIsVisible.assertValues(false)
     }
 
     @Test
@@ -203,7 +203,7 @@ class ProjectPageViewModelTest : KSRobolectricTestCase() {
 
         this.vm.intent(Intent().putExtra(IntentKey.PROJECT, project))
 
-        playButtonIsVisible.assertValues(true, true)
+        playButtonIsVisible.assertValues(true)
     }
 
     @Test
@@ -221,7 +221,7 @@ class ProjectPageViewModelTest : KSRobolectricTestCase() {
 
         this.vm.intent(Intent().putExtra(IntentKey.PROJECT, project))
 
-        backingViewGroupIsVisible.assertValues(true, true)
+        backingViewGroupIsVisible.assertValues(true)
     }
 
     @Test
@@ -239,7 +239,7 @@ class ProjectPageViewModelTest : KSRobolectricTestCase() {
 
         this.vm.intent(Intent().putExtra(IntentKey.PROJECT, project))
 
-        backingViewGroupIsVisible.assertValues(false, false)
+        backingViewGroupIsVisible.assertValues(false)
     }
 
     @Test

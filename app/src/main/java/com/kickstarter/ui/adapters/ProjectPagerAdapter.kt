ec084dca7fbc33df551ad8578f168d4c99a2ae98
hadia <hadiamohamed.iti@gmail.com>
2021-10-21 23:14:56 +0200

    Add ProjectRiskFragment and tab to adapter

--- app/src/main/java/com/kickstarter/ui/adapters/ProjectPagerAdapter.kt
+++ app/src/main/java/com/kickstarter/ui/adapters/ProjectPagerAdapter.kt
@@ -10,6 +10,7 @@ import com.kickstarter.ui.data.ProjectData
 import com.kickstarter.ui.fragments.projectpage.FrequentlyAskedQuestionFragment
 import com.kickstarter.ui.fragments.projectpage.ProjectEnvironmentalCommitmentsFragment
 import com.kickstarter.ui.fragments.projectpage.ProjectOverviewFragment
+import com.kickstarter.ui.fragments.projectpage.ProjectRiskFragment
 
 class ProjectPagerAdapter(
     private val fragmentManager: FragmentManager,
@@ -26,6 +27,8 @@ class ProjectPagerAdapter(
                 ProjectOverviewFragment.newInstance(position)
             position == ProjectPagerTabs.FAQS.ordinal && pagerAdapterMap[ProjectPagerTabs.FAQS] == true ->
                 FrequentlyAskedQuestionFragment.newInstance(position)
+            position == ProjectPagerTabs.RISKS.ordinal && pagerAdapterMap[ProjectPagerTabs.RISKS] == true ->
+                ProjectRiskFragment.newInstance(position)
             /*position == ProjectPagerTabs.CAMPAIGN.ordinal && pagerAdapterMap[ProjectPagerTabs.CAMPAIGN] == true ->
                 ProjectCampaignFragment.newInstance(position) */ // TODO bring back ProjectCampaignFragment.newInstance(position), on second place once the HTML parser is in place
             position == ProjectPagerTabs.ENVIRONMENTAL_COMMITMENT.ordinal && pagerAdapterMap[ProjectPagerTabs.ENVIRONMENTAL_COMMITMENT] == true ->

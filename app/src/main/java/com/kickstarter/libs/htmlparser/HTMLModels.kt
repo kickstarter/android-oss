aa6747130e79f5ba1d8f081ddda8cfca05ff9932
Isabel Martin <arkariang@gmail.com>
2021-11-09 12:47:09 -0800

    - images working nice

--- app/src/main/java/com/kickstarter/libs/htmlparser/HTMLModels.kt
+++ app/src/main/java/com/kickstarter/libs/htmlparser/HTMLModels.kt
@@ -37,13 +37,11 @@ data class TextComponent(val text: String, val link: String?) {
     }
 }
 
-data class EmbeddedLinkViewElement(
-    val href: String,
-    override val src: String,
-    val caption: String?
-) : ImageViewElement(src)
-
-open class ImageViewElement(open val src: String) : ViewElement
+data class ImageViewElement(
+    val src: String,
+    val href: String? = null,
+    val caption: String? = null
+) : ViewElement
 
 data class ExternalSourceViewElement(
     val htmlContent: String
@@ -55,32 +53,87 @@ enum class ViewElementType(val tag: String?) {
     VIDEO("video"),
     EXTERNAL_SOURCES("iframe"),
     EMBEDDED_LINK(null),
-    OEMBED(null),
     UNKNOWN(null);
 
     companion object {
         fun initialize(element: Element): ViewElementType {
             val tag = element.tag().name
-            if (tag == "div") {
-
-                for (attribute in element.attributes()) {
-                    if (attribute.key == "class" && attribute.value == "template oembed") {
-                        if (element.children().getOrNull(0)?.tag()?.name == EXTERNAL_SOURCES.tag) {
-                            return EXTERNAL_SOURCES
-                        }
-                        return OEMBED
-                    }
+            when {
+                tag == "a" -> {
+                    element.children().find { it.tagName() == "div" }?.let { return@let divUnWrapper(it) }
+                    // TODO: Return text element in case is only a link not an image/video wrapped in a link
                 }
-            } else if (TextComponent.TextStyleType.initialize(tag) == TextComponent.TextStyleType.LINK && !element.getElementsByTag("img").isEmpty()) {
-                return EMBEDDED_LINK
-            } else if (TextComponent.TextStyleType.values().map { it.tag }.contains(tag)) {
-                return TEXT
-            } else if (tag == IMAGE.tag) {
-                return IMAGE
-            } else if (tag == VIDEO.tag) {
-                return VIDEO
+                tag == "div" -> {
+                    return divUnWrapper(element)
+                }
+                TextComponent.TextStyleType.values().map { it.tag }.contains(tag) -> {
+                    return TEXT
+                }
+                tag == VIDEO.tag -> {
+                    return VIDEO
+                }
             }
             return UNKNOWN
         }
     }
+}
+
+private fun divUnWrapper(element: Element): ViewElementType {
+    var type: ViewElementType = ViewElementType.UNKNOWN
+
+    if (element.isImageStructure()) {
+        if (element.children().getOrNull(0)?.children()?.getOrNull(0)?.tag()?.name == ViewElementType.IMAGE.tag) {
+            type = ViewElementType.IMAGE
+        }
+    } else if (element.isIframeStructure()) {
+        if (element.children().getOrNull(0)?.children()?.getOrNull(0)?.tag()?.name == ViewElementType.EXTERNAL_SOURCES.tag) {
+            type = ViewElementType.EXTERNAL_SOURCES
+        }
+    }
+
+    return type
+}
+
+fun Element.isIframeStructure(): Boolean {
+    val isTemplateDiv = this.attributes().filter {
+        it.key == "class" && it.value == "template oembed"
+    }
+
+    return !isTemplateDiv.isNullOrEmpty()
+}
+
+fun Element.isImageStructure(): Boolean {
+    val isTemplateDiv = this.attributes().filter {
+        it.key == "class" && it.value == "template asset"
+    }
+
+    return !isTemplateDiv.isNullOrEmpty()
+}
+
+fun Element.parseImageElement(): ImageViewElement {
+    var src: String = ""
+    var caption: String? = null
+    var href: String? = null
+
+    if (this.tagName() == "a") {
+        href = this.attr("href")
+    } else {
+        val pair = this.extractSourceAndCaption()
+        src = pair.first
+        caption = pair.second
+    }
+
+    return ImageViewElement(src = src, href = href, caption = caption)
+}
+
+fun Element.extractSourceAndCaption(): Pair<String, String?> {
+    var caption: String? = null
+    var src: String = ""
+
+    if (this.tagName() == "div") {
+        caption = this.attr("data-caption")
+        src = this.children().getOrNull(0)?.children()?.getOrNull(0)?.attr("src").toString()
+    }
+
+    return Pair(src, caption)
 }

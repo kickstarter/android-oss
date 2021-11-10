05d8be22a753a54fd95f025fc40ae6d1f098ff22
hadia <hadiamohamed.iti@gmail.com>
2021-11-10 15:30:30 +0200

    Add extenation to load gif images

--- app/src/main/java/com/kickstarter/ui/extensions/ImageViewExt.kt
+++ app/src/main/java/com/kickstarter/ui/extensions/ImageViewExt.kt
@@ -2,6 +2,7 @@ package com.kickstarter.ui.extensions
 
 import android.content.Context
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.kickstarter.libs.transformations.CircleTransformation
 import com.kickstarter.libs.utils.extensions.isKSApplication
 import com.squareup.picasso.Picasso
@@ -19,5 +20,16 @@ fun ImageView.loadImage(url: String?, context: Context) {
         Picasso.get().load(url).into(this)
     } else {
         // TODO: suitable for tests, Picasso library not acting
+    }
+}
+
+fun ImageView.loadGifImage(url: String?, context: Context) {
+    if (context.applicationContext.isKSApplication()) {
+        Glide.with(context)
+            .asGif()
+            .load(url)
+            .into(this)
+    } else {
+        // TODO: suitable for tests, Glide library not acting
     }
 }

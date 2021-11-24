c86cd4f1b2feb5bf7cb63c44ba1fb3c78c4983e7
Isabel Martin <arkariang@gmail.com>
2021-11-24 18:59:32 +0100

    - Added PNG's for the new tests

--- app/src/androidTest/java/com/kickstarter/screenshoot/testing/ui/components/TextViewElementShotTest.kt
+++ app/src/androidTest/java/com/kickstarter/screenshoot/testing/ui/components/TextViewElementShotTest.kt
@@ -109,5 +109,6 @@ class TextViewElementShotTest : ScreenshotTest {
         val element = listOfElements.first() as TextViewElement
 
         textView.text = element.getStyledComponents(body, headerSize, app)
+        compareScreenshot(textView)
     }
 }

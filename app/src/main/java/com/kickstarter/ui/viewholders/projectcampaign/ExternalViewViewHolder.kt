f8b7ebedf22c3955018ccabb7836377c52a11c95
Hadia <hadiamohamed.iti@gmail.com>
2021-12-09 14:59:43 +0200

    NTV-291: Add Youtube fullscreen support (#1480)
    * smoothScrollToPosition after closing video or youtube in full screen
    
    * Fix ui

--- app/src/main/java/com/kickstarter/ui/viewholders/projectcampaign/ExternalViewViewHolder.kt
+++ app/src/main/java/com/kickstarter/ui/viewholders/projectcampaign/ExternalViewViewHolder.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import androidx.fragment.app.FragmentActivity
 import com.kickstarter.databinding.ViewElementExternalSourceFromHtmlBinding
 import com.kickstarter.libs.htmlparser.ExternalSourceViewElement
+import com.kickstarter.ui.adapters.projectcampaign.ViewElementAdapter
 import com.kickstarter.ui.viewholders.KSViewHolder
 import com.kickstarter.ui.views.WebChromeVideoFullScreenClient
 
 class ExternalViewViewHolder(
     val binding: ViewElementExternalSourceFromHtmlBinding,
+    private val fullScreenDelegate: ViewElementAdapter.FullScreenDelegate,
     val requireActivity: FragmentActivity
 ) : KSViewHolder(binding.root) {
     private val webView = binding.externalSourceWebView
@@ -22,7 +24,10 @@ class ExternalViewViewHolder(
     @SuppressLint("SetJavaScriptEnabled")
     private fun setupWebView() {
         webView.settings.javaScriptEnabled = true
-        webView.webChromeClient = WebChromeVideoFullScreenClient(requireActivity)
+        webView.webChromeClient = WebChromeVideoFullScreenClient(
+            requireActivity,
+            fullScreenDelegate, absoluteAdapterPosition
+        )
     }
 
     override fun bindData(data: Any?) {

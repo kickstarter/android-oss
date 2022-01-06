b760aedac5d032261bfc8c7d255b7f00edf5e8e6
Isabel Martin <arkariang@gmail.com>
2022-01-06 16:31:22 +0100

    - tests

--- app/src/main/java/com/kickstarter/models/Category.kt
+++ app/src/main/java/com/kickstarter/models/Category.kt
@@ -1,17 +1,17 @@
 package com.kickstarter.models
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class Category internal constructor(
     private val analyticsName: String,
-    private val color: Int,
+    private val color: Int?,
     private val id: Long,
     private val name: String,
     private val parent: Category?,
     private val parentId: Long,
-    private val parentName: String,
+    private val parentName: String?,
     private val position: Int,
     private val projectsCount: Int,
     private val slug: String
@@ -31,26 +31,26 @@ class Category internal constructor(
     @Parcelize
     data class Builder(
         private var analyticsName: String = "",
-        private var color: Int = 0,
+        private var color: Int? = 0,
         private var id: Long = 0L,
         private var name: String = "",
         private var parent: Category? = null,
         private var parentId: Long = 0L,
-        private var parentName: String = "",
+        private var parentName: String? = "",
         private var position: Int = 0,
         private var projectsCount: Int = 0,
         private var slug: String = ""
     ) : Parcelable {
         fun analyticsName(analyticsName: String?) = apply { analyticsName?.let { this.analyticsName = it } }
-        fun color(color: Int?) = apply { color?.let { this.color = it } }
-        fun id(id: Long?) = apply { id?.let { this.id = it } }
-        fun name(name: String?) = apply { name?.let { this.name = it } }
-        fun parent(parent: Category?) = apply { parent?.let { this.parent = it } }
-        fun parentId(parentId: Long?) = apply { parentId?.let { this.parentId = it } }
-        fun parentName(parentName: String?) = apply { parentName?.let { this.parentName = it } }
-        fun position(position: Int) = apply { position?.let { this.position = it } }
-        fun projectsCount(projectsCount: Int?) = apply { projectsCount?.let { this.projectsCount = it } }
-        fun slug(slug: String?) = apply { slug?.let { this.slug = it } }
+        fun color(color: Int?) = apply { this.color = color }
+        fun id(id: Long?) = apply { this.id = id ?: 0L }
+        fun name(name: String?) = apply { this.name = name ?: "" }
+        fun parent(parent: Category?) = apply { this.parent = parent }
+        fun parentId(parentId: Long?) = apply { this.parentId = parentId ?: 0L }
+        fun parentName(parentName: String?) = apply { this.parentName = parentName }
+        fun position(position: Int?) = apply { this.position = position ?: 0 }
+        fun projectsCount(projectsCount: Int?) = apply { this.projectsCount = projectsCount ?: 0 }
+        fun slug(slug: String?) = apply { this.slug = slug ?: "" }
         fun build() = Category(
             analyticsName = analyticsName,
             color = color,
@@ -95,6 +95,23 @@ class Category internal constructor(
         return other.root()?.let { root()?.name()?.compareTo(it.name()) } ?: 0
     }
 
+    override fun equals(other: Any?): Boolean {
+        var equals = super.equals(other)
+        if (other is Category) {
+            equals = id() == other.id() &&
+                analyticsName() == other.analyticsName() &&
+                color() == other.color() &&
+                parentName() == other.parentName() &&
+                parent() == other.parent() &&
+                name() == other.name() &&
+                projectsCount() == other.projectsCount() &&
+                position() == other.position() &&
+                parentId() == other.parentId() &&
+                slug() == other.slug()
+        }
+        return equals
+    }
+
     val isRoot: Boolean
         get() = parentId() == 0L
 
@@ -104,5 +121,9 @@ class Category internal constructor(
 
     fun rootId(): Long {
         return if (isRoot) id() else parentId()
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
     }
 }

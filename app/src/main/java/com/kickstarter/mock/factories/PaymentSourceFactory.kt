0a59f8785d3c689e3ddbdfa56f09a2af174c06d0
Isabel Martin <arkariang@gmail.com>
2022-01-11 09:13:16 -0800

    NTV-337: PaymentSource Model (#1504)
    Co-authored-by: Hadia <hadiamohamed.iti@gmail.com>

--- app/src/main/java/com/kickstarter/mock/factories/PaymentSourceFactory.kt
+++ app/src/main/java/com/kickstarter/mock/factories/PaymentSourceFactory.kt
@@ -1,13 +1,13 @@
 package com.kickstarter.mock.factories
 
-import com.kickstarter.models.Backing
+import com.kickstarter.models.PaymentSource
 import type.CreditCardPaymentType
 import java.util.Date
 
 class PaymentSourceFactory private constructor() {
     companion object {
-        fun visa(): Backing.PaymentSource {
-            return Backing.PaymentSource.builder()
+        fun visa(): PaymentSource {
+            return PaymentSource.builder()
                 .id(IdFactory.id().toString())
                 .expirationDate(Date())
                 .lastFour("4321")
@@ -17,24 +17,24 @@ class PaymentSourceFactory private constructor() {
                 .build()
         }
 
-        fun googlePay(): Backing.PaymentSource {
-            return Backing.PaymentSource.builder()
+        fun googlePay(): PaymentSource {
+            return PaymentSource.builder()
                 .id(IdFactory.id().toString())
                 .paymentType(CreditCardPaymentType.ANDROID_PAY.rawValue())
                 .state("ACTIVE")
                 .build()
         }
 
-        fun applePay(): Backing.PaymentSource {
-            return Backing.PaymentSource.builder()
+        fun applePay(): PaymentSource {
+            return PaymentSource.builder()
                 .id(IdFactory.id().toString())
                 .paymentType(CreditCardPaymentType.APPLE_PAY.rawValue())
                 .state("ACTIVE")
                 .build()
         }
 
-        fun bankAccount(): Backing.PaymentSource {
-            return Backing.PaymentSource.builder()
+        fun bankAccount(): PaymentSource {
+            return PaymentSource.builder()
                 .id(IdFactory.id().toString())
                 .paymentType(CreditCardPaymentType.BANK_ACCOUNT.rawValue())
                 .state("ACTIVE")

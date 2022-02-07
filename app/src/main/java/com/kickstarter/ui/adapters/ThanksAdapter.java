475bdd37ff5fc3158b007c66695772fdd96e8ccb
hadia <hadiamohamed.iti@gmail.com>
2022-02-07 13:20:52 +0200

    update thanks activity

--- app/src/main/java/com/kickstarter/ui/adapters/ThanksAdapter.java
+++ app/src/main/java/com/kickstarter/ui/adapters/ThanksAdapter.java
@@ -63,7 +63,7 @@ public final class ThanksAdapter extends KSAdapter {
 
   public void takeData(final @NonNull ThanksData data) {
     setSection(SECTION_SHARE_VIEW, Collections.singletonList(data.getBackedProject()));
-    setSection(SECTION_RECOMMENDED_PROJECTS_VIEW, ProjectExt.combineProjectsAndParams(data.getRecommendedProjects(), DiscoveryParams.builder().build()));
+    setSection(SECTION_RECOMMENDED_PROJECTS_VIEW, data.getRecommendedProjects());
     setSection(SECTION_CATEGORY_VIEW, Collections.singletonList(data.getCategory()));
     notifyDataSetChanged();
   }
--- app/src/main/java/com/kickstarter/ui/adapters/data/ThanksData.kt
+++ app/src/main/java/com/kickstarter/ui/adapters/data/ThanksData.kt
@@ -1,10 +1,12 @@
 package com.kickstarter.ui.adapters.data
 
+import android.util.Pair
 import com.kickstarter.models.Category
 import com.kickstarter.models.Project
+import com.kickstarter.services.DiscoveryParams
 
 class ThanksData(
     val backedProject: Project,
     val category: Category,
-    val recommendedProjects: List<Project>
+    val recommendedProjects: List<Pair<Project, DiscoveryParams>>
 )

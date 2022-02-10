9d3f45058808ba4a0ff02725ebd0d66b3e5aed52
Isabel Martin <arkariang@gmail.com>
2022-02-10 10:23:15 -0800

    NTV-360: Zoom on campaign images (#1539)

--- app/src/main/java/com/kickstarter/ui/extensions/ImageViewExt.kt
+++ app/src/main/java/com/kickstarter/ui/extensions/ImageViewExt.kt
@@ -2,23 +2,39 @@ package com.kickstarter.ui.extensions
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.graphics.drawable.toBitmap
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.kickstarter.R
 import com.kickstarter.libs.transformations.CircleTransformation
 import com.kickstarter.libs.utils.extensions.isKSApplication
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
 fun ImageView.loadCircleImage(url: String?) {
     url?.let {
+        val target = this
         Picasso.get().load(it)
             .transform(CircleTransformation())
             .into(this)
     }
 }
 
-fun ImageView.loadImage(url: String?, context: Context) {
+fun ImageView.loadImage(url: String?, context: Context, imageViewPlaceholder: AppCompatImageView? = null) {
+    val target = this
     if (context.applicationContext.isKSApplication()) {
-        Picasso.get().load(url).into(this)
+        Picasso.get().load(url).into(
+            this,
+            object : Callback {
+                override fun onSuccess() {
+                    imageViewPlaceholder?.setImageBitmap(target.drawable.toBitmap())
+                }
+
+                override fun onError(e: Exception?) {
+                }
+            }
+        )
     } else {
         this.setImageResource(R.drawable.image_placeholder)
     }
@@ -28,6 +44,8 @@ fun ImageView.loadGifImage(url: String?, context: Context) {
     if (context.applicationContext.isKSApplication()) {
         Glide.with(context)
             .asGif()
+            .error(R.drawable.image_placeholder)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .load(url)
             .into(this)
     } else {

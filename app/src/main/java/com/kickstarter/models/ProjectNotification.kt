cb67ebdf15787346ab34aba4729f802535039983
Isabel Martin <arkariang@gmail.com>
2022-03-16 11:07:30 -0700

    no message

--- app/src/main/java/com/kickstarter/models/ProjectNotification.kt
+++ app/src/main/java/com/kickstarter/models/ProjectNotification.kt
@@ -1,29 +1,53 @@
 package com.kickstarter.models
 
+import android.os.Parcelable
 import auto.parcel.AutoParcel
 import com.kickstarter.libs.qualifiers.AutoGson
-import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-@AutoParcel
-@AutoGson
-abstract class ProjectNotification : Parcelable {
-    abstract fun project(): Project?
-    abstract fun id(): Long
-    abstract fun email(): Boolean
-    abstract fun mobile(): Boolean
-    abstract fun urls(): Urls?
+@Parcelize
+class ProjectNotification private constructor(
+    private val project: Project?,
+    private val id: Long,
+    private val email: Boolean,
+    private val mobile: Boolean,
+    private val urls: Urls?
+) : Parcelable {
+    fun project() = this.project
+    fun id() = this.id
+    fun email() = this.email
+    fun mobile() = this.mobile
+    fun urls() = this.urls
 
-    @AutoParcel.Builder
-    abstract class Builder {
-        abstract fun project(project: Project?): Builder
-        abstract fun id(id: Long): Builder
-        abstract fun email(email: Boolean): Builder
-        abstract fun mobile(mobile: Boolean): Builder
-        abstract fun urls(urls: Urls?): Builder
-        abstract fun build(): ProjectNotification?
+    @Parcelize
+    data class Builder(
+        private var project: Project? = null,
+        private var id: Long = 0L,
+        private var email: Boolean = false,
+        private var mobile: Boolean = false,
+        private var urls: Urls? = null
+    ) : Parcelable {
+        fun project(project: Project?) = apply { this.project = project }
+        fun id(id: Long?) = apply { id?.let { this.id = it } }
+        fun email(email: Boolean?) = apply { email?.let { this.email = it } }
+        fun mobile(mobile: Boolean?) = apply { mobile?.let { this.mobile = it } }
+        fun urls(urls: Urls?) = apply { this.urls = urls }
+        fun build() = ProjectNotification(
+            project = project,
+            id = id,
+            email = email,
+            mobile = mobile,
+            urls = urls
+        )
     }
 
-    abstract fun toBuilder(): Builder
+    fun toBuilder() = Builder(
+        project = project,
+        id = id,
+        email = email,
+        mobile = mobile,
+        urls = urls
+    )
 
     @AutoParcel
     @AutoGson
@@ -87,7 +111,7 @@ abstract class ProjectNotification : Parcelable {
     companion object {
         @JvmStatic
         fun builder(): Builder {
-            return AutoParcel_ProjectNotification.Builder()
+            return Builder()
         }
     }
 }

05bdf4962beabba38ad8095d89179d58a63e876e
hadia <hadiamohamed.iti@gmail.com>
2022-03-22 19:51:15 +0200

    Migrate NumberOptions.java to kotlin and deprecate Autoparcel

--- app/src/main/java/com/kickstarter/libs/NumberOptions.kt
+++ app/src/main/java/com/kickstarter/libs/NumberOptions.kt
@@ -1,39 +1,78 @@
-package com.kickstarter.libs;
+package com.kickstarter.libs
 
-import android.os.Parcelable;
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.math.RoundingMode
 
-import java.math.RoundingMode;
+@Parcelize
+class NumberOptions private constructor(
+    private val bucketAbove: Float?,
+    private val bucketPrecision: Int?,
+    private val currencyCode: String?,
+    private val currencySymbol: String?,
+    private val precision: Int?,
+    private val roundingMode: RoundingMode?
+) : Parcelable {
+    fun bucketAbove() = this.bucketAbove
+    fun bucketPrecision() = this.bucketPrecision
+    fun currencyCode() = this.currencyCode
+    fun currencySymbol() = this.currencySymbol
+    fun precision() = this.precision
+    fun roundingMode() = this.roundingMode
 
-import androidx.annotation.Nullable;
-import auto.parcel.AutoParcel;
+    @Parcelize
+    data class Builder(
+        private var bucketAbove: Float? = null,
+        private var bucketPrecision: Int? = null,
+        private var currencyCode: String? = null,
+        private var currencySymbol: String? = null,
+        private var precision: Int? = null,
+        private var roundingMode: RoundingMode? = null
+    ) : Parcelable {
+        fun bucketAbove(bucketAbove: Float?) = apply { this.bucketAbove = bucketAbove }
+        fun bucketPrecision(bucketPrecision: Int?) = apply { this.bucketPrecision = bucketPrecision }
+        fun currencyCode(currencyCode: String?) = apply { this.currencyCode = currencyCode }
+        fun currencySymbol(currencySymbol: String?) = apply { this.currencySymbol = currencySymbol }
+        fun precision(precision: Int?) = apply { this.precision = precision }
+        fun roundingMode(roundingMode: RoundingMode?) = apply { this.roundingMode = roundingMode }
 
-@AutoParcel
-public abstract class NumberOptions implements Parcelable {
-  public abstract @Nullable Float bucketAbove();
-  public abstract @Nullable Integer bucketPrecision();
-  public abstract @Nullable String currencyCode();
-  public abstract @Nullable String currencySymbol();
-  public abstract @Nullable Integer precision();
-  public abstract @Nullable RoundingMode roundingMode();
+        fun build() = NumberOptions(
+            bucketAbove = bucketAbove,
+            bucketPrecision = bucketPrecision,
+            currencyCode = currencyCode,
+            currencySymbol = currencySymbol,
+            precision = precision,
+            roundingMode = roundingMode
+        )
+    }
 
-  @AutoParcel.Builder
-  public abstract static class Builder {
-    public abstract Builder bucketAbove(Float __);
-    public abstract Builder bucketPrecision(Integer __);
-    public abstract Builder currencyCode(String __);
-    public abstract Builder currencySymbol(String __);
-    public abstract Builder precision(Integer __);
-    public abstract Builder roundingMode(RoundingMode __);
-    public abstract NumberOptions build();
-  }
+    fun toBuilder() = Builder(
+        bucketAbove = bucketAbove,
+        bucketPrecision = bucketPrecision,
+        currencyCode = currencyCode,
+        currencySymbol = currencySymbol,
+        precision = precision,
+        roundingMode = roundingMode
+    )
 
-  public static Builder builder() {
-    return new AutoParcel_NumberOptions.Builder();
-  }
+    override fun equals(obj: Any?): Boolean {
+        var equals = super.equals(obj)
+        if (obj is NumberOptions) {
+            equals = bucketAbove() == obj.bucketAbove() &&
+                bucketPrecision() == obj.bucketPrecision() &&
+                currencyCode() == obj.currencyCode() &&
+                currencySymbol() == obj.currencySymbol() &&
+                precision() == obj.precision() &&
+                roundingMode() == obj.roundingMode()
+        }
+        return equals
+    }
 
-  public abstract Builder toBuilder();
+    val isCurrency: Boolean
+        get() = currencySymbol() != null
 
-  public boolean isCurrency() {
-    return currencySymbol() != null;
-  }
+    companion object {
+        @JvmStatic
+        fun builder() = Builder()
+    }
 }

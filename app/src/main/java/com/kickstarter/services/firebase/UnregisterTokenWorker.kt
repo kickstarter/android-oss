79eb7f23ba589f5e10969fa5e2abadd619187784
Isabel Martin <arkariang@gmail.com>
2022-03-23 10:26:37 -0700

    - remove a dependency using the ktx anotation

--- app/src/main/java/com/kickstarter/services/firebase/UnregisterTokenWorker.kt
+++ app/src/main/java/com/kickstarter/services/firebase/UnregisterTokenWorker.kt
@@ -1,12 +1,9 @@
 package com.kickstarter.services.firebase
 
 import android.content.Context
-import android.os.Bundle
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.kickstarter.KSApplication
 import com.kickstarter.libs.Build
@@ -36,7 +33,6 @@ class UnregisterTokenWorker(@ApplicationContext applicationContext: Context, pri
         if (this.build.isDebug) {
             Timber.d(successMessage)
         }
-        Firebase.analytics.logEvent("Successfully_unregistered_push_token", Bundle())
     }
 
     private fun logError(ioException: IOException) {

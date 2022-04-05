53104011eb3d47580e24b220d93bb84fcbdf29e3
hadia <hadiamohamed.iti@gmail.com>
2022-04-05 23:53:53 +0200

    Migrate AccessTokenEnvelope.java to kotlin and deprecate Autoparcel

--- app/src/main/java/com/kickstarter/services/apiresponses/AccessTokenEnvelope.kt
+++ app/src/main/java/com/kickstarter/services/apiresponses/AccessTokenEnvelope.kt
@@ -1,50 +1,46 @@
-package com.kickstarter.services.apiresponses;
+package com.kickstarter.services.apiresponses
 
-import android.os.Parcelable;
+import android.os.Parcelable
+import com.kickstarter.models.User
+import kotlinx.parcelize.Parcelize
 
-import androidx.annotation.Nullable;
+@Parcelize
+class AccessTokenEnvelope private constructor(
+    private val accessToken: String,
+    private val user: User
+) : Parcelable {
+    fun accessToken() = this.accessToken
+    fun user() = this.user
 
-import com.kickstarter.libs.qualifiers.AutoGson;
-import com.kickstarter.models.User;
-
-import java.util.Objects;
-
-import auto.parcel.AutoParcel;
-
-@AutoGson
-@AutoParcel
-public abstract class AccessTokenEnvelope implements Parcelable {
-  public abstract String accessToken();
-  public abstract User user();
-
-  @AutoParcel.Builder
-  public abstract static class Builder {
-    public abstract Builder accessToken(String __);
-    public abstract Builder user(User __);
-    public abstract AccessTokenEnvelope build();
-  }
-
-  public static Builder builder() {
-    return new AutoParcel_AccessTokenEnvelope.Builder();
-  }
-
-  public abstract Builder toBuilder();
-
-  @Override
-  public boolean equals(final @Nullable Object obj) {
-    boolean equals = super.equals(obj);
-
-    if (obj instanceof AccessTokenEnvelope) {
-      final AccessTokenEnvelope otherEnvelope = (AccessTokenEnvelope) obj;
-      equals = Objects.equals(this.accessToken(), otherEnvelope.accessToken()) &&
-            Objects.equals(this.user(), otherEnvelope.user());
+    @Parcelize
+    data class Builder(
+        private var accessToken: String = "",
+        private var user: User = User.builder().build()
+    ) : Parcelable {
+        fun accessToken(accessToken: String) = apply { this.accessToken = accessToken }
+        fun user(user: User) = apply { this.user = user }
+        fun build() = AccessTokenEnvelope(
+            accessToken = accessToken,
+            user = user
+        )
     }
 
-    return equals;
-  }
+    override fun equals(obj: Any?): Boolean {
+        var equals = super.equals(obj)
+        if (obj is AccessTokenEnvelope) {
+            equals = accessToken() == obj.accessToken() &&
+                user() == obj.user()
+        }
+        return equals
+    }
 
-  @Override
-  public int hashCode() {
-    return super.hashCode();
-  }
+    fun toBuilder() = Builder(
+        accessToken = accessToken,
+        user = user
+    )
+
+    companion object {
+        @JvmStatic
+        fun builder() = Builder()
+    }
 }

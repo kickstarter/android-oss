fa64fa678204dc4d7b9c75f5da42ed3d37a1d302
hadia <hadiamohamed.iti@gmail.com>
2022-04-13 15:06:11 +0200

    Migrate ActivityResult.java to kotlin and deprecate Autoparcel

--- app/src/main/java/com/kickstarter/ui/data/ActivityResult.kt
+++ app/src/main/java/com/kickstarter/ui/data/ActivityResult.kt
@@ -1,50 +1,72 @@
-package com.kickstarter.ui.data;
+package com.kickstarter.ui.data
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Parcelable;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.app.Activity
+import android.content.Intent
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-import auto.parcel.AutoParcel;
+@Parcelize
+class ActivityResult(
+    val requestCode: Int,
+    val resultCode: Int,
+    val intent: Intent?
+) : Parcelable {
+    fun requestCode() = this.requestCode
+    fun resultCode() = this.resultCode
+    fun intent() = this.intent
 
-@AutoParcel
-public abstract class ActivityResult implements Parcelable {
-  public abstract int requestCode();
-  public abstract int resultCode();
-  public abstract @Nullable Intent intent();
+    @Parcelize
+    data class Builder(
+        private var requestCode: Int = 0,
+        private var resultCode: Int = 0,
+        private var intent: Intent? = null
+    ) : Parcelable {
+        fun requestCode(requestCode: Int) = apply { this.requestCode = requestCode }
+        fun resultCode(resultCode: Int) = apply { this.resultCode = resultCode }
+        fun intent(intent: Intent?) = apply { this.intent = intent }
+        fun build() = ActivityResult(
+            requestCode = requestCode,
+            resultCode = resultCode,
+            intent = intent
+        )
+    }
 
-  @AutoParcel.Builder
-  public abstract static class Builder {
-    public abstract Builder requestCode(int __);
-    public abstract Builder resultCode(int __);
-    public abstract Builder intent(Intent __);
-    public abstract ActivityResult build();
-  }
+    fun toBuilder() = Builder(
+        requestCode = requestCode,
+        resultCode = resultCode,
+        intent = intent
+    )
 
-  public static @NonNull ActivityResult create(final int requestCode, final int resultCode, final @Nullable Intent intent) {
-    return ActivityResult.builder()
-      .requestCode(requestCode)
-      .resultCode(resultCode)
-      .intent(intent)
-      .build();
-  }
+    override fun equals(obj: Any?): Boolean {
+        var equals = super.equals(obj)
+        if (obj is ActivityResult) {
+            equals = requestCode() == obj.requestCode() &&
+                resultCode() == obj.resultCode() &&
+                intent() == obj.intent()
+        }
+        return equals
+    }
 
-  public static Builder builder() {
-    return new AutoParcel_ActivityResult.Builder();
-  }
+    val isCanceled: Boolean
+        get() = resultCode() == Activity.RESULT_CANCELED
+    val isOk: Boolean
+        get() = resultCode() == Activity.RESULT_OK
 
-  public abstract Builder toBuilder();
+    fun isRequestCode(v: Int): Boolean {
+        return requestCode() == v
+    }
 
-  public boolean isCanceled() {
-    return resultCode() == Activity.RESULT_CANCELED;
-  }
+    companion object {
+        @JvmStatic
+        fun create(requestCode: Int, resultCode: Int, intent: Intent?): ActivityResult {
+            return builder()
+                .requestCode(requestCode)
+                .resultCode(resultCode)
+                .intent(intent)
+                .build()
+        }
 
-  public boolean isOk() {
-    return resultCode() == Activity.RESULT_OK;
-  }
-
-  public boolean isRequestCode(final int v) {
-    return requestCode() == v;
-  }
+        @JvmStatic
+        fun builder() = Builder()
+    }
 }

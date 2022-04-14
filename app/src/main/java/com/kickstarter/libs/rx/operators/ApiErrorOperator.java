facaa31c32b51aa234c53027b4faa8bbd9cb8bd4
Isabel Martin <arkariang@gmail.com>
2022-04-14 15:27:30 -0700

    no message

--- app/src/main/java/com/kickstarter/libs/rx/operators/ApiErrorOperator.java
+++ app/src/main/java/com/kickstarter/libs/rx/operators/ApiErrorOperator.java
@@ -1,7 +1,6 @@
 package com.kickstarter.libs.rx.operators;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.kickstarter.services.ApiException;
 import com.kickstarter.services.ResponseException;
 import com.kickstarter.services.apiresponses.ErrorEnvelope;

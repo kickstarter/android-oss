23e8eef163cfbd192cacd4384f4d2a747b8c1929
Isabel Martin <arkariang@gmail.com>
2022-04-26 09:30:28 -0700

    PAY-1524: Added new feature flag + updated Schema.json (#1613)

--- app/src/main/java/com/kickstarter/libs/models/OptimizelyFeature.kt
+++ app/src/main/java/com/kickstarter/libs/models/OptimizelyFeature.kt
@@ -3,6 +3,7 @@ package com.kickstarter.libs.models
 class OptimizelyFeature {
     enum class Key(val key: String) {
         LIGHTS_ON("android_lights_on"),
-        ANDROID_STORY_TAB("android_story_tab")
+        ANDROID_STORY_TAB("android_story_tab"),
+        ANDROID_LOCAL_PICKUP("android_local_pickup"),
     }
 }

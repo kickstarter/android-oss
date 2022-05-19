477d2ce606d0f768d858ed49fd952333d6f4d7b8
hadia <hadiamohamed.iti@gmail.com>
2022-05-19 14:35:50 +0200

    Fix test

--- app/src/test/java/com/kickstarter/models/ProjectNotificationTest.kt
+++ app/src/test/java/com/kickstarter/models/ProjectNotificationTest.kt
@@ -48,8 +48,12 @@ class ProjectNotificationTest : TestCase() {
 
     @Test
     fun testProjectNotificationToBuilder() {
-        val projectNotification = ProjectNotificationFactory.enabled()
         val project = ProjectNotification.Project.builder().build()
+
+        val projectNotification = ProjectNotificationFactory.enabled().toBuilder()
+            .project(project)
+            .build()
+
         val urls = ProjectNotification.Urls.builder().build()
         val projectNotification1 = projectNotification.toBuilder()
             .email(false)

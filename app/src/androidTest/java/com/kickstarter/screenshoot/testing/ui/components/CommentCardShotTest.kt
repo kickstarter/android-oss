8eca97edc0b2d33d49500ace447b928cb0ff60bb
Leigh Douglas <leighcdouglas1@gmail.com>
2022-06-29 18:04:54 -0400

    Linter

--- app/src/androidTest/java/com/kickstarter/screenshoot/testing/ui/components/CommentCardShotTest.kt
+++ app/src/androidTest/java/com/kickstarter/screenshoot/testing/ui/components/CommentCardShotTest.kt
@@ -88,8 +88,8 @@ class CommentCardShotTest : ScreenshotTest {
         commentCard.setCommentCardStatus(CommentCardStatus.FLAGGED_COMMENT)
         commentCard.setFlaggedMessage(
             getInstrumentation().targetContext.getString(R.string.FPO_this_comment_is_under_review_for_potentially_violating) +
-                    " " +
-                    getInstrumentation().targetContext.getString(R.string.FPO_kickstarters_community_guidelines)
+                " " +
+                getInstrumentation().targetContext.getString(R.string.FPO_kickstarters_community_guidelines)
         )
         compareScreenshot(commentCard)
     }

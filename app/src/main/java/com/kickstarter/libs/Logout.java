b2222e7b163407e2fd806af9bb1c13cd5f5bd483
hadia <hadiamohamed.iti@gmail.com>
2022-07-01 00:12:19 +0200

    revert LOGOUT

---
+++ app/src/main/java/com/kickstarter/libs/Logout.java
@@ -0,0 +1,23 @@
+package com.kickstarter.libs;
+
+import com.facebook.login.LoginManager;
+
+import java.net.CookieManager;
+
+import androidx.annotation.NonNull;
+
+public final class Logout {
+    private final CookieManager cookieManager;
+    private final CurrentUserType currentUser;
+
+    public Logout(final @NonNull CookieManager cookieManager, final @NonNull CurrentUserType currentUser) {
+        this.cookieManager = cookieManager;
+        this.currentUser = currentUser;
+    }
+
+    public void execute() {
+        this.currentUser.logout();
+        this.cookieManager.getCookieStore().removeAll();
+        LoginManager.getInstance().logOut();
+    }
+}

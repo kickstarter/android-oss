1e1a460748c367875529710297931c4de93840bc
hadia <hadiamohamed.iti@gmail.com>
2022-07-18 19:46:52 +0200

    Migrate ProjectNotificationViewModel

--- app/src/main/java/com/kickstarter/viewmodels/ProjectNotificationSettingsViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/ProjectNotificationSettingsViewModel.kt
@@ -1,64 +1,50 @@
-package com.kickstarter.viewmodels;
+package com.kickstarter.viewmodels
 
-import com.kickstarter.libs.ActivityViewModel;
-import com.kickstarter.libs.Environment;
-import com.kickstarter.models.ProjectNotification;
-import com.kickstarter.services.ApiClientType;
-import com.kickstarter.ui.activities.ProjectNotificationSettingsActivity;
+import com.kickstarter.libs.ActivityViewModel
+import com.kickstarter.libs.Environment
+import com.kickstarter.libs.rx.transformers.Transformers
+import com.kickstarter.models.ProjectNotification
+import com.kickstarter.services.ApiClientType
+import com.kickstarter.ui.activities.ProjectNotificationSettingsActivity
+import rx.Observable
+import rx.subjects.BehaviorSubject
 
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import rx.Notification;
-import rx.Observable;
-import rx.subjects.BehaviorSubject;
-
-import static com.kickstarter.libs.rx.transformers.Transformers.errors;
-import static com.kickstarter.libs.rx.transformers.Transformers.values;
-
-public interface ProjectNotificationSettingsViewModel {
-
-  interface Outputs {
-    Observable<List<ProjectNotification>> projectNotifications();
-
-    Observable<Void> unableToFetchProjectNotificationsError();
-  }
-
-  final class ViewModel extends ActivityViewModel<ProjectNotificationSettingsActivity> implements Outputs {
-
-    private final ApiClientType client;
-
-    public ViewModel(final @NonNull Environment environment) {
-      super(environment);
-
-      this.client = environment.apiClient();
-
-      final Observable<Notification<List<ProjectNotification>>> projectNotificationsNotification =
-        this.client
-          .fetchProjectNotifications()
-          .materialize();
-
-      projectNotificationsNotification
-        .compose(values())
-        .compose(bindToLifecycle())
-        .subscribe(this.projectNotifications::onNext);
-
-      projectNotificationsNotification
-        .compose(errors())
-        .compose(bindToLifecycle())
-        .subscribe(e -> this.unableToFetchProjectNotificationsError.onNext(null));
+interface ProjectNotificationSettingsViewModel {
+    interface Outputs {
+        fun projectNotifications(): Observable<List<ProjectNotification>>
+        fun unableToFetchProjectNotificationsError(): Observable<Void>
     }
 
-    private final BehaviorSubject<List<ProjectNotification>> projectNotifications = BehaviorSubject.create();
-    private final BehaviorSubject<Void> unableToFetchProjectNotificationsError = BehaviorSubject.create();
+    class ViewModel(environment: Environment) :
+        ActivityViewModel<ProjectNotificationSettingsActivity>(environment), Outputs {
+        private val client: ApiClientType
+        private val projectNotifications = BehaviorSubject.create<List<ProjectNotification>>()
+        private val unableToFetchProjectNotificationsError = BehaviorSubject.create<Void>()
 
-    public final Outputs outputs = this;
+        val outputs: Outputs = this
 
-    @Override public @NonNull Observable<List<ProjectNotification>> projectNotifications() {
-      return this.projectNotifications;
+        override fun projectNotifications(): Observable<List<ProjectNotification>> = projectNotifications
+
+        override fun unableToFetchProjectNotificationsError(): Observable<Void> = unableToFetchProjectNotificationsError
+
+        init {
+            client = requireNotNull(environment.apiClient())
+
+            val projectNotificationsNotification = client
+                .fetchProjectNotifications()
+                .materialize()
+
+            projectNotificationsNotification
+                .compose(Transformers.values())
+                .compose(bindToLifecycle())
+                .subscribe {
+                    projectNotifications.onNext(it)
+                }
+
+            projectNotificationsNotification
+                .compose(Transformers.errors())
+                .compose(bindToLifecycle())
+                .subscribe { unableToFetchProjectNotificationsError.onNext(null) }
+        }
     }
-    @Override public @NonNull Observable<Void> unableToFetchProjectNotificationsError() {
-      return this.unableToFetchProjectNotificationsError;
-    }
-  }
 }

c192f186848f05de82afc11098c20b5e03d5db7a
Isabel Martin <arkariang@gmail.com>
2022-08-04 09:38:04 -0700

    - progress button

--- app/src/main/java/com/kickstarter/ui/viewholders/RewardAddCardViewHolder.kt
+++ app/src/main/java/com/kickstarter/ui/viewholders/RewardAddCardViewHolder.kt
@@ -1,11 +1,14 @@
 package com.kickstarter.ui.viewholders
 
 import com.kickstarter.databinding.ItemAddCardBinding
+import com.kickstarter.libs.utils.extensions.setGone
 
 class RewardAddCardViewHolder(val binding: ItemAddCardBinding, val delegate: Delegate) : KSViewHolder(binding.root) {
 
     init {
         this.binding.addCardButton.setOnClickListener {
+            this.binding.newPaymentPlusIcon.setGone(true)
+            this.binding.newPaymentProgress.setGone(false)
             this.delegate.addNewCardButtonClicked()
         }
     }

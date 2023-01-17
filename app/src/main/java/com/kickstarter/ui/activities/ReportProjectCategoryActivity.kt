a9bc01d2942ab868f50e87d3d1f487f3c323c6e5
Isabel Martin <arkariang@gmail.com>
2023-01-17 12:29:23 -0800

    - New screen for reporting a project

---
+++ app/src/main/java/com/kickstarter/ui/activities/ReportProjectCategoryActivity.kt
@@ -0,0 +1,16 @@
+package com.kickstarter.ui.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ReportProjectCategoryActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+//            BasicCodelabTheme {
+//                MyApp()
+//            }
+        }
+    }
+}

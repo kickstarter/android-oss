b831a813dbae74cd9d99f0b7dfc5f2c40d1ce691
Leigh Douglas <leigh@kickstarter.com>
2023-02-21 16:02:46 -0500

    Update modal with localized copy, remove placeholder strings

--- app/src/main/java/com/kickstarter/ui/fragments/ConsentManagementDialogFragment.kt
+++ app/src/main/java/com/kickstarter/ui/fragments/ConsentManagementDialogFragment.kt
@@ -21,12 +21,12 @@ class ConsentManagementDialogFragment : DialogFragment() {
 
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            builder.setTitle(R.string.FPO_allow_app_to_track)
-            builder.setMessage(R.string.FPO_Allow_Kickstarter_to_track_analytics_to_provide_a_more_personalized_experience)
-                .setPositiveButton(R.string.FPO_allow) { dialog, id ->
+            builder.setTitle(R.string.Allow_app_to_track)
+            builder.setMessage(R.string.We_use_personal_data_to_provide_a_good_experience_on_Kickstarter_and_to_help_connect_you_with_projects_you_ll_love)
+                .setPositiveButton(R.string.Allow) { dialog, id ->
                     this.viewModel.inputs.userConsentPreference(true)
                 }
-                .setNegativeButton(R.string.FPO_deny) { dialog, id ->
+                .setNegativeButton(R.string.Decline) { dialog, id ->
                     this.viewModel.inputs.userConsentPreference(false)
                 }
             builder.create()

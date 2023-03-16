69414eff184f5a6ded88c60b7f90a568451171c5
Isabel Martin <arkariang@gmail.com>
2023-03-16 13:46:40 -0700

    - Removed PaymentSheet on user settings

--- app/src/main/java/com/kickstarter/libs/models/OptimizelyFeature.kt
+++ app/src/main/java/com/kickstarter/libs/models/OptimizelyFeature.kt
@@ -5,7 +5,6 @@ class OptimizelyFeature {
         LIGHTS_ON("android_lights_on"),
         ANDROID_PAYMENTSHEET("android_paymentsheet"),
         ANDROID_FACEBOOK_LOGIN_REMOVE("android_facebook_login_remove"),
-        ANDROID_PAYMENTSHEET_SETTINGS("android_paymentsheet_user_settings"),
         ANDROID_HIDE_APP_RATING_DIALOG("android_hide_app_rating_dialog"),
         ANDROID_CONSENT_MANAGEMENT("android_consent_management"),
         ANDROID_CAPI_INTEGRATION("android_capi_integration"),

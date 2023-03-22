f7952206a0fb2d0fa9f1a9321e5201c2b7904540
Isabel Martin <arkariang@gmail.com>
2023-03-22 15:20:16 -0700

    - Remove Lights_On editorial

--- app/src/main/java/com/kickstarter/libs/models/OptimizelyFeature.kt
+++ app/src/main/java/com/kickstarter/libs/models/OptimizelyFeature.kt
@@ -2,8 +2,6 @@ package com.kickstarter.libs.models
 
 class OptimizelyFeature {
     enum class Key(val key: String) {
-        LIGHTS_ON("android_lights_on"),
-        ANDROID_PAYMENTSHEET("android_paymentsheet"),
         ANDROID_FACEBOOK_LOGIN_REMOVE("android_facebook_login_remove"),
         ANDROID_HIDE_APP_RATING_DIALOG("android_hide_app_rating_dialog"),
         ANDROID_CONSENT_MANAGEMENT("android_consent_management"),

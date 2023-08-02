f9996d6e5824fac776b6966b425e8d1cb4bd30b3
Isa Martin <arkariang@gmail.com>
2023-08-02 16:25:50 -0700

    - completed designs

--- app/src/main/java/com/kickstarter/ui/activities/compose/projectpage/AiDisclosureScreen.kt
+++ app/src/main/java/com/kickstarter/ui/activities/compose/projectpage/AiDisclosureScreen.kt
@@ -1,23 +1,32 @@
 package com.kickstarter.ui.activities.compose.projectpage
 
 import android.content.res.Configuration
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kickstarter.R
 import com.kickstarter.models.AiDisclosure
 import com.kickstarter.ui.compose.designsystem.KSTheme
@@ -29,6 +38,8 @@ import com.kickstarter.viewmodels.projectpage.ProjectAIViewModel
 fun AiDisclosureScreenPreview() {
     KSTheme {
         val aiDisclosure = AiDisclosure.builder()
+            .fundingForAiOption(true)
+            .fundingForAiConsent(true)
             .generatedByAiConsent("Some generated consent")
             .generatedByAiDetails("Some generated details")
             .otherAiDetails("Other details to include")
@@ -54,88 +65,15 @@ fun AiDisclosureScreen(
                     top = KSTheme.dimensions.paddingMedium,
                     end = KSTheme.dimensions.paddingMedium
                 )
-            ),
+            )
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(KSTheme.dimensions.paddingMediumSmall)
     ) {
-        Text(
-            text = stringResource(id = R.string.Use_of_ai_fpo),
-            style = KSTheme.typography.title1Bold,
-            color = KSTheme.colors.kds_support_700
-        )
-        Text(
-            text = stringResource(id = R.string.My_project_seeks_founding_fpo),
-            style = KSTheme.typography.headline,
-            color = KSTheme.colors.kds_support_700
-        )
-        Text(
-            text = stringResource(id = R.string.For_the_database_orsource_fpo),
-            style = KSTheme.typography.footnote,
-            color = KSTheme.colors.kds_support_700
-        )
-        Text(
-            text = stringResource(id = R.string.The_owners_of_fpo),
-            style = KSTheme.typography.footnote,
-            color = KSTheme.colors.kds_support_700
-        )
-        Text(
-            text = stringResource(id = R.string.There_is_or_will_be_fpo),
-            style = KSTheme.typography.footnote,
-            color = KSTheme.colors.kds_support_700
-        )
-        Spacer(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = KSTheme.colors.kds_support_300
-                )
-                .height(KSTheme.dimensions.dividerThickness)
-        )
-        Text(
-            text = stringResource(id = R.string.I_plan_to_use_AI_fpo),
-            style = KSTheme.typography.headline,
-            color = KSTheme.colors.kds_support_700
-        )
-        Text(
-            text = state.aiDisclosure?.generatedByAiDetails ?: "",
-            style = KSTheme.typography.footnote,
-            color = KSTheme.colors.kds_support_700
-        )
+        InvolvesFundingSection(state)
 
-        Text(
-            text = state.aiDisclosure?.generatedByAiConsent ?: "",
-            style = KSTheme.typography.footnote,
-            color = KSTheme.colors.kds_support_700
-        )
+        InvolvesGenerationSection(state)
 
-        Spacer(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = KSTheme.colors.kds_support_300
-                )
-                .height(KSTheme.dimensions.dividerThickness)
-        )
-        Text(
-            text = stringResource(id = R.string.I_am_incorporating_AI_fpo),
-            style = KSTheme.typography.headline,
-            color = KSTheme.colors.kds_support_700
-        )
-
-        Text(
-            text = state.aiDisclosure?.otherAiDetails ?: "",
-            style = KSTheme.typography.footnote,
-            color = KSTheme.colors.kds_support_700
-        )
-
-        // - TODO: extract as reusable composable to DesignSystem
-        Spacer(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = KSTheme.colors.kds_support_300
-                )
-                .height(KSTheme.dimensions.dividerThickness)
-        )
+        InvolvesOtherSection(state)
 
         // - TODO: extract to reusable composable to DesignSysmtem, only requires the callback
         ClickableText(
@@ -149,6 +87,126 @@ fun AiDisclosureScreen(
             onClick = {
                 clickCallback(state.openExternalUrl)
             }
+        )
+    }
+}
+
+@Composable
+private fun InvolvesOtherSection(state: ProjectAIViewModel.UiState) {
+    val otherDetails = state.aiDisclosure?.otherAiDetails ?: ""
+    if (otherDetails.isNotEmpty()) {
+        Text(
+            text = stringResource(id = R.string.I_am_incorporating_AI_fpo),
+            style = KSTheme.typography.headline,
+            color = KSTheme.colors.kds_support_700
+        )
+
+        Text(
+            text = otherDetails,
+            style = KSTheme.typography.footnote,
+            color = KSTheme.colors.kds_support_700
+        )
+
+        // - TODO: extract as reusable composable to DesignSystem
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = KSTheme.colors.kds_support_300
+                )
+                .height(KSTheme.dimensions.dividerThickness)
+        )
+    }
+}
+
+@Composable
+private fun InvolvesGenerationSection(state: ProjectAIViewModel.UiState) {
+    val details = state.aiDisclosure?.generatedByAiDetails ?: ""
+    val consent = state.aiDisclosure?.generatedByAiConsent ?: ""
+    if (details.isNotEmpty() || consent.isNotEmpty()) {
+        Text(
+            text = stringResource(id = R.string.I_plan_to_use_AI_fpo),
+            style = KSTheme.typography.headline,
+            color = KSTheme.colors.kds_support_700
+        )
+
+        Text(
+            text = details,
+            style = KSTheme.typography.footnote,
+            color = KSTheme.colors.kds_support_700
+        )
+
+        Text(
+            text = consent,
+            style = KSTheme.typography.footnote,
+            color = KSTheme.colors.kds_support_700
+        )
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = KSTheme.colors.kds_support_300
+                )
+                .height(KSTheme.dimensions.dividerThickness)
+        )
+    }
+}
+
+@Composable
+private fun InvolvesFundingSection(state: ProjectAIViewModel.UiState) {
+    val fundingAiAttr = state.aiDisclosure?.fundingForAiAttribution ?: false
+    val fundingAiConsent = state.aiDisclosure?.fundingForAiConsent ?: false
+    val fundingAiOption = state.aiDisclosure?.fundingForAiOption ?: false
+
+    if (fundingAiAttr || fundingAiConsent || fundingAiOption) {
+        Text(
+            text = stringResource(id = R.string.Use_of_ai_fpo),
+            style = KSTheme.typography.title2Bold,
+            color = KSTheme.colors.kds_support_700
+        )
+        Text(
+            text = stringResource(id = R.string.My_project_seeks_founding_fpo),
+            style = KSTheme.typography.headline,
+            color = KSTheme.colors.kds_support_700
+        )
+
+        if (fundingAiConsent) AiDisclosureRow(stringResId = R.string.For_the_database_orsource_fpo)
+        if (fundingAiAttr) AiDisclosureRow(stringResId = R.string.The_owners_of_fpo)
+        if (fundingAiOption) AiDisclosureRow(stringResId = R.string.There_is_or_will_be_fpo)
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = KSTheme.colors.kds_support_300
+                )
+                .height(KSTheme.dimensions.dividerThickness)
+        )
+    }
+}
+
+@Composable
+fun AiDisclosureRow(
+    @DrawableRes iconId: Int = R.drawable.icon__check_green,
+    @StringRes stringResId: Int
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(KSTheme.dimensions.paddingSmall)
+    ) {
+        Image(
+            modifier = Modifier
+                .width(18.dp)
+                .height(18.dp),
+            painter = painterResource(
+                id = iconId
+            ),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(id = stringResId),
+            style = KSTheme.typography.footnote,
+            color = KSTheme.colors.kds_support_700
         )
     }
 }

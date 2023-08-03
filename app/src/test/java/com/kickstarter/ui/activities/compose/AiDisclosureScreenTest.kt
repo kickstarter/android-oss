ff494bc6aeb4001c16f99efb73256207d1cc9648
Isa Martin <arkariang@gmail.com>
2023-08-03 16:15:51 -0700

    - linter

--- app/src/test/java/com/kickstarter/ui/activities/compose/AiDisclosureScreenTest.kt
+++ app/src/test/java/com/kickstarter/ui/activities/compose/AiDisclosureScreenTest.kt
@@ -3,7 +3,6 @@ package com.kickstarter.ui.activities.compose
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.R

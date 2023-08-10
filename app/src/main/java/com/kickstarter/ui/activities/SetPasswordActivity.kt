078b24f03250baa3566a6e95d75e4dec632c922f
leighdouglas <leighcdouglas1@gmail.com>
2023-08-10 18:31:41 -0400

    MBL-798: SetPasswordViewModel.kt to Jetpack ViewModel and RxJava2.2 (#1830)
    * Migrate setpassword vm to rx2
    
    * Fix tests, cleanup
    
    * Linter
    
    * Fix mapper function returning null
    
    * Cleanup

--- app/src/main/java/com/kickstarter/ui/activities/SetPasswordActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/SetPasswordActivity.kt
@@ -1,27 +1,37 @@
 package com.kickstarter.ui.activities
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.kickstarter.R
 import com.kickstarter.databinding.ActivitySetPasswordBinding
-import com.kickstarter.libs.BaseActivity
-import com.kickstarter.libs.qualifiers.RequiresActivityViewModel
-import com.kickstarter.libs.rx.transformers.Transformers
 import com.kickstarter.libs.utils.ViewUtils
+import com.kickstarter.libs.utils.extensions.addToDisposable
+import com.kickstarter.libs.utils.extensions.getEnvironment
 import com.kickstarter.ui.extensions.onChange
 import com.kickstarter.viewmodels.SetPasswordViewModel
-import rx.android.schedulers.AndroidSchedulers
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 
-@RequiresActivityViewModel(SetPasswordViewModel.ViewModel::class)
-class SetPasswordActivity : BaseActivity<SetPasswordViewModel.ViewModel>() {
+class SetPasswordActivity : AppCompatActivity() {
+    private lateinit var viewModelFactory: SetPasswordViewModel.Factory
+    private val viewModel: SetPasswordViewModel.SetPasswordViewModel by viewModels { viewModelFactory }
     private lateinit var binding: ActivitySetPasswordBinding
     private var errorTitleString = R.string.general_error_oops
+    private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        this.getEnvironment()?.let { env ->
+            viewModelFactory = SetPasswordViewModel.Factory(env)
+        }
+
         binding = ActivitySetPasswordBinding.inflate(layoutInflater)
 
+        viewModel.configureWith(intent)
         setContentView(binding.root)
         setSupportActionBar(binding.resetPasswordToolbar.loginToolbar)
         binding.resetPasswordToolbar.loginToolbar.setTitle(getString(R.string.Set_your_password))
@@ -34,23 +44,19 @@ class SetPasswordActivity : BaseActivity<SetPasswordViewModel.ViewModel>() {
         }
 
         this.viewModel.outputs.progressBarIsVisible()
-            .compose(bindToLifecycle())
-            .compose(Transformers.observeForUI())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
                 binding.progressBar.isGone = !it
-            }
+            }.addToDisposable(disposables)
 
         this.viewModel.outputs.setUserEmail()
-            .compose(bindToLifecycle())
-            .compose(Transformers.observeForUI())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
-                binding.setPasswordHint.text = this.environment().ksString()?.format(getString(R.string.We_will_be_discontinuing_the_ability_to_log_in_via_FB), "email", it)
-            }
+                binding.setPasswordHint.text = getEnvironment()?.ksString()?.format(getString(R.string.We_will_be_discontinuing_the_ability_to_log_in_via_FB), "email", it)
+            }.addToDisposable(disposables)
 
         this.viewModel.outputs.passwordWarning()
-            .compose(bindToLifecycle())
-            .compose(Transformers.observeForUI())
-            .subscribe {
+            .observeOn(AndroidSchedulers.mainThread()).subscribe {
                 binding.warning.text = when {
                     it != null -> {
                         getString(it)
@@ -58,27 +64,32 @@ class SetPasswordActivity : BaseActivity<SetPasswordViewModel.ViewModel>() {
                     else -> null
                 }
                 binding.warning.isVisible = (it != null)
-            }
+            }.addToDisposable(disposables)
 
         this.viewModel.outputs.error()
-            .compose(bindToLifecycle())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { ViewUtils.showDialog(this, getString(this.errorTitleString), it) }
+            .addToDisposable(disposables)
 
         this.viewModel.outputs.isFormSubmitting()
-            .compose(bindToLifecycle())
-            .compose(Transformers.observeForUI())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe { this.setFormDisabled(it) }
+            .addToDisposable(disposables)
 
         this.viewModel.outputs.saveButtonIsEnabled()
-            .compose(bindToLifecycle())
-            .compose(Transformers.observeForUI())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe { this.setFormEnabled(it) }
+            .addToDisposable(disposables)
 
         this.viewModel.outputs.success()
-            .compose(bindToLifecycle())
-            .compose(Transformers.observeForUI())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe { finish() }
+            .addToDisposable(disposables)
+    }
+
+    override fun onDestroy() {
+        disposables.clear()
+        super.onDestroy()
     }
 
     private fun setFormEnabled(isEnabled: Boolean) {
@@ -87,9 +98,5 @@ class SetPasswordActivity : BaseActivity<SetPasswordViewModel.ViewModel>() {
 
     private fun setFormDisabled(isDisabled: Boolean) {
         setFormEnabled(!isDisabled)
-    }
-
-    override fun back() {
-        // Disable back action Gesture
     }
 }

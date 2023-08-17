4e580549b186f25a6134f1d9f92bdfa35468ed0f
Isabel Martin <arkariang@gmail.com>
2023-08-17 09:46:13 -0700

    MBL-942: HTMLParser now accepts headers (#1835)

--- app/src/internal/java/com/kickstarter/ui/activities/PlaygroundActivity.kt
+++ app/src/internal/java/com/kickstarter/ui/activities/PlaygroundActivity.kt
@@ -3,7 +3,6 @@ package com.kickstarter.ui.activities
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
 import android.util.Pair
 import android.view.View
 import androidx.annotation.RequiresApi
@@ -34,32 +33,22 @@ class PlaygroundActivity : BaseActivity<PlaygroundViewModel.ViewModel?>() {
         view = binding.root
         setContentView(view)
 
-        // - Allow clickable spans
-        binding.text.linksClickable = true
-        binding.text.isClickable = true
-        binding.text.movementMethod = LinkMovementMethod.getInstance()
+        val html2 = "<h1>This is heading 1</h1>\n" +
+            "<h2>This is heading 2</h2>\n" +
+            "<h3>This is heading 3</h3>\n" +
+            "<h4>This is heading 4</h4>\n" +
+            "<h5>This is heading 5</h5>\n" +
+            "<h6>This is heading 6</h6>"
 
-        val headerSize = resources.getDimensionPixelSize(R.dimen.title_3)
-        val body = resources.getDimensionPixelSize(R.dimen.callout)
+        val listOfElements = HTMLParser().parse(html2)
 
-        val url = "https://www.meneame.net/"
-        val html = "<ul>\n" +
-            "   <li>This</li>\n" +
-            "   <li>This and more text <a href=\\\"http://record.pt\\\" target=\\\"_blank\\\" rel=\\\"noopener\\\"><em><strong>lalalalalala</strong></em></a></li>\n" +
-            "   <li>More text here <em><strong>lalalalalala</strong></em> and here </li>\n" +
-            "   <li><strong>is</strong></li>\n" +
-            "   <li>is <strong>is</strong> is</li>\n" +
-            "   <li><em><strong>is</strong></em> with some <strong>text</strong> in the middle <em><strong>is</strong></em></li>\n" +
-            "   <li><em>a</em></li>\n" +
-            "   <li><a href=\\\"http://record.pt\\\" target=\\\"_blank\\\" rel=\\\"noopener\\\">link</a></li>\n" +
-            "   <li>text with <a href=\\\"http://record.pt\\\" target=\\\"_blank\\\" rel=\\\"noopener\\\">link</a></li>\n" +
-            "   <li>Hola <strong><em>que tal </em></strong> majete</li>\n" +
-            "   <li><a href=$url target=\\\"_blank\\\" rel=\\\"noopener\\\"><em><strong>Meneane </strong></em></a><a href=$url target=\\\"_blank\\\" rel=\\\"noopener\\\">Another URL in this list</a> and some text</li>"
-        "</ul>"
-        val listOfElements = HTMLParser().parse(html)
-        val element: TextViewElement = listOfElements.first() as TextViewElement
-
-        binding.text.text = element.getStyledComponents(body, headerSize, this)
+        // - The parser detects 6 elements and applies the style to each one
+        binding.h1.text = (listOfElements[0] as TextViewElement).getStyledComponents(this)
+        binding.h2.text = (listOfElements[1] as TextViewElement).getStyledComponents(this)
+        binding.h3.text = (listOfElements[2] as TextViewElement).getStyledComponents(this)
+        binding.h4.text = (listOfElements[3] as TextViewElement).getStyledComponents(this)
+        binding.h5.text = (listOfElements[4] as TextViewElement).getStyledComponents(this)
+        binding.h6.text = (listOfElements[5] as TextViewElement).getStyledComponents(this)
 
         setStepper()
         setProjectActivityButtonClicks()

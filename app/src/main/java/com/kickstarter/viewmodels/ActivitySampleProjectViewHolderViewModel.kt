02dfd64942dd2a57b761fbb2a735d2d61d60ff2a
mtgriego <matthew.t.griego@gmail.com>
2023-10-16 15:28:15 -0700

    [MBL-741] Update Activitysampleviewholderviewmodel to RX2 (#1873)
    * update activitysampleviewholderviewmodel to rx2
    
    * remove viewmodel extension

--- app/src/main/java/com/kickstarter/viewmodels/ActivitySampleProjectViewHolderViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/ActivitySampleProjectViewHolderViewModel.kt
@@ -1,44 +1,48 @@
 package com.kickstarter.viewmodels
 
-import androidx.annotation.NonNull
-import com.kickstarter.libs.ActivityViewModel
-import com.kickstarter.libs.Environment
+import com.kickstarter.libs.utils.extensions.addToDisposable
 import com.kickstarter.libs.utils.extensions.isNotNull
 import com.kickstarter.models.Activity
-import com.kickstarter.ui.viewholders.ActivitySampleProjectViewHolder
-import rx.Observable
-import rx.subjects.BehaviorSubject
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
 
 class ActivitySampleProjectViewHolderViewModel {
     interface Inputs {
         /** Configure with current [Activity]. */
         fun configureWith(activity: Activity)
+
+        fun onCleared()
     }
 
     interface Outputs {
         fun bindActivity(): Observable<Activity>
     }
 
-    class ViewModel(@NonNull val environment: Environment) : ActivityViewModel<ActivitySampleProjectViewHolder>(environment), Inputs, Outputs {
+    class ActivitySampleProjectHolderViewModel : Inputs, Outputs {
         val inputs: Inputs = this
         val outputs: Outputs = this
 
         private val activityInput = BehaviorSubject.create<Activity>()
         private val bindActivity = BehaviorSubject.create<Activity>()
+        private val disposables = CompositeDisposable()
         init {
             activityInput
                 .filter { it.isNotNull() }
                 .filter { it.user().isNotNull() }
                 .filter { it.project().isNotNull() }
                 .map { requireNotNull(it) }
-                .compose(bindToLifecycle())
                 .subscribe {
                     bindActivity.onNext(it)
-                }
+                }.addToDisposable(disposables)
         }
 
         override fun configureWith(activity: Activity) =
             this.activityInput.onNext(activity)
         override fun bindActivity(): Observable<Activity> = this.bindActivity
+
+        override fun onCleared() {
+            disposables.clear()
+        }
     }
 }

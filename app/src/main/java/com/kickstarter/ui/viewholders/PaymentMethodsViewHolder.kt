35767da02e5ab5916cf0f80d8327c7a7e5673d19
Matthew <m.griego@kickstarter.com>
2023-10-17 14:40:43 -0700

    convert paymentmethodsviewholderviewmodel to rx2

--- app/src/main/java/com/kickstarter/ui/viewholders/PaymentMethodsViewHolder.kt
+++ app/src/main/java/com/kickstarter/ui/viewholders/PaymentMethodsViewHolder.kt
@@ -1,19 +1,21 @@
 package com.kickstarter.ui.viewholders
 
-import androidx.annotation.NonNull
 import com.kickstarter.R
 import com.kickstarter.databinding.ItemPaymentMethodBinding
-import com.kickstarter.libs.rx.transformers.Transformers.observeForUI
+import com.kickstarter.libs.rx.transformers.Transformers.observeForUIV2
+import com.kickstarter.libs.utils.extensions.addToDisposable
 import com.kickstarter.models.StoredCard
 import com.kickstarter.viewmodels.PaymentMethodsViewHolderViewModel
+import io.reactivex.disposables.CompositeDisposable
 
-class PaymentMethodsViewHolder(private val binding: ItemPaymentMethodBinding, @NonNull val delegate: Delegate) : KSViewHolder(binding.root) {
+class PaymentMethodsViewHolder(private val binding: ItemPaymentMethodBinding, val delegate: Delegate) : KSViewHolder(binding.root) {
 
     private val ksString = requireNotNull(environment().ksString())
-    private val vm: PaymentMethodsViewHolderViewModel.ViewModel = PaymentMethodsViewHolderViewModel.ViewModel(environment())
+    private val vm: PaymentMethodsViewHolderViewModel.PaymentMethodsViewHolderViewModel = PaymentMethodsViewHolderViewModel.PaymentMethodsViewHolderViewModel()
 
     private val creditCardExpirationString = this.context().getString(R.string.Credit_card_expiration)
     private val cardEndingInString = this.context().getString(R.string.Card_ending_in_last_four)
+    private val disposables = CompositeDisposable()
 
     interface Delegate {
         fun deleteCardButtonClicked(paymentMethodsViewHolder: PaymentMethodsViewHolder, paymentSourceId: String)
@@ -22,29 +24,29 @@ class PaymentMethodsViewHolder(private val binding: ItemPaymentMethodBinding, @N
     init {
 
         this.vm.outputs.issuerImage()
-            .compose(bindToLifecycle())
-            .compose(observeForUI())
+            .compose(observeForUIV2())
             .subscribe { binding.creditCardLogo.setImageResource(it) }
+            .addToDisposable(disposables)
 
         this.vm.outputs.expirationDate()
-            .compose(bindToLifecycle())
-            .compose(observeForUI())
+            .compose(observeForUIV2())
             .subscribe { setExpirationDateTextView(it) }
+            .addToDisposable(disposables)
 
         this.vm.outputs.id()
-            .compose(bindToLifecycle())
-            .compose(observeForUI())
+            .compose(observeForUIV2())
             .subscribe { this.delegate.deleteCardButtonClicked(this, it) }
+            .addToDisposable(disposables)
 
         this.vm.outputs.lastFour()
-            .compose(bindToLifecycle())
-            .compose(observeForUI())
+            .compose(observeForUIV2())
             .subscribe { setLastFourTextView(it) }
+            .addToDisposable(disposables)
 
         this.vm.outputs.issuer()
-            .compose(bindToLifecycle())
-            .compose(observeForUI())
+            .compose(observeForUIV2())
             .subscribe { binding.creditCardLogo.contentDescription = it }
+            .addToDisposable(disposables)
 
         binding.deleteCard.setOnClickListener { this.vm.inputs.deleteIconClicked() }
     }
@@ -63,5 +65,10 @@ class PaymentMethodsViewHolder(private val binding: ItemPaymentMethodBinding, @N
 
     private fun setLastFourTextView(lastFour: String) {
         binding.creditCardLastFourDigits.text = this.ksString.format(this.cardEndingInString, "last_four", lastFour)
+    }
+
+    override fun destroy() {
+        disposables.clear()
+        super.destroy()
     }
 }

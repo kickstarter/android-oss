a6db8a98643c5e768ecb82d8897ee12e19ea9150
Isa Martin <arkariang@gmail.com>
2024-01-03 12:58:58 -0800

    - ShippingRuleViewHolder migrated to RXJava2

--- app/src/main/java/com/kickstarter/ui/viewholders/ShippingRuleViewHolder.kt
+++ app/src/main/java/com/kickstarter/ui/viewholders/ShippingRuleViewHolder.kt
@@ -2,10 +2,12 @@ package com.kickstarter.ui.viewholders
 
 import android.util.Pair
 import com.kickstarter.databinding.ItemShippingRuleBinding
+import com.kickstarter.libs.utils.extensions.addToDisposable
 import com.kickstarter.models.Project
 import com.kickstarter.models.ShippingRule
 import com.kickstarter.viewmodels.ShippingRuleViewHolderViewModel
-import rx.android.schedulers.AndroidSchedulers
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 
 class ShippingRuleViewHolder(private val binding: ItemShippingRuleBinding, val delegate: Delegate) : KSArrayViewHolder(binding.root) {
 
@@ -14,13 +16,14 @@ class ShippingRuleViewHolder(private val binding: ItemShippingRuleBinding, val d
     }
     private lateinit var shippingRule: ShippingRule
     private val viewModel = ShippingRuleViewHolderViewModel.ViewModel(environment())
+    private val disposables = CompositeDisposable()
 
     init {
 
         this.viewModel.outputs.shippingRuleText()
-            .compose(bindToLifecycle())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { this.binding.shippingRulesItemTextView.text = it }
+            .addToDisposable(disposables)
 
         this.binding.shippingRuleRoot.setOnClickListener {
             this.delegate.ruleSelected(this.shippingRule)
@@ -33,5 +36,10 @@ class ShippingRuleViewHolder(private val binding: ItemShippingRuleBinding, val d
         val project = requireNotNull(shippingRuleAndProject.second) { Project::class.java.toString() + " required to be non-null." }
 
         this.viewModel.inputs.configureWith(this.shippingRule, project)
+    }
+
+    override fun destroy() {
+        this.viewModel.onCleared()
+        disposables.clear()
     }
 }
--- app/src/main/java/com/kickstarter/viewmodels/ShippingRuleViewHolderViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/ShippingRuleViewHolderViewModel.kt
@@ -1,15 +1,15 @@
 package com.kickstarter.viewmodels
 
 import android.util.Pair
-import com.kickstarter.libs.ActivityViewModel
 import com.kickstarter.libs.Environment
+import com.kickstarter.libs.utils.extensions.addToDisposable
 import com.kickstarter.libs.utils.extensions.isNotNull
 import com.kickstarter.models.Project
 import com.kickstarter.models.ShippingRule
-import com.kickstarter.ui.viewholders.ShippingRuleViewHolder
-import rx.Observable
-import rx.subjects.BehaviorSubject
-import rx.subjects.PublishSubject
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
 
 interface ShippingRuleViewHolderViewModel {
 
@@ -23,11 +23,13 @@ interface ShippingRuleViewHolderViewModel {
         fun shippingRuleText(): Observable<String>
     }
 
-    class ViewModel(val environment: Environment) : ActivityViewModel<ShippingRuleViewHolder>(environment), Inputs, Outputs {
+    class ViewModel(val environment: Environment) : Inputs, Outputs {
 
         private val shippingRuleAndProject = PublishSubject.create<Pair<ShippingRule, Project>>()
 
         private val shippingRuleText = BehaviorSubject.create<String>()
+
+        private val disposables = CompositeDisposable()
 
         val inputs: Inputs = this
         val outputs: Outputs = this
@@ -35,13 +37,19 @@ interface ShippingRuleViewHolderViewModel {
         init {
             this.shippingRuleAndProject
                 .filter { it.first.isNotNull() }
-                .map { it.first.location()?.displayableName() }
-                .compose(bindToLifecycle())
-                .subscribe(this.shippingRuleText)
+                .map { it.first.location()?.displayableName() ?: "" }
+                .subscribe {
+                    this.shippingRuleText.onNext(it)
+                }
+                .addToDisposable(disposables)
         }
 
         override fun configureWith(shippingRule: ShippingRule, project: Project) = this.shippingRuleAndProject.onNext(Pair.create(shippingRule, project))
 
         override fun shippingRuleText(): Observable<String> = this.shippingRuleText
+
+        fun onCleared() {
+            disposables.clear()
+        }
     }
 }

eb88a4581ba78e6be94dc8da4d9934fb8e6dbfc3
Matthew <m.griego@kickstarter.com>
2024-01-17 10:36:10 -0800

    Add post campaign feature flag

--- app/src/main/java/com/kickstarter/libs/featureflag/FeatureFlagClient.kt
+++ app/src/main/java/com/kickstarter/libs/featureflag/FeatureFlagClient.kt
@@ -61,7 +61,8 @@ enum class FlagKey(val key: String) {
     ANDROID_GOOGLE_ANALYTICS("android_google_analytics"),
     ANDROID_PRE_LAUNCH_SCREEN("android_pre_launch_screen"),
     ANDROID_AI_TAB("android_ai_tab"),
-    ANDROID_DARK_MODE_ENABLED("android_dark_mode_enabled")
+    ANDROID_DARK_MODE_ENABLED("android_dark_mode_enabled"),
+    ANDROID_POST_CAMPAIGN_PLEDGES("android_post_campaign_pledges")
 }
 
 fun FeatureFlagClient.getFetchInterval(): Long =

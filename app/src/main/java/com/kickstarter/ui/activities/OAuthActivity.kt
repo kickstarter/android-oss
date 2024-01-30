17b9184c29131e318ba99e5b3684fe9957db2a52
Isa Martin <arkariang@gmail.com>
2024-01-30 09:24:33 -0800

    - linter

--- app/src/main/java/com/kickstarter/ui/activities/OAuthActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/OAuthActivity.kt
@@ -1,18 +1,12 @@
 package com.kickstarter.ui.activities
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.addCallback
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import com.kickstarter.libs.featureflag.FlagKey
-import com.kickstarter.libs.utils.extensions.getEnvironment
-import com.kickstarter.ui.SharedPreferenceKey
 import com.kickstarter.ui.extensions.finishWithAnimation
 import com.kickstarter.ui.extensions.setUpConnectivityStatusCheck
-import com.kickstarter.viewmodels.LoginViewModel
 
 class OAuthActivity : AppCompatActivity() {
 

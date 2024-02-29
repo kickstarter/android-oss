865cde6fc50d7ac6a61124345b7170803b972922
Yun <y.cheng@kickstarter.com>
2024-02-29 14:18:09 -0500

    Fix lint

--- app/src/main/java/com/kickstarter/services/mutations/CreateAttributionEventData.kt
+++ app/src/main/java/com/kickstarter/services/mutations/CreateAttributionEventData.kt
@@ -4,10 +4,9 @@ package com.kickstarter.services.mutations
  * CreateAttributionEventData is the corresponding internal data model to the GraphQL model
  * [CreateAttributionEventInput]
  */
-data class CreateAttributionEventData (
+data class CreateAttributionEventData(
     val eventName: String,
     val eventProperties: Map<String, String>? = null,
     val projectId: String? = null,
     val clientMutationId: String? = null
 )
-

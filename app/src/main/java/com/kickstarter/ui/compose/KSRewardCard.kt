4ee3ab1bcddff24bfc11327f8002df1915bed7ea
Matthew <m.griego@kickstarter.com>
2024-03-20 14:38:18 -0700

    remove unused import

--- app/src/main/java/com/kickstarter/ui/compose/KSRewardCard.kt
+++ app/src/main/java/com/kickstarter/ui/compose/KSRewardCard.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kickstarter.R
-import com.kickstarter.ui.activities.compose.login.LogInSignUpClickableDisclaimerText
 import com.kickstarter.ui.compose.designsystem.KSGreenBadge
 import com.kickstarter.ui.compose.designsystem.KSPrimaryGreenButton
 import com.kickstarter.ui.compose.designsystem.KSTheme

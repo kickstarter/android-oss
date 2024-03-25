a191909b048c2f356d6af1a54bca748f9ed1e1a8
Yun <y.cheng@kickstarter.com>
2024-03-25 11:04:55 -0400

    Fix bug where addOnCount is not remembered when navigating back to addons

--- app/src/main/java/com/kickstarter/ui/activities/compose/projectpage/AddOnsScreen.kt
+++ app/src/main/java/com/kickstarter/ui/activities/compose/projectpage/AddOnsScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -127,7 +128,7 @@ fun AddOnsScreen(
     val interactionSource = remember {
         MutableInteractionSource()
     }
-    var addOnCount by remember {
+    var addOnCount by rememberSaveable {
         mutableStateOf(0)
     }
     //val rewardSelections: MutableMap<Reward, Int> = mutableMapOf()

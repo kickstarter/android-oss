c19d5b88ca392a413020aa00782d9ea7115c39a9
Matthew <m.griego@kickstarter.com>
2024-04-01 10:49:45 -0700

    updated location of state emission to on completion instead of in each state

--- app/src/main/java/com/kickstarter/viewmodels/projectpage/ConfirmDetailsViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/projectpage/ConfirmDetailsViewModel.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.rx2.asFlow
@@ -257,10 +258,11 @@ class ConfirmDetailsViewModel(val environment: Environment) : ViewModel() {
                     .asFlow()
                     .map { checkoutPayment ->
                         mutableCheckoutPayment.emit(checkoutPayment)
-                        emitCurrentState()
                     }
                     .catch {
                         errorAction.invoke(null)
+                    }
+                    .onCompletion {
                         emitCurrentState()
                     }
                     .collect()

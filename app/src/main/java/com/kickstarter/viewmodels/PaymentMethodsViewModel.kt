00efeb0306c315126f42c719bcd3eabf9caaddcd
Yun <y.cheng@kickstarter.com>
2024-05-15 13:06:39 -0400

    Lint

--- app/src/main/java/com/kickstarter/viewmodels/PaymentMethodsViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/PaymentMethodsViewModel.kt
@@ -1,11 +1,9 @@
 package com.kickstarter.viewmodels
 
 import DeletePaymentSourceMutation
-import android.util.Pair
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.kickstarter.libs.Environment
-import com.kickstarter.libs.rx.transformers.Transformers.combineLatestPair
 import com.kickstarter.libs.rx.transformers.Transformers.errorsV2
 import com.kickstarter.libs.rx.transformers.Transformers.neverErrorV2
 import com.kickstarter.libs.rx.transformers.Transformers.takeWhenV2

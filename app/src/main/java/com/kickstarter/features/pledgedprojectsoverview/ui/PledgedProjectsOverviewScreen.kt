9cccf098581c011ab050459202bb5abcce2e6de3
leighdouglas <leighcdouglas1@gmail.com>
2024-06-11 17:39:24 +0200

    MBL-1472: Create PPO screen activity, ui, and viewmodel (#2056)
    * Create ppo screen scaffold, launch on menu item press
    
    * linter
    
    * remove unneeded file
    
    * Address feedback
    
    * linter
    
    * Renaming
    
    * cleanup
    
    * fix test
    
    ---------
    
    Co-authored-by: Leigh Douglas <leigh@kickstarter.com>
    Co-authored-by: Yun Cheng <129205442+ycheng-kickstarter@users.noreply.github.com>

---
+++ app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/ui/PledgedProjectsOverviewScreen.kt
@@ -0,0 +1,153 @@
+package com.kickstarter.features.pledgedprojectsoverview.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.kickstarter.R
+import com.kickstarter.ui.compose.designsystem.KSTheme
+import com.kickstarter.ui.compose.designsystem.KSTheme.colors
+import com.kickstarter.ui.compose.designsystem.KSTheme.dimensions
+import com.kickstarter.ui.compose.designsystem.KSTheme.typography
+import com.kickstarter.ui.toolbars.compose.TopToolBar
+import kotlinx.coroutines.flow.flowOf
+
+@Composable
+@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+private fun PledgedProjectsOverviewScreenPreview() {
+    KSTheme {
+        Scaffold(
+            backgroundColor = colors.backgroundSurfacePrimary
+        ) { padding ->
+            val ppoCardList1 = (0..10).map {
+                PPOCardDataMock()
+            }
+            val ppoCardList = flowOf(PagingData.from(ppoCardList1)).collectAsLazyPagingItems()
+            PledgedProjectsOverviewScreen(
+                modifier = Modifier.padding(padding),
+                lazyColumnListState = rememberLazyListState(),
+                ppoCards = ppoCardList,
+                totalAlerts = 10,
+                onBackPressed = {}
+            )
+        }
+    }
+}
+
+@Composable
+fun PledgedProjectsOverviewScreen(
+    modifier: Modifier,
+    onBackPressed: () -> Unit,
+    lazyColumnListState: LazyListState,
+    ppoCards: LazyPagingItems<PPOCardDataMock>,
+    totalAlerts: Int = 0
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Scaffold(
+            modifier = modifier,
+            topBar = {
+                TopToolBar(
+                    title = stringResource(id = R.string.project_alerts_fpo),
+                    titleColor = colors.textPrimary,
+                    leftOnClickAction = onBackPressed,
+                    leftIconColor = colors.icon,
+                    leftIconModifier = Modifier.testTag(PledgedProjectsOverviewScreenTestTag.BACK_BUTTON.name),
+                    backgroundColor = colors.backgroundSurfacePrimary,
+                )
+            },
+            backgroundColor = colors.backgroundSurfacePrimary
+        ) { padding ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(
+                        start = dimensions.paddingMedium,
+                        end = dimensions.paddingMedium,
+                        top = dimensions.paddingMedium
+                    )
+                    .padding(paddingValues = padding),
+                state = lazyColumnListState
+            ) {
+                item {
+                    Text(
+                        text = stringResource(id = R.string.alerts_fpo, totalAlerts),
+                        style = typography.title3Bold,
+                        color = colors.textPrimary
+                    )
+                }
+
+                items(
+                    count = ppoCards.itemCount
+                ) { index ->
+                    Spacer(modifier = Modifier.height(dimensions.paddingMedium))
+
+                    ppoCards[index]?.let {
+                        PPOCardView(
+                            viewType = it.viewType,
+                            onCardClick = { },
+                            projectName = it.projectName,
+                            pledgeAmount = it.pledgeAmount,
+                            imageUrl = it.imageUrl,
+                            imageContentDescription = it.imageContentDescription,
+                            creatorName = it.creatorName,
+                            sendAMessageClickAction = { },
+                            shippingAddress = it.shippingAddress,
+                            showBadge = it.showBadge,
+                            onActionButtonClicked = { },
+                            onSecondaryActionButtonClicked = { },
+                            timeNumberForAction = it.timeNumberForAction
+                        )
+                    }
+                }
+
+                item {
+                    Spacer(modifier = Modifier.height(dimensions.paddingDoubleLarge))
+                }
+            }
+        }
+    }
+}
+
+enum class PledgedProjectsOverviewScreenTestTag {
+    BACK_BUTTON,
+}
+
+// For preview purposes only, will remove once we have the PPO Card payload model from graph
+data class PPOCardDataMock(
+    val viewType: PPOCardViewType = PPOCardViewType.FIX_PAYMENT,
+    val onCardClick: () -> Unit = { },
+    val projectName: String = "This is a project name",
+    val pledgeAmount: String = "$14.00",
+    val imageUrl: String = "",
+    val imageContentDescription: String = "",
+    val creatorName: String = "Creator Name",
+    val sendAMessageClickAction: () -> Unit = { },
+    val shippingAddress: String = "",
+    val showBadge: Boolean = true,
+    val onActionButtonClicked: () -> Unit = {},
+    val onSecondaryActionButtonClicked: () -> Unit = {},
+    val timeNumberForAction: Int = 25
+)

e74f3558d5363660b309bfd9b51c4ee94e9d80d0
Leigh Douglas <leigh@kickstarter.com>
2024-07-15 19:41:44 -0400

    Fix test:

--- app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/viewmodel/PledgedProjectsOverviewViewModel.kt
+++ app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/viewmodel/PledgedProjectsOverviewViewModel.kt
@@ -27,7 +27,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.rx2.asFlow
 
 data class PledgedProjectsOverviewUIState(
-    var totalAlerts: Int = 1,
+    var totalAlerts: Int = 0,
     val isLoading: Boolean = false,
     val isErrored: Boolean = false,
 )

b5c7919027229062536395feee75f8f5cab1d460
Matthew <m.griego@kickstarter.com>
2024-07-30 10:50:22 -0700

    update test to reflect updated screen calls

--- app/src/test/java/com/kickstarter/features/pledgedprojectsoverview/ui/PledgedProjectsOverviewScreenTest.kt
+++ app/src/test/java/com/kickstarter/features/pledgedprojectsoverview/ui/PledgedProjectsOverviewScreenTest.kt
@@ -10,6 +10,7 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.features.pledgedprojectsoverview.data.PPOCardFactory
 import com.kickstarter.ui.compose.designsystem.KSTheme
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flowOf
 import org.junit.Test
 
@@ -38,7 +39,8 @@ PledgedProjectsOverviewScreenTest : KSRobolectricTestCase() {
                     onAddressConfirmed = {},
                     onProjectPledgeSummaryClick = {},
                     onSeeAllBackedProjectsClick = {},
-                    onFixPaymentClick = {}
+                    onPrimaryActionButtonClicked = {},
+                    onSecondaryActionButtonClicked = {}
                 )
             }
         }

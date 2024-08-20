36c6ff0a6b84415356834a5ae1d1c5c67e3a8a6b
Matthew <m.griego@kickstarter.com>
2024-08-19 18:03:52 -0700

    add estimated shipping visuals to pledge checkout screens

--- app/src/main/java/com/kickstarter/libs/utils/RewardViewUtils.kt
+++ app/src/main/java/com/kickstarter/libs/utils/RewardViewUtils.kt
@@ -156,6 +156,8 @@ object RewardViewUtils {
         isAddOn: Boolean,
         multipleQuantitiesAllowed: Boolean,
         shippingRules: List<ShippingRule>?,
+        useUserPreference: Boolean = false,
+        useAbout: Boolean = true
     ): String {
         var min = ""
         var max = ""
@@ -165,14 +167,30 @@ object RewardViewUtils {
                     it.location()?.id() == selectedShippingRule.location()?.id()
                 }.map {
                     if (it.estimatedMin() <= 0 || it.estimatedMax() <= 0) return ""
-                    min = ksCurrency.format(it.estimatedMin(), project, RoundingMode.HALF_UP)
-                    max = ksCurrency.format(it.estimatedMax(), project, RoundingMode.HALF_UP)
+                    min = if (useUserPreference) {
+                        ksCurrency.formatWithUserPreference(it.estimatedMin(), project, RoundingMode.HALF_UP, precision = 2)
+                    } else {
+                        ksCurrency.format(it.estimatedMin(), project, RoundingMode.HALF_UP)
+                    }
+                    max = if (useUserPreference) {
+                        ksCurrency.formatWithUserPreference(it.estimatedMin(), project, RoundingMode.HALF_UP, precision = 2)
+                    } else {
+                        ksCurrency.format(it.estimatedMax(), project, RoundingMode.HALF_UP)
+                    }
                 }
             } else return ""
         } else {
             if (selectedShippingRule.estimatedMin() <= 0 || selectedShippingRule.estimatedMax() <= 0) return ""
-            min = ksCurrency.format(selectedShippingRule.estimatedMin(), project, RoundingMode.HALF_UP)
-            max = ksCurrency.format(selectedShippingRule.estimatedMax(), project, RoundingMode.HALF_UP)
+            min = if (useUserPreference) {
+                ksCurrency.formatWithUserPreference(selectedShippingRule.estimatedMin(), project, RoundingMode.HALF_UP, precision = 2)
+            } else {
+                ksCurrency.format(selectedShippingRule.estimatedMin(), project, RoundingMode.HALF_UP)
+            }
+            max = if (useUserPreference) {
+                ksCurrency.formatWithUserPreference(selectedShippingRule.estimatedMax(), project, RoundingMode.HALF_UP, precision = 2)
+            } else {
+                ksCurrency.format(selectedShippingRule.estimatedMax(), project, RoundingMode.HALF_UP)
+            }
         }
 
         if (min.isEmpty() || max.isEmpty()) return ""
@@ -180,11 +198,11 @@ object RewardViewUtils {
         // TODO: Replace with defined string
         val minToMaxString = if (multipleQuantitiesAllowed) "$min-$max each" else "$min-$max"
 
-        return ksString.format(
-            context.getString(R.string.About_reward_amount),
-            "reward_amount",
+        return if (useAbout) {
+            ksString.format(context.getString(R.string.About_reward_amount), "reward_amount", minToMaxString)
+        } else {
             minToMaxString
-        )
+        }
     }
 
     /**

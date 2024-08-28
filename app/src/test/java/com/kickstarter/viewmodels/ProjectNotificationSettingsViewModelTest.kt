b2cc47f631fd4ed0289289287eca5692ff6ff87a
Matthew <m.griego@kickstarter.com>
2024-08-28 10:08:25 -0700

    update project notifications settings to RX2

--- app/src/test/java/com/kickstarter/viewmodels/ProjectNotificationSettingsViewModelTest.kt
+++ app/src/test/java/com/kickstarter/viewmodels/ProjectNotificationSettingsViewModelTest.kt
@@ -2,33 +2,41 @@ package com.kickstarter.viewmodels
 
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.libs.Environment
+import com.kickstarter.libs.utils.extensions.addToDisposable
 import com.kickstarter.mock.factories.ProjectNotificationFactory
-import com.kickstarter.mock.services.MockApiClient
+import com.kickstarter.mock.services.MockApiClientV2
 import com.kickstarter.models.ProjectNotification
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subscribers.TestSubscriber
+import org.junit.After
 import org.junit.Test
-import rx.Observable
-import rx.observers.TestSubscriber
 import java.util.Collections
 
 class ProjectNotificationSettingsViewModelTest : KSRobolectricTestCase() {
 
-    private lateinit var vm: ProjectNotificationSettingsViewModel.ViewModel
+    private lateinit var vm: ProjectNotificationSettingsViewModel.ProjectNotificationSettingsViewModel
 
     private val projectNotifications = TestSubscriber<List<ProjectNotification>>()
-    private val unableToFetchProjectNotificationsError = TestSubscriber<Void>()
+    private val unableToFetchProjectNotificationsError = TestSubscriber<Unit>()
+    private val disposables = CompositeDisposable()
 
     private fun setUpEnvironment(environment: Environment) {
-        this.vm = ProjectNotificationSettingsViewModel.ViewModel(environment)
+        this.vm =
+            ProjectNotificationSettingsViewModel.ProjectNotificationSettingsViewModel(environment)
 
-        this.vm.outputs.projectNotifications().subscribe(this.projectNotifications)
-        this.vm.outputs.unableToFetchProjectNotificationsError().subscribe(this.unableToFetchProjectNotificationsError)
+        this.vm.outputs.projectNotifications().subscribe { this.projectNotifications.onNext(it) }
+            .addToDisposable(disposables)
+        this.vm.outputs.unableToFetchProjectNotificationsError()
+            .subscribe { this.unableToFetchProjectNotificationsError.onNext(it) }
+            .addToDisposable(disposables)
     }
 
     @Test
     fun testProjectNotifications() {
         val projectNotifications = Collections.singletonList(ProjectNotificationFactory.disabled())
         setUpEnvironment(
-            environment().toBuilder().apiClient(object : MockApiClient() {
+            environment().toBuilder().apiClientV2(object : MockApiClientV2() {
                 override fun fetchProjectNotifications(): Observable<List<ProjectNotification>> {
                     return Observable.just(projectNotifications)
                 }
@@ -41,7 +49,7 @@ class ProjectNotificationSettingsViewModelTest : KSRobolectricTestCase() {
     @Test
     fun testUnableToFetchProjectNotificationsError() {
         setUpEnvironment(
-            environment().toBuilder().apiClient(object : MockApiClient() {
+            environment().toBuilder().apiClientV2(object : MockApiClientV2() {
                 override fun fetchProjectNotifications(): Observable<List<ProjectNotification>> {
                     return Observable.error(Throwable("error"))
                 }
@@ -49,5 +57,10 @@ class ProjectNotificationSettingsViewModelTest : KSRobolectricTestCase() {
         )
 
         this.unableToFetchProjectNotificationsError.assertValueCount(1)
+    }
+
+    @After
+    fun clear() {
+        disposables.clear()
     }
 }

17ca649191c6cc321fe30e1d345d58b2b425191d
Isa Martin <arkariang@gmail.com>
2024-09-04 16:55:56 -0700

    - linter

--- app/src/main/java/com/kickstarter/ui/activities/compose/projectpage/AddOnsContainer.kt
+++ app/src/main/java/com/kickstarter/ui/activities/compose/projectpage/AddOnsContainer.kt
@@ -19,7 +19,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment

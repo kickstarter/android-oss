c28b488cec75aa7f6b2f43bd48aefe0bdf451fd9
jlplks <j.pulido.c@kickstarter.com>
2024-10-02 16:14:00 -0700

    MLB-1740: Fix Circle CI Pipeline to work with sdk 35 (Android 15) (#2141)
    * Android sdk 35
    
    - Change compile and target SDK to version 35
    - change tag for CircleCI from 2024.01.1 to 2024.09.1
    
    * downgrade tag to 2024.08.1
    
    * 2024.07.1
    
    * update gradle version
    
    * add JDK_VERSION to executor environment
    
    * UPGRADE VERSIONS
    
    Kotlin version from 1.8.10 1.8.22
    Jacoco_version from 0.8.8 to 0.8.11
    compose version from 1.4.3 to 1.4.8
    
    
    setted java 17 on the circle ci environment
    
    * add validate java version on circle ci pipeline
    
    * force java 17 on circle ci
    
    * fix typo
    
    * explicity install java 17
    
    * specify jvm 17 on gradle
    
    * force assembleExternalRelease to use java 17
    
    * Fix lint external release
    
    * change name of intent on ThreadView model
    
    Changed the name for function intent and PublishSubject intent so it be more clear to read
    
    * code more clean
    
    
    * fix ktlint

--- app/src/main/java/com/kickstarter/ui/activities/ThreadActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/ThreadActivity.kt
@@ -66,7 +66,7 @@ class ThreadActivity :
             env
         }
 
-        viewModel.intent(intent)
+        viewModel.handleIntent(intent)
 
         recyclerViewPaginator = RecyclerViewPaginatorV2(
             binding.commentRepliesRecyclerView,

852dae9231a577bd606e29309fc44b2a9a045f8b
mtgriego <matthew.t.griego@gmail.com>
2024-10-09 09:49:20 -0700

    [NoTicket] various RXjava cleanup (#2149)
    * various RXjava cleanup
    
    * lint

--- app/src/main/java/com/kickstarter/ui/intentmappers/ProjectIntentMapper.kt
+++ app/src/main/java/com/kickstarter/ui/intentmappers/ProjectIntentMapper.kt
@@ -7,13 +7,9 @@ import com.kickstarter.libs.RefTag
 import com.kickstarter.libs.utils.KsOptional
 import com.kickstarter.libs.utils.extensions.query
 import com.kickstarter.models.Project
-import com.kickstarter.services.ApiClientType
 import com.kickstarter.services.ApiClientTypeV2
-import com.kickstarter.services.ApolloClientType
 import com.kickstarter.services.ApolloClientTypeV2
-import com.kickstarter.services.apiresponses.PushNotificationEnvelope
 import com.kickstarter.ui.IntentKey
-import rx.Observable
 import java.util.regex.Pattern
 
 object ProjectIntentMapper {
@@ -42,50 +38,6 @@ object ProjectIntentMapper {
             .filter { it.isNotEmpty() }
             .switchMap { slug: String? ->
                 slug?.let { apolloClient.getProject(it) }
-            }
-            .retry(3)
-        return projectFromParceledProject
-            .mergeWith(projectFromParceledParam)
-    }
-
-    fun project(intent: Intent, apolloClient: ApolloClientType): Observable<Project> {
-        val intentProject = projectFromIntent(intent)
-        val projectFromParceledProject =
-            if (intentProject == null) Observable.empty() else Observable.just(intentProject)
-                .switchMap { project: Project? ->
-                    project?.let { apolloClient.getProject(it) }
-                }
-                .startWith(intentProject)
-                .retry(3)
-
-        val projectFromParceledParam = Observable.just(paramFromIntent(intent) ?: "")
-            .filter { it.isNotEmpty() }
-            .switchMap { slug: String? ->
-                slug?.let { apolloClient.getProject(it) }
-            }
-            .retry(3)
-        return projectFromParceledProject
-            .mergeWith(projectFromParceledParam).last()
-    }
-
-    /**
-     * Returns an observable of projects retrieved from intent data. May hit the API if the intent only contains a project
-     * param rather than a parceled project.
-     */
-    fun project(intent: Intent, client: ApiClientType): Observable<Project> {
-        val intentProject = projectFromIntent(intent)
-        val projectFromParceledProject =
-            if (intentProject == null) Observable.empty() else Observable.just(intentProject)
-                .flatMap { project: Project? ->
-                    project?.let { client.fetchProject(it) }
-                }
-                .startWith(intentProject)
-                .retry(3)
-
-        val projectFromParceledParam = Observable.just(paramFromIntent(intent) ?: "")
-            .filter { it.isNotEmpty() }
-            .flatMap { param: String? ->
-                param?.let { client.fetchProject(it) }
             }
             .retry(3)
         return projectFromParceledProject
@@ -128,15 +80,6 @@ object ProjectIntentMapper {
      */
     fun deepLinkSaveFlag(intent: Intent): io.reactivex.Observable<Boolean> {
         return io.reactivex.Observable.just(intent.getBooleanExtra(IntentKey.SAVE_FLAG_VALUE, false))
-    }
-
-    /**
-     * Returns an observable of push notification envelopes from the intent data. This will emit only when the project
-     * is launched from a push notification.
-     */
-    fun pushNotificationEnvelope(intent: Intent): Observable<PushNotificationEnvelope> {
-        return Observable.just<Any?>(intent.getParcelableExtra(IntentKey.PUSH_NOTIFICATION_ENVELOPE))
-            .ofType(PushNotificationEnvelope::class.java)
     }
 
     /**

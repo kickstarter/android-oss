6e128d1f7fc2922d63b5ab6810cf33e4e5f0fb47
Isa Martin <arkariang@gmail.com>
2024-10-29 10:34:22 -0700

    - Remove all RXJava1 transformers

--- app/src/main/java/com/kickstarter/libs/rx/transformers/Transformers.java
+++ app/src/main/java/com/kickstarter/libs/rx/transformers/Transformers.java
@@ -5,31 +5,9 @@ import androidx.annotation.NonNull;
 import com.kickstarter.services.ApiException;
 import com.kickstarter.services.apiresponses.ErrorEnvelope;
 
-import rx.Observable;
-
 public final class Transformers {
   private Transformers() {}
 
-  /**
-   * Transforms `null` values emitted from an observable into `theDefault`.
-   */
-  public static @NonNull <T> CoalesceTransformer<T> coalesce(final @NonNull T theDefault) {
-    return new CoalesceTransformer<>(theDefault);
-  }
-
-  /**
-   * Emits when a materialized stream is completed.
-   */
-  public static @NonNull <T> CompletedTransformer<T> completed() {
-    return new CompletedTransformer<>();
-  }
-
-  /**
-   * Emits when an error is thrown in a materialized stream.
-   */
-  public static @NonNull <T> ErrorsTransformer<T> errors() {
-    return new ErrorsTransformer<>();
-  }
 
   /**
    * Emits when an error is thrown in a materialized stream.
@@ -41,24 +19,10 @@ public final class Transformers {
 
   /**
    * Prevents an observable from erroring by chaining `onErrorResumeNext`.
-   */
-  public static <T> NeverErrorTransformer<T> neverError() {
-    return new NeverErrorTransformer<>();
-  }
-
-  /**
-   * Prevents an observable from erroring by chaining `onErrorResumeNext`.
    * Adapted to RxJava 2
    */
   public static <T> NeverErrorTransformerV2<T> neverErrorV2() {
     return new NeverErrorTransformerV2<>();
-  }
-
-  /**
-   * Prevents an observable from erroring on any {@link ApiException} exceptions.
-   */
-  public static <T> NeverApiErrorTransformer<T> neverApiError() {
-    return new NeverApiErrorTransformer<>();
   }
 
   /**
@@ -87,15 +51,6 @@ public final class Transformers {
    * Emits the latest value of the source `when` observable whenever the
    * `when` observable emits.
    */
-  // TODO: Delete this when last RX1 is removed
-  public static <S, T> TakePairWhenTransformer<S, T> takePairWhen(final @NonNull Observable<T> when) {
-    return new TakePairWhenTransformer<>(when);
-  }
-
-  /**
-   * Emits the latest value of the source `when` observable whenever the
-   * `when` observable emits.
-   */
   public static <S, T> TakePairWhenTransformerV2<S, T> takePairWhenV2(final @NonNull io.reactivex.Observable<T> when) {
     return new TakePairWhenTransformerV2<>(when);
   }
@@ -109,47 +64,12 @@ public final class Transformers {
   /**
    * Emits the latest values from two observables whenever either emits.
    */
-  // TODO: Delete this when last RX1 is removed
-  public static <S, T> CombineLatestPairTransformer<S, T> combineLatestPair(final @NonNull Observable<T> second) {
-    return new CombineLatestPairTransformer<>(second);
-  }
-
-  /**
-   * Emits the latest values from two observables whenever either emits.
-   */
   public static <S, T> CombineLatestPairTransformerV2<S, T> combineLatestPair(final @NonNull io.reactivex.Observable<T> second) {
     return new CombineLatestPairTransformerV2<>(second);
   }
 
-  /**
-   * Converts an observable of any type into an observable of `null`s. This is useful for forcing
-   * Java's type system into knowing we have a stream of `Void`. Simply doing `.map(__ -> null)`
-   * is not enough since Java doesn't know if that is a `null` String, Integer, Void, etc.
-   *
-   * This transformer allows the following pattern:
-   *
-   * ```
-   * myObservable
-   *   .compose(takeWhen(click))
-   *   .compose(ignoreValues())
-   *   .subscribe(subject::onNext)
-   * ```
-   */
-  public static @NonNull <S> IgnoreValuesTransformer<S> ignoreValues() {
-    return new IgnoreValuesTransformer<>();
-  }
-
   public static @NonNull <S> IgnoreValuesTransformerV2<S> ignoreValuesV2() {
     return new IgnoreValuesTransformerV2<>();
-  }
-
-
-  /**
-   * Emits the number of times the source has emitted for every emission of the source. The
-   * first emitted value will be `1`.
-   */
-  public static @NonNull <T> IncrementalCountTransformer<T> incrementalCount() {
-    return new IncrementalCountTransformer<>();
   }
 
   /**
@@ -160,12 +80,6 @@ public final class Transformers {
     return new IncrementalCountTransformerV2<>();
   }
 
-  /**
-   * Emits an observable of values from a materialized stream.
-   */
-  public static @NonNull <T> ValuesTransformer<T> values() {
-    return new ValuesTransformer<>();
-  }
 
   /**
    * Emits an observable of values from a materialized stream.

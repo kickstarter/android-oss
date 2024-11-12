c3bb35f99972202ef8e1ccc7e77effd48423f98b
jlplks <j.pulido.c@kickstarter.com>
2024-11-12 16:58:14 -0700

    Added plot selected badge to PledgeItemizedDetails
    - added plotSelected prop, created component, added tests and added previews

--- app/src/main/java/com/kickstarter/ui/views/compose/checkout/PledgeItemizedDetails.kt
+++ app/src/main/java/com/kickstarter/ui/views/compose/checkout/PledgeItemizedDetails.kt
@@ -2,12 +2,15 @@ package com.kickstarter.ui.views.compose.checkout
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -39,6 +42,7 @@ fun ItemizedContainerForNoRewardPreview() {
                 initialBonusSupport = "US$ 0",
                 totalBonusSupport = "US$ 1",
                 shippingAmount = -1.0,
+                plotSelected = false
             )
         }
     }
@@ -66,7 +70,29 @@ fun ItemizedRewardListContainerPreview() {
                 totalBonusSupport = "0",
                 deliveryDateString = "",
                 rewardsHaveShippables = false,
+                plotSelected = false,
                 disclaimerText = stringResource(id = R.string.If_the_project_reaches_its_funding_goal_you_will_be_charged_total_on_project_deadline_and_receive_proof_of_pledge)
+            )
+        }
+    }
+}
+
+@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ItemizedContainerForNoRewardPlotSelectedPreview() {
+    KSTheme {
+        Scaffold(
+            backgroundColor = KSTheme.colors.backgroundAccentGraySubtle
+        ) { padding ->
+            ItemizedRewardListContainer(
+                modifier = Modifier.padding(paddingValues = padding),
+                totalAmount = "US$ 1",
+                totalAmountCurrencyConverted = "About CA$ 1.38",
+                initialBonusSupport = "US$ 0",
+                totalBonusSupport = "US$ 1",
+                shippingAmount = -1.0,
+                plotSelected = true
             )
         }
     }
@@ -85,6 +111,7 @@ enum class PledgeItemizedDetailsTestTag {
     SHIPPING_TITLE,
     SHIPPING_AMOUNT,
     CURRENCY_CONVERSION,
+    PLOT_SELECTED_BADGE,
 }
 
 @Composable
@@ -101,7 +128,8 @@ fun ItemizedRewardListContainer(
     totalBonusSupport: String,
     deliveryDateString: String = "",
     rewardsHaveShippables: Boolean = false,
-    disclaimerText: String = ""
+    disclaimerText: String = "",
+    plotSelected: Boolean
 ) {
     Column(
         modifier = Modifier
@@ -252,6 +280,45 @@ fun ItemizedRewardListContainer(
                         color = colors.textPrimary
                     )
                 }
+            }
+        }
+
+        if (plotSelected) {
+            Spacer(modifier = Modifier.height(dimensions.paddingMedium))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Box(
+                    modifier = modifier
+                        .background(
+                            color = colors.kds_create_700.copy(alpha = 0.2f),
+                            shape = RoundedCornerShape(dimensions.radiusSmall)
+                        )
+                        .padding(
+                            start = dimensions.paddingSmall,
+                            end = dimensions.paddingSmall,
+                            top = dimensions.paddingXSmall,
+                            bottom = dimensions.paddingXSmall,
+                        )
+                ) {
+                    Text(
+                        modifier = Modifier.testTag(PledgeItemizedDetailsTestTag.PLOT_SELECTED_BADGE.name),
+                        text = stringResource(
+                            id = R.string.fpo_pledge_over_time
+                        ),
+                        style = typography.body2Medium,
+                        color = colors.kds_create_700
+                    )
+                }
+                Text(
+                    modifier = Modifier.testTag(PledgeItemizedDetailsTestTag.DISCLAIMER_TEXT.name),
+                    text = stringResource(id = R.string.fpo_charged_as_4_payments),
+                    style = typography.footnote,
+                    color = colors.textPrimary
+                )
             }
         }
 

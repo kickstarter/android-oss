fb33a024e6fa4bd881db3ca66bd3c2fbfdd662e4
Isa Martin <arkariang@gmail.com>
2024-11-26 17:31:36 -0800

    - updateUserEmail

--- app/src/main/java/com/kickstarter/services/KSApolloClientV2.kt
+++ app/src/main/java/com/kickstarter/services/KSApolloClientV2.kt
@@ -603,8 +603,7 @@ class KSApolloClientV2(val service: ApolloClient, val gson: Gson) : ApolloClient
             )
             service.mutation(
                 mutation
-            ).toFlow()
-                .asObservable()
+            ).rxSingle()
                 .doOnError { throwable ->
                     ps.onError(throwable)
                 }
@@ -616,7 +615,7 @@ class KSApolloClientV2(val service: ApolloClient, val gson: Gson) : ApolloClient
                         ps.onNext(data)
                     }
                     ps.onComplete()
-                }.dispose()
+                }.addToDisposable(disposables)
             return@defer ps
         }
     }

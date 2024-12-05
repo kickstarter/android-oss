e8bf6a61c39d4f477f15d538cbe46490d40762b8
jlplks <j.pulido.c@kickstarter.com>
2024-12-05 14:31:49 -0700

    fix ktlint

--- app/src/main/java/com/kickstarter/services/mutations/CreateBackingData.kt
+++ app/src/main/java/com/kickstarter/services/mutations/CreateBackingData.kt
@@ -4,6 +4,4 @@ import com.kickstarter.libs.RefTag
 import com.kickstarter.models.Project
 import com.kickstarter.models.Reward
 
-data class CreateBackingData(val project: Project, val amount: String, val paymentSourceId: String? = null, val setupIntentClientSecret: String? = null, val locationId: String?, val reward: Reward? = null, val rewardsIds: List<Reward>? = null, val refTag: RefTag?, val stripeCardId: String? = null, val incremental: Boolean = false) {
-
-}
+data class CreateBackingData(val project: Project, val amount: String, val paymentSourceId: String? = null, val setupIntentClientSecret: String? = null, val locationId: String?, val reward: Reward? = null, val rewardsIds: List<Reward>? = null, val refTag: RefTag?, val stripeCardId: String? = null, val incremental: Boolean = false)

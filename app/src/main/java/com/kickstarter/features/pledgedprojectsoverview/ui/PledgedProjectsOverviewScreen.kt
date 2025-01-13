5afdd5476d219b981415d4a1cbe2be63c9b94c4e
Isa Martin <arkariang@gmail.com>
2025-01-13 14:06:35 -0800

    - Android 15 padding respecting the system toolbar

--- app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/ui/PledgedProjectsOverviewScreen.kt
+++ app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/ui/PledgedProjectsOverviewScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -179,6 +180,7 @@ fun PledgedProjectsOverviewScreen(
         pullRefreshCallback,
     )
     Scaffold(
+        modifier = Modifier.systemBarsPadding(),
         snackbarHost = {
             SnackbarHost(
                 modifier = Modifier.padding(dimensions.paddingSmall),

b6ab5da8d28b551f3490e7028fe5fde328e36437
jlplks <j.pulido.c@kickstarter.com>
2025-01-30 09:46:49 -0800

    MBL-2052 PLOT projects not working after scheme change on BE (#2215)
    * Remove 'projectIsPledgeOverTimeAllowed' from the project
    
    * fix showPlotWidget
    
    ---------
    
    Co-authored-by: Isabel Martin <arkariang@gmail.com>
    Co-authored-by: Leigh Douglas <leighcdouglas1@gmail.com>

--- app/src/main/java/com/kickstarter/models/PaymentPlan.kt
+++ app/src/main/java/com/kickstarter/models/PaymentPlan.kt
@@ -7,25 +7,20 @@ import kotlinx.parcelize.Parcelize
 data class PaymentPlan(
     val amountIsPledgeOverTimeEligible: Boolean,
     val paymentIncrements: List<PaymentIncrement>?,
-    val projectIsPledgeOverTimeAllowed: Boolean,
 ) : Parcelable {
     fun amountIsPledgeOverTimeEligible() = this.amountIsPledgeOverTimeEligible
     fun paymentIncrements() = this.paymentIncrements
-    fun projectIsPledgeOverTimeAllowed() = this.projectIsPledgeOverTimeAllowed
 
     @Parcelize
     data class Builder(
         var amountIsPledgeOverTimeEligible: Boolean = false,
         var paymentIncrements: List<PaymentIncrement>? = null,
-        var projectIsPledgeOverTimeAllowed: Boolean = false,
     ) : Parcelable {
         fun amountIsPledgeOverTimeEligible(amountIsPledgeOverTimeEligible: Boolean) = apply { this.amountIsPledgeOverTimeEligible = amountIsPledgeOverTimeEligible }
         fun paymentIncrements(paymentIncrements: List<PaymentIncrement>?) = apply { this.paymentIncrements = paymentIncrements }
-        fun projectIsPledgeOverTimeAllowed(projectIsPledgeOverTimeAllowed: Boolean) = apply { this.projectIsPledgeOverTimeAllowed = projectIsPledgeOverTimeAllowed }
         fun build() = PaymentPlan(
             amountIsPledgeOverTimeEligible = amountIsPledgeOverTimeEligible,
             paymentIncrements = paymentIncrements,
-            projectIsPledgeOverTimeAllowed = projectIsPledgeOverTimeAllowed,
         )
     }
 
@@ -36,15 +31,13 @@ data class PaymentPlan(
     fun toBuilder() = Builder(
         amountIsPledgeOverTimeEligible = amountIsPledgeOverTimeEligible,
         paymentIncrements = paymentIncrements,
-        projectIsPledgeOverTimeAllowed = projectIsPledgeOverTimeAllowed,
     )
 
     override fun equals(other: Any?): Boolean {
         var equals = super.equals(other)
         if (other is PaymentPlan) {
             equals = amountIsPledgeOverTimeEligible() == other.amountIsPledgeOverTimeEligible() &&
-                paymentIncrements() == other.paymentIncrements() &&
-                projectIsPledgeOverTimeAllowed() == other.projectIsPledgeOverTimeAllowed()
+                paymentIncrements() == other.paymentIncrements()
         }
         return equals
     }

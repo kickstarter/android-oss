8a42601f39753e3be746878d8806f26f0ab54015
Yun Cheng <129205442+ycheng-kickstarter@users.noreply.github.com>
2025-02-04 13:07:07 -0500

    Move the image field out of the reward fragment and into the FetchProjectRewards query only

--- app/src/test/java/com/kickstarter/services/GraphQLTransformersTest.kt
+++ app/src/test/java/com/kickstarter/services/GraphQLTransformersTest.kt
@@ -275,6 +275,7 @@ class GraphQLTransformersTest : KSRobolectricTestCase() {
         RewardType.base,
         AllowedAddons(listOf(Reward.Node("UmV3YXJkLTk3MDA2NjA"))),
         null,
+        Reward.Image("https://www.kickstarter.com", "")
     )
 
     @Test

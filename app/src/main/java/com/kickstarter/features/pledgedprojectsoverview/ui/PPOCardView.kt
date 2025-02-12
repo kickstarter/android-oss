16a728564b95e5682c0cf2791e579df6f1332f79
Leigh Douglas <leigh@kickstarter.com>
2025-02-12 14:02:12 -0500

    added new rounded chevron to ppo cards

--- app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/ui/PPOCardView.kt
+++ app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/ui/PPOCardView.kt
@@ -92,7 +92,7 @@ fun PPOCardPreview() {
                     onActionButtonClicked = {},
                     onSecondaryActionButtonClicked = {},
                     onProjectPledgeSummaryClick = {},
-                    flags = listOf(Flag.builder().message("Address locks in 7 days").type("warning").icon("time").build(), Flag.builder().message("Address locks in 7 days").type("warning").icon("time").build(), Flag.builder().message("Address").type("warning").icon("time").build()),
+                    flags = listOf(Flag.builder().message("Address locks in 7 days").type("warning").icon("time").build(), Flag.builder().message("Address").type("warning").icon("time").build()),
                 )
 
                 Spacer(modifier = Modifier.height(dimensions.paddingMedium))
@@ -269,6 +269,14 @@ fun ProjectPledgeSummaryView(
                 style = typography.caption2
             )
         }
+
+        Image(
+            modifier = Modifier.padding(start = dimensions.paddingSmall),
+            imageVector = ImageVector.vectorResource(id = R.drawable.chevron_rounded_right),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(color = colors.textSecondary)
+        )
+
     }
 }
 
@@ -329,7 +337,7 @@ fun CreatorNameSendMessageView(
                 modifier = Modifier.size(dimensions.paddingMediumSmall),
                 imageVector = ImageVector.vectorResource(id = R.drawable.chevron_right),
                 contentDescription = null,
-                colorFilter = ColorFilter.tint(color = colors.textAccentGreen)
+                colorFilter = ColorFilter.tint(color = colors.textSecondary)
             )
         }
     }

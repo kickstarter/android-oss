54cd0892025a54524d3115f1ec19d51bd7656bde
Yun Cheng <129205442+ycheng-kickstarter@users.noreply.github.com>
2025-02-26 11:10:46 -0500

    Match BackingFragment

--- app/src/main/java/com/kickstarter/ui/fragments/BackingFragment.kt
+++ app/src/main/java/com/kickstarter/ui/fragments/BackingFragment.kt
@@ -1,6 +1,7 @@
 package com.kickstarter.ui.fragments
 
 import PaymentSchedule
+import android.content.Context
 import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Spannable
@@ -23,6 +24,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import coil.Coil
 import coil.ImageLoader
 import coil.request.ImageRequest
 import com.kickstarter.R
@@ -580,16 +582,14 @@ class BackingFragment : Fragment() {
     }
 
     private fun preloadImages(rewards: List<Reward>) {
-        // Preload reward images asynchronously
-        val imageLoader = ImageLoader.Builder(requireContext())
-            .crossfade(true)
-            .build()
-        for (reward in rewards) {
+        val rewardsIterator = rewards.iterator();
+        while (rewardsIterator.hasNext()) {
+            val reward = rewardsIterator.next()
             reward.image()?.let {
                 val request = ImageRequest.Builder(requireContext())
                     .data(reward.image()?.full())
                     .build()
-                imageLoader.enqueue(request)
+                Coil.imageLoader(requireContext()).enqueue(request)
             }
         }
     }

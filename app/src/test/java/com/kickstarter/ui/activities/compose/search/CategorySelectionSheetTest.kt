8a38b6f8095c51e18ddfbd637d59b19a5eab0223
Jose Luis Pulido Laveaga <j.pulido.c@kickstarter.com>
2025-03-01 06:14:05 -0700

    Fix lint

--- app/src/test/java/com/kickstarter/ui/activities/compose/search/CategorySelectionSheetTest.kt
+++ app/src/test/java/com/kickstarter/ui/activities/compose/search/CategorySelectionSheetTest.kt
@@ -1,18 +1,15 @@
 package com.kickstarter.ui.activities.compose.search
 
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import com.kickstarter.KSRobolectricTestCase
 import com.kickstarter.ui.compose.designsystem.KSTheme
-import org.junit.Rule
 import org.junit.Test
 
 class CategorySelectionSheetTest : KSRobolectricTestCase() {
 
     private val dismissButton =
         composeTestRule.onNodeWithTag(CategorySelectionSheetTestTag.DISMISS_BUTTON.name)
-
 
     @Test
     fun `test tapping dismiss button should register dismiss`() {

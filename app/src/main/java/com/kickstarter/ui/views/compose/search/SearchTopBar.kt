ed7acc10d1eeee7796d7103efbd67799174ed3b9
Isa Martin <arkariang@gmail.com>
2025-03-20 09:28:31 -0700

    - linter

--- app/src/main/java/com/kickstarter/ui/views/compose/search/SearchTopBar.kt
+++ app/src/main/java/com/kickstarter/ui/views/compose/search/SearchTopBar.kt
@@ -36,8 +36,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import com.kickstarter.R
-import com.kickstarter.libs.featureflag.FlagKey
-import com.kickstarter.libs.utils.extensions.getEnvironment
 import com.kickstarter.ui.activities.compose.search.SearchScreenTestTag
 import com.kickstarter.ui.compose.designsystem.IconPillButton
 import com.kickstarter.ui.compose.designsystem.KSTheme

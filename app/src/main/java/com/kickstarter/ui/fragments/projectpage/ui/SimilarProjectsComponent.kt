766dae5ed5b9b318ee67f4aa0384a379d4a5e503
Jose Luis Pulido Laveaga <j.pulido.c@kickstarter.com>
2025-03-21 14:16:19 -0700

    Revert "Merge branch 'tony/mbl-2207-spc-loading-error-state' into jpulido/MBL-2209"
    This reverts commit f623e5e0292c2433c7f100aeb2db58d19ed10b3b, reversing
    changes made to 231dcdf2d86dc044b4a37c372a6af68a6d18460c.

--- app/src/main/java/com/kickstarter/ui/fragments/projectpage/ui/SimilarProjectsComponent.kt
+++ app/src/main/java/com/kickstarter/ui/fragments/projectpage/ui/SimilarProjectsComponent.kt
@@ -91,7 +91,6 @@ fun SimilarProjectCardPreview() {
 @Composable
 fun SimilarProjectCard(
     project: Project,
-    modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
 ) {
     val context = LocalContext.current
@@ -100,7 +99,7 @@ fun SimilarProjectCard(
     val fundedString = stringResource(R.string.discovery_baseball_card_stats_funded)
 
     KSProjectCardLarge(
-        modifier = modifier,
+        modifier = Modifier,
         photo = project.photo(),
         title = project.name(),
         titleMinMaxLines = 2..2,
@@ -191,9 +190,6 @@ fun SimilarProjectsComponent(
 ) {
     val projects = uiState.value.data
 
-    // Will use `isLoading` & `error` when necessary, but this is the simple truth right now
-    val showPlaceholder = projects.isNullOrEmpty()
-
     val pagerState = rememberPagerState { projects?.size ?: 0 }
 
     Column {
@@ -213,7 +209,7 @@ fun SimilarProjectsComponent(
             )
             Row(
                 Modifier
-                    .weight(1f)
+                    .weight(1f, true)
                     .wrapContentHeight(),
                 horizontalArrangement = Arrangement.End
             ) {
@@ -237,24 +233,22 @@ fun SimilarProjectsComponent(
                 .height(12.dp)
         )
         Box(
+            modifier = Modifier,
             contentAlignment = Alignment.Center
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .absolutePadding(18.dp, 0.dp, 24.dp, 0.dp)
-                    .alpha(if (showPlaceholder) 1f else 0f)
+                    .alpha(0f)
                     .clickable(false) {},
                 contentAlignment = Alignment.Center
             ) {
-                SimilarProjectPlaceholderCard {
-                    SimilarProjectCard(placeholderProject)
-                }
+                SimilarProjectCard(placeholderProject)
             }
             HorizontalPager(
                 state = pagerState,
                 contentPadding = PaddingValues.Absolute(18.dp, 0.dp, 24.dp, 0.dp),
-                verticalAlignment = Alignment.CenterVertically,
                 pageSpacing = 12.dp,
                 modifier = Modifier.pointerInput(Unit) {
                     detectTouch { touching ->
@@ -271,7 +265,8 @@ fun SimilarProjectsComponent(
             }
         }
         Spacer(
-            modifier = Modifier.height(24.dp)
+            modifier = Modifier
+                .height(24.dp)
         )
     }
 }

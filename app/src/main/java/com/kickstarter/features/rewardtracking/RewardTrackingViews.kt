c18d2a16e8584bbb673aba057b1ac09c8c0fb8a6
Leigh Douglas <leigh@kickstarter.com>
2025-04-07 18:12:43 -0400

    update to use internal ks async image

--- app/src/main/java/com/kickstarter/features/rewardtracking/RewardTrackingViews.kt
+++ app/src/main/java/com/kickstarter/features/rewardtracking/RewardTrackingViews.kt
@@ -15,19 +15,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.kickstarter.R
-import com.kickstarter.libs.utils.DateTimeUtils
 import com.kickstarter.libs.utils.extensions.format
-import com.kickstarter.type.DateTime
+import com.kickstarter.models.Photo
+import com.kickstarter.ui.compose.KSAsyncImage
 import com.kickstarter.ui.compose.TextWithStartIcon
 import com.kickstarter.ui.compose.designsystem.KSButton
 import com.kickstarter.ui.compose.designsystem.KSTheme
@@ -44,7 +39,7 @@ fun RewardTrackingActivityPreview() {
             trackingNumber = "123291242342",
             modifier = Modifier.background(color = colors.backgroundSurfacePrimary),
             projectName = "This is a project name",
-            publishedAt = "2 days ago"
+            publishedAt = "2 days ago",
         )
     }
 }
@@ -65,7 +60,7 @@ fun RewardTrackingViewYourPledgePreview() {
 fun RewardTrackingActivityFeed(
     modifier: Modifier = Modifier,
     trackingNumber: String,
-    projectPhotoUrl: String? = null,
+    photo: Photo? = null,
     projectName: String,
     publishedAt: String,
 ) {
@@ -73,7 +68,7 @@ fun RewardTrackingActivityFeed(
         modifier = modifier.fillMaxWidth()
     ) {
         ProjectInfoHeader(
-            projectPhotoUrl = projectPhotoUrl,
+            photo = photo,
             projectName = projectName
         )
 
@@ -167,24 +162,19 @@ fun RewardTrackingModal(
 
 @Composable
 fun ProjectInfoHeader(
-    projectPhotoUrl: String? = null,
+    photo: Photo?,
     projectName: String,
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(projectPhotoUrl)
-                .crossfade(true)
-                .build(),
-            contentDescription = "project photo",
+        KSAsyncImage(
+            image = photo,
             modifier = Modifier
                 .height(dimensions.activityFeedProjectImageHeight)
                 .width(dimensions.activityFeedProjectImageWidth),
-            placeholder = ColorPainter(color = colors.backgroundDisabled),
-            contentScale = ContentScale.FillWidth
         )
+
         Spacer(modifier = Modifier.width(dimensions.paddingMediumSmall))
 
         Text(

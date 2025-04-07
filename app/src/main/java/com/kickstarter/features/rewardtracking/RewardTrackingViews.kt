bdd24375cc13ef23f1003806cd5fdc70ae272247
Leigh Douglas <leigh@kickstarter.com>
2025-04-07 17:33:19 -0400

    create tracking modal for view your pledge screen

--- app/src/main/java/com/kickstarter/features/rewardtracking/RewardTrackingViews.kt
+++ app/src/main/java/com/kickstarter/features/rewardtracking/RewardTrackingViews.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
@@ -24,7 +25,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.kickstarter.R
+import com.kickstarter.libs.utils.DateTimeUtils
 import com.kickstarter.libs.utils.extensions.format
+import com.kickstarter.type.DateTime
 import com.kickstarter.ui.compose.TextWithStartIcon
 import com.kickstarter.ui.compose.designsystem.KSButton
 import com.kickstarter.ui.compose.designsystem.KSTheme
@@ -40,7 +43,20 @@ fun RewardTrackingActivityPreview() {
         RewardTrackingActivityFeed(
             trackingNumber = "123291242342",
             modifier = Modifier.background(color = colors.backgroundSurfacePrimary),
-            projectName = "This is a project name"
+            projectName = "This is a project name",
+            publishedAt = "2 days ago"
+        )
+    }
+}
+
+@Composable
+@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun RewardTrackingViewYourPledgePreview() {
+    KSTheme {
+        RewardTrackingViewYourPledge(
+            trackingNumber = "123291242342",
+            modifier = Modifier.background(color = colors.backgroundSurfacePrimary),
         )
     }
 }
@@ -50,7 +66,8 @@ fun RewardTrackingActivityFeed(
     modifier: Modifier = Modifier,
     trackingNumber: String,
     projectPhotoUrl: String? = null,
-    projectName: String
+    projectName: String,
+    publishedAt: String,
 ) {
     Column(
         modifier = modifier.fillMaxWidth()
@@ -69,38 +86,64 @@ fun RewardTrackingActivityFeed(
         Spacer(modifier = Modifier.height(dimensions.paddingMediumSmall))
 
         Text(
-            text = "2 days ago",
+            text = publishedAt,
             style = typographyV2.bodyBoldXXS,
             color = colors.textSecondary
         )
 
         Spacer(modifier = Modifier.height(dimensions.paddingMediumSmall))
 
+        RewardTrackingModal(
+            trackingNumber,
+            RewardTrackingPageType.ACTIVITY_FEED,
+        )
+    }
+}
+
+@Composable
+fun RewardTrackingViewYourPledge(
+    modifier: Modifier = Modifier,
+    trackingNumber: String,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(shape = RoundedCornerShape(dimensions.radiusMediumSmall), color = colors.kds_support_200)
+            .padding(dimensions.paddingMedium)
+    ) {
+
+        RewardTrackingModal(
+            trackingNumber,
+            RewardTrackingPageType.VIEW_YOUR_PLEDGE,
+        )
+    }
+}
+
+@Composable
+fun RewardTrackingModal(
+    trackingNumber: String,
+    pageType: RewardTrackingPageType,
+    onClick: () -> Unit = {},
+) {
+    Column {
         Row {
             TextWithStartIcon(
                 modifier = Modifier,
                 text = stringResource(id = R.string.fpo_your_reward_has_shipped),
                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_shipping),
-                style = typographyV2.headingLG,
+                style = when (pageType) {
+                    RewardTrackingPageType.VIEW_YOUR_PLEDGE -> typographyV2.headingMD
+                    RewardTrackingPageType.ACTIVITY_FEED -> typographyV2.headingLG
+                },
                 iconHeight = dimensions.imageSizeMedium,
                 iconPadding = dimensions.paddingXSmall,
                 textColor = colors.textPrimary,
                 iconColor = colors.icon
             )
         }
+
         Spacer(modifier = Modifier.height(dimensions.paddingXSmall))
 
-        TrackingCardFooter(
-            trackingNumber
-        )
-    }
-}
-
-@Composable
-fun TrackingCardFooter(
-    trackingNumber: String
-) {
-    Column {
         Text(
             text = stringResource(R.string.fpo_tracking_number).format(key1 = "tracking_number", value1 = trackingNumber),
             style = typographyV2.bodyMD,
@@ -113,7 +156,7 @@ fun TrackingCardFooter(
             modifier = Modifier,
             backgroundColor = colors.kds_black,
             textColor = colors.kds_white,
-            onClickAction = { },
+            onClickAction = onClick,
             shape = RoundedCornerShape(size = KSTheme.dimensions.radiusExtraSmall),
             text = stringResource(R.string.fpo_track_shipment),
             textStyle = typographyV2.buttonLabel,
@@ -150,4 +193,9 @@ fun ProjectInfoHeader(
             color = colors.textPrimary
         )
     }
+}
+
+enum class RewardTrackingPageType(name : String) {
+    VIEW_YOUR_PLEDGE("view_your_pledge"),
+    ACTIVITY_FEED("activity_feed")
 }

e0a18d80f5f93e26724e1d037ed1229026796295
Isa Martin <arkariang@gmail.com>
2025-07-10 14:46:25 -0700

    - Tests

--- app/src/main/java/com/kickstarter/features/search/viewmodel/SearchAndFilterViewModel.kt
+++ app/src/main/java/com/kickstarter/features/search/viewmodel/SearchAndFilterViewModel.kt
@@ -126,8 +126,8 @@ class SearchAndFilterViewModel(
                 this.location(location)
                 this.amountBucket(amountBucket)
                 this.goalBucket(goalBucket)
-                this.recommended( if (recommended) true else null)
-                this.staffPicks( if (projectsLoved) true else null)
+                this.recommended(if (recommended) true else null)
+                this.staffPicks(if (projectsLoved) true else null)
                 this.starred(if (savedProjects) 1 else null)
                 this.social(if (social) 1 else null)
             }

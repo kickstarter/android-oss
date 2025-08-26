27c0c0abfa07032c6f7c089397bdb42213fb7d09
Isa Martin <arkariang@gmail.com>
2025-08-26 14:26:37 -0700

    no message

--- app/src/external/java/com/kickstarter/ExternalApplicationModule.java
+++ app/src/external/java/com/kickstarter/ExternalApplicationModule.java
@@ -8,11 +8,15 @@ import com.kickstarter.libs.NoopInternalTools;
 import com.kickstarter.libs.utils.Secrets;
 import com.kickstarter.mock.services.MockApiClientV2;
 import com.kickstarter.mock.services.MockApolloClientV2;
+import com.kickstarter.services.ApiClient;
+import com.kickstarter.services.ApiClientType;
 import com.kickstarter.services.ApiClientTypeV2;
 import com.kickstarter.services.ApiClientV2;
+import com.kickstarter.services.ApiService;
 import com.kickstarter.services.ApiServiceV2;
 import com.kickstarter.services.ApolloClientTypeV2;
 import com.kickstarter.services.KSApolloClientV2;
+import com.kickstarter.services.MockApiClient;
 
 import javax.inject.Singleton;
 
@@ -47,15 +51,14 @@ public final class ExternalApplicationModule {
   @Provides
   @Singleton
   @NonNull
-  static ApiClientType provideApiClientType(final @NonNull ApiService apiService, final @NonNull Gson gson) {
-    return Secrets.IS_OSS ? new MockApiClient() : new ApiClient(apiService, gson);
+  static ApolloClientTypeV2 provideApolloClientTypeV2(final @NonNull ApolloClient apolloClient, final @NonNull Gson gson) {
+    return Secrets.IS_OSS ? new MockApolloClientV2() : new KSApolloClientV2(apolloClient, gson);
   }
-
 
   @Provides
   @Singleton
   @NonNull
-  static ApolloClientTypeV2 provideApolloClientTypeV2(final @NonNull ApolloClient apolloClient, final @NonNull Gson gson) {
-    return Secrets.IS_OSS ? new MockApolloClientV2() : new KSApolloClientV2(apolloClient, gson);
+  static ApiClientType provideApiClientType(final @NonNull ApiService apiService, final @NonNull Gson gson) {
+    return Secrets.IS_OSS ? new MockApiClient() : new ApiClient(apiService, gson);
   }
 }

001dce9b54fd7aa1b814bff9a23ed9657d66aad1
Isa Martin <arkariang@gmail.com>
2025-08-27 11:45:10 -0700

    no message

--- app/src/androidTest/java/com/kickstarter/screenshoot/testing/di/AndroidTestApplicationModule.kt
+++ app/src/androidTest/java/com/kickstarter/screenshoot/testing/di/AndroidTestApplicationModule.kt
@@ -8,11 +8,16 @@ import com.kickstarter.ApplicationModule
 import com.kickstarter.libs.ApiEndpoint
 import com.kickstarter.libs.InternalToolsType
 import com.kickstarter.libs.NoopInternalTools
+import com.kickstarter.libs.utils.Secrets
 import com.kickstarter.mock.services.MockApiClientV2
 import com.kickstarter.mock.services.MockApolloClientV2
+import com.kickstarter.services.ApiClient
+import com.kickstarter.services.ApiClientType
 import com.kickstarter.services.ApiClientTypeV2
+import com.kickstarter.services.ApiService
 import com.kickstarter.services.ApiServiceV2
 import com.kickstarter.services.ApolloClientTypeV2
+import com.kickstarter.services.MockApiClient
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -43,5 +48,11 @@ class AndroidTestApplicationModule(private val application: Application) : Appli
     @Singleton
     internal fun provideApolloClientType(apolloClient: ApolloClient): ApolloClientTypeV2 {
         return MockApolloClientV2()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiClientType(apiService: ApiService, gson: Gson): ApiClientType {
+        return if (Secrets.IS_OSS) MockApiClient() else ApiClient(apiService, gson)
     }
 }

8abb8dbd85e7ed5fcb523e48cb68701e60f2557b
Isabel Martin <arkariang@gmail.com>
2025-08-28 10:24:57 -0700

    MBL-2699: V1 Rest API using coroutines (#2413)

--- app/src/internal/java/com/kickstarter/InternalApplicationModule.java
+++ app/src/internal/java/com/kickstarter/InternalApplicationModule.java
@@ -13,11 +13,15 @@ import com.kickstarter.libs.qualifiers.ApiEndpointPreference;
 import com.kickstarter.libs.utils.Secrets;
 import com.kickstarter.mock.services.MockApiClientV2;
 import com.kickstarter.mock.services.MockApolloClientV2;
+import com.kickstarter.services.ApiClient;
+import com.kickstarter.services.ApiClientType;
 import com.kickstarter.services.ApiClientTypeV2;
 import com.kickstarter.services.ApiClientV2;
+import com.kickstarter.services.ApiService;
 import com.kickstarter.services.ApiServiceV2;
 import com.kickstarter.services.ApolloClientTypeV2;
 import com.kickstarter.services.KSApolloClientV2;
+import com.kickstarter.services.MockApiClient;
 
 import javax.inject.Singleton;
 
@@ -60,5 +64,12 @@ public final class InternalApplicationModule {
   @NonNull
   static ApiClientTypeV2 provideApiClientTypeV2(final @NonNull ApiServiceV2 apiService, final @NonNull Gson gson) {
     return Secrets.IS_OSS ? new MockApiClientV2() : new ApiClientV2(apiService, gson);
+  }
+
+  @Provides
+  @Singleton
+  @NonNull
+  static ApiClientType provideApiClientType(final @NonNull ApiService apiService, final @NonNull Gson gson) {
+    return Secrets.IS_OSS ? new MockApiClient() : new ApiClient(apiService, gson, true);
   }
 }

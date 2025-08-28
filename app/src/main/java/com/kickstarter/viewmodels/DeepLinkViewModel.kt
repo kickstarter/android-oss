999a1066d90054708cfc30a005bf10195dffaa58
Leigh Douglas <leighcdouglas1@gmail.com>
2025-08-28 14:41:13 -0400

    MBL-2745: Setup remote config initialization functions in Deeplink viewmodel (#2411)

--- app/src/main/java/com/kickstarter/viewmodels/DeepLinkViewModel.kt
+++ app/src/main/java/com/kickstarter/viewmodels/DeepLinkViewModel.kt
@@ -6,6 +6,7 @@ import android.text.TextUtils
 import android.util.Pair
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.kickstarter.libs.CurrentUserTypeV2
 import com.kickstarter.libs.Environment
 import com.kickstarter.libs.RefTag
@@ -30,6 +31,7 @@ import com.kickstarter.libs.utils.extensions.isProjectUpdateUri
 import com.kickstarter.libs.utils.extensions.isProjectUri
 import com.kickstarter.libs.utils.extensions.isRewardFulfilledDl
 import com.kickstarter.libs.utils.extensions.isSettingsUrl
+import com.kickstarter.libs.utils.extensions.isTrue
 import com.kickstarter.models.Project
 import com.kickstarter.models.User
 import com.kickstarter.services.ApiClientTypeV2
@@ -39,6 +41,9 @@ import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -102,7 +107,7 @@ interface DeepLinkViewModel {
         private val projectObservable: Observable<Project>
         private val startPreLaunchProjectActivity = BehaviorSubject.create<Pair<Uri, Project>>()
 
-        private val ffClient = requireNotNull(environment.featureFlagClient())
+        private val ffClient = environment.featureFlagClient()
 
         private val disposables = CompositeDisposable()
         private fun intent() = intent?.let { Observable.just(it) } ?: Observable.empty()
@@ -323,6 +328,25 @@ interface DeepLinkViewModel {
                 .subscribe {
                     startBrowser.onNext(it)
                 }.addToDisposable(disposables)
+        }
+
+        fun runInitializations(intent: Intent) {
+            viewModelScope.launch {
+                try {
+                    val ffClientInitialization = async { initializeFeatureFlagClient() }
+                    val isInitialized = awaitAll(ffClientInitialization)
+
+                    if (isInitialized.isNotEmpty() && isInitialized.all { it.isTrue() }) {
+                        // parse intent and determine user navigation
+                    } else {
+                        throw Exception()
+                    }
+                } catch (e: Exception) { }
+            }
+        }
+
+        private suspend fun initializeFeatureFlagClient(): Boolean? {
+            return ffClient?.fetchAndActivate()
         }
 
         private fun onDeepLinkToProjectPage(it: Pair<Uri, Project>, startProjectPage: BehaviorSubject<Uri>) {

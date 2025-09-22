de2a3f1fc6c193d561a29bc1f0d4216363494156
Isa Martin <arkariang@gmail.com>
2025-09-22 15:00:14 -0700

    no message

--- app/src/main/java/com/kickstarter/ui/activities/ReportProjectActivity.kt
+++ app/src/main/java/com/kickstarter/ui/activities/ReportProjectActivity.kt
@@ -8,7 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf

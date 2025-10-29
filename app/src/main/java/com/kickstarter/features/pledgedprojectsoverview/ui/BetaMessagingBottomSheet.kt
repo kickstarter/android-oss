a281074a3e374a9a03abe81417b8f4ad5aa00d27
Isa Martin <arkariang@gmail.com>
2025-10-29 13:23:41 -0700

    no message

--- app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/ui/BetaMessagingBottomSheet.kt
+++ app/src/main/java/com/kickstarter/features/pledgedprojectsoverview/ui/BetaMessagingBottomSheet.kt
@@ -1,10 +1,8 @@
 package com.kickstarter.features.pledgedprojectsoverview.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,9 +11,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource

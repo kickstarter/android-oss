9f15f07ecb824718cc5c9cf807ae27f1adf63f8a
Isa Martin <arkariang@gmail.com>
2026-02-10 11:27:48 -0800

    no message
    (cherry picked from commit 9ad015980736183b07e6775e9b41be9816b6ab66)

--- app/src/main/java/com/kickstarter/features/videofeed/PreloadStatusControl.kt
+++ app/src/main/java/com/kickstarter/features/videofeed/PreloadStatusControl.kt
@@ -7,15 +7,15 @@ import androidx.media3.exoplayer.source.preload.TargetPreloadStatusControl
 
 @androidx.media3.common.util.UnstableApi
 class PreloadStatusControl(private val currentPlayingIndexProvider: () -> Int) : TargetPreloadStatusControl<Int, DefaultPreloadManager.PreloadStatus> {
-    override fun getTargetPreloadStatus(rankingData: Int): DefaultPreloadManager.PreloadStatus? {
+    override fun getTargetPreloadStatus(rankingData: Int): DefaultPreloadManager.PreloadStatus {
         val currentIdx = currentPlayingIndexProvider()
         val distance = rankingData - currentIdx
 
         return when {
-            distance == 0 -> null
+            distance == 0 -> DefaultPreloadManager.PreloadStatus.PRELOAD_STATUS_NOT_PRELOADED
             distance == 1 -> specifiedRangeLoaded(5_000_000L) // Preload 5s
-            distance <= 3 -> DefaultPreloadManager.PreloadStatus.TRACKS_SELECTED
-            else -> null // Too far away, don't load
+            distance <= 3 -> DefaultPreloadManager.PreloadStatus.PRELOAD_STATUS_SOURCE_PREPARED
+            else -> DefaultPreloadManager.PreloadStatus.PRELOAD_STATUS_NOT_PRELOADED // Too far away, don't load
         }
     }
 }

531ceb217fc983b1d6d64e62cb8c30ec2b2c301f
Jose Luis Pulido Laveaga <jlpl.tec@gmail.com>
2026-02-18 15:49:03 -0800

    fix import order

--- app/src/main/java/com/kickstarter/ui/compose/designsystem/KSButtons.kt
+++ app/src/main/java/com/kickstarter/ui/compose/designsystem/KSButtons.kt
@@ -34,8 +34,8 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import com.kickstarter.R

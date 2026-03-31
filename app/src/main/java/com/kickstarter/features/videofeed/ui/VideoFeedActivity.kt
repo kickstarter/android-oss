32ea4b0df242eeb22be5d30a84f1e1d9212b3f33
Isa Martin <arkariang@gmail.com>
2026-03-31 10:10:42 -0700

    no message

--- app/src/main/java/com/kickstarter/features/videofeed/ui/VideoFeedActivity.kt
+++ app/src/main/java/com/kickstarter/features/videofeed/ui/VideoFeedActivity.kt
@@ -12,11 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kickstarter.features.videofeed.data.KSVideoBadgeType

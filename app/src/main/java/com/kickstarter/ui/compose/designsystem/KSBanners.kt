9d2b091d79958f26ce966186b525270eca3d7e21
Isa Martin <arkariang@gmail.com>
2026-06-16 11:29:51 -0700

    - linter

--- app/src/main/java/com/kickstarter/ui/compose/designsystem/KSBanners.kt
+++ app/src/main/java/com/kickstarter/ui/compose/designsystem/KSBanners.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
